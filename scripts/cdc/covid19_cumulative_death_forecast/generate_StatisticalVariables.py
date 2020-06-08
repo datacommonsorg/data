b02b1dbe77d7bc8b6ed34efa46e7474814829c4e
Lijuan Qian <lijuanqian@google.com>
2020-06-08 21:10:56 +0000

    add measuredProperty to statistical variables

--- scripts/cdc/covid19_cumulative_death_forecast/generate_StatisticalVariables.py
+++ scripts/cdc/covid19_cumulative_death_forecast/generate_StatisticalVariables.py
@@ -9,8 +9,10 @@ def main():
     STATVAR_TEMPLATE = "Node: dcid:COVID19{}DeathPrediction{}\n" + \
     	"typeOf: schema:StatisticalVariable\n" + \
     	"populationType: dcs:MedicalConditionIncident\n" + \
+    	"measuredProperty: dcs:{}\n" + \
     	"statType: dcs:MeasuredValue\n\n"
     countType = ["Cumulative", "Increased"]
+    measuredProperty = ["cumulativeCount", "count"]
     variable = ["", "Quantile_0.025", "Quantile_0.975"]
     
     
@@ -23,9 +25,9 @@ def main():
     
     with open('./COVID19_DeathPredictionCDC_StatisticalVariable.mcf', 'w', newline='') as f_out:
         #write the statistical variables
-        for cnt in countType:
+        for cnt,meas in zip(countType, measuredProperty):
             for var in variable:
-                f_out.write(STATVAR_TEMPLATE.format(cnt, var))
+                f_out.write(STATVAR_TEMPLATE.format(cnt, var, meas))
         #write the models
         
         for _,row in model_info.iterrows():
