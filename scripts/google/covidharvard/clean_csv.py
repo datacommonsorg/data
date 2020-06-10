6136373620f4c7f9e7595bf6dd4b1e528ba2e264
Eduardo Morales <edumorales@google.com>
2020-06-10 17:25:49 -0400

    Adds incremental count

--- scripts/google/covidharvard/clean_csv.py
+++ scripts/google/covidharvard/clean_csv.py
@@ -3,29 +3,29 @@ import numpy as np
 from datetime import datetime
 
 class CovidHarvard:
-    def __init__(self, confirmed_csv: str, deaths_csv: str, region: 'County' or 'State'):
+    def __init__(self, confirmed_cumulative_csv: str, deaths_cumulative_csv: str, region: 'County' or 'State'):
         if region != 'State' and region != 'County':
             raise Exception("Invalid region!")
 
         print("Reading CSVs")
-        confirmed = pd.read_csv(confirmed_csv)
-        deaths = pd.read_csv(deaths_csv)
+        confirmed_cumulative_df = pd.read_csv(confirmed_cumulative_csv)
+        deaths_cumulative_df = pd.read_csv(deaths_cumulative_csv)
 
         print("Converting COUNTY column to fips")
         if region == 'County':
-            confirmed = confirmed.rename(columns={'COUNTY': 'fips'})
-            deaths = deaths.rename(columns={'COUNTY': 'fips'})
+            confirmed_cumulative_df = confirmed_cumulative_df.rename(columns={'COUNTY': 'fips'})
+            deaths_cumulative_df = deaths_cumulative_df.rename(columns={'COUNTY': 'fips'})
 
         print("Converting fips to DC dcid")
-        confirmed = self.convert_fips_to_dcid(confirmed, region)
-        deaths = self.convert_fips_to_dcid(deaths, region)
+        confirmed_cumulative_df = self.convert_fips_to_dcid(confirmed_cumulative_df, region)
+        deaths_cumulative_df = self.convert_fips_to_dcid(deaths_cumulative_df, region)
 
         print("Dropping unecessary columns")
-        confirmed = self.drop_unecessary_columns(confirmed)
-        deaths = self.drop_unecessary_columns(deaths)
+        confirmed_cumulative_df = self.drop_unecessary_columns(confirmed_cumulative_df)
+        deaths_cumulative_df = self.drop_unecessary_columns(deaths_cumulative_df)
 
         print("Combining DataFrames into one")
-        combined = self.combine_dfs(confirmed=confirmed, deaths=deaths)
+        combined = self.combine_dfs(confirmed=confirmed_cumulative_df, deaths=deaths_cumulative_df)
 
         print("Exporting to ./output directory")
         combined.to_csv(f"./output/{region}_COVID_Harvard.csv", index=False)
@@ -42,14 +42,31 @@ class CovidHarvard:
         df = df.set_index("dcid")
         return df
 
+    @staticmethod
+    def get_value(df, dcid, date):
+        if not dcid:
+            raise Exception("dcid cannot be None!")
+        if not date:
+            raise Exception("date cannot be None!")
+        if df.empty:
+            raise Exception("DataFrame is either empty or None!")
+        if not date or not dcid:
+            return np.nan
+        if not date in df or not dcid in df[date]:
+            return np.nan
+        return df[date][dcid]
+
     def combine_dfs(self, confirmed, deaths):
+        confirmed_incremental = confirmed.T.diff().T
+        deaths_incremental = deaths.T.diff().T
         Date = []
         GeoId = []
-        COVID19CumulativeCases = []
-        COVID19CumulativeDeaths = []
+        COVID19CumulativeCases, COVID19CumulativeDeaths = [], []
+        COVID19IncrementalCases, COVID19IncrementalDeaths = [], []
         for dcid in confirmed.index:
             for date in confirmed:
                 date_iso = date
+                # Somtimes the data is in the form of XX/XX/XX, let's convert it to ISO
                 if '/' in date:
                     date_split = date.split('/')
                     date_iso = datetime(int(date_split[2]), int(date_split[0]), int(date_split[1])).isoformat()
@@ -57,27 +74,27 @@ class CovidHarvard:
 
                 print(dcid, date_iso)
 
-                if not date in confirmed or not dcid in confirmed[date]:
-                    confirmed_val = np.nan
-                else:
-                    confirmed_val = confirmed[date][dcid]
-
-                if not date in deaths or not dcid in deaths[date]:
-                    deaths_val = np.nan
-                else:
-                    deaths_val = deaths[date][dcid]
+                confirmed_cumulative_value = self.get_value(df=confirmed, dcid=dcid, date=date)
+                deaths_cumulative_value = self.get_value(df=deaths, dcid=dcid, date=date)
+                confirmed_incremental_value = self.get_value(df=confirmed_incremental, dcid=dcid, date=date)
+                deaths_incremental_value = self.get_value(df=deaths_incremental, dcid=dcid, date=date)
 
                 Date.append(date_iso)
                 GeoId.append(dcid)
-                COVID19CumulativeCases.append(confirmed_val)
-                COVID19CumulativeDeaths.append(deaths_val)
+                COVID19CumulativeCases.append(confirmed_cumulative_value)
+                COVID19CumulativeDeaths.append(deaths_cumulative_value)
+                COVID19IncrementalCases.append(confirmed_incremental_value)
+                COVID19IncrementalDeaths.append(deaths_incremental_value)
+
         return pd.DataFrame({
                 "Date": Date,
                 "GeoId": GeoId,
-                "COVID19CumulativeCases": COVID19CumulativeCases,
-                "COVID19CumulativeDeaths": COVID19CumulativeDeaths
+                "HaravardCOVID19CumulativeCases": COVID19CumulativeCases,
+                "HarvardCOVID19CumulativeDeaths": COVID19CumulativeDeaths,
+                "HarvardCOVI19IncrementalCases": COVID19IncrementalCases,
+                "HarvardCOVID19IncrementalDeaths": COVID19IncrementalDeaths,
             })
 
-CovidHarvard(confirmed_csv='./input/US_State_Confirmed.csv',
-             deaths_csv='./input/US_State_Deaths.csv',
-             region='State')
+CovidHarvard(confirmed_cumulative_csv='./input/US_County_Confirmed.csv',
+             deaths_cumulative_csv='./input/US_County_Deaths.csv',
+             region='County')
