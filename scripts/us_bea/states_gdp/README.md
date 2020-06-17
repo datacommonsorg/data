a43b0a15ec66fa1a87646bc0062bdb568a5e09c2
Francisco Pernice <fpernice@google.com>
2020-06-17 16:38:07 -0700

    Pulled and cleaned US States quarterly GDP data.

---
+++ scripts/us_bea/states_gdp/README.md
@@ -0,0 +1,9 @@
+# Importing US Bureau of Economic Analysis quarterly GDP data per State
+
+## Cleaned Data
+1. [states_gdp.csv](states_gdp.csv).
+
+## Scripts
+1. [import_data.py](import_data.py)
+  1. Pulls data from BEA API and cleans it as in    [states_gdp.csv](states_gdp.csv).
+  2. TODO(fpernice): write the mcf and tmcf files.
---
+++ scripts/us_bea/states_gdp/import_data.py
@@ -0,0 +1,113 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Pulls data from the US Bureau of Economic Analysis (BEA) on quarterly GDP
+per State. Saves output as CSV.
+
+    Typical usage:
+
+    python3 import_data.py
+"""
+import json
+from absl import app
+from absl import flags
+import requests
+import pandas as pd
+
+# currently not used, but left here for mcf generation flags.
+FLAGS = flags.FLAGS
+
+# TODO (fpernice): replace this by Google-level key.
+MY_KEY = 'D431C2CE-8BD2-4D9E-AD7A-00F95CAB60CE'
+QUARTERLY_GDP_TABLE = 'SQGDP1'
+
+REQUEST = """http://apps.bea.gov/api/data/?&\
+UserID={key}&\
+method=GetData&\
+datasetname=Regional&\
+TableName={table}&\
+GeoFIPS=STATE&\
+LineCode=1&\
+ResultFormat=json&"""
+
+US_STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
+             'California', 'Colorado', 'Connecticut', 'Delaware',
+             'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+             'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+             'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+             'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
+             'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+             'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+             'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+             'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+             'West Virginia', 'Wisconsin', 'Wyoming']
+
+
+class StateGdpDataLoader:
+    """Pulls GDP state data from BEA.
+
+    Attributes:
+        response: Raw response from BEA API in JSON format.
+        df: DataFrame with the cleaned data.
+    """
+    def __init__(self, request):
+        """Makes Http request, cleans the data and stores it in instance DF."""
+        self.response = json.loads(requests.get(request).text)
+        df = pd.DataFrame(self.response['BEAAPI']['Results']['Data'])
+
+        # Filters out columns that are not US States (e.g. New England).
+        # TODO: Add non-state entities.
+        df = df[df['GeoName'].isin(US_STATES)]
+
+        qtr_month_map = {
+            'Q1':'03',
+            'Q2':'06',
+            'Q3':'09',
+            'Q4':'12'
+        }
+        # Changes date format to include both observation date and period
+        def date_to_obs_date(date):
+            """Converts date format e.g. 2005Q3 to e.g. 2005-09."""
+            return date[:4] + "-" + qtr_month_map[date[4:]]
+        df['ObservationDate'] = df['TimePeriod'].apply(date_to_obs_date)
+
+        def date_to_obs_period(date):
+            """Converts quarter format e.g. 2005Q3 to e.g. P3M."""
+            return "P{}M".format(date[-1:])
+        df['ObservationPeriod'] = df['TimePeriod'].apply(date_to_obs_period)
+
+        # Created GeoId column.
+        df['GeoId'] = df['GeoFips'].apply(lambda id: "geoId" + id[:2])
+
+        # Gets rid of unused columns
+        df = df.drop(columns=['Code', 'UNIT_MULT', 'TimePeriod', 'GeoFips'])
+
+        self.df = df
+
+    def save_csv(self, filename='states_gdp.csv'):
+        """Saves instance DF to specified csv file."""
+        self.df.to_csv(filename)
+
+
+def main(argv):
+    del argv # not currently used.
+    loader = StateGdpDataLoader(REQUEST.format(
+        key=MY_KEY,
+        table=QUARTERLY_GDP_TABLE
+    ))
+    loader.save_csv()
+
+
+if __name__ == '__main__':
+    app.run(main)
