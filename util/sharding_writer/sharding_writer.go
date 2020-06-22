46d5c0ef1a1928e6ab8abdaf9a905bea421d0764
Robert Snedegar <viking@gmail.com>
2020-06-22 01:44:01 -0700

    Add Go util package to do sharded writing. Include a battery of tests top ensure correctness and robustness of the library.

---
+++ util/sharding_writer/sharding_writer.go
@@ -0,0 +1,283 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sharding_writer
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	// DefaultShardSize is 100 MB.
+	DefaultShardSize = 100 * 1024 * 1024
+)
+
+// option is an interface for any type of behavior we want to configure on the
+// Writer. The interface methods are private because we do not support options
+// implemented outside this package.
+type Option interface {
+	set(*Options)
+}
+
+// Options is the set of options for the writer. The type is opaque to allow
+// for future additions and subtractions of supported features.
+type Options struct {
+	shardSize int64
+
+	dataType dataType
+}
+
+// shardSize is a byte count to split on.
+// TODO(rsned): What do we do is someone wants shard larger the 4GB?
+type shardSize int64
+
+// ShardSize returns an option that sets the desired shard size in bytes.
+func ShardSize(numBytes int64) Option { return shardSize(numBytes) }
+
+func (x shardSize) set(opts *Options) {
+	opts.shardSize = int64(x)
+}
+
+// dataType is an enum of the different types of writing behavior based
+// on the data expected to be writen.
+type dataType int
+
+const (
+	// dataTypeRaw is for plain bytes, no smarts, just split on exact byte count.
+	//
+	// TODO(rsned): Is raw the clearest name for this? Maybe dataTypeBytes?
+	// This is just the basic write whatever comes in as-is. dataTypePlain?
+	dataTypeRaw dataType = iota
+
+	// dataTypeText is for textual values that will wrap on the next newline
+	// that comes after the byte count requested.
+	dataTypeText
+
+	// dataTypeMCF is for textual types that have some structure, and the
+	// shard split will only occur after the line that closes an MCF block.
+	dataTypeMCF
+
+	// TODO: If there are other formats of data being sharded for writing
+	// that need more smarts, add them here. (e.g. JSON should split on the
+	// close of a complete record so that all file shards can be independently
+	// processed safely.
+)
+
+// RawDataType configures the writer to treat data as plain bytes.
+func RawDataType() Option { return dataTypeRaw }
+
+// TextDataType configures the writer to treat data as text, so data
+// shards are split on line breaks and not mid-line.
+func TextDataType() Option { return dataTypeText }
+
+// MCFDataType configures the writer to treat data as MCF/TMCF so record are
+// not split between file shards.
+func MCFDataType() Option { return dataTypeMCF }
+
+func (d dataType) set(opts *Options) {
+	opts.dataType = d
+}
+
+// Writer implements sharded writing, spliting files to be as close to the given
+// byte size as possible. This type implements io.WriterCloser and io.StringWriter.
+//
+// If an error occurs writing to the Writer, no more data will be accepted and all
+// subsequent writes will return the error.
+type Writer struct {
+	basePath  string
+	extension string // extension with out any leading period.
+	opts      *Options
+
+	currentFile  *os.File // file handle currently being written to.
+	currentShard int      // current shard number.
+	bytesWritten int64    // number of bytes written to current shard.
+
+	// track if we should be failing all writes going forward.
+	errorTriggered bool
+}
+
+// NewWriter returns a sharding writer for the given name and file extension.
+// If no options are given, the writer defaults to 100 MB shard size, and
+// writing out the data as bytes with no smarts on split points.
+//
+// If multiple of the same option are supplied, only the last will be used.
+// e.g. ShardSize(1024), ShardSize(100), the shard size will be 100 bytes.
+func NewWriter(basePath, extension string, opts ...Option) io.WriteCloser {
+	w := &Writer{
+		basePath:  basePath,
+		extension: extension,
+
+		opts: &Options{
+			shardSize: DefaultShardSize,
+		},
+	}
+
+	// If the user accidentally includes the dot before the extension,
+	// chop it off since we insert it when making the filenames.
+	if strings.HasPrefix(w.extension, ".") {
+		w.extension = w.extension[1:]
+	}
+
+	// Check opts and set any the user has chosen to set explicitly.
+	for _, o := range opts {
+		o.set(w.opts)
+	}
+
+	var err error
+	w.currentFile, err = os.Create(w.currentFileName())
+	if err != nil {
+		log.Fatalf("unable to create sharded file %q", w.currentFileName())
+	}
+	return w
+}
+
+// currentFileName is a helper to format the current filename.
+func (w *Writer) currentFileName() string {
+	if w.extension == "" {
+		return fmt.Sprintf("%s_%d", w.basePath, w.currentShard)
+	}
+	return fmt.Sprintf("%s_%d.%s", w.basePath, w.currentShard, w.extension)
+}
+
+// Write writes len(p) bytes from p to the underlying data stream. It returns
+// the number of bytes written from p (0 <= n <= len(p)) and any error encountered
+// that caused the write to stop early.
+func (w *Writer) Write(p []byte) (n int, err error) {
+	if w.errorTriggered {
+		return 0, fmt.Errorf("error occurred during previous writes")
+	}
+
+	// Will this fit without needing to start a new shard.
+	if w.opts.shardSize-w.bytesWritten-int64(len(p)) > 0 {
+		n, err := w.currentFile.Write(p)
+		w.bytesWritten += int64(n)
+		if err != nil || n != len(p) {
+			w.errorTriggered = true
+		}
+		return n, err
+	}
+
+	// TODO(rsned): Implment the sharded writing of bytes.
+	return -1, fmt.Errorf("sharded write not implemented")
+}
+
+// WriteString writes the contents of the given string. It returns the number of
+// bytes written from from the string (0 <= n <= len(s)) and any error encountered
+// that caused the write to stop early.
+func (w *Writer) WriteString(s string) (n int, err error) {
+	if w.errorTriggered {
+		return 0, fmt.Errorf("error occurred during previous writes")
+	}
+
+	// TODO(rsned): Maybe allow a +/- 5% leeway so we don't end up
+	// writing out a 100 MB shard and a 1 kB shard when it could all
+	// end up in one file.
+
+	// Check if we can write this without triggering a new shard.
+	if w.opts.shardSize-w.bytesWritten-int64(len(s)) > 0 {
+		n, err := w.currentFile.WriteString(s)
+		w.bytesWritten += int64(n)
+		if err != nil || n != len(s) {
+			w.errorTriggered = true
+		}
+		return n, err
+	}
+
+	var bytesWritten int64
+
+	switch w.opts.dataType {
+	case dataTypeRaw:
+		// We will need more than the current shard to complete the write.
+		for {
+			// Figure out how much of the input we can use before sharding.
+			splitPoint := w.opts.shardSize - w.bytesWritten
+			// If we don't have enough to fill out a shard, adjust the cut point.
+			if splitPoint > int64(len(s)) {
+				splitPoint = int64(len(s))
+			}
+
+			n, err = w.currentFile.WriteString(s[:splitPoint])
+			if (err != nil) || int64(n) != splitPoint {
+				w.errorTriggered = true
+			}
+			bytesWritten += int64(n)
+
+			// drop the portion written so far from the string.
+			s = s[splitPoint:]
+
+			// If there is no more to write, we are done with the loop.
+			if len(s) == 0 {
+				break
+			}
+			// Else, there is still more to write, create a new
+			// shard and keep going.
+			w.newShard()
+		}
+	case dataTypeText:
+		// TODO(rsned): implement
+		return -1, fmt.Errorf("sharded writestring for text not implemented yet.")
+	case dataTypeMCF:
+		// TODO(rsned): implement
+		return -1, fmt.Errorf("sharded writestring for MCF not implemented yet.")
+	}
+
+	return int(bytesWritten), err
+}
+
+// WriteByte writes a single byte to the underlying data stream. It returns
+// any error encountered that caused the write to fail.
+func (w *Writer) WriteByte(c byte) error {
+	if w.errorTriggered {
+		return fmt.Errorf("error occurred during previous writes")
+	}
+
+	// Will this byte fit, or do we need to start a new shard.
+	if w.opts.shardSize-w.bytesWritten-1 <= 0 {
+		w.newShard()
+	}
+
+	n, err := w.currentFile.Write([]byte{c})
+	w.bytesWritten += 1
+	if err != nil || n != 1 {
+		w.errorTriggered = true
+	}
+	return err
+}
+
+// Close implements io.Closer interface.
+func (w *Writer) Close() error {
+	return w.currentFile.Close()
+}
+
+// newShard is used to close the existing shard, start a new one, and reset the counters.
+func (w *Writer) newShard() {
+	if w.currentFile != nil {
+		// Do we need to do something if there was an error closing
+		// the current file?
+		w.currentFile.Close()
+	}
+
+	w.currentShard++
+	w.bytesWritten = 0
+
+	var err error
+	w.currentFile, err = os.Create(w.currentFileName())
+	if err != nil {
+		w.errorTriggered = true
+	}
+}
---
+++ util/sharding_writer/sharding_writer_test.go
@@ -0,0 +1,491 @@
+package sharding_writer
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"testing"
+)
+
+var (
+	// tmpDir is picked at start time for tests done during this session.
+	tmpDir string
+)
+
+func init() {
+	var err error
+	tmpDir, err = ioutil.TempDir("", "sharding_writer_test")
+	if err != nil {
+		log.Fatalf("unable to create temporary directory for tests")
+	}
+}
+
+// makeFilename constructs a filename relative to the tmpDir for these tests.
+func makeFilename(pieces ...string) string {
+	return path.Join(append([]string{tmpDir}, pieces...)...)
+}
+
+const (
+	// baseString is ranged over to get the desired length of data.
+	// The string is 73 characters long so it should lead to non-identical
+	// rows when repeated line after line.
+	baseString = `abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWXYZ.`
+
+	// TODO(roberts): Add in a test string that includes varying byte width
+	// UTF-8 characters such as:
+	//
+	// 2 byte utf-8
+	// U+00A2       ¢   // currency symbol
+	//
+	// 3 byte utf-8
+	// U+20AC       €   // the Euro
+	//
+	// 4 byte utf-8
+	// U+2070E	𠜎
+	// U+20731	𠜱
+
+	// sampleMCF is a simple string that will have the x replaced with a
+	// counter when being generated as filler.
+	sampleMCF = `
+Node: dcid:UnitTest_Generated_Node_{x}_Entry
+name: "UnitTest_Generated_Node_{x}_Entry"
+typeOf: dcs:StatisticalVariable
+populationType: dcs:TotalPopulation
+measuredProperty: dcs:measuredProperty
+statType: dcs:measuredValue
+measurementMethod: dcs:UnitTestingGenerated
+unit: dcs:Percent
+`
+)
+
+// generateRawString generates a regular set of characters up to the given
+// number, without newlines, whitespace, or punctuation.
+func generateRawString(count int64) string {
+	// short circuit requests for negative or 0.
+	if count < 1 {
+		return ""
+	}
+
+	var buf bytes.Buffer
+
+	// If the num bytes is more than the length of the src string, range
+	// until we have written enough whole strings.
+	for i := int64(0); i < (count / int64(len(baseString))); i++ {
+		n, err := buf.WriteString(baseString)
+		if n != len(baseString) || err != nil {
+			fmt.Printf("error writing to buffer, had %d, wrote %d, err: %v",
+				len(baseString), n, err)
+		}
+	}
+
+	// Write out the bytes less than a full line to get to the full amount.
+	buf.WriteString(baseString[:count%int64(len(baseString))])
+
+	return buf.String()
+}
+
+func TestGenerateRawString(t *testing.T) {
+	tests := []struct {
+		count int64
+		want  string
+	}{
+		{count: -3, want: ""},
+		{count: 0, want: ""},
+		{count: 1, want: "a"},
+		{count: 2, want: "ab"},
+		{count: 73, want: baseString},
+		{count: 74, want: baseString + baseString[0:1]},
+		{count: 75, want: baseString + baseString[0:2]},
+		{count: 80, want: baseString + baseString[0:7]},
+		{count: 100, want: baseString + baseString[0:27]},
+		{count: 150, want: baseString + baseString + baseString[0:4]},
+	}
+
+	for _, test := range tests {
+		if got := generateRawString(test.count); got != test.want {
+			t.Errorf("generateRawString(%d) = %q, want %q", test.count, got, test.want)
+		}
+	}
+}
+
+// generateLines generates the given number of lines of text as 61 or 47 character
+// long lines in a regular fashion.
+func generateLines(count int64) string {
+	// short circuit requests for negative or 0.
+	if count < 1 {
+		return ""
+	}
+
+	var buf bytes.Buffer
+	var start, end int64
+	var lenBase = int64(len(baseString))
+
+	for i := int64(0); i < count; i++ {
+		// 61 char long
+		if i%2 == 0 {
+			end = start + 61
+			// if we dont have to wrap, write it out and move on.
+			if end < lenBase {
+
+				buf.WriteString(baseString[start:end])
+				buf.WriteString("\n")
+				start = end
+				continue
+			}
+			// We need to wrap. Write to the end of the string.
+			buf.WriteString(baseString[start:])
+			// Jump back to the start.
+			start = 0
+			// The end is the remainder from dividing by length.
+			end = end % lenBase
+			buf.WriteString(baseString[start:end])
+			buf.WriteString("\n")
+			start = end
+			continue
+		}
+
+		// else 47 char long
+		end = start + 47
+		// if we dont have to wrap, write it out and move on.
+		if end < lenBase {
+			buf.WriteString(baseString[start:end])
+			buf.WriteString("\n")
+			start = end
+			continue
+		}
+		// We need to wrap. Write to the end of the string.
+		buf.WriteString(baseString[start:])
+		// Jump back to the start.
+		start = 0
+		// The end is the remainder from dividing by length.
+		end = end % lenBase
+		buf.WriteString(baseString[start:end])
+		buf.WriteString("\n")
+		start = end
+		continue
+
+	}
+	return buf.String()
+}
+
+func TestGenerateLines(t *testing.T) {
+	tests := []struct {
+		count int64
+		want  string
+	}{
+		{count: -7, want: ""},
+		{count: 0, want: ""},
+		{
+			count: 1,
+			want:  "abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOP\n",
+		},
+		{
+			count: 2,
+			want: `abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOP
+Q;RSTUVWXYZ.abcdef_ghijkl,mnopqr?stuvwx+yz0123-
+`,
+		},
+		{
+			count: 3,
+			want: `abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOP
+Q;RSTUVWXYZ.abcdef_ghijkl,mnopqr?stuvwx+yz0123-
+456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWXYZ.abcdef_ghijkl,mnopqr?st
+`,
+		},
+		{
+			count: 4,
+			want: `abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOP
+Q;RSTUVWXYZ.abcdef_ghijkl,mnopqr?stuvwx+yz0123-
+456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWXYZ.abcdef_ghijkl,mnopqr?st
+uvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWX
+`,
+		},
+		{
+			count: 5,
+			want: `abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOP
+Q;RSTUVWXYZ.abcdef_ghijkl,mnopqr?stuvwx+yz0123-
+456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWXYZ.abcdef_ghijkl,mnopqr?st
+uvwx+yz0123-456789*0ABCDE/FGHIJK:LMNOPQ;RSTUVWX
+YZ.abcdef_ghijkl,mnopqr?stuvwx+yz0123-456789*0ABCDE/FGHIJK:LM
+`,
+		},
+	}
+
+	for _, test := range tests {
+		if got := generateLines(test.count); got != test.want {
+			t.Errorf("generateLines(%d) = %q, want %q", test.count, got, test.want)
+		}
+	}
+}
+
+// generateMCF generates a series of MCF definitions with a blank line separator
+// between each entry.
+func generateMCF(count int64) string {
+	// short circuit requests for negative or 0.
+	if count < 1 {
+		return ""
+	}
+
+	var buf bytes.Buffer
+
+	// TODO: generate the MCF
+
+	return buf.String()
+}
+
+func TestGenerateMCF(t *testing.T) {
+	tests := []struct {
+		count int64
+		want  string
+	}{
+		{count: -1, want: ""},
+		{count: 0, want: ""},
+	}
+
+	for _, test := range tests {
+		if got := generateMCF(test.count); got != test.want {
+			t.Errorf("generateMCF(%d) = %q, want %q", test.count, got, test.want)
+		}
+	}
+}
+
+// setup does the creation work for a given test, creating the path it
+// will use for its tests.
+func setup(testPath string) {
+	if err := os.Mkdir(makeFilename(testPath), os.ModeDir|0750); err != nil {
+		log.Fatalf("unable to create working dir for test %q: %v", testPath, err)
+	}
+}
+
+// tearDown cleans up the generated files for the given test.
+func tearDown(testPath string) {
+	if err := os.RemoveAll(makeFilename(testPath)); err != nil {
+		fmt.Printf("unable to clean up for test path %q", testPath)
+	}
+}
+
+func checkExpectedNumShards(t *testing.T, testPath string, numExpected int) {
+	dir, err := os.Open(makeFilename(testPath))
+	if err != nil {
+		t.Errorf("unable to open directory to check shards: %v", err)
+		return
+	}
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		t.Errorf("unable to read directory: %v", err)
+		return
+	}
+
+	if len(fileInfos) != numExpected {
+		t.Errorf("num shards = %v, want %v", len(fileInfos), numExpected)
+	}
+}
+
+func checkFilesSumToInputSize(t *testing.T, testPath string, wantSize int64) {
+	dir, err := os.Open(makeFilename(testPath))
+	if err != nil {
+		t.Errorf("unable to open directory to check shards: %v", err)
+		return
+	}
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		t.Errorf("unable to read directory: %v", err)
+		return
+	}
+
+	var sumSize int64
+	for _, fi := range fileInfos {
+		sumSize += fi.Size()
+	}
+
+	if sumSize != wantSize {
+		t.Errorf("sum of file sizes = %d, want %d", sumSize, wantSize)
+	}
+}
+
+func cmpBytes(t *testing.T, label string, a, b []byte) {
+	if len(a) != len(b) {
+		t.Errorf("bytes not the same length: %v != %v", len(a), len(b))
+	}
+
+	for i, val := range a {
+		if b[i] != val {
+			t.Errorf("%s: slices differ starting at element %d: 0x%x (%s) vs 0x%x (%s)",
+				label, i, val, string(val), b[i], string(b[i]))
+			return
+		}
+	}
+}
+
+// checkFileBeginEndContents checks that a file begins and ends with the expected values.
+func checkFileBeginEndContents(t *testing.T, testPath, filename string, begins, ends []byte) {
+	fn := makeFilename(testPath, filename)
+	contents, err := ioutil.ReadFile(fn)
+	if err != nil {
+		t.Errorf("error reading %q for content checks: %v", fn, err)
+	}
+
+	if len(contents) < len(begins) {
+		t.Errorf("file %q to short to compare with starting bytes %v", fn, begins)
+	}
+	if len(contents) < len(ends) {
+		t.Errorf("file %q to short to compare with ending bytes %v", fn, begins)
+	}
+
+	cmpBytes(t, fn, contents[0:len(begins)], begins)
+	cmpBytes(t, fn, contents[len(contents)-len(ends):], ends)
+}
+
+func TestWriter(t *testing.T) {
+	tests := []struct {
+		label string   // descriptive label of this case
+		path  string   // directory name piece to distinguish test data
+		opts  []Option // selection of opts to set.
+		count int64    // how many we want to generate
+
+		// what generator to use. Note this can be different
+		// than what the writer is configured to use to test poor choices.
+		dataType dataType
+
+		wantNumShards int
+
+		// Check the first and last bytes/chars of the first shard.
+		wantFirstShardBegin string
+		wantFirstShardEnd   string
+
+		// Check the first and last bytes/chars of the last shard.
+		// Note that these are only checked if there is more than one shard.
+		wantLastShardBegin string
+		wantLastShardEnd   string
+	}{
+		{
+			label:               "Write 1 byte to default shard, default opts, raw data",
+			path:                "1_byte_to_default_shard_all_defaults",
+			count:               1,
+			dataType:            dataTypeRaw,
+			wantNumShards:       1,
+			wantFirstShardBegin: "a",
+			wantFirstShardEnd:   "a",
+		},
+		{
+			label: "Write 1 byte to 1 shard, raw data",
+			path:  "1_byte_to_1_byte_shard_raw",
+			opts: []Option{
+				RawDataType(),
+				ShardSize(1),
+			},
+			dataType:          dataTypeRaw,
+			count:             1,
+			wantNumShards:     1,
+			wantFirstShardEnd: "a",
+		},
+		{
+			label: "Write 256 bytes to 1 shard, raw data",
+			path:  "256_bytes_to_100mb_shard_raw",
+			opts: []Option{
+				RawDataType(),
+			},
+			count:               256,
+			dataType:            dataTypeRaw,
+			wantNumShards:       1,
+			wantFirstShardBegin: "abcdef_g",
+			wantFirstShardEnd:   "z0123-45",
+		},
+		{
+			label: "Write 256 bytes to 32-byte shards, raw data",
+			path:  "256_bytes_to_32_byte_shards_raw",
+			opts: []Option{
+				RawDataType(),
+				ShardSize(32),
+			},
+			count:               256,
+			dataType:            dataTypeRaw,
+			wantNumShards:       8,
+			wantFirstShardBegin: "abcdef_g",
+			wantFirstShardEnd:   "vwx+yz01",
+			wantLastShardBegin:  "f_ghijkl",
+			wantLastShardEnd:    "z0123-45",
+		},
+		{
+			label: "Write 257 bytes to 11-byte shards, raw data",
+			path:  "257_bytes_to_11_byte_shards_raw",
+			opts: []Option{
+				RawDataType(),
+				ShardSize(11),
+			},
+			count:               257,
+			dataType:            dataTypeRaw,
+			wantNumShards:       24,
+			wantFirstShardBegin: "abcdef_g",
+			wantFirstShardEnd:   "def_ghij",
+			wantLastShardBegin:  "-456",
+			wantLastShardEnd:    "-456",
+		},
+
+		// TODO(rsned): Add tests for text based and MCF based data
+		// as those methods get implemented.
+	}
+
+	for _, test := range tests {
+		setup(test.path)
+		// Create a sharded writer with the options in the test case.
+		w := NewWriter(makeFilename(test.path, "sharded_data"), "dat",
+			test.opts...)
+
+		// Generate data to be written out.
+		var data string
+		switch test.dataType {
+		case dataTypeRaw:
+			data = generateRawString(test.count)
+		case dataTypeText:
+			data = generateLines(test.count)
+		case dataTypeMCF:
+			data = generateMCF(test.count)
+		}
+
+		// io.WriterString calls an io.Writer's WriteString if the Writer
+		// also implements StringWriter, which we do.
+		n, err := io.WriteString(w, data)
+		if err != nil {
+			t.Errorf("error writing to the sharding writer: %v", err)
+			// move on to next case since the remaining checks wont work
+			continue
+		}
+		if n != len(data) {
+			t.Errorf("num bytes written doesn't match the input. wrote %d, had %d",
+				n, len(data))
+			// move on to next case since the remaining checks wont work
+			continue
+		}
+
+		// Close the writer to ensure it's all saved.
+		w.Close()
+
+		// Now perform the various checks of the outputs.
+		checkExpectedNumShards(t, test.path, test.wantNumShards)
+
+		checkFilesSumToInputSize(t, test.path, int64(len(data)))
+
+		firstFile := "sharded_data_0.dat"
+		checkFileBeginEndContents(t, test.path, firstFile,
+			[]byte(test.wantFirstShardBegin),
+			[]byte(test.wantFirstShardEnd))
+
+		if test.wantNumShards > 1 {
+			lastFile := fmt.Sprintf("sharded_data_%d.dat", test.wantNumShards-1)
+			checkFileBeginEndContents(t, test.path, lastFile,
+				[]byte(test.wantLastShardBegin),
+				[]byte(test.wantLastShardEnd))
+
+		}
+		// If you want to debug output or leave the results on disk,
+		// comment out the tearDown call which removes this tests data.
+		tearDown(test.path)
+	}
+}
