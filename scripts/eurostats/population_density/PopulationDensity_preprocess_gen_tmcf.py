2e94aabf584b1cb16d2ab4b84d6991ee16c865e2
eftekhari-mhs <eftekhari.mhs@gmail.com>
2020-07-07 20:05:25 -0700

    changed to works with tsv file

--- scripts/eurostats/population_density/PopulationDensity_preprocess_gen_tmcf.py
+++ scripts/eurostats/population_density/PopulationDensity_preprocess_gen_tmcf.py
@@ -13,63 +13,61 @@
 # limitations under the License.
 
 import pandas as pd
-pd.DataFrame()
-
-import pandas as pd
-import numpy as np
-import io
 import csv
 
-original_file = "demo_r_d3area_1_Data.csv"
-cleaned_file = "PopulationDensity_Eurostat_NUTS3.csv"
+# data_url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/demo_r_d3area.tsv.gz"
+source_tsv = "demo_r_d3dens.tsv"
+source_csv_long = "demo_r_d3dens.csv"
+cleaned_csv = "PopulationDensity_Eurostat_NUTS3.csv"
 tmcf= "PopulationDensity_Eurostat_NUTS3.tmcf"
-df = pd.read_csv(original_file)
-df.head()
 
-# Get a list of column names:
-df.columns
+df = pd.read_csv(source_tsv, delimiter='\t')
+assert df.head!=[]
+print(df.head())
+header = list(df.columns.values)
+years = header[1:]
+
+# Pandas.melt() unpivots a DataFrame from wide format to long format
+df = pd.melt(df, id_vars =header[0], value_vars =years, 
+              var_name ='time', value_name ='value') 
+
+
+# separate geo and unit columns
+new = df[header[0]].str.split(",", n = 1, expand = True) 
+df['geo']= new[1] 
+df['unit']= new[0]    
+df.drop(columns =[header[0]], inplace = True) 
+
+# remove empty rows, clean values to have all digits
+df = df[df.value.str.contains('[0-9]')]
+possible_flags=[' ', ':', 'b', 'e']
+for flag in possible_flags:
+  df['value'] = df['value'].str.replace(flag,'')
+
+print(df.head())
+df.to_csv(source_csv_long, index=False)
+print(df.columns)
 
 output_columns = ['Date', 'GeoId',
-                  'PopulationDensity_Eurostat_NUTS3',
+                  'Count_Person_AsAFractionOfArea',
                  ]
-with open(cleaned_file, 'w', newline='') as f_out:
+with open(cleaned_csv, 'w', newline='') as f_out:
   writer = csv.DictWriter(f_out, fieldnames=output_columns, lineterminator='\n')
-  with open(original_file) as response:
+  with open(source_csv_long) as response:
     reader = csv.DictReader(response)
 
     writer.writeheader()
     for row_dict in reader:
+      assert not row_dict['value'].isdigit()
+      assert not row_dict['time'].isdigit()
       processed_dict = {
-          'Date': '%s' % (row_dict['TIME'][:4]),
-          'GeoId': 'dcid:nuts/%s' % row_dict['GEO'],
-          'PopulationDensity_Eurostat_NUTS3': row_dict['Value'],
-      }   
-
+              # 'Date': '%s-%s-%s' % (row_dict['TIME'][:4], '01', '01'),
+              'Date': '%s' % (row_dict['time'][:4]),
+              'GeoId': 'dcid:nuts/%s' % row_dict['geo'],
+              'Count_Person_AsAFractionOfArea': float(row_dict['value']),
+      }    
       writer.writerow(processed_dict)
 
 # View the result:
-df_cleaned = pd.read_csv(cleaned_file)
-df_cleaned.head()
-
-# Automate Template MCF generation since there are many Statistical Variables.
-TEMPLATE_MCF_TEMPLATE = """
-Node: E:EurostatNUTS3_DensityTracking->E{index}
-typeOf: dcs:StatVarObservation
-variableMeasured: dcs:{stat_var}
-observationAbout: C:EurostatNUTS3_DensityTracking->GeoId
-observationDate: C:EurostatNUTS3_DensityTracking->Dates
-value: C:EurostatNUTS3_DensityTracking->{stat_var}
-"""
-
-stat_vars = output_columns[2:]
-with open(tmcf, 'w', newline='') as f_out:
-  for i in range(len(stat_vars)):
-    f_out.write(TEMPLATE_MCF_TEMPLATE.format_map({'index': i, 'stat_var': output_columns[2:][i]}))
-
-# View the result:
-df_cleaned = pd.read_csv(tmcf)
-df_cleaned.head()
-
-
-# Uncomment the following to download to your local computer.
-# files.download(tmcf)
+df_cleaned = pd.read_csv(cleaned_csv)
+print(df_cleaned.head())
