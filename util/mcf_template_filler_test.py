6c2dbde7336bf569d187e7d93090f7cc4ca6eb77
Robert Snedegar <rsned@users.noreply.github.com>
2020-07-08 12:59:02 -0700

    Enable unit tests for python code in cloud build triggers (#95)
    * This library implements the start of a sharded writer to generate output
    files of the desired size for Data Commons imports (100MB).
    Added a variety of tests and test helper methods to make sure a wide
    selection of test cases can be comprehensively covered.
    
    * Enable unit testing on python code.
    Add the commands to lint and format check once existing files are
    updated.
    Update requirements.txt
    
    * uncomment the format check on util/.
    add pylint disable tags for method docstrings in test file.
    clean up imports on unittest.

--- util/mcf_template_filler_test.py
+++ util/mcf_template_filler_test.py
@@ -13,8 +13,14 @@
 # limitations under the License.
 """Tests for util.mcf_template_filler."""
 
-import mcf_template_filler
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+from __future__ import absolute_import
 import unittest
+
+from util import mcf_template_filler
 
 POP_TEMPLATE = """
 Node: Pop_payroll_est_{geo_id}_{naics_code}_{operation_type}_{tax_status}
@@ -53,8 +59,8 @@ measuredValue: {mval}
 
 class MCFTemplateFillerTest(unittest.TestCase):
 
-    def test_example_usage(self):
-        example_template = '''
+  def test_example_usage(self):
+    example_template = """
 Node: People_in_geoId_{geo_id}_{race}_{gender}_{random_field}
 typeOf: schema:StatisticalPopulation
 populationType: schema:Person
@@ -62,45 +68,45 @@ location: geoId/{geo_id}
 race: dcs:{race}
 gender: dcs:{gender}
 randomOptionalProperty: {random_field}
-'''
+"""
 
-        templater = mcf_template_filler.Filler(example_template,
-                                               required_vars=['geo_id'])
-        var_dict1 = {'geo_id': '05', 'race': 'White'}
-        pop1 = templater.fill(var_dict1)
-        expected = """
+    templater = mcf_template_filler.Filler(
+        example_template, required_vars=['geo_id'])
+    var_dict1 = {'geo_id': '05', 'race': 'White'}
+    pop1 = templater.fill(var_dict1)
+    expected = """
 Node: People_in_geoId_05_White__
 typeOf: schema:StatisticalPopulation
 populationType: schema:Person
 location: geoId/05
 race: dcs:White
 """
-        self.assertEqual(pop1, expected)
+    self.assertEqual(pop1, expected)
 
-        var_dict2 = {'geo_id': '05', 'gender': 'Female'}
-        pop2 = templater.fill(var_dict2)
-        expected = """
+    var_dict2 = {'geo_id': '05', 'gender': 'Female'}
+    pop2 = templater.fill(var_dict2)
+    expected = """
 Node: People_in_geoId_05__Female_
 typeOf: schema:StatisticalPopulation
 populationType: schema:Person
 location: geoId/05
 gender: dcs:Female
 """
-        self.assertEqual(pop2, expected)
+    self.assertEqual(pop2, expected)
 
-    def test_pop_and_2_obs_with_all_pv(self):
-        """Use separate templates for Pop Obs, and use Obs template repeatedly."""
-        templater = mcf_template_filler.Filler(POP_TEMPLATE,
-                                               required_vars=['geo_id'])
-        template_vars = {
-            'geo_id': 'geoId/06',
-            'naics_code': '11',
-            'operation_type': 'Manufacturer',
-            'tax_status': 'ExemptFromTax'
-        }
-        result = templater.fill(template_vars)
+  def test_pop_and_2_obs_with_all_pv(self):
+    """Use separate templates for Pop Obs, and use Obs template repeatedly."""
+    templater = mcf_template_filler.Filler(
+        POP_TEMPLATE, required_vars=['geo_id'])
+    template_vars = {
+        'geo_id': 'geoId/06',
+        'naics_code': '11',
+        'operation_type': 'Manufacturer',
+        'tax_status': 'ExemptFromTax'
+    }
+    result = templater.fill(template_vars)
 
-        expected = """
+    expected = """
 Node: Pop_payroll_est_geoId/06_11_Manufacturer_ExemptFromTax
 typeOf: schema:StatisticalPopulation
 populationType: dcs:USCEstablishment
@@ -110,16 +116,16 @@ naics: dcs:NAICS/11
 operationType: dcs:Manufacturer
 taxStatus: dcs:ExemptFromTax
 """
-        self.assertEqual(result, expected)
+    self.assertEqual(result, expected)
 
-        templater = mcf_template_filler.Filler(
-            OBS_TEMPLATE, required_vars=['year', 'mprop', 'mval'])
-        template_vars['year'] = '2000'
-        template_vars['mprop'] = 'count'
-        template_vars['mval'] = 0
-        result = templater.fill(template_vars)
+    templater = mcf_template_filler.Filler(
+        OBS_TEMPLATE, required_vars=['year', 'mprop', 'mval'])
+    template_vars['year'] = '2000'
+    template_vars['mprop'] = 'count'
+    template_vars['mval'] = 0
+    result = templater.fill(template_vars)
 
-        expected = """
+    expected = """
 Node: Obs_on_Pop_payroll_est_geoId/06_11_Manufacturer_ExemptFromTax_2000_count
 typeOf: schema:Observation
 observedNode: l:Pop_payroll_est_geoId/06_11_Manufacturer_ExemptFromTax
@@ -127,14 +133,14 @@ observationDate: "2000"
 measuredProperty: dcs:count
 measuredValue: 0
 """
-        self.assertEqual(result, expected)
+    self.assertEqual(result, expected)
 
-        template_vars['year'] = '2001'
-        template_vars['mprop'] = 'count'
-        template_vars['mval'] = 144
-        result = templater.fill(template_vars)
+    template_vars['year'] = '2001'
+    template_vars['mprop'] = 'count'
+    template_vars['mval'] = 144
+    result = templater.fill(template_vars)
 
-        expected = """
+    expected = """
 Node: Obs_on_Pop_payroll_est_geoId/06_11_Manufacturer_ExemptFromTax_2001_count
 typeOf: schema:Observation
 observedNode: l:Pop_payroll_est_geoId/06_11_Manufacturer_ExemptFromTax
@@ -142,24 +148,24 @@ observationDate: "2001"
 measuredProperty: dcs:count
 measuredValue: 144
 """
-        self.assertEqual(result, expected)
+    self.assertEqual(result, expected)
 
-    def test_unified_pop_obs_with_missing_optional_pv(self):
-        # Can combine templates, like Pop + Obs
-        pop_obs_template = POP_TEMPLATE + OBS_TEMPLATE
-        templater = mcf_template_filler.Filler(
-            pop_obs_template, required_vars=['geo_id', 'year', 'mprop', 'mval'])
-        template_vars = {
-            'geo_id': 'geoId/06',
-            'naics_code': '11',
-            'tax_status': 'ExemptFromTax',
-            'year': '2000',
-            'mprop': 'count',
-            'mval': 42,
-        }
-        result = templater.fill(template_vars)
+  def test_unified_pop_obs_with_missing_optional_pv(self):
+    # Can combine templates, like Pop + Obs
+    pop_obs_template = POP_TEMPLATE + OBS_TEMPLATE
+    templater = mcf_template_filler.Filler(
+        pop_obs_template, required_vars=['geo_id', 'year', 'mprop', 'mval'])
+    template_vars = {
+        'geo_id': 'geoId/06',
+        'naics_code': '11',
+        'tax_status': 'ExemptFromTax',
+        'year': '2000',
+        'mprop': 'count',
+        'mval': 42,
+    }
+    result = templater.fill(template_vars)
 
-        expected = """
+    expected = """
 Node: Pop_payroll_est_geoId/06_11__ExemptFromTax
 typeOf: schema:StatisticalPopulation
 populationType: dcs:USCEstablishment
@@ -175,33 +181,33 @@ observationDate: "2000"
 measuredProperty: dcs:count
 measuredValue: 42
 """
-        self.assertEqual(result, expected)
+    self.assertEqual(result, expected)
 
-    def test_pop_with_missing_req_pv(self):
-        templater = mcf_template_filler.Filler(
-            POP_TEMPLATE, required_vars=['geo_id', 'tax_status'])
-        template_vars = {
-            'geo_id': 'geoId/06',
-            'naics_code': '11',
-            'operation_type': 'Manufacturer',
-        }
-        with self.assertRaises(ValueError):
-            templater.fill(template_vars)
+  def test_pop_with_missing_req_pv(self):
+    templater = mcf_template_filler.Filler(
+        POP_TEMPLATE, required_vars=['geo_id', 'tax_status'])
+    template_vars = {
+        'geo_id': 'geoId/06',
+        'naics_code': '11',
+        'operation_type': 'Manufacturer',
+    }
+    with self.assertRaises(ValueError):
+      templater.fill(template_vars)
 
-    def test_require_node_name(self):
-        with self.assertRaises(ValueError):
-            mcf_template_filler.Filler(NAMELESS_POP_TEMPLATE)
+  def test_require_node_name(self):
+    with self.assertRaises(ValueError):
+      mcf_template_filler.Filler(NAMELESS_POP_TEMPLATE)
 
-        with self.assertRaises(ValueError):
-            mcf_template_filler.Filler(NAMELESS_OBS_TEMPLATE)
+    with self.assertRaises(ValueError):
+      mcf_template_filler.Filler(NAMELESS_OBS_TEMPLATE)
 
-        bad_node = """
+    bad_node = """
 typeOf: badNode
 location: dcid:badPlace
 """
-        with self.assertRaises(ValueError):
-            mcf_template_filler.Filler(POP_TEMPLATE + bad_node)
+    with self.assertRaises(ValueError):
+      mcf_template_filler.Filler(POP_TEMPLATE + bad_node)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
