fc10dc3ef0f79e3cac4be1125ab7e35c0c5c5867
Ian Costello <iancostello1@gmail.com>
2020-07-08 17:31:29 -0700

    More PR Changes

--- scripts/us_bls/jolts/bls_jolts.py
+++ scripts/us_bls/jolts/bls_jolts.py
@@ -12,152 +12,170 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Fetches, cleans, and downloads BLS Jolts data into Data Commons Knowledge Graph.
+"""Fetches and cleans BLS Jolts data into Data Commons Knowledge Graph.
 
-Data is fetched from various data sources within the BLS site then combined and cleaned.
-StatisticalVariables are generated and cleaned CSV is output.
+Data is fetched from various data sources within the BLS site
+then combined and cleaned.
+
+Statistical Variables are generated and cleaned CSV is output.
 
 Download the requirements.txt via pip and execute the file with Python 3.
+
+Dataset being processed: https://download.bls.gov/pub/time.series/jt/
 """
 from absl import app
 import pandas as pd
-import urllib.request
 import textwrap
 
-# JOLTS dataset contains a mix of NAICS industry codes and custom BLS jolts aggregations
-# Existing NAICS Codes are mapped directly
-# Custom JOLTS codes include a colon distinguishing their new name 
-CODE_MAPPINGS = {
- '000000': '000000:Total nonfarm', # New Code
- '100000': '10', 
- '110099': '110099:Mining and logging', # New Code
- '230000': '23', 
- '300000': '300000:Manufacturing', # New Code
- '320000': '320000:Durable goods manufacturing', # New Code
- '340000': '340000:Nondurable goods manufacturing', # New Code
- '400000': '400000:Trade, transportation, and utilities', # New Code
+# JOLTS dataset contains both NAICS industry codes and BLS jolts aggregations.
+# Existing NAICS Codes are mapped directly while
+# custom JOLTS codes include a colon distinguishing their new name.
+_CODE_MAPPINGS = {
+ '000000': '000000:Total nonfarm',  # New Code
+ '100000': '10',
+ '110099': '110099:Mining and logging',  # New Code
+ '230000': '23',
+ '300000': '300000:Manufacturing',  # New Code
+ '320000': '320000:Durable goods manufacturing',  # New Code
+ '340000': '340000:Nondurable goods manufacturing',  # New Code
+ '400000': '400000:Trade, transportation, and utilities',  # New Code
  '420000': '42',
  '440000': '44',
- '480099': '480099:Transportation warehousing, and utilities', # New Code
+ '480099': '480099:Transportation warehousing, and utilities',  # New Code
  '510000': '51',
- '510099': '510099:Financial activities', # New Code
+ '510099': '510099:Financial activities',  # New Code
  '520000': '52',
  '530000': '53',
- '540099': '540099:Professional and business services', # New Code
- '600000': '600000:Education and health services', # New Code
+ '540099': '540099:Professional and business services',  # New Code
+ '600000': '600000:Education and health services',  # New Code
  '610000': '61',
  '620000': '62',
- '700000': '700000:Leisure and hospitality', # New Code
+ '700000': '700000:Leisure and hospitality',  # New Code
  '710000': '71',
  '720000': '72',
  '810000': '81',
- '900000': '900000:Government', # New Code
- '910000': '910000:Federal', # New Code
+ '900000': '900000:Government',  # New Code
+ '910000': '910000:Federal',  # New Code
  '920000': '92',
- '923000': '923000:State and local government education', # New Code
- '929000': '929000:State and local government excluding education'# New Code
+ '923000': '923000:State and local government education',  # New Code
+ '929000': '929000:State and local government excluding education'  # New Code
 }
 
-### Step 1) Data Download
-# Dataset being processed: https://download.bls.gov/pub/time.series/jt/
 def generate_cleaned_dataframe():
   """ Fetches and combines BLS Jolts data sources.
 
   Downloads detailed series information from the entire JOLTS dataset.
-  Each of the files is read, combined into a single dataframe, and post-processed.
+  Each of the files is read, combined into a single dataframe, and processed.
 
   Returns:
-    jolts_df: A dataframe representing the 6 job data categories by industry, year, and seasonal adjustment.
-    schema_mapping: A list of tuples that contains information for each data set .
+    jolts_df: The 6 job data categories by industry, year, and adjustment.
+    schema_mapping: List of tuples that contains information for each dataset.
   """
-  
-  # Download series descriptions (used for seasonally adjusted status and industry code).
-  expected_series_columns = ['series_id','seasonal', 'industry_code', 'region_code', 'dataelement_code', 'ratelevel_code', 'footnote_codes','begin_year', 'begin_period', 'end_year', 'end_period']
-  series_desc = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.series", sep="\s+")
-  assert len(series_desc.columns) == len(expected_series_columns) and False not in (series_desc.columns == expected_series_columns)
+
+  # Series descriptions are used for adjustment status and industry code.
+  exp_series_columns = ['series_id', 'seasonal', 'industry_code',
+      'region_code', 'dataelement_code', 'ratelevel_code', 'footnote_codes',
+      'begin_year', 'begin_period', 'end_year', 'end_period']
+
+  series_desc = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.series",
+      converters={'industry_code': lambda col: str(col)},
+      sep="\s+")
+  assert len(series_desc.columns) == len(exp_series_columns)
+  assert False not in (series_desc.columns == exp_series_columns)
   series_desc = series_desc.set_index("series_id")
 
-  # Download various series datapoints -- find more info here https://download.bls.gov/pub/time.series/jt/jt.txt.
-  expected_job_columns = ['series_id', 'year', 'period', 'value', 'footnote_codes']
-  job_openings = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.2.JobOpenings", sep="\s+")
-  job_hires = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.3.Hires", sep="\s+")
-  total_seps = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.4.TotalSeparations", sep="\s+")
-  total_quits = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.5.Quits", sep="\s+")
-  total_layoffs = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.6.LayoffsDischarges", sep="\s+")
-  total_other_seps = pd.read_csv("https://download.bls.gov/pub/time.series/jt/jt.data.7.OtherSeparations", sep="\s+")
-  
+  # Download various series datapoints
+  job_openings = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.2.JobOpenings",
+      sep="\s+")
+  job_hires = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.3.Hires",
+      sep="\s+")
+  total_seps = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.4.TotalSeparations",
+      sep="\s+")
+  total_quits = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.5.Quits",
+      sep="\s+")
+  total_layoffs = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.6.LayoffsDischarges",
+      sep="\s+")
+  total_other_seps = pd.read_csv(
+      "https://download.bls.gov/pub/time.series/jt/jt.data.7.OtherSeparations",
+      sep="\s+")
   # Additional information about each dataframe.
-  # Tuple Format: Statistical Variable name, Stat Var population, Stat Var Job Change Type If Relevant, Dataframe for Stat Var. 
+  # Tuple Format: Statistical Variable name, Stat Var population,
+  #   Stat Var Job Change Type If Relevant, Dataframe for Stat Var.
   schema_mapping = [
-    ("NumJobOpening", "schema:JobPosting", "", job_openings),
-    ("NumJobHire", "dcs:BLSWorker", "Hire", job_hires),
-    ("NumSeparation", "dcs:BLSWorker", "Separation", total_seps),
-    ("NumVoluntarySeparation", "dcs:BLSWorker", "VoluntarySeparation", total_quits),
-    ("NumInvoluntarySeparation", "dcs:BLSWorker", "InvoluntarySeparation", total_layoffs),
-    ("NumOtherSeparation", "dcs:BLSWorker", "OtherSeparation", total_other_seps),
+      ("NumJobOpening", "schema:JobPosting",
+          "", job_openings),
+      ("NumJobHire", "dcs:BLSWorker",
+          "Hire", job_hires),
+      ("NumSeparation", "dcs:BLSWorker",
+          "Separation", total_seps),
+      ("NumVoluntarySeparation", "dcs:BLSWorker",
+          "VoluntarySeparation", total_quits),
+      ("NumInvoluntarySeparation", "dcs:BLSWorker",
+          "InvoluntarySeparation", total_layoffs),
+      ("NumOtherSeparation", "dcs:BLSWorker",
+          "OtherSeparation", total_other_seps),
   ]
-
   # Combine datasets into a single dataframe including origin of data.
   jolts_df = pd.DataFrame()
-  columns_to_keep = ['series_id', 'year', 'period', 'value', 'footnote_codes']
+  exp_job_columns = ['series_id', 'year', 'period', 'value', 'footnote_codes']
 
   for schema_name, population_type, job_change_event, df in schema_mapping:
     # Assert columns are as expected.
-    assert len(df.columns) == len(expected_job_columns) and False not in (df.columns == expected_job_columns)
+    assert len(df.columns) == len(exp_job_columns)
+    assert False not in (df.columns == exp_job_columns)
 
     # Add to general dataframe.
-    df = df.loc[:, columns_to_keep]
-    df['statistical_variable'] = schema_name
-    df['job_change_event'] = job_change_event
-    df['population_type'] = population_type
+    df = df.loc[:, exp_job_columns]
+    df.loc[:, 'statistical_variable'] = schema_name
+    df.loc[:, 'job_change_event'] = job_change_event
+    df.loc[:, 'population_type'] = population_type
     jolts_df = jolts_df.append(df)
 
-  # Drop non-monthly data.
-  jolts_df = jolts_df.query("period != 'M13'")
+  # Drop non-monthly data and throw away slice.
+  jolts_df = jolts_df.query("period != 'M13'").copy()
 
-  # Change date to ISO Format (YYYY-MM).
+  # Change date to ISO format (YYYY-MM).
   def period_year_to_iso_8601(row):
     month = row['period'].lstrip("M")
     year = row['year']
     return f"{year}-{month}"
-
-  jolts_df.loc[:, 'Date'] = jolts_df.apply(period_year_to_iso_8601, axis=1)
+  jolts_df['Date'] = jolts_df.apply(period_year_to_iso_8601, axis=1)
 
   # Add relevant columns from series information.
-  jolts_df = jolts_df.merge(series_desc[['industry_code', 'region_code', 'seasonal', 'ratelevel_code']], left_on=["series_id"], right_index=True)
+  series_cols = ['industry_code', 'region_code', 'seasonal', 'ratelevel_code']
+  jolts_df = jolts_df.merge(series_desc[series_cols],
+                            left_on=["series_id"], right_index=True)
 
-  # Drop rate data.
+  # Drop rate data, preliminary data, and non-national data.
   jolts_df = jolts_df.query("ratelevel_code == 'L'")
-
-  # Drop prelimary data.
   jolts_df = jolts_df.query("footnote_codes != 'P'")
-
-  # Drop non-national data.
   jolts_df = jolts_df.query("region_code == '00'")
 
   # Map industries.
   def jolts_code_map(row):
     """ Maps industry code used by BLS Jolts to NAICS or BLS aggregation """
     industry_code = row['industry_code']
-    assert industry_code in CODE_MAPPINGS
+    assert industry_code in _CODE_MAPPINGS, f"{industry_code} not mapped!"
+    mapped_code = _CODE_MAPPINGS[industry_code]
 
-    mapped_code = CODE_MAPPINGS[industry_code]
-
-    # New Jolts code have a prepended JOLTS id.
     if ":" in mapped_code:
-      JOLTS_CODE = mapped_code.split(":")[0]
-      row['industry_code'] = "JOLTS_" + JOLTS_CODE
-
-    # Just map original NAICS codes directly.
+      # New Jolts code have a prepended JOLTS id.
+      row['industry_code'] = "JOLTS_" + mapped_code.split(":")[0]
     else:
+      # Just map original NAICS codes directly.
       row['industry_code'] = mapped_code
-    
-    return row
 
+    return row
   jolts_df = jolts_df.apply(jolts_code_map, axis=1)
 
-  def map_row_to_statistical_variable(row):
-    """ Maps a row of the dataframe to the Statistical Variable that describes it. """
+  def row_to_stat_var(row):
+    """ Maps a row of the df to the Statistical Variable that describes it. """
     base_stat_var = row['statistical_variable']
     industry_code = row['industry_code']
     seasonal_adjustment = row['seasonal_adjustment']
@@ -165,23 +183,25 @@ def generate_cleaned_dataframe():
     return f"dcs:{base_stat_var}_NAICS_{industry_code}_{seasonal_adjustment}"
 
   # Build map to Statistical Variable.
-  jolts_df['seasonal_adjustment'] = jolts_df['seasonal'].apply(lambda adjustment: "Adjusted" if adjustment == "S" else "Unadjusted")
-  jolts_df['StatisticalVariable'] = jolts_df.apply(map_row_to_statistical_variable, axis=1)
+  jolts_df['seasonal_adjustment'] = jolts_df['seasonal'].apply(
+      lambda adj: "Adjusted" if adj == "S" else "Unadjusted")
+  jolts_df['StatisticalVariable'] = jolts_df.apply(row_to_stat_var, axis=1)
   jolts_df['Value'] = jolts_df['value']
 
   return jolts_df, schema_mapping
 
-
 def create_statistical_variables(jolts_df, schema_mapping):
   """ Creates Statistical Variable nodes.
 
-    A new statistical industry is needed for each of the 6 job variables and for every industry.
-    The industry codes may be either NAICS or BLS_JOLTS aggregations. The schema_mapping is used 
-    for additional information for each of the 6 job variables. These new variables are written
+    A new statistical industry is needed for each of the 6 job variables
+    and for every industry.
+    The industry codes may be either NAICS or BLS_JOLTS aggregations.
+    The schema_mapping is used for additional information for
+    each of the 6 job variables. These new variables are written
     to the statistical variables mcf file.
 
     Args:
-      jolts_df: The dataframe of BLS Jolts data created by generate_cleaned_dataframe.
+      jolts_df: The df of BLS Jolts data created by generate_cleaned_dataframe.
       schema_mapping: The schema mapping created by generate_cleaned_dataframe.
   """
   template_stat_var = """
@@ -195,7 +215,8 @@ def create_statistical_variables(jolts_df, schema_mapping):
   naics: dcid:NAICS/{INDUSTRY}
   """
   # Map industry and seasonal adjustment to statistical variable name.
-  adjustment_types = [("Adjusted", "dcs:BLSSeasonallyAdjusted"), ("Unadjusted", "dcs:BLSSeasonallyUnadjusted")]
+  adjustment_types = [("Adjusted", "dcs:BLSSeasonallyAdjusted"),
+                      ("Unadjusted", "dcs:BLSSeasonallyUnadjusted")]
 
   # Output the schema mapping to a new file.
   with open("BLSJolts_StatisticalVariables.mcf", "w+", newline="") as f_out:
@@ -207,20 +228,21 @@ def create_statistical_variables(jolts_df, schema_mapping):
 
             # Remove separation type entry if not includes.
             if job_change_event == "":
-              stat_var_schema = stat_var_schema.replace("jobChangeEvent: dcs:{JOB_CHANGE_EVENT}\n", "")
+              stat_var_schema = stat_var_schema \
+                  .replace("jobChangeEvent: dcs:{JOB_CHANGE_EVENT}\n", "")
 
             # Replace all other fields.
-            stat_var_schema = stat_var_schema.replace("{STAT_CLASS}", schema_name)   \
-                                            .replace("{INDUSTRY}", industry_code)   \
-                                            .replace("{ADJUSTMENT}", adjusted_dcid_map)   \
-                                            .replace("{BLS_ADJUSTMENT}", adjusted_schema)   \
-                                            .replace("{POPULATION}", pop_type)      \
-                                            .replace("{JOB_CHANGE_EVENT}", job_change_event)  
-
-            f_out.write(stat_var_schema)
+            f_out.write(stat_var_schema
+                .replace("{STAT_CLASS}", schema_name)
+                .replace("{INDUSTRY}", industry_code)
+                .replace("{ADJUSTMENT}", adjusted_dcid_map)
+                .replace("{BLS_ADJUSTMENT}", adjusted_schema)
+                .replace("{POPULATION}", pop_type)
+                .replace("{JOB_CHANGE_EVENT}", job_change_event))
 
 def main(argv):
-  """ Executes the downloading, preprocessing, and outputting of required MCF and CSV for JOLTS data.
+  """ Executes the downloading, preprocessing, and outputting of
+  required MCF and CSV for JOLTS data.
 
     Args:
       argv: Not used. Required by absl run.
