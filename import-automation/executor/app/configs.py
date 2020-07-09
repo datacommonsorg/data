9df92d359fa323b58ec0ad0dd2eb503fb1d8d07a
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-09 13:38:44 -0500

    configs

--- import-automation/executor/app/configs.py
+++ import-automation/executor/app/configs.py
@@ -1,23 +1,23 @@
-from google.cloud import secretmanager
-
+from google.cloud import datastore
 
 PROJECT_ID = 'google.com:datcom-data'
+CONFIGS_NAMESPACE = 'configs'
+CONFIGS_KIND = 'config'
 MANIFEST_FILENAME = 'manifest.json'
 REQUIREMENTS_FILENAME = 'requirements.txt'
 REPO_OWNER_USERNAME = 'intrepiditee'
 GITHUB_AUTH_USERNAME = 'intrepiditee'
 
 
-def _get_secret(secret_id, version_id='latest'):
-    client = secretmanager.SecretManagerServiceClient()
-    name = client.secret_version_path(PROJECT_ID, secret_id, version_id)
-    response = client.access_secret_version(name)
-    return response.payload.data.decode('UTF-8')
+def _get_config(entity_id):
+    client = datastore.Client(project=PROJECT_ID, namespace=CONFIGS_NAMESPACE)
+    key = client.key(CONFIGS_KIND, entity_id)
+    return client.get(key)[entity_id]
 
 
 def get_dashboard_oauth_client_id(version_id='latest'):
-    return _get_secret('DASHBOARD_OAUTH_CLIENT_ID', version_id)
+    return _get_config('DASHBOARD_OAUTH_CLIENT_ID')
 
 
 def get_github_auth_access_token(version_id='latest'):
-    return _get_secret('GITHUB_AUTH_ACCESS_TOKEN', version_id)
+    return _get_config('GITHUB_AUTH_ACCESS_TOKEN')
