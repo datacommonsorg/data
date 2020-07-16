58b232f694ff611ec2b49e452ca3786769e2c2e1
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-16 17:23:09 -0500

    tests for GCSBucketIO

--- import-automation/executor/app/service/gcs_io.py
+++ import-automation/executor/app/service/gcs_io.py
@@ -5,15 +5,14 @@ from google.cloud import storage
 from app import configs
 
 
-
 class GCSBucketIO:
     def __init__(self, path_prefix='', bucket_name=configs.BUCKET_NAME, bucket=None, client=None):
         if not client:
             client = storage.Client()
         if not bucket:
             bucket = client.bucket(bucket_name)
-        self.client = storage.Client()
-        self.bucket = self.client.bucket(bucket_name)
+        self.client = client
+        self.bucket = bucket
         self.prefix = path_prefix
 
     # def download_dir(self, src, dest):
@@ -36,9 +35,22 @@ class GCSBucketIO:
     #                 self.upload_file(entry.path, os.path.join(dest, entry.name))
 
     def upload_file(self, src, dest):
-        blob = self.bucket.blob(f'{self.prefix}/{dest}')
+        """Uploads a file to the bucket.
+
+        Args:
+            src: Path to the file to upload, as a string.
+            dest: Destination in the bucket as a string. This will be prefixed
+                by the prefix attribute.
+        """
+        blob = self.bucket.blob(os.path.join(self.prefix, dest))
         blob.upload_from_filename(src)
 
     def update_version(self, version):
-        blob = self.bucket.blob(f'{self.prefix}/latest_version.txt')
+        """Updates the version file in the bucket at
+        <prefix>/latest_version.txt.
+
+        Args:
+            version: New version as a string
+        """
+        blob = self.bucket.blob(os.path.join(self.prefix, 'latest_version.txt'))
         blob.upload_from_string(version)
