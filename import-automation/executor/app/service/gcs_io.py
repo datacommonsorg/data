3a35c75905af2bab0eab1c1e9bd7dd36db98e3f4
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-16 19:43:24 -0500

    remove unused code

--- import-automation/executor/app/service/gcs_io.py
+++ import-automation/executor/app/service/gcs_io.py
@@ -15,25 +15,6 @@ class GCSBucketIO:
         self.bucket = bucket
         self.prefix = path_prefix
 
-    # def download_dir(self, src, dest):
-    #     os.makedirs(dest, exist_ok=True)
-    #     for blob in self.bucket.list_blobs(prefix=src):
-    #         file_dest = os.path.join(dest, os.path.basename(blob.name))
-    #         self.download_file(blob.name, file_dest)
-    #
-    # def download_file(self, src, dest):
-    #     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    #     blob = self.bucket.blob(src)
-    #     blob.download_to_filename(dest)
-
-    # def upload_dir(self, src, dest):
-    #     with os.scandir(src) as entry_iter:
-    #         for entry in entry_iter:
-    #             if entry.is_dir(follow_symlinks=False):
-    #                 self.upload_dir(entry.path, os.path.join(dest, entry.name))
-    #             elif entry.is_file(follow_symlinks=False):
-    #                 self.upload_file(entry.path, os.path.join(dest, entry.name))
-
     def upload_file(self, src, dest):
         """Uploads a file to the bucket.
 
