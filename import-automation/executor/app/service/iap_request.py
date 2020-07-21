bbca768a54f1e6025ef7eab73e05b81afc86440d
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-21 10:10:21 -0500

    change constructors

--- import-automation/executor/app/service/iap_request.py
+++ import-automation/executor/app/service/iap_request.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
@@ -7,21 +21,19 @@ from app import configs
 
 class IAPRequest:
 
-    def __init__(self, client_id=None):
-        if not client_id:
-            client_id = configs.get_dashboard_oauth_client_id()
+    def __init__(self, client_id: str):
         self.client_id = client_id
 
-    def get(self, url, **kwargs):
+    def get(self, url: str, **kwargs):
         return self._request(url, 'GET', **kwargs)
 
-    def put(self, url, **kwargs):
+    def put(self, url: str, **kwargs):
         return self._request(url, 'PUT', **kwargs)
 
-    def post(self, url, **kwargs):
+    def post(self, url: str, **kwargs):
         return self._request(url, 'POST', **kwargs)
 
-    def patch(self, url, **kwargs):
+    def patch(self, url: str, **kwargs):
         return self._request(url, 'PATCH', **kwargs)
 
     def _request(self, url, method, **kwargs):
--- import-automation/executor/test/test_dashboard_api.py
+++ import-automation/executor/test/test_dashboard_api.py
@@ -1,23 +1,34 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 from unittest import mock
-import subprocess
 
+from requests import exceptions
+
+from test import utils
 from app.service import dashboard_api
-from app.service import iap
 
 
 @mock.patch('app.utils.utctime', lambda: '2020-07-15T12:07:17.365264+00:00')
 class DashboardAPITest(unittest.TestCase):
 
-    @mock.patch('app.configs.get_dashboard_oauth_client_id',
-                lambda: 'id')
-    @mock.patch('app.service.iap.IAPRequest',
-                mock.MagicMock(spec=iap.IAPRequest))
     def setUp(self):
-        self.dashboard = dashboard_api.DashboardAPI()
+        self.dashboard = dashboard_api.DashboardAPI('client-id')
 
-    @mock.patch('app.service.dashboard_api.DashboardAPI.log')
-    def test_log_helper(self, log):
+    @mock.patch('app.service.iap_request.IAPRequest.post')
+    def test_log_helper(self, post):
         args = {
             'message': 'message',
             'level': 'level',
@@ -25,13 +36,21 @@ class DashboardAPITest(unittest.TestCase):
             'attempt_id': 'attempt',
             'time_logged': 'time'
         }
-        self.dashboard._log_helper(**args)
-        log.assert_called_with(args)
-        with mock.patch('app.service.dashboard_api.DashboardAPI.log') as log:
-            self.assertRaises(ValueError, self.dashboard._log_helper, 'message', 'level')
+        expected = {
+            'message': 'message',
+            'level': 'level',
+            'run_id': 'run',
+            'attempt_id': 'attempt',
+            'time_logged': 'time',
+            'log_id': 'random-id'
+        }
+        post.return_value = utils.ResponseMock(200, expected)
+        self.assertEqual(expected, self.dashboard._log_helper(**args))
+        post.assert_called_once_with(
+            'https://datcom-data.uc.r.appspot.com/logs', json=args)
 
-    @mock.patch('app.service.dashboard_api.DashboardAPI.log')
-    def test_log_helper_time(self, log):
+    @mock.patch('app.service.iap_request.IAPRequest.post')
+    def test_log_helper_time(self, post):
         """Tests that time_logged is generated if not supplied."""
         args = {
             'message': 'message',
@@ -42,106 +61,80 @@ class DashboardAPITest(unittest.TestCase):
             'message': 'message',
             'level': 'level',
             'run_id': 'run',
-            'time_logged': '2020-07-15T12:07:17.365264+00:00'
+            'time_logged': '2020-07-15T12:07:17.365264+00:00',
+            'log_id': 'random-id'
         }
-        self.dashboard._log_helper(**args)
-        log.assert_called_with(expected)
+        post.return_value = utils.ResponseMock(200, expected)
+        self.assertEqual(expected, self.dashboard._log_helper(**args))
 
-    @mock.patch('app.service.dashboard_api.DashboardAPI.log')
-    def test_log_helper_raise(self, _):
+    def test_log_helper_id(self):
         """Tests that at least one of run_id and attempt_id
         need to be specified."""
-        self.assertRaises(ValueError, self.dashboard._log_helper, 'message',
-                          'level')
+        self.assertRaises(ValueError,
+                          self.dashboard._log_helper, 'message', 'level')
 
-    def test_construct_log(self):
-        """Tests that level and time_logged are added if not supplied."""
-        args = {
-            'message': 'message',
-            'attempt_id': 'attempt'
-        }
-        expected = {
-            'message': 'message',
-            'level': 'info',
-            'attempt_id': 'attempt',
-            'time_logged': '2020-07-15T12:07:17.365264+00:00'
-        }
-        self.assertEqual(expected, dashboard_api.construct_log(**args))
+    @mock.patch('app.service.iap_request.IAPRequest.post')
+    def test_log_helper_http(self, post):
+        """Tests that an exception is thrown is the HTTP request fails."""
+        post.return_value = utils.ResponseMock(400)
+        self.assertRaises(exceptions.HTTPError,
+                          self.dashboard._log_helper,
+                          'message', 'level', 'attempt')
 
-    def test_construct_log_raise(self):
-        """Tests that at least one of run_id and attempt_id
-        need to be supplied."""
-        self.assertRaises(ValueError, dashboard_api.construct_log, 'message')
-
-    def test_construct_log_process(self):
-        """Tests logging a subprocess."""
-        process = subprocess.run(
-            'echo "out" & >&2 echo "err" & exit 1',
-            shell=True, text=True, capture_output=True)
-        args = {
-            'message': 'message',
-            'process': process,
-            'run_id': 'run',
-            'level': 'info',
-            'time_logged': 'time'
-        }
-        expected = {
-            'message': 'message',
-            'time_logged': 'time',
-            'return_code': 1,
-            'run_id': 'run',
-            'stdout': 'out\n',
-            'stderr': 'err\n',
-            'level': 'critical',
-            'args': 'echo "out" & >&2 echo "err" & exit 1'
-        }
-        self.assertEqual(expected, dashboard_api.construct_log(**args))
-
-    def test_update_attempt(self):
-        attempt = {'hello': 'world'}
-        self.dashboard.update_attempt(attempt, 'id')
-        self.dashboard.iap.patch.assert_called_with(
+    @mock.patch('app.service.iap_request.IAPRequest.patch')
+    def test_update_attempt(self, patch):
+        attempt = {'import_name': 'treasury'}
+        patch.return_value = utils.ResponseMock(200, {})
+        self.assertEqual({}, self.dashboard.update_attempt(attempt, 'id'))
+        patch.assert_called_with(
             'https://datcom-data.uc.r.appspot.com/import_attempts/id',
-            json={'hello': 'world'})
+            json=attempt)
 
-    def test_update_attempt_id(self):
+    @mock.patch('app.service.iap_request.IAPRequest.patch')
+    def test_update_attempt_id(self, patch):
         """Tests that attempt_id can be found in the attempt body
         if not supplied as an argument."""
-        attempt = {'hello': 'world', 'attempt_id': 'id'}
-        self.dashboard.update_attempt(attempt)
+        attempt = {'import_name': 'treasury', 'attempt_id': 'idd'}
+        patch.return_value = utils.ResponseMock(200, {})
+        self.assertEqual({}, self.dashboard.update_attempt(attempt, 'idd'))
         self.dashboard.iap.patch.assert_called_with(
-            'https://datcom-data.uc.r.appspot.com/import_attempts/id',
-            json={'hello': 'world', 'attempt_id': 'id'})
+            'https://datcom-data.uc.r.appspot.com/import_attempts/idd',
+            json={'import_name': 'treasury', 'attempt_id': 'idd'})
 
     def test_update_attempt_no_id(self):
         """Tests that an exception is raised if attempt_id is not found."""
-        attempt = {'hello': 'world'}
+        attempt = {'import_name': 'treasury'}
         self.assertRaises(ValueError, self.dashboard.update_attempt, attempt)
 
-    def test_update_run(self):
-        run = {'hello': 'world'}
-        self.dashboard.update_run(run, 'id')
-        self.dashboard.iap.patch.assert_called_with(
+    @mock.patch('app.service.iap_request.IAPRequest.patch')
+    def test_update_run(self, patch):
+        run = {'commit_sha': 'commit-sha'}
+        patch.return_value = utils.ResponseMock(200, {})
+        self.assertEqual({}, self.dashboard.update_run(run, 'id'))
+        patch.assert_called_with(
             'https://datcom-data.uc.r.appspot.com/system_runs/id',
-            json={'hello': 'world'})
+            json=run)
 
-    def test_update_run_id(self):
+    @mock.patch('app.service.iap_request.IAPRequest.patch')
+    def test_update_run_id(self, patch):
         """Tests that run_id can be found in the run body
         if not supplied as an argument."""
-        run = {'hello': 'world', 'run_id': 'id'}
-        self.dashboard.update_run(run)
+        run = {'commit_sha': 'commit-sha', 'attempt_id': 'idddd'}
+        patch.return_value = utils.ResponseMock(200, {})
+        self.assertEqual({}, self.dashboard.update_attempt(run))
         self.dashboard.iap.patch.assert_called_with(
-            'https://datcom-data.uc.r.appspot.com/system_runs/id',
-            json={'hello': 'world', 'run_id': 'id'})
+            'https://datcom-data.uc.r.appspot.com/import_attempts/idddd',
+            json={'commit_sha': 'commit-sha', 'attempt_id': 'idddd'})
 
     def test_update_run_no_id(self):
         """Tests that an exception is raised if run_id is not found."""
-        run = {'hello': 'world'}
+        run = {'commit_sha': 'commit-sha'}
         self.assertRaises(ValueError, self.dashboard.update_run, run)
 
-    @mock.patch('app.service.dashboard_api.DashboardAPI.log')
-    def test_levels(self, log):
-        """Tests that the convenient logging functions add the right
+    @mock.patch('app.service.iap_request.IAPRequest.post',
+                lambda self, url, json=None: utils.ResponseMock(200, json))
+    def test_levels(self):
+        """Tests that the convenient logging functions set the right
         logging levels.  """
         args = {
             'message': 'message',
@@ -156,44 +149,4 @@ class DashboardAPITest(unittest.TestCase):
             (self.dashboard.debug, 'debug')
         ]
         for func, level in funcs:
-            func(**args)
-            self.assertEqual(level, log.call_args[0][0]['level'])
-
-    @mock.patch('app.configs.standalone', lambda: True)
-    @mock.patch('app.service.iap.IAPRequest')
-    def test_standalone(self, iap_request):
-        # Tests that no IAP calls are made in standalone mode
-        dashboard = dashboard_api.DashboardAPI(client_id=1)
-        dashboard.iap = iap_request()
-        logging_funcs = [
-            dashboard.critical,
-            dashboard.error,
-            dashboard.warning,
-            dashboard.info,
-            dashboard.debug
-        ]
-        log = {
-            'message': 'message',
-            'run_id': 'run',
-            'attempt_id': 'attempt',
-            'time_logged': 'time'
-        }
-        for func in logging_funcs:
-            func(**log)
-
-        dashboard._log_helper('message', 'level', 'attempt')
-        dashboard.log({
-            'message': 'message',
-            'level': 'level',
-            'attempt_id': 'attempt'
-        })
-        dashboard.init_run({})
-        dashboard.init_attempt({})
-        dashboard.update_run({'run_id': 'run'})
-        dashboard.update_attempt({'attempt_id': 'attempt'})
-
-        dashboard.iap.get.assert_not_called()
-        dashboard.iap.put.assert_not_called()
-        dashboard.iap.post.assert_not_called()
-        dashboard.iap.patch.assert_not_called()
-        dashboard.iap._request.assert_not_called()
+            self.assertEqual(level, func(**args)['level'])
