0540c25110530ba8bc9533aad76672d484235bc7
石俊杰 <31870027+intrepiditee@users.noreply.github.com>
2020-07-21 22:28:05 -0500

    replace Python script with gcloud command

--- import-automation/cloudbuild/task.sh
+++
@@ -1,4 +0,0 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-python3 -m pip install -r import-automation/cloudbuild/requirements.txt
-python3 import-automation/cloudbuild/create_task.py
