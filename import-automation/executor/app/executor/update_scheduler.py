61151904f9e466f1464dccd00c9e642a9267f287
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-22 10:27:25 -0500

    add helper and yapf format

--- import-automation/executor/app/executor/update_scheduler.py
+++ import-automation/executor/app/executor/update_scheduler.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Class for scheduling import updates using Google Cloud Scheduler.
 """
@@ -36,9 +35,9 @@ class UpdateScheduler:
     Returns:
         Created job as a dict.
     """
-    def __init__(self,
-                client: scheduler.CloudSchedulerClient,
-                config: configs.ExecutorConfig):
+
+    def __init__(self, client: scheduler.CloudSchedulerClient,
+                 config: configs.ExecutorConfig):
         self.client = client
         self.config = config
 
@@ -48,7 +47,7 @@ class UpdateScheduler:
         Attributes:
             absolute_import_name: Absolute import name of the import to be
                 updated. This is used as the name of the job.
-            schedule: Cron schedule for the updates. See
+            schedule: Cron schedule for the updates as a string. See
                 https://en.wikipedia.org/wiki/Cron.
 
         Returns:
@@ -57,35 +56,13 @@ class UpdateScheduler:
         Raises:
             Same exceptions as CloudSchedulerClient.create_job.
         """
-        job = {
-            'name': absolute_import_name,
-            'schedule': schedule,
-            'time_zone': 'utc',
-            'app_engine_http_target': {
-                'http_method': 'POST',
-                'app_engine_routing': {
-                    'service': 'default',
-                },
-                'relative_uri': '/update',
-                'headers': {
-                    'Content-Type': 'application/json'
-                },
-                'body': json.dumps({
-                    'absolute_import_name': absolute_import_name
-                }).encode()
-            },
-            'retry_config': {
-                'retry_count': 2
-            },
-            'attempt_deadline': {
-                # 24h
-                'seconds': 86400
-            }
-        }
         location_path = self.client.location_path(
             _fix_project_id(self.config.gcp_project_id),
             self.config.scheduler_location)
-        return dict(self.client.create_job(location_path, job))
+        return dict(
+            self.client.create_job(
+                location_path, _create_job_body(absolute_import_name,
+                                                schedule)))
 
     def delete_schedule(self, absolute_import_name):
         """Deletes an update schedule for an import.
@@ -99,9 +76,51 @@ class UpdateScheduler:
         """
         job_path = self.client.job_path(
             _fix_project_id(self.config.gcp_project_id),
-            self.config.scheduler_location,
-            absolute_import_name)
+            self.config.scheduler_location, absolute_import_name)
         self.client.delete_job(job_path)
+
+
+def _create_job_body(absolute_import_name: str, schedule: str) -> Dict:
+    """Creates the body of a Cloud Scheduler job for updating an import.
+
+    This function does not schedule any jobs.
+
+    Args:
+        absolute_import_name: Absolute import name of the import as a string.
+        schedule: Cron schedule for the updates as a string.
+
+    Returns:
+        The body of a Cloud Scheduler job as a dict that is ready to be used
+        as an argument to CloudSchedulerClient.create_job.
+    """
+    return {
+        'name': absolute_import_name,
+        'schedule': schedule,
+        'time_zone': 'utc',
+        'app_engine_http_target': {
+            'http_method':
+                'POST',
+            'app_engine_routing': {
+                'service': 'default',
+            },
+            'relative_uri':
+                '/update',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body':
+                json.dumps({
+                    'absolute_import_name': absolute_import_name
+                }).encode()
+        },
+        'retry_config': {
+            'retry_count': 2
+        },
+        'attempt_deadline': {
+            # 24h
+            'seconds': 86400
+        }
+    }
 
 
 def _fix_project_id(project_id: str) -> str:
