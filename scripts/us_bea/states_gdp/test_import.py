2547b5bc0fbe31d8309633b7b72496ed7eea7a22
Jeffrey Oldham <jeffrey.oldham@gmail.com>
2020-07-22 18:25:54 -0700

    Address a few pylint warnings.  No semantic changes are wanted.

--- scripts/us_bea/states_gdp/test_import.py
+++ scripts/us_bea/states_gdp/test_import.py
@@ -30,7 +30,7 @@ class USStateQuarterlyGDPImportTest(unittest.TestCase):
 
     def test_date_converter(self):
         """Tests the date converter function used to process raw data."""
-        date_conv_fn = import_data.StateGDPDataLoader._date_to_obs_date
+        date_conv_fn = import_data.StateGDPDataLoader.date_to_obs_date
 
         self.assertEqual(date_conv_fn("2005:Q1"), "2005-03")
         self.assertEqual(date_conv_fn("2005:Q2"), "2005-06")
@@ -41,7 +41,7 @@ class USStateQuarterlyGDPImportTest(unittest.TestCase):
 
     def test_geoid_converter(self):
         """Tests the geoid converter function used to process raw data."""
-        geoid_conv_fn = import_data.StateGDPDataLoader._convert_geoid
+        geoid_conv_fn = import_data.StateGDPDataLoader.convert_geoid
 
         self.assertEqual(geoid_conv_fn('   "1000"'), "geoId/10")
         self.assertEqual(geoid_conv_fn("1000"), "geoId/10")
@@ -85,7 +85,7 @@ class USStateQuarterlyPerIndustryImportTest(unittest.TestCase):
 
     def test_value_converter(self):
         """Tests value converter function that cleans out empty datapoints."""
-        val_conv_fn = import_industry_data_and_gen_mcf.StateGDPIndustryDataLoader._value_converter
+        val_conv_fn = import_industry_data_and_gen_mcf.StateGDPIndustryDataLoader.value_converter
         self.assertEqual(val_conv_fn("(D)"), -1)
         self.assertEqual(val_conv_fn("(E)"), -1)
         self.assertEqual(val_conv_fn("356785)"), -1)
@@ -98,7 +98,8 @@ class USStateQuarterlyPerIndustryImportTest(unittest.TestCase):
         """Tests industry class converter function that cleans out empty
         datapoints.
         """
-        ind_conv_fn = import_industry_data_and_gen_mcf.StateGDPIndustryDataLoader._convert_industry_class
+        ind_conv_fn = (
+            import_industry_data_and_gen_mcf.StateGDPIndustryDataLoader.convert_industry_class)
         prefix = "dcs:USStateQuarterlyIndustryGDP_NAICS_"
         self.assertEqual(ind_conv_fn("35"), prefix + "35")
         self.assertEqual(ind_conv_fn("987"), prefix + "987")
