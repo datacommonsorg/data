fe41b0bb869e341a4c0e7ac922727686d527923e
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-27 17:05:55 -0500

    resolve yapf and pylint warnings (#166)
    Co-authored-by: jeffreyoldham <64656025+jeffreyoldham@users.noreply.github.com>

--- scripts/us_bls/cpi/generate_csv.py
+++ scripts/us_bls/cpi/generate_csv.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 '''
 Downloading and converting BLS CPI raw csv files to csv files of two columns:
 "date" and "cpi", where "date" is of the form "YYYY-MM" and "cpi" is numeric.
@@ -19,13 +18,11 @@ Downloading and converting BLS CPI raw csv files to csv files of two columns:
 Usage: python3 generate_csv.py
 '''
 
-
-from io import StringIO
+import io
 
 import requests
 import frozendict
 import pandas as pd
-
 
 # Dict from series names to download links
 CSV_URLS = frozendict.frozendict({
@@ -46,6 +43,7 @@ SERIES_IDS = frozendict.frozendict({
 
 
 def main():
+    """Runs the script. See module docstring."""
     for series_name, url in CSV_URLS.items():
         series_id = SERIES_IDS[series_name]
 
@@ -55,7 +53,7 @@ def main():
         response = requests.get(url)
         response.raise_for_status()
 
-        buffer = StringIO(response.text)
+        buffer = io.StringIO(response.text)
 
         # The raw csv has four columns: "series_id", "year", "period", "value",
         # and "footnote_codes".
@@ -63,10 +61,10 @@ def main():
         # "year" is of the form "YYYY".
         # "period" is the months of the observations and is of the form "MM"
         # preceded by char 'M', e.g. "M05".
-        in_df = pd.read_csv(buffer, sep="\s+", dtype="str")
+        in_df = pd.read_csv(buffer, sep=r"\s+", dtype="str")
         # "M13" is annual averages
         in_df = in_df[(in_df["series_id"] == series_id) &
-            (in_df["period"] != "M13")]
+                      (in_df["period"] != "M13")]
         # Format "date" column as "YYYY-MM"
         in_df["date"] = in_df["year"] + "-" + in_df["period"].str[-2:]
         in_df = in_df[["date", "value"]]
