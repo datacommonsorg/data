aa14543c4278965ec986d7bb92fb292bc39f6c72
Robert Snedegar <rsned@users.noreply.github.com>
2020-07-28 20:57:43 -0700

    Finish up changes to BEA States GDP to get tests fully running. (#172)
    * Add pylint config file to ensure consistent options.
    
    * add import_automation to the covered test paths
    
    * fix typo on import-automation
    
    * Finish the changes to bea states data GDP to get tests running and code
    cleaner.
    
    * add scripts/ to pythonpath to ease tool running
    
    Co-authored-by: jeffreyoldham <64656025+jeffreyoldham@users.noreply.github.com>

--- scripts/us_bea/states_gdp/test_import.py
+++ scripts/us_bea/states_gdp/test_import.py
@@ -11,21 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Tests the import_data.py script.
+"""Tests the import_data.py script.
 
     Typical usage:
 
     python3 test_import.py
 """
-
+import os
 import unittest
 
 import pandas as pd
 from us_bea.states_gdp import import_data
 from us_bea.states_gdp import import_industry_data_and_gen_mcf
 
-TEST_DATA_DIR = "test_csvs/"
+# _MODULE_DIR is the path to where this test is running from.
+_MODULE_DIR = os.path.dirname(__file__)
+
+_TEST_DATA_DIR = os.path.join(_MODULE_DIR, "test_csvs")
 
 
 class USStateQuarterlyGDPImportTest(unittest.TestCase):
@@ -56,8 +58,10 @@ class USStateQuarterlyGDPImportTest(unittest.TestCase):
 
     def test_data_processing_tiny(self):
         """Tests end-to-end data cleaning on a tiny example."""
-        raw_df = pd.read_csv(TEST_DATA_DIR + "test_tiny_raw.csv", index_col=0)
-        clean_df = pd.read_csv(TEST_DATA_DIR + "test_tiny_cleaned.csv",
+        raw_df = pd.read_csv(os.path.join(_TEST_DATA_DIR, "test_tiny_raw.csv"),
+                             index_col=0)
+        clean_df = pd.read_csv(os.path.join(_TEST_DATA_DIR,
+                                            "test_tiny_cleaned.csv"),
                                index_col=0)
         loader = import_data.StateGDPDataLoader()
         loader.process_data(raw_df)
@@ -65,8 +69,10 @@ class USStateQuarterlyGDPImportTest(unittest.TestCase):
 
     def test_data_processing_small(self):
         """Tests end-to-end data cleaning on a small example."""
-        raw_df = pd.read_csv(TEST_DATA_DIR + "test_small_raw.csv", index_col=0)
-        clean_df = pd.read_csv(TEST_DATA_DIR + "test_small_cleaned.csv",
+        raw_df = pd.read_csv(os.path.join(_TEST_DATA_DIR, "test_small_raw.csv"),
+                             index_col=0)
+        clean_df = pd.read_csv(os.path.join(_TEST_DATA_DIR,
+                                            "test_small_cleaned.csv"),
                                index_col=0)
         loader = import_data.StateGDPDataLoader()
         loader.process_data(raw_df)
@@ -77,9 +83,11 @@ class USStateQuarterlyPerIndustryImportTest(unittest.TestCase):
 
     def test_data_processing_tiny(self):
         """Tests end-to-end data cleaning on a tiny example."""
-        raw_df = pd.read_csv(TEST_DATA_DIR + "test_industry_tiny_raw.csv",
+        raw_df = pd.read_csv(os.path.join(_TEST_DATA_DIR,
+                                          "test_industry_tiny_raw.csv"),
                              index_col=0)
-        clean_df = pd.read_csv(TEST_DATA_DIR + "test_industry_tiny_cleaned.csv",
+        clean_df = pd.read_csv(os.path.join(_TEST_DATA_DIR,
+                                            "test_industry_tiny_cleaned.csv"),
                                index_col=0)
         loader = import_industry_data_and_gen_mcf.StateGDPIndustryDataLoader()
         loader.process_data(raw_df)
@@ -99,6 +107,7 @@ class USStateQuarterlyPerIndustryImportTest(unittest.TestCase):
 
     def test_industry_class(self):
         """Tests industry class converter function that cleans out empty
+
         datapoints.
         """
         ind_conv_fn = (import_industry_data_and_gen_mcf.
@@ -112,5 +121,5 @@ class USStateQuarterlyPerIndustryImportTest(unittest.TestCase):
         self.assertEqual(ind_conv_fn("13-97,2,45-78"), prefix + "13_97&2&45_78")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
