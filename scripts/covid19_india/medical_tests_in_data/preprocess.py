167cd701404e4e955abe5c8e101b5a7335645082
Thejesh GN <i@thejeshgn.com>
2020-08-16 22:13:06 +0530

    removed the dependency on requests package

--- scripts/covid19_india/medical_tests_in_data/preprocess.py
+++ scripts/covid19_india/medical_tests_in_data/preprocess.py
@@ -1,6 +1,6 @@
 import json
 import csv
-import requests
+import urllib.request
 
 # 1. All counts are at India level, so geographic reference code will always be IN
 # 2. Only cumulative counts are available at this point
@@ -15,15 +15,15 @@ output_columns = [
 # Get the data from github.com/datameet/covid19n which scrapes the data
 # On daily basis from the ICMR PDFs, get from master
 
-response = requests.get("https://raw.githubusercontent.com/datameet/covid19/master/data/icmr_testing_status.json")
-data = response.json()
-rows = data["rows"]
-with open('COVID19_tests_india.csv', 'w', newline='') as f_out:
-    writer = csv.DictWriter(f_out, fieldnames=output_columns, lineterminator='\n')
-    writer.writeheader()
-    for row in rows:
-        processed_dict = {}
-        processed_dict["Date"] = (row["value"]["report_time"])[:10]
-        processed_dict["isoCode"] = INDIA
-        processed_dict["CumulativeCount_MedicalTest_COVID_19"] = row["value"]["samples"]
-        writer.writerow(processed_dict)
+with urllib.request.urlopen("https://raw.githubusercontent.com/datameet/covid19/master/data/icmr_testing_status.json") as response:
+    data = json.load(response)
+    rows = data["rows"]
+    with open('COVID19_tests_india.csv', 'w', newline='') as f_out:
+        writer = csv.DictWriter(f_out, fieldnames=output_columns, lineterminator='\n')
+        writer.writeheader()
+        for row in rows:
+            processed_dict = {}
+            processed_dict["Date"] = (row["value"]["report_time"])[:10]
+            processed_dict["isoCode"] = INDIA
+            processed_dict["CumulativeCount_MedicalTest_COVID_19"] = row["value"]["samples"]
+            writer.writerow(processed_dict)
