bd6cf6b7fd0e002e8e178a28095345f4adcdeab6
TJann <18621425+tjann@users.noreply.github.com>
2020-08-19 13:58:21 -0700

    Make OECD changes for better resolution (#245)
    * Limit place name resolver QPS to 50.
    
    * Append AdministrativeArea1 to TL2 geo names.
    
    * Add logic to hardcode country, nuts, and US states in the map-making script.
    
    * Remove pandas to_csv quoting NONE; regenerate CSV from previous changes.
    
    * Sort the regid2dcid.json for better diffing.

--- scripts/oecd/regional_demography/life_expectancy_and_mortality/preprocess_csv.py
+++ scripts/oecd/regional_demography/life_expectancy_and_mortality/preprocess_csv.py
@@ -14,7 +14,7 @@
 
 import sys
 sys.path.append('../')
-from utils import multi_index_to_single_index, generate_geo_id
+from utils import multi_index_to_single_index
 import csv
 import json
 import pandas as pd
@@ -24,11 +24,9 @@ df = pd.read_csv('REGION_DEMOGR_life_expectancy_and_mortality.csv')
 df = df[['TL', 'REG_ID', 'Region', 'VAR', 'SEX', 'Year', 'Value']]
 # First remove geos with names that we don't have mappings to dcid for.
 regid2dcid = dict(json.loads(open('../regid2dcid.json').read()))
-nuts = dict(json.loads(open('../region_nuts_codes.json').read()))
-df = df[df['REG_ID'].isin(nuts.keys()) | df['REG_ID'].isin(regid2dcid.keys())]
+df = df[df['REG_ID'].isin(regid2dcid.keys())]
 # Second, replace the names with dcids
-df['Region'] = df.apply(lambda row: generate_geo_id(row, nuts, regid2dcid),
-                        axis=1)
+df.replace({'Region': regid2dcid}, inplace=True)
 
 # process the source data
 df = df[['REG_ID', 'Region', 'VAR', 'SEX', 'Year', 'Value']]
@@ -79,9 +77,7 @@ VAR_to_statsvars = {
 }
 
 df_cleaned.rename(columns=VAR_to_statsvars, inplace=True)
-df_cleaned.to_csv('OECD_life_expectancy_and_mortality_cleaned.csv',
-                  index=False,
-                  quoting=csv.QUOTE_NONE)
+df_cleaned.to_csv('OECD_life_expectancy_and_mortality_cleaned.csv', index=False)
 
 TEMPLATE_MCF_TEMPLATE = """
 Node: E:OECD_life_expectancy_and_mortality_cleaned->E{index}
