e8838a3f2208fe065b86eebe5cfe7c7ac5d119b2
Lijuan Qian <lijuanqian@google.com>
2020-08-28 18:39:57 -0400

    lint

--- scripts/eurostat/life_expectancy/generate_mcf.py
+++ scripts/eurostat/life_expectancy/generate_mcf.py
@@ -15,6 +15,7 @@
 import pandas as pd
 import re
 
+
 def convert_range(s_input):
     """Convert range values from the format in statvar names (e.g. 10YearsOrMore)
      to the format as QuantityRange (e.g. [Years 10 -]) in Data Commons."""
@@ -39,7 +40,8 @@ def convert_range(s_input):
         num = match.group(1)
         unit = match.group(2)
         return '[{} {}]'.format(unit, num)
-  
+
+
 def generate_statvar(statvars, path):
     """Generate the statvars from the list of statvar names."""
     by_age_template = ('Node: dcid:{stat_var}\n'
@@ -48,7 +50,7 @@ def generate_statvar(statvars, path):
                        'age: {age}\n'
                        'measuredProperty: dcs:lifeExpectancy\n'
                        'statType: dcs:measuredValue\n\n')
-                       
+
     by_age_gender_template = by_age_template[:-1] + 'gender: schema:{gender}\n\n'
 
     with open(path, 'w') as f_out:
@@ -57,14 +59,21 @@ def generate_statvar(statvars, path):
             if len(keys) < 3:
                 raise Exception("Invalid StatVar")
             age = convert_range(keys[2])
-            if len(keys) == 3: # measuredPropery_populationType_age
-                f_out.write(by_age_template.format_map({'stat_var':stat_var,
-                    'age':age}))
-            elif len(keys) == 4: # measuredPropery_populationType_age_gender
+            if len(keys) == 3:  # measuredPropery_populationType_age
+                f_out.write(
+                    by_age_template.format_map({
+                        'stat_var': stat_var,
+                        'age': age
+                    }))
+            elif len(keys) == 4:  # measuredPropery_populationType_age_gender
                 gender = keys[-1]
-                f_out.write(by_age_gender_template.format_map({'stat_var':stat_var,
-                    'age':age, 'gender': gender}))
-                
+                f_out.write(
+                    by_age_gender_template.format_map({
+                        'stat_var': stat_var,
+                        'age': age,
+                        'gender': gender
+                    }))
+
 
 def generate_tmcf(statvars, path):
     """Generate the template mcf."""
@@ -75,26 +84,33 @@ def generate_tmcf(statvars, path):
                         'observationDate: C:lifexp->{year_col}\n'
                         'measurementMethod: dcs:EurostatRegionalStatistics\n'
                         'value: C:lifexp->{stat_var}\n\n')
-    
+
     with open(path, 'w') as f_out:
         i = 1
         for stat_var in statvars:
-            f_out.write(statvar_template.format_map({'sv_index': i,
-                'stat_var': stat_var, 'year_col': stat_var+'_year', 
-                'geo_col': stat_var+'_geo'}))
+            f_out.write(
+                statvar_template.format_map({
+                    'sv_index': i,
+                    'stat_var': stat_var,
+                    'year_col': stat_var + '_year',
+                    'geo_col': stat_var + '_geo'
+                }))
             i += 1
+
 
 def main():
     CLEANED_CSV = "demo_r_mlifexp_cleaned.csv"
     statvars = []
     # Get all the statvar names from the columns of csv files.
     # Each statvar has three columns: sv, sv_geo, sv_year.
-    for col in pd.read_csv(CLEANED_CSV,nrows=0).columns:
+    for col in pd.read_csv(CLEANED_CSV, nrows=0).columns:
         if col[-4:] != '_geo' and col[-5:] != '_year':
-           statvars.append(col)
-    generate_statvar(statvars, CLEANED_CSV.replace('_cleaned.csv', '_statvar.mcf'))
+            statvars.append(col)
+    generate_statvar(statvars,
+                     CLEANED_CSV.replace('_cleaned.csv', '_statvar.mcf'))
     generate_tmcf(statvars, CLEANED_CSV.replace('_cleaned.csv', '.tmcf'))
     return
+
 
 if __name__ == '__main__':
     main()
--- scripts/eurostat/life_expectancy/preprocess.py
+++ scripts/eurostat/life_expectancy/preprocess.py
@@ -17,29 +17,32 @@ import re
 
 PATH = 'demo_r_mlifexp.tsv'
 
+
 def nuts_to_iso(data):
     """Convert 2-letter NUTS codes for countries to ISO 3166-1 alpha-3 codes."""
     ISO_2_TO_3_PATH = ('./countries_codes_and_coordinates.csv')
     codes = pd.read_csv(ISO_2_TO_3_PATH)
     codes["Alpha-2 code"] = codes["Alpha-2 code"].str.extract(r'"([a-zA-Z]+)"')
     codes["Alpha-3 code"] = codes["Alpha-3 code"].str.extract(r'"([a-zA-Z]+)"')
-    # NUTS code matches ISO 3166-1 alpha-2 with two exceptions 
+    # NUTS code matches ISO 3166-1 alpha-2 with two exceptions
     codes["NUTS"] = codes["Alpha-2 code"]
-    codes.loc[codes["NUTS"] == "GR", "NUTS"] = "EL" 
+    codes.loc[codes["NUTS"] == "GR", "NUTS"] = "EL"
     codes.loc[codes["NUTS"] == "GB", "NUTS"] = "UK"
     code_dict = codes.set_index('NUTS').to_dict()['Alpha-3 code']
-    data.loc[data.index,'geo'] = data['geo'].map(code_dict)
+    data.loc[data.index, 'geo'] = data['geo'].map(code_dict)
     assert (~data['geo'].isnull()).all()
     return data
+
 
 def obtain_value(entry):
     """Extract value from entry. 
     The entries could be: '81.6', ': ', '79.9 e', ': e'
     """
-    entry = entry.split(' ', maxsplit=-1)[0] # Discard notes.
+    entry = entry.split(' ', maxsplit=-1)[0]  # Discard notes.
     if not entry or entry == ':':
         return None
     return float(entry)
+
 
 def preprocess(filepath):
     """Preprocess the tsv file for importing into DataCommons."""
@@ -50,55 +53,65 @@ def preprocess(filepath):
     assert identifier in data.columns
     years = list(data.columns.values)
     years.remove(identifier)
-    data = pd.melt(data, id_vars=identifier, value_vars=years, 
-                   var_name='year', value_name='life_expectancy')
+    data = pd.melt(data,
+                   id_vars=identifier,
+                   value_vars=years,
+                   var_name='year',
+                   value_name='life_expectancy')
 
     # Format string into desired format.
-    data['year'] = data['year'].astype(int) # remove spaces, e.g. "2018 "
+    data['year'] = data['year'].astype(int)  # remove spaces, e.g. "2018 "
     data['life_expectancy'] = data['life_expectancy'].apply(obtain_value)
-    # num_null = data[data['life_expectancy'].isin([': ', ': e'])].shape[0]
-    # data['life_expectancy'] = data['life_expectancy'].str.extract("(\d+\.\d+|\d+)")
-    # assert num_null == data['life_expectancy'].isnull().sum()
-    
+
     # Generate the statvars that each row belongs to.
-    data[['unit','sex','age','geo']] = data[identifier].str.split(',', expand=True)
+    data[['unit', 'sex', 'age',
+          'geo']] = data[identifier].str.split(',', expand=True)
     assert (data['unit'] == 'YR').all()
-    data['sex'] = data['sex'].map({'F':'_Female','M':'_Male','T':''})
-    assert (~data['sex'].isnull()).all() 
-    age_except = data['age'].isin(['Y_GE85','Y_LT1'])
+    data['sex'] = data['sex'].map({'F': '_Female', 'M': '_Male', 'T': ''})
+    assert (~data['sex'].isnull()).all()
+    age_except = data['age'].isin(['Y_GE85', 'Y_LT1'])
     data.loc[age_except, 'age'] = data.loc[age_except, 'age'].map({
-        'Y_GE85': '85OrMoreYears', 'Y_LT1': 'Upto1Years'})
-    data.loc[~age_except,'age']= data.loc[~age_except, 'age'].str.replace('Y', '') + "Years"
+        'Y_GE85': '85OrMoreYears',
+        'Y_LT1': 'Upto1Years'
+    })
+    data.loc[~age_except, 'age'] = data.loc[~age_except, 'age'].str.replace(
+        'Y', '') + "Years"
     data = data.drop(columns=[identifier])
     data['StatVar'] = "LifeExpectancy_Person_" + data['age'] + data['sex']
     data = data.drop(columns=['unit', 'sex', 'age'])
     statvars = data['StatVar'].unique()
 
-    # Convert the nuts codes to dcids 
+    # Convert the nuts codes to dcids
     data_country = data[data['geo'].str.len() <= 2]
     data_nuts = data[~(data['geo'].str.len() <= 2)]
-    data_country = nuts_to_iso(data_country) # convert nuts code to ISO 3166-1 alpha-3
-    data.loc[data_country.index,'geo'] = 'dcid:country/' + data_country['geo']
+    data_country = nuts_to_iso(
+        data_country)  # convert nuts code to ISO 3166-1 alpha-3
+    data.loc[data_country.index, 'geo'] = 'dcid:country/' + data_country['geo']
     data.loc[data_nuts.index, 'geo'] = 'dcid:nuts/' + data_nuts['geo']
 
     # Separate data of different StatVars from one column into multiple columns
-    # For example: 
+    # For example:
     # geo       year  StatVar           sv1_geo   sv1_year   sv2_geo   sv2_year
     # nuts/AT1  2018  sv1         =>    nuts/AT1  2018      nuts/AT2   2018
     # nuts/AT2  2018  sv2
     data_grouped = data.groupby('StatVar')
     subsets = []
     for _, subset in data_grouped:
-        pivot = subset['StatVar'].iloc[0] # get the statvar name
-        subset = subset.rename(columns = {'geo': pivot+'_geo',
-            'year': pivot + '_year', 'life_expectancy': pivot})
-        subset = subset.drop(columns = ['StatVar']).reset_index(drop=True)
+        pivot = subset['StatVar'].iloc[0]  # get the statvar name
+        subset = subset.rename(
+            columns={
+                'geo': pivot + '_geo',
+                'year': pivot + '_year',
+                'life_expectancy': pivot
+            })
+        subset = subset.drop(columns=['StatVar']).reset_index(drop=True)
         subsets.append(subset)
-    data = pd.concat(subsets, axis=1, join = 'outer')
+    data = pd.concat(subsets, axis=1, join='outer')
 
     # Save the processed data into CSV file.
-    data.to_csv(filepath[:-4]+'_cleaned.csv',index=False)
-    return 
+    data.to_csv(filepath[:-4] + '_cleaned.csv', index=False)
+    return
+
 
 if __name__ == '__main__':
     preprocess(PATH)
