5f8b1ec0b1d949eeb91f7ffb64ca07ea44754309
Eduardo Morales <edumorales@google.com>
2020-09-01 02:21:40 -0400

    adds wikiId

--- scripts/wikipedia_covid/timeseries/WikiParser.py
+++ scripts/wikipedia_covid/timeseries/WikiParser.py
@@ -6,17 +6,25 @@ import numpy as np
 import pandas as pd
 from urllib.request import urlopen
 import datetime
-from typing import Dict
+from typing import Dict, List
 
-def _get_css_value(css: str, find: str) -> str:
-    if not css or not find:
+
+def _get_css_value(css: str, key: str) -> str:
+    # CSS input has to be valid.
+    # key input has to be valid.
+    if not css or not key:
         return ""
 
-    styles = css.split(';')
+    # Search for the key
+    styles: List[str] = css.split(';')
     for style in styles:
         key, value = style.split(":")
+        # key found in CSS. Return it.
         if find == key.strip():
             return value
+    
+    # key has not be found in CSS.
+    return ""
 
 def _is_valid_date(date_text: str) -> bool:
     try:
@@ -25,12 +33,21 @@ def _is_valid_date(date_text: str) -> bool:
     except ValueError:
         return False
 
-def parse_time_series_chart(url: str) -> Dict[str, Dict[str, int]]:
+def get_wiki_id(url: str) -> str:
     html = urlopen(url)
     soup = BeautifulSoup(html, 'html.parser')
+    li_wikidata = soup.find("li", {"id": "t-wikibase"})
+    a_wikidata = li_wikidata.find('a')
+    entity_page = a_wikidata['href'].split(':')[-1]
+    wiki_id = entity_page.split('/')[-1]
+    print(wiki_id)
+    return wiki_id
 
-    # heading = soup.find("h1", {"id": "firstHeading"})
 
+def parse_time_series_chart(url: str) -> Dict[str, Dict[str, int]]:
+    print(url)
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
     table = soup.findAll("table")[0]
 
     span_labels = table.findAll("span", {"style": "font-size:90%; margin:0px"})
@@ -38,9 +55,15 @@ def parse_time_series_chart(url: str) -> Dict[str, Dict[str, int]]:
     color_to_label = {}
 
     for span_label in span_labels:
-        span, label = span_label.contents
+        if len(span_label.contents) < 2:
+            continue
+
+        span = span_label.contents[0]
+        label = span_label.contents[1].lower()
+        
         style = span['style']
         color = _get_css_value(style, "color")
+
         if color and label:
             color_to_label[color] = label.strip("\xa0")
 
@@ -49,12 +72,18 @@ def parse_time_series_chart(url: str) -> Dict[str, Dict[str, int]]:
     data = {}
 
     for row in rows:
-        date = row.find("td", {"class": "bb-04em"}).contents[0]
+        tds = row.findAll("td")
+
+        if len(tds) < 2:
+            continue
+
+        td_date, td_colors = tds[0], tds[1]
+        date = td_date.contents[0]
+
         if not _is_valid_date(date):
             continue
-        data[date] = {}
 
-        td_colors = row.find("td", {"class": "bb-lr"})
+        data[date] = {}
         div_labels = td_colors.findAll("div")
 
         for div_label in div_labels:
--- scripts/wikipedia_covid/timeseries/main.py
+++ scripts/wikipedia_covid/timeseries/main.py
@@ -1,9 +1,28 @@
-from Config import config
+from ConfigWikiId import config
 from WikiParser import parse_time_series_chart
 import pandas as pd
+from typing import Dict, List
 
-for country in config:
-    data = parse_time_series_chart(country['url'])
-    table = pd.DataFrame(data).T
-    table['geoId'] = country['geoId']
-    print(table)
+# Stores a list of all the tables, for each place.
+tables: List[pd.DataFrame] = []
+
+for place in config:
+    url: str = place['url']
+    # Parse the Wikipedia Page's table.
+    data: Dict[str, int] = parse_time_series_chart(url)
+    # Flip the table.
+    table: pd.DataFrame = pd.DataFrame(data).T
+    table['wikiId'] = place['wikiId']
+
+    # Store the table.
+    tables.append(table)    
+
+# Create one big table from the list of tables.
+main_table = pd.concat(tables)
+
+# Cases = Active Cases + Recoveries OR Confirmed Cases
+main_table['cases'] = main_table['active cases'] + main_table['recoveries'] + main_table['confirmed cases']
+# Get rid of any unecessary columns.
+main_table = main_table[['recoveries', 'active cases', 'deaths', 'cases', 'wikiId']]
+# Export table to CSV.
+main_table.to_csv('output.csv', mode="w+")
