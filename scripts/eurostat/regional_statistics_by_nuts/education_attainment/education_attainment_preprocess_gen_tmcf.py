fbc5c93b4dcdc0b853304e4879bc0f73a9fa9974
eftekhari-mhs <eftekhari.mhs@gmail.com>
2020-09-04 16:34:37 -0700

    read .tsv.gz from data_url

--- scripts/eurostat/regional_statistics_by_nuts/education_attainment/education_attainment_preprocess_gen_tmcf.py
+++ scripts/eurostat/regional_statistics_by_nuts/education_attainment/education_attainment_preprocess_gen_tmcf.py
@@ -16,12 +16,10 @@ import pandas as pd
 import io
 import csv
 
-# _DATA_URL = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/edat_lfse_04.tsv.gz"
 
+_DATA_URL = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/edat_lfse_04.tsv.gz"
 _CLEANED_CSV = "./Eurostats_NUTS2_Edat.csv"
 _TMCF = "./Eurostats_NUTS2_Edat.tmcf"
-_SOURCE_TSV = "./edat_lfse_04.tsv"
-_SOURCE_CSV_LONG = "./edat_lfse_04.csv"
 
 _OUTPUT_CLOUMNS = [
     'Date',
@@ -41,8 +39,8 @@ _OUTPUT_CLOUMNS = [
 ]
 
 
-def translate_wide_to_long(source_tsv, source_csv_long):
-    df = pd.read_csv(source_tsv, delimiter='\t')
+def translate_wide_to_long(data_url):
+    df = pd.read_csv(data_url, delimiter='\t')
     assert df.head
 
     header = list(df.columns.values)
@@ -80,68 +78,49 @@ def translate_wide_to_long(source_tsv, source_csv_long):
                         index=['geo', 'time', 'unit', 'age'],
                         columns=['sex-level'],
                         aggfunc='first').reset_index().rename_axis(None, axis=1)
+    return df
 
-    df.to_csv(source_csv_long, index=False)
 
-
-def preprocess(cleaned_csv, source_csv_long, output_columns):
+def preprocess(df, cleaned_csv):
     with open(cleaned_csv, 'w', newline='') as f_out:
         writer = csv.DictWriter(f_out,
-                                fieldnames=output_columns,
+                                fieldnames=_OUTPUT_CLOUMNS,
                                 lineterminator='\n')
-        with open(source_csv_long) as response:
-            reader = csv.DictReader(response)
-
-            writer.writeheader()
-            for row_dict in reader:
-                assert not row_dict['F_ED0-2'].isdigit()
-                assert not row_dict['F_ED3-8'].isdigit()
-                assert not row_dict['F_ED3_4'].isdigit()
-                assert not row_dict['F_ED5-8'].isdigit()
-                assert not row_dict['M_ED0-2'].isdigit()
-                assert not row_dict['M_ED3-8'].isdigit()
-                assert not row_dict['M_ED3_4'].isdigit()
-                assert not row_dict['M_ED5-8'].isdigit()
-                assert not row_dict['T_ED0-2'].isdigit()
-                assert not row_dict['T_ED3-8'].isdigit()
-                assert not row_dict['T_ED3_4'].isdigit()
-                assert not row_dict['T_ED5-8'].isdigit()
-                assert not row_dict['time'].isdigit()
-                processed_dict = {
+        writer.writeheader()
+        for _, row in df.iterrows():
+            writer.writerow({
                     'Date':
-                        '%s' % (row_dict['time'][:4]),
+                        '%s' % (row['time'][:4]),
                     'GeoId':
-                        'dcid:nuts/%s' % row_dict['geo'],
+                        'dcid:nuts/%s' % (row['geo']),
                     'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_AsAFractionOfCount_Person_25To64Years':
-                        (row_dict['T_ED0-2']),
+                        (row['T_ED0-2']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_AsAFractionOfCount_Person_25To64Years':
-                        (row_dict['T_ED3-8']),
+                        (row['T_ED3-8']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_AsAFractionOfCount_Person_25To64Years':
-                        (row_dict['T_ED3_4']),
+                        (row['T_ED3_4']),
                     'Count_Person_25To64Years_TertiaryEducation_AsAFractionOfCount_Person_25To64Years':
-                        (row_dict['T_ED5-8']),
+                        (row['T_ED5-8']),
                     'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                        (row_dict['F_ED0-2']),
+                        (row['F_ED0-2']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                        (row_dict['F_ED3-8']),
+                        (row['F_ED3-8']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                        (row_dict['F_ED3_4']),
+                        (row['F_ED3_4']),
                     'Count_Person_25To64Years_TertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                        (row_dict['F_ED5-8']),
+                        (row['F_ED5-8']),
                     'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                        (row_dict['M_ED0-2']),
+                        (row['M_ED0-2']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                        (row_dict['M_ED3-8']),
+                        (row['M_ED3-8']),
                     'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                        (row_dict['M_ED3_4']),
+                        (row['M_ED3_4']),
                     'Count_Person_25To64Years_TertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                        (row_dict['M_ED5-8']),
-                }
-
-        writer.writerow(processed_dict)
+                        (row['M_ED5-8']),
+                })
 
 
-def get_template_mcf(tmcf, output_columns):
+def get_template_mcf(output_columns):
     # Automate Template MCF generation since there are many Statistical Variables.
     TEMPLATE_MCF_TEMPLATE = """
   Node: E:EurostatsNUTS2_Education_Attainment->E{index}
@@ -155,7 +134,7 @@ def get_template_mcf(tmcf, output_columns):
   """
 
     stat_vars = output_columns[2:]
-    with open(tmcf, 'w', newline='') as f_out:
+    with open(_TMCF, 'w', newline='') as f_out:
         for i in range(len(stat_vars)):
             f_out.write(
                 TEMPLATE_MCF_TEMPLATE.format_map({
@@ -164,7 +143,17 @@ def get_template_mcf(tmcf, output_columns):
                 }))
 
 
+def test_col_names(cleaned_csv, tmcf):
+    """ check if all the column names specified in the template mcf
+        is found in the CSV file"""
+    cols = pd.read_csv(cleaned_csv, nrows=0).columns
+    with open(tmcf, "r") as file:
+        for line in file:
+            if " C:" in line:
+                col_name = line[:-1].split("->")[1]
+                assert col_name in cols
+
 if __name__ == "__main__":
-    translate_wide_to_long(_SOURCE_TSV, _SOURCE_CSV_LONG)
-    preprocess(_CLEANED_CSV, _SOURCE_CSV_LONG, _OUTPUT_CLOUMNS)
-    get_template_mcf(_TMCF, _OUTPUT_CLOUMNS)
+    preprocess(translate_wide_to_long(_DATA_URL),
+               _CLEANED_CSV)
+    get_template_mcf(_OUTPUT_CLOUMNS)
