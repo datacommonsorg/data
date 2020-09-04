0f8c99ae0d09420b3f0bc153d10bf3a91c0111c7
tjann <18621425+tjann@users.noreply.github.com>
2020-09-03 18:25:20 -0700

    Address shanth's comments.

--- scripts/oecd/regional_demography/gen_place_mapping_stats.py
+++ scripts/oecd/regional_demography/gen_place_mapping_stats.py
@@ -14,22 +14,29 @@ This only tracks the resolution statistics from `tools/place_name_resolver/`.
 The final mapping used by the OECD datasets overrides the `place_name_resolver`
 mapping for special cases such as NUTS codes, USA states, etc., where we know
 the OECD Region ID corresponds with a well-known standard, such as NUTS or
-FIPS. To see changes in the final mapping, just use diffs in regid2dcid.json.
+FIPS. We also address the many-to-one OECD to DCID mapping issues by removeing
+DCIDs for lower level places that resolve to a DCID already used by a higher
+level place. These optimizations are made in `clean_geos_resolved_to_dict.py`.
+
+To see changes in the final mapping, just use diffs in regid2dcid.json.
 
 To regenerate this file, run `python3 gen_place_mapping_stats.py > stats.txt`
 """)
 
-print("\n1. Summary of resolution errors (NaN = no error):\n")
+print("\n1. Number of resolved DCIDs by namespace (ignoring N to 1 issues):")
+print(df.namespace.value_counts())
+
+print("\n2. Summary of resolution errors (NaN = no error):\n")
 print(df.errors.value_counts(dropna=False))
 
 print("""
-2. Number of OECD places mapped to each DCID. Observe N to 1 issues. (NaN =
+3. Number of OECD places mapped to each DCID. Observe N to 1 issues. (NaN =
    places that failed to resolve to DCID):
 """)
 print(df.dcid.value_counts(dropna=False).head(40))
 
 print("""
-3. Number of OECD places mapped to each DCID, grouped by namespace (only those
+4. Number of OECD places mapped to each DCID, grouped by namespace (only those
    with N to 1 issues):
 """)
 with pd.option_context('display.max_rows', None):
@@ -37,7 +44,5 @@ with pd.option_context('display.max_rows', None):
         df.groupby(['namespace', 'dcid']).filter(lambda x: len(x) > 1).groupby(
             ['namespace', 'dcid']).size())
 
-print("\n4. Number of resolved DCIDs by namespace (ignoring N to 1 issues):")
-print(df.namespace.value_counts())
 ## PART 2: Including hardcoding for nuts, USA, etc.
 #  Just use the diffs in regid2dcid.json.
