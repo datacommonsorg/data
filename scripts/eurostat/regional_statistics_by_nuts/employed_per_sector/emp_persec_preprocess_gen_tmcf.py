30fa7760fbaa7874b252f6e3f3cca0178cae8a10
eftekhari-mhs <eftekhari.mhs@gmail.com>
2020-09-05 15:33:33 -0700

    preprocessing script

--- scripts/eurostat/regional_statistics_by_nuts/employed_per_sector/emp_persec_preprocess_gen_tmcf.py
+++ scripts/eurostat/regional_statistics_by_nuts/employed_per_sector/emp_persec_preprocess_gen_tmcf.py
@@ -16,25 +16,28 @@ import pandas as pd
 import io
 import csv
 
-_DATA_URL = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/edat_lfse_04.tsv.gz"
-_CLEANED_CSV = "./Eurostats_NUTS2_Edat.csv"
-_TMCF = "./Eurostats_NUTS2_Edat.tmcf"
+_DATA_URL = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/nama_10r_3empers.tsv.gz"
+_CLEANED_CSV = "./Eurostats_NUTS3_Empers.csv"
+_TMCF = "./Eurostats_NUTS3_Empers.tmcf"
 
-_OUTPUT_COLUMNS = [
+_OUTPUT_CLOUMNS = [
     'Date',
     'GeoId',
-    'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_AsAFractionOfCount_Person_25To64Years',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_AsAFractionOfCount_Person_25To64Years',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_AsAFractionOfCount_Person_25To64Years',
-    'Count_Person_25To64Years_TertiaryEducation_AsAFractionOfCount_Person_25To64Years',
-    'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Female_AsAFractionOfCount_Person_25To64Years_Female',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female',
-    'Count_Person_25To64Years_TertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female',
-    'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Male_AsAFractionOfCount_Person_25To64Years_Male',
-    'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male',
-    'Count_Person_25To64Years_TertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male',
+    'Count_Person_Employed_NACE/A',
+    'Count_Person_Employed_NACE/B-E',
+    'Count_Person_Employed_NACE/C',
+    'Count_Person_Employed_NACE/F',
+    'Count_Person_Employed_NACE/G-I',
+    'Count_Person_Employed_NACE/G-J',
+    'Count_Person_Employed_NACE/J',
+    'Count_Person_Employed_NACE/K',
+    'Count_Person_Employed_NACE/K-N',
+    'Count_Person_Employed_NACE/L',
+    'Count_Person_Employed_NACE/M-N',
+    'Count_Person_Employed_NACE/O-Q',
+    'Count_Person_Employed_NACE/O-U',
+    'Count_Person_Employed_NACE/R-U',
+    'Count_Person_Employed',
 ]
 
 
@@ -56,26 +59,27 @@ def translate_wide_to_long(data_url):
     new = df[header[0]].str.split(",", n=-1, expand=True)
     df = df.join(
         pd.DataFrame({
-            'geo': new[4],
-            'unit': new[3],
-            'age': new[2],
-            'level': new[1],
-            'sex': new[0]
+            'geo': new[3],
+            'nace_r2': new[2],
+            'wstatus': new[1],
+            'unit': new[0]
         }))
     df.drop(columns=[header[0]], inplace=True)
 
-    df["sex-level"] = df["sex"] + "_" + df["level"]
+    df["wstatus-nace"] = df["wstatus"] + "_"+df["nace_r2"]
 
     # Remove empty rows, clean values to have all digits.
     df = df[df.value.str.contains('[0-9]')]
-    possible_flags = [' ', ':', 'b', 'd', 'e', 'u']
+    possible_flags = [' ', ':', 'b', 'd', 'e', 'u', 'p']
     for flag in possible_flags:
         df['value'] = df['value'].str.replace(flag, '')
 
     df['value'] = pd.to_numeric(df['value'])
+    df['value'] = df['value'].mul(1000)
+
     df = df.pivot_table(values='value',
-                        index=['geo', 'time', 'unit', 'age'],
-                        columns=['sex-level'],
+                        index=['geo', 'time', 'unit'],
+                        columns=['wstatus-nace'],
                         aggfunc='first').reset_index().rename_axis(None, axis=1)
     return df
 
@@ -83,7 +87,7 @@ def translate_wide_to_long(data_url):
 def preprocess(df, cleaned_csv):
     with open(cleaned_csv, 'w', newline='') as f_out:
         writer = csv.DictWriter(f_out,
-                                fieldnames=_OUTPUT_COLUMNS,
+                                fieldnames=_OUTPUT_CLOUMNS,
                                 lineterminator='\n')
         writer.writeheader()
         for _, row in df.iterrows():
@@ -92,56 +96,62 @@ def preprocess(df, cleaned_csv):
                     '%s' % (row['time'][:4]),
                 'GeoId':
                     'dcid:nuts/%s' % (row['geo']),
-                'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_AsAFractionOfCount_Person_25To64Years':
-                    (row['T_ED0-2']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_AsAFractionOfCount_Person_25To64Years':
-                    (row['T_ED3-8']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_AsAFractionOfCount_Person_25To64Years':
-                    (row['T_ED3_4']),
-                'Count_Person_25To64Years_TertiaryEducation_AsAFractionOfCount_Person_25To64Years':
-                    (row['T_ED5-8']),
-                'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                    (row['F_ED0-2']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                    (row['F_ED3-8']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                    (row['F_ED3_4']),
-                'Count_Person_25To64Years_TertiaryEducation_Female_AsAFractionOfCount_Person_25To64Years_Female':
-                    (row['F_ED5-8']),
-                'Count_Person_25To64Years_LessThanPrimaryEducationOrPrimaryEducationOrLowerSecondaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                    (row['M_ED0-2']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrHigher_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                    (row['M_ED3-8']),
-                'Count_Person_25To64Years_UpperSecondaryEducationOrPostSecondaryNonTertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                    (row['M_ED3_4']),
-                'Count_Person_25To64Years_TertiaryEducation_Male_AsAFractionOfCount_Person_25To64Years_Male':
-                    (row['M_ED5-8']),
+                'Count_Person_Employed_NACE/A':
+                    (row['EMP_A']),
+                'Count_Person_Employed_NACE/B-E':
+                    (row['EMP_B-E']),
+                'Count_Person_Employed_NACE/C':
+                    (row['EMP_C']),
+                'Count_Person_Employed_NACE/F':
+                    (row['EMP_F']),
+                'Count_Person_Employed_NACE/G-I':
+                    (row['EMP_G-I']),
+                'Count_Person_Employed_NACE/G-J':
+                    (row['EMP_G-J']),
+                'Count_Person_Employed_NACE/J':
+                    (row['EMP_J']),
+                'Count_Person_Employed_NACE/K':
+                    (row['EMP_K']),
+                'Count_Person_Employed_NACE/K-N':
+                    (row['EMP_K-N']),
+                'Count_Person_Employed_NACE/L':
+                    (row['EMP_L']),
+                'Count_Person_Employed_NACE/M-N':
+                    (row['EMP_M_N']),
+                'Count_Person_Employed_NACE/O-Q':
+                    (row['EMP_O-Q']),
+                'Count_Person_Employed_NACE/O-U':
+                    (row['EMP_O-U']),
+                'Count_Person_Employed_NACE/R-U':
+                    (row['EMP_R-U']),
+                'Count_Person_Employed':
+                    (row['EMP_TOTAL']),
             })
 
 
-def get_template_mcf(output_columns):
+def get_template_mcf():
     # Automate Template MCF generation since there are many Statistical Variables.
     TEMPLATE_MCF_TEMPLATE = """
-  Node: E:EurostatsNUTS2_Education_Attainment->E{index}
+  Node: E:EurostatsNUTS3_Employed_per_Sector->E{index}
   typeOf: dcs:StatVarObservation
   variableMeasured: dcs:{stat_var}
-  observationAbout: C:EurostatsNUTS2_Education_Attainment->GeoId
-  observationDate: C:EurostatsNUTS2_Education_Attainment->Date
-  value: C:EurostatsNUTS2_Education_Attainment->{stat_var}
-  scalingFactor: 100
+  observationAbout: C:EurostatsNUTS3_Employed_per_Sector->GeoId
+  observationDate: C:EurostatsNUTS3_Employed_per_Sector->Date
+  value: C:EurostatsNUTS3_Employed_per_Sector->{stat_var}
+  sourceScalingFactor: 1000
   measurementMethod: dcs:EurostatRegionalStatistics
   """
 
-    stat_vars = output_columns[2:]
+    stat_vars = _OUTPUT_CLOUMNS[2:]
     with open(_TMCF, 'w', newline='') as f_out:
         for i in range(len(stat_vars)):
             f_out.write(
                 TEMPLATE_MCF_TEMPLATE.format_map({
                     'index': i,
-                    'stat_var': output_columns[2:][i]
+                    'stat_var': _OUTPUT_CLOUMNS[2:][i]
                 }))
 
 
 if __name__ == "__main__":
     preprocess(translate_wide_to_long(_DATA_URL), _CLEANED_CSV)
-    get_template_mcf(_OUTPUT_COLUMNS)
+    get_template_mcf()
