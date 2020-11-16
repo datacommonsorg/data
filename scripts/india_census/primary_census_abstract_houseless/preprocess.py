fce332ddb6afdaadbb594ef6572a91753254aa25
Thejesh GN <i@thejeshgn.com>
2020-11-16 22:16:28 +0530

    updated the main readme to include houseless dataset

---
+++ scripts/india_census/primary_census_abstract_houseless/preprocess.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from ..common.base import CensusPrimaryAbstractDataLoaderBase
+
+
+class CensusPrimaryCensusAbstractHouselessDataLoader(
+        CensusPrimaryAbstractDataLoaderBase):
+
+    def _download_and_standardize(self):
+        CensusPrimaryAbstractDataLoaderBase._download_and_standardize(self)
+        #These columns don't exist in this dataset, since they are at
+        #District level. We are census adding standard code with 0s
+        #to make their census_location_id uniform
+        self.raw_df["Subdistt"] = "00000"
+        self.raw_df["Town/Village"] = "000000"
+        self.raw_df["Ward"] = "0000"
+        self.raw_df["EB"] = "000000"
+
+    def _get_base_name(self, row):
+        name = "Count_" + row["populationType"]
+        name = name + "_" + "Houseless"
+        return name
+
+    def _get_base_constraints(self, row):
+        constraints = "livingInHouseholdType: Houseless \n"
+        return constraints
+
+
+if __name__ == '__main__':
+    data_file_path = os.path.join(os.path.dirname(__file__),
+                                  'data/PCA_HL_2011_Release.xls')
+    #data_file_path = "https://censusindia.gov.in/2011-Documents/PCA_HL_2011_Release.xls"
+
+    metadata_file_path = os.path.join(
+        os.path.dirname(__file__),
+        '../common/primary_abstract_data_variables.csv')
+
+    existing_stat_var = []
+
+    mcf_file_path = os.path.join(
+        os.path.dirname(__file__),
+        './IndiaCensus2011_Primary_Abstract_Houseless.mcf')
+    tmcf_file_path = os.path.join(
+        os.path.dirname(__file__),
+        './IndiaCensus2011_Primary_Abstract_Houseless.tmcf')
+
+    csv_file_path = os.path.join(
+        os.path.dirname(__file__),
+        './IndiaCensus2011_Primary_Abstract_Houseless.csv')
+    loader = CensusPrimaryCensusAbstractHouselessDataLoader(
+        data_file_path=data_file_path,
+        metadata_file_path=metadata_file_path,
+        mcf_file_path=mcf_file_path,
+        tmcf_file_path=tmcf_file_path,
+        csv_file_path=csv_file_path,
+        existing_stat_var=existing_stat_var,
+        census_year=2011,
+        dataset_name="Primary_Abstract_Houseless")
+    loader.process()
