77d219e2acaa66d95a0d5da87ccc31553832b3a0
tjann <18621425+tjann@users.noreply.github.com>
2020-12-28 15:43:27 -0800

    Undo fix for mDenom, leave for a bug fix. I filed Issue 351.

--- scripts/world_bank/wdi/worldbank.py
+++ scripts/world_bank/wdi/worldbank.py
@@ -207,7 +207,7 @@ def group_stat_vars_by_observation_properties(indicator_codes):
     """
     # All the statistical observation properties that we included.
     properties_of_stat_var_observation = ([
-        'measurementMethod', 'scalingFactor',
+        'measurementMethod', 'measurementDenominator', 'scalingFactor',
         'sourceScalingFactor', 'unit'
     ])
     # List of tuples to return.
