d8ed130d5fb97bc01cf2ccd872ae295bdeef34bc
hanlu09205 <59888187+hanlu09205@users.noreply.github.com>
2021-01-13 13:43:32 -0800

    move owid preprocessor to owid/covid19 (#363)
    * move OWID preprocess to ourworldindata/covid19 directory
    generate tmcf file
    
    * add README

--- scripts/ourworldindata/covid19/preprocess_csv.py
+++ scripts/ourworldindata/covid19/preprocess_csv.py
@@ -18,7 +18,7 @@ import ssl
 import urllib.request
 import sys
 
-sys.path.insert(1, '../../util')
+sys.path.insert(1, '../../../util')
 from alpha2_to_dcid import COUNTRY_MAP
 
 country_set = set(COUNTRY_MAP.values())
@@ -35,7 +35,7 @@ output_columns = [
     'CumulativeCount_MedicalTest_ConditionCOVID_19',
     'IncrementalCount_MedicalTest_ConditionCOVID_19'
 ]
-with open('COVID_OWID.csv', 'w', newline='') as f_out:
+with open('OurWorldInData_Covid19.csv', 'w', newline='') as f_out:
     writer = csv.DictWriter(f_out,
                             fieldnames=output_columns,
                             lineterminator='\n')
@@ -79,3 +79,23 @@ with open('COVID_OWID.csv', 'w', newline='') as f_out:
             }
 
             writer.writerow(processed_dict)
+
+# Automate Template MCF generation since there are many Statitical Variables.
+TEMPLATE_MCF_TEMPLATE = """
+Node: E:OurWorldInData_Covid19->E{index}
+typeOf: dcs:StatVarObservation
+variableMeasured: dcs:{stat_var}
+measurementMethod: dcs:OurWorldInData_COVID19
+observationAbout: C:OurWorldInData_Covid19->GeoId
+observationDate: C:OurWorldInData_Covid19->Date
+value: C:OurWorldInData_Covid19->{stat_var}
+"""
+
+stat_vars = output_columns[2:]
+with open('OurWorldInData_Covid19.tmcf', 'w', newline='') as f_out:
+    for i in range(len(stat_vars)):
+        f_out.write(
+            TEMPLATE_MCF_TEMPLATE.format_map({
+                'index': i,
+                'stat_var': output_columns[2:][i]
+            }))
