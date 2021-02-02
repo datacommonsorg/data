97c1fffdc946cda9d9ada25c47a33e1c32188a19
hanlu09205 <hanluc@google.com>
2021-02-02 13:58:39 -0800

    add 2016 data process

--- scripts/fbi/crime/preprocess.py
+++ scripts/fbi/crime/preprocess.py
@@ -19,6 +19,7 @@ import urllib.request
 import sys
 import requests
 import re
+import os
 
 import pandas as pd
 import logging
@@ -63,30 +64,30 @@ YEARS_WITH_TWO_RAPE_COLUMNS = {'2013', '2014', '2015', '2016'}
 YEARS_WITHOUT_POPULATION_COLUMN = {'2016'}
 
 YEAR_TO_URL = {
-    # '2019':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2019/crime-in-the-u.s.-2019/tables/table-8/table-8.xls',
-    # '2018':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2018/crime-in-the-u.s.-2018/tables/table-8/table-8.xls',
-    # '2017':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2017/crime-in-the-u.s.-2017/tables/table-8/table-8.xls',
+    '2019':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2019/crime-in-the-u.s.-2019/tables/table-8/table-8.xls',
+    '2018':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2018/crime-in-the-u.s.-2018/tables/table-8/table-8.xls',
+    '2017':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2017/crime-in-the-u.s.-2017/tables/table-8/table-8.xls',
     '2016': 'https://ucr.fbi.gov/crime-in-the-u.s/2016/crime-in-the-u.s.-2016/tables/table-8/table-8.xls',
-    # '2015':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2015.xls',
-    # '2014':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2014/crime-in-the-u.s.-2014/tables/table-8/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2014.xls',
-    # '2013':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2013.xls',
-    # '2012':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2012/crime-in-the-u.s.-2012/tables/8tabledatadecpdf/table_8_offenses_known_to_law_enforcement_by_state_by_city_2012.xls',
-    # '2011':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2011/crime-in-the-u.s.-2011/tables/table_8_offenses_known_to_law_enforcement_by_state_by_city_2011.xls',
-    # # Sanity check 2008-2010 don't have duplicate city state.
-    # '2010':
-    #     'https://ucr.fbi.gov/crime-in-the-u.s/2010/crime-in-the-u.s.-2010/tables/10tbl08.xls',
-    # '2009':
-    #     'https://www2.fbi.gov/ucr/cius2009/data/documents/09tbl08.xls',
-    # '2008':
-    #     'https://www2.fbi.gov/ucr/cius2008/data/documents/08tbl08.xls',
+    '2015':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2015.xls',
+    '2014':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2014/crime-in-the-u.s.-2014/tables/table-8/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2014.xls',
+    '2013':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2013.xls',
+    '2012':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2012/crime-in-the-u.s.-2012/tables/8tabledatadecpdf/table_8_offenses_known_to_law_enforcement_by_state_by_city_2012.xls',
+    '2011':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2011/crime-in-the-u.s.-2011/tables/table_8_offenses_known_to_law_enforcement_by_state_by_city_2011.xls',
+    # Sanity check 2008-2010 don't have duplicate city state.
+    '2010':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2010/crime-in-the-u.s.-2010/tables/10tbl08.xls',
+    '2009':
+        'https://www2.fbi.gov/ucr/cius2009/data/documents/09tbl08.xls',
+    '2008':
+        'https://www2.fbi.gov/ucr/cius2008/data/documents/08tbl08.xls',
 }
 
 
@@ -206,6 +207,9 @@ def _should_skip_special_line(year, field):
         return True
     if year == '2011' and field[_STATE_INDEX] == 'OREGON' and field[
             _CITY_INDEX] == 'Portland' and field[_POPULATION_INDEX] == '521.0':
+        return True
+    # 2016 CSV has footnote that we want to skip here.
+    if year == '2016' and field[_STATE_INDEX] is None:
         return True
     return False
 
@@ -334,12 +338,11 @@ def clean_crime_file(f_input, f_output, year):
         field[_CITY_INDEX] = _remove_digits(field[_CITY_INDEX])
         count_city += 1
 
-        # If duplicate lines.
+        # If duplicate lines or footnote lines. 
         if _should_skip_special_line(year, field):
             continue
 
         # Keep the first n fields. Some of the files contain extra empty fields.
-        print(field)
         output_line = '{},{}\n'.format(year,
                                        ','.join(field[:_FIELDS_IN_CRIME_FILE]))
         f_output.write(output_line)
@@ -398,6 +401,10 @@ if __name__ == '__main__':
                 logging.info('clean crime file for year %s', year)
                 clean_crime_file(f_input, f_output, year)
 
-        # TODO(hanlu): Clean transient files.
-        # update_and_calculate_crime_csv(geo_codes, year + '_cleaned.csv',
-                                    #    year + '_calculated.csv')
+        update_and_calculate_crime_csv(geo_codes, year + '_cleaned.csv',
+                                       year + '_calculated.csv')
+
+        # Clean files.  
+        os.remove(xls_file)
+        os.remove(csv_file)
+        os.remove(cleaned_csv_file)
