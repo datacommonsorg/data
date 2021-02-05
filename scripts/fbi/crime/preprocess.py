d87de40a5f3cfb38cec361d4e36ffee1b7e3bdb6
hanlu09205 <hanluc@google.com>
2021-02-04 22:15:31 -0800

    FBI change 2016 to the right table

--- scripts/fbi/crime/preprocess.py
+++ scripts/fbi/crime/preprocess.py
@@ -19,6 +19,7 @@ import urllib.request
 import sys
 import requests
 import re
+import os
 
 import pandas as pd
 import logging
@@ -55,28 +56,62 @@ _CRIME_FIELDS = [
     'PropertyArson',
 ]
 
-_CALCULATED_CRIME_FIELDS = _CRIME_FIELDS + ['Total', 'Geocode']
+GEO_CODE = 'Geocode'
+TOTAL = 'Total'
+
+OUTPUT_COLUMNS = [
+    'Year', 'GeoId', 'Count_CriminalActivities_ViolentCrime',
+    'Count_CriminalActivities_MurderAndNonNegligentManslaughter',
+    'Count_CriminalActivities_ForcibleRape', 'Count_CriminalActivities_Robbery',
+    'Count_CriminalActivities_AggravatedAssault',
+    'Count_CriminalActivities_PropertyCrime',
+    'Count_CriminalActivities_Burglary',
+    'Count_CriminalActivities_LarcenyTheft',
+    'Count_CriminalActivities_MotorVehicleTheft',
+    'Count_CriminalActivities_Arson', 'Count_CriminalActivities_CombinedCrime'
+]
+
+# Automate Template MCF generation since there are many Statitical Variables.
+TEMPLATE_MCF_TEMPLATE = """
+Node: E:FBI_Crime->E{index}
+typeOf: dcs:StatVarObservation
+variableMeasured: dcs:{stat_var}
+measurementMethod: dcs:FBI_Crime
+observationAbout: C:FBI_Crime->GeoId
+observationDate: C:FBI_Crime->Year
+value: C:FBI_Crime->{stat_var}
+"""
 
 # From 2013-2016, the FBI reported statistics for two different definitions of rape before fully transitioning to the current definition in 2017.
 # We add a dummy column after it (so allyears have two Rape columns).
 YEARS_WITH_TWO_RAPE_COLUMNS = {'2013', '2014', '2015', '2016'}
-YEARS_WITHOUT_POPULATION_COLUMN = {'2016'}
 
 YEAR_TO_URL = {
     '2019':
         'https://ucr.fbi.gov/crime-in-the-u.s/2019/crime-in-the-u.s.-2019/tables/table-8/table-8.xls',
-    # TODO(hanlu): un-comment these years after run code agaisnt them and check they are all fine.
-    # '2018': 'https://ucr.fbi.gov/crime-in-the-u.s/2018/crime-in-the-u.s.-2018/tables/table-8/table-8.xls',
-    # '2017': 'https://ucr.fbi.gov/crime-in-the-u.s/2017/crime-in-the-u.s.-2017/tables/table-8/table-8.xls',
-    # '2016': 'https://ucr.fbi.gov/crime-in-the-u.s/2016/crime-in-the-u.s.-2016/tables/table-8/table-8.xls',
-    # '2015': 'https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2015.xls',
-    # '2014': 'https://ucr.fbi.gov/crime-in-the-u.s/2014/crime-in-the-u.s.-2014/tables/table-8/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2014.xls',
-    # '2013': 'https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2013.xls',
-    # '2012': 'https://ucr.fbi.gov/crime-in-the-u.s/2012/crime-in-the-u.s.-2012/tables/8tabledatadecpdf/table_8_offenses_known_to_law_enforcement_by_state_by_city_2012.xls',
-    # '2011': 'https://ucr.fbi.gov/crime-in-the-u.s/2011/crime-in-the-u.s.-2011/tables/table_8_offenses_known_to_law_enforcement_by_state_by_city_2011.xls',
-    # '2010': 'https://ucr.fbi.gov/crime-in-the-u.s/2010/crime-in-the-u.s.-2010/tables/10tbl08.xls',
-    # '2009': 'https://www2.fbi.gov/ucr/cius2009/data/documents/09tbl08.xls',
-    # '2008': 'https://www2.fbi.gov/ucr/cius2008/data/documents/08tbl08.xls',
+    '2018':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2018/crime-in-the-u.s.-2018/tables/table-8/table-8.xls',
+    '2017':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2017/crime-in-the-u.s.-2017/tables/table-8/table-8.xls',
+    '2016':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2016/crime-in-the-u.s.-2016/tables/table-6/table-6.xls',
+    '2015':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2015.xls',
+    '2014':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2014/crime-in-the-u.s.-2014/tables/table-8/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2014.xls',
+    '2013':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/table-8/table_8_offenses_known_to_law_enforcement_by_state_by_city_2013.xls',
+    '2012':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2012/crime-in-the-u.s.-2012/tables/8tabledatadecpdf/table_8_offenses_known_to_law_enforcement_by_state_by_city_2012.xls',
+    '2011':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2011/crime-in-the-u.s.-2011/tables/table_8_offenses_known_to_law_enforcement_by_state_by_city_2011.xls',
+    # Sanity check 2008-2010 don't have duplicate city state.
+    '2010':
+        'https://ucr.fbi.gov/crime-in-the-u.s/2010/crime-in-the-u.s.-2010/tables/10tbl08.xls',
+    '2009':
+        'https://www2.fbi.gov/ucr/cius2009/data/documents/09tbl08.xls',
+    '2008':
+        'https://www2.fbi.gov/ucr/cius2008/data/documents/08tbl08.xls',
 }
 
 
@@ -111,15 +146,11 @@ def _int_from_field(f):
         return 0
 
 
-# When there is two entries for the same city, skip the one with wrong population data.
-def _should_skip_special_line(year, field):
-    if year == '2019' and field[_STATE_INDEX] == 'OREGON' and field[
-            _CITY_INDEX] == 'Ashland' and field[_POPULATION_INDEX] == '2670.0':
-        return True
-    if year == '2019' and field[_STATE_INDEX] == 'OREGON' and field[
-            _CITY_INDEX] == 'Lebanon' and field[_POPULATION_INDEX] == '25959.0':
-        return True
-    return False
+# Trim 2016 state name.
+def _get_2016_state(state):
+    state = state.replace(' - Metropolitan Counties', '')
+    state = state.replace(' - Nonmetropolitan Counties', '')
+    return state
 
 
 def calculate_crimes(r):
@@ -171,12 +202,12 @@ def calculate_crimes(r):
                                                         property_computed))
 
     # Category 3: Arson
-    arson = int_from_field(r['PropertyArson'])
+    arson = _int_from_field(r['PropertyArson'])
     r['PropertyArson'] = arson
 
     total = violent_computed + property_computed + arson
     # Write back the totals
-    r['Total'] = total
+    r[TOTAL] = total
     r['Violent'] = violent_computed
     r['Property'] = property_computed
 
@@ -205,7 +236,13 @@ def clean_crime_file(f_input, f_output, year):
         if line.startswith('#'):
             count_comments += 1
             continue
-        field = line.split(',')
+        # Split by comma and exclude comma from quotes in split
+        # For case like PENNSYLVANIA,"Abington Township, Montgomery County",55476.0,53.0,0.0,6.0,0,15.0,32.0,934.0,32.0,883.0,19.0,2.0
+        field = [
+            '"{}"'.format(x)
+            for x in list(csv.reader([line], delimiter=',', quotechar='"'))[0]
+        ]
+
         # Skip incomplete lines
         if len(field) < _FIELDS_IN_CRIME_FILE:
             count_incomplete_lines += 1
@@ -228,15 +265,13 @@ def clean_crime_file(f_input, f_output, year):
         if field[_STATE_INDEX]:
             # Remove numeric values from state names (comes from footnotes)
             state = _remove_digits(field[_STATE_INDEX])
+            if year == "2016":
+                state = _get_2016_state(state)
             count_state += 1
         field[_STATE_INDEX] = state
         # Remove any numeric characters from city names.
         field[_CITY_INDEX] = _remove_digits(field[_CITY_INDEX])
         count_city += 1
-
-        # If duplicate lines.
-        if _should_skip_special_line(year, field):
-            continue
 
         # Keep the first n fields. Some of the files contain extra empty fields.
         output_line = '{},{}\n'.format(year,
@@ -250,21 +285,46 @@ def clean_crime_file(f_input, f_output, year):
     logging.info('%d states', count_state)
 
 
-def update_and_calculate_crime_csv(geo_codes, crime_csv, calculated_csv):
+def update_and_calculate_crime_csv(geo_codes, crime_csv, writer):
     with open(crime_csv) as crime_f:
         crimes = csv.DictReader(crime_f, fieldnames=_CRIME_FIELDS)
-        with open(calculated_csv, 'w') as csv_output_f:
-            writer = csv.DictWriter(csv_output_f,
-                                    fieldnames=_CALCULATED_CRIME_FIELDS)
 
-            found_set = set()
-            cities_not_found_set = set()
-            for crime in crimes:
-                if geocode_cities.update_crime_geocode(crime, geo_codes,
-                                                       found_set,
-                                                       cities_not_found_set):
-                    calculate_crimes(crime)
-                    writer.writerow(crime)
+        found_set = set()
+        cities_not_found_set = set()
+        for crime in crimes:
+            if geocode_cities.update_crime_geocode(crime, geo_codes, found_set,
+                                                   cities_not_found_set):
+                calculate_crimes(crime)
+
+                processed_dict = {
+                    'Year':
+                        crime['Year'],
+                    'GeoId':
+                        "dcid:geoId/{}".format(crime[GEO_CODE]),
+                    'Count_CriminalActivities_ViolentCrime':
+                        crime['Violent'],
+                    'Count_CriminalActivities_MurderAndNonNegligentManslaughter':
+                        crime['ViolentMurderAndNonNegligentManslaughter'],
+                    'Count_CriminalActivities_ForcibleRape':
+                        crime['ViolentRape'],
+                    'Count_CriminalActivities_Robbery':
+                        crime['ViolentRobbery'],
+                    'Count_CriminalActivities_AggravatedAssault':
+                        crime['ViolentAggravatedAssault'],
+                    'Count_CriminalActivities_PropertyCrime':
+                        crime['Property'],
+                    'Count_CriminalActivities_Burglary':
+                        crime['PropertyBurglary'],
+                    'Count_CriminalActivities_LarcenyTheft':
+                        crime['PropertyLarcenyTheft'],
+                    'Count_CriminalActivities_MotorVehicleTheft':
+                        crime['PropertyMotorVehicleTheft'],
+                    'Count_CriminalActivities_Arson':
+                        crime['PropertyArson'],
+                    'Count_CriminalActivities_CombinedCrime':
+                        crime[TOTAL],
+                }
+                writer.writerow(processed_dict)
 
         # Output the cities not_found
         with open('city_not_found.txt', 'w') as cities_not_found_f:
@@ -275,27 +335,46 @@ def update_and_calculate_crime_csv(geo_codes, crime_csv, calculated_csv):
             len(found_set), len(cities_not_found_set)))
 
 
+def create_tmcf_file(tmcf_file_path):
+    stat_vars = OUTPUT_COLUMNS[2:]
+    with open(tmcf_file_path, 'w', newline='') as f_out:
+        for i in range(len(stat_vars)):
+            f_out.write(
+                TEMPLATE_MCF_TEMPLATE.format_map({
+                    'index': i,
+                    'stat_var': stat_vars[i]
+                }))
+
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    for year, url in YEAR_TO_URL.items():
-        response = requests.get(url)
-        xls_file = year + '.xls'
-        csv_file = year + '.csv'
-        cleaned_csv_file = year + '_cleaned.csv'
-        with open(xls_file, 'wb') as file:
-            file.write(response.content)
-        read_file = pd.read_excel(xls_file, skiprows=[0, 1, 2])
-        if year in YEARS_WITHOUT_POPULATION_COLUMN:
-            read_file.insert(_POPULATION_INDEX, 'Population', 1)
-        if year not in YEARS_WITH_TWO_RAPE_COLUMNS:
-            read_file.insert(_DUMMY_RAPE_INDEX, 'Dummy', 0)
-        read_file.to_csv(csv_file, index=None, header=True)
-        with open(csv_file, "r") as f_input:
-            with open(cleaned_csv_file, "w") as f_output:
-                logging.info('clean crime file for year %s', year)
-                clean_crime_file(f_input, f_output, year)
-
     geo_codes = geocode_cities.read_geocodes()
-    # TODO(hanlu): update code logic to iterate over year
-    update_and_calculate_crime_csv(geo_codes, '2019_cleaned.csv',
-                                   '2019_calculated.csv')
+
+    with open('calculated_crime.csv', 'w') as csv_output_f:
+        writer = csv.DictWriter(csv_output_f, fieldnames=OUTPUT_COLUMNS)
+        writer.writeheader()
+
+        for year, url in YEAR_TO_URL.items():
+            response = requests.get(url)
+            xls_file = year + '.xls'
+            csv_file = year + '.csv'
+            cleaned_csv_file = year + '_cleaned.csv'
+            with open(xls_file, 'wb') as file:
+                file.write(response.content)
+            read_file = pd.read_excel(xls_file, skiprows=[0, 1, 2])
+            if year not in YEARS_WITH_TWO_RAPE_COLUMNS:
+                read_file.insert(_DUMMY_RAPE_INDEX, 'Dummy', 0)
+            read_file.to_csv(csv_file, index=None, header=True)
+            with open(csv_file, "r") as f_input:
+                with open(cleaned_csv_file, "w") as f_output:
+                    logging.info('clean crime file for year %s', year)
+                    clean_crime_file(f_input, f_output, year)
+
+            update_and_calculate_crime_csv(geo_codes, cleaned_csv_file, writer)
+
+            # Clean intermediate files.
+            os.remove(xls_file)
+            os.remove(csv_file)
+            os.remove(cleaned_csv_file)
+
+    create_tmcf_file("FBI_crime.tmcf")
