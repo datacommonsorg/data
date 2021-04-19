cd6953e82496b10db533c54cda0cb169d9c37d5e
Senthamizhan <senthamizhan1998@gmail.com>
2021-04-19 14:58:55 +0530

    India NHM Data: Implemented requested changes

--- scripts/india_nhm/maternal_health/preprocess.py
+++ scripts/india_nhm/maternal_health/preprocess.py
@@ -1,4 +1,16 @@
-# -*- coding: utf-8 -*-
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
 sys.path.append("..")
@@ -30,9 +42,9 @@ if __name__ == '__main__':
     
     # Preprocess files; Generate CSV; Generate TMCF file
     loader = NHMDataLoaderBase(data_folder='../data/', dataset_name=dataset_name, 
-                              cols_dict=cols_to_nodes)   
+                              cols_dict=cols_to_nodes, final_csv_path=f"{dataset_name}.csv")   
     loader.generate_csv()
-    loader.create_tmcf()
+    loader.create_mcf_tmcf()
     
     # Write README file
     readme_gen = ReadMeGen(dataset_name=dataset_name, dataset_description="Maternal Health Data",
