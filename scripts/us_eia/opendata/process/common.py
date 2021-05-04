2e534b2694e741a45d9529fb55860be03c9ed316
Prashanth Radhakrishnan <shanth@google.com>
2021-05-04 01:56:15 +0000

    Ensure date is number.

--- scripts/us_eia/opendata/process/common.py
+++ scripts/us_eia/opendata/process/common.py
@@ -38,6 +38,11 @@ _QUARTER_MAP = {
 def _parse_date(d):
     """Given a date from EIA JSON convert to DC compatible date."""
 
+    try:
+      _ = int(d)
+    except ValueError:
+      return None
+
     if len(d) == 4:
         # Yearly
         return d
