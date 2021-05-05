7c51d06952a1268fdad167c97fbdd13671977773
Prashanth Radhakrishnan <shanth@google.com>
2021-05-05 21:12:31 +0000

    Generate all

--- scripts/us_eia/opendata/process/generate.sh
+++ scripts/us_eia/opendata/process/generate.sh
@@ -12,6 +12,6 @@ if [[ "$1" == "all" ]]; then
     python3 main.py --dataset="$d" --data_dir=tmp_raw_data/"$d"
   done
 else
-  python3 ../download_bulk.py --datasets=$1 --data_dir=tmp_raw_data/
+  python3 ../download_bulk.py --datasets=$1 --data_dir=tmp_raw_data
   python3 main.py --dataset="$1" --data_dir=tmp_raw_data/"$1"
 fi
