21d8903f0de6c35e0091b0a4f08d9364a2162d48
Prashanth Radhakrishnan <shanth@google.com>
2021-05-11 21:49:25 +0000

    Update script to include COAL

--- scripts/us_eia/opendata/process/common_test.py
+++ scripts/us_eia/opendata/process/common_test.py
@@ -99,7 +99,7 @@ class TestProcess(unittest.TestCase):
             self.assertEqual(exp_tmcf_data, act_tmcf_data)
 
 
-    def test_cleanup(self):
+    def test_cleanup_name(self):
       self.assertEqual('Natural Gas Gross Withdrawals, Monthly',
                        common.cleanup_name(
                            ' Natural Gas Gross Withdrawals, Monthly'))
