a3658528edec669a826aea6938f52b91ff4e4523
Carolyn Au <cyin@google.com>
2021-05-12 16:28:17 -0700

    small mcf bugfix

--- scripts/us_eia/opendata/process/nuclear.py
+++ scripts/us_eia/opendata/process/nuclear.py
@@ -27,7 +27,7 @@ def extract_place_statvar(series_id, counters):
 
     Returns a (place, raw-stat-var, is_us_place) tuple.
     """
-    m = re.match(r"^NUC_STATUS\.([^.]+)\.([^.]+)\.([AQMD])$", series_id)
+    m = re.match(r"^NUC_STATUS\.([^.]+)\.([^.]+)\.(D)$", series_id)
     if m:
         measure = m.group(1)
         place = m.group(2)
@@ -46,7 +46,7 @@ def extract_place_statvar(series_id, counters):
 
 _SV_MAP = {
     'CAP': [
-        'Capacity_Nuclear_ForEnergyGeneration',
+        'Daily_Capacity_Nuclear_ForEnergyGeneration',
         'typeOf: dcid:StatisticalVariable',
         'name: "Nuclear Power Plant generating capacity"',
         'description: \\""Nuclear generating capacity for a power plant. See https://www.eia.gov/nuclear/outages/.  For more information visit, Nuclear Regulatory Commission\'s Power Reactor Status Report\\""',
@@ -54,9 +54,11 @@ _SV_MAP = {
         'energySource: dcs:Nuclear',
         'measuredProperty: dcs:capacity',
         'statType: dcs:measuredValue',
+        # TODO(shanth): use new property in next iteration
+        'measurementQualifier: dcs:Daily',
     ],
     'OUT': [
-        'CapacityOutage_Nuclear_ForEnergyGeneration',
+        'Daily_CapacityOutage_Nuclear_ForEnergyGeneration',
         'typeOf: dcid:StatisticalVariable',
         'name: "Nuclear Power Plant generating capacity outage."',
         'description: \\""Nuclear generating capacity outage at a power plant. See https://www.eia.gov/nuclear/outages/.  For more information visit, Nuclear Regulatory Commission\'s Power Reactor Status Report\\""',
@@ -64,16 +66,20 @@ _SV_MAP = {
         'energySource: dcs:Nuclear',
         'measuredProperty: dcs:outage',
         'statType: dcs:measuredValue',
+        # TODO(shanth): use new property in next iteration
+        'measurementQualifier: dcs:Daily',
     ],
     'OUT_PCT': [
-        'CapacityOutage_Nuclear_ForEnergyGeneration_AsAFractionOf_Capacity',
+        'Daily_CapacityOutage_Nuclear_ForEnergyGeneration_AsAFractionOf_Capacity',
         'typeOf: dcid:StatisticalVariable',
         'description: \\""Nuclear generating capacity percent outage at a power plant. See https://www.eia.gov/nuclear/outages/.  For more information visit, Nuclear Regulatory Commission\'s Power Reactor Status Report\\""',
         'populationType: dcs:EnergyGeneration',
         'energySource: dcs:Nuclear',
         'measuredProperty: dcs:outage',
         'statType: dcs:measuredValue',
-        'measurementDenominator: dcs:Capacity_Nuclear_EnergyGeneration',
+        'measurementDenominator: dcs:Daily_Capacity_Nuclear_ForEnergyGeneration',
+        # TODO(shanth): use new property in next iteration
+        'measurementQualifier: dcs:Daily',
     ],
 }
 
@@ -98,7 +104,7 @@ def generate_statvar_schema(raw_sv, rows, sv_map, counters):
     counters['generate_statvar_schema'] += 1
 
     # NUC_STATUS.{Measure}.{Period}
-    m = re.match(r"^NUC_STATUS\.([^.]+)\.([AQMD])$", raw_sv)
+    m = re.match(r"^NUC_STATUS\.([^.]+)\.(D)$", raw_sv)
     if m:
         measure = m.group(1)
         period = m.group(2)
@@ -113,18 +119,13 @@ def generate_statvar_schema(raw_sv, rows, sv_map, counters):
         counters[f'error_missing_measure-{measure}'] += 1
         return None
 
-    sv_id_parts = [common.PERIOD_MAP[period], measure_pvs[0]]
-    sv_pvs = measure_pvs[1:] + [
-        # TODO(shanth): use new property in next iteration
-        f'measurementQualifier: dcs:{common.PERIOD_MAP[period]}',
-    ]
+    sv_id = measure_pvs[0]
+    sv_pvs = measure_pvs[1:]
 
     if measure not in _UNIT_MAP:
         counters[f'error_missing_unit-{measure}'] += 1
         return None
     (unit, sfactor) = _UNIT_MAP[measure]
-
-    sv_id = '_'.join(sv_id_parts)
 
     # Update the rows with new StatVar ID value and additional properties.
     for row in rows:
