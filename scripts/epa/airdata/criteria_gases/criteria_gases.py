fd73d3b19cb611ac786f60e280f98cd011a3901c
Natalie Diaz <nhdiaz@google.com>
2021-06-20 16:31:25 -0700

    rewrite criteria gases

---
+++ scripts/epa/airdata/criteria_gases/criteria_gases.py
@@ -0,0 +1,175 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''
+Generates cleaned CSV and template MCF files for the EPA AirData Criteria
+Gases.
+
+Usage: python3 criteria_gases.py
+'''
+import csv, os
+from zipfile import ZipFile
+from io import TextIOWrapper
+
+SOURCE_DATA = "source_data"
+
+CSV_COLUMNS = [
+    'Date', 'Site_Number', 'Site_Name', 'Site_Location', 'County', 
+    'Mean_Name', 'Mean_Value', 'Max_Name', 'Max_Value', 'AQI_Name', 'AQI_Value',
+    'Units', 'Method'
+]
+
+# Template MCF for StatVarObservation
+TEMPLATE_MCF = '''
+Node: E:EPA_CriteriaGases->E1
+typeOf: dcs:StatVarObservation
+variableMeasured: C:EPA_CriteriaGases->Mean_Name
+measurementMethod: C:EPA_CriteriaGases->Method
+observationDate: C:EPA_CriteriaGases->Date
+observationAbout: E:EPA_CriteriaGases->E0
+observationPeriod: dcs:"P1D"
+value: C:EPA_CriteriaGases->Mean_Value
+unit: C:EPA_CriteriaGases->Units
+
+Node: E:EPA_CriteriaGases->E2
+typeOf: dcs:StatVarObservation
+variableMeasured: C:EPA_CriteriaGases->Max_Name
+measurementMethod: C:EPA_CriteriaGases->Method
+observationDate: C:EPA_CriteriaGases->Date
+observationAbout: E:EPA_CriteriaGases->E0
+observationPeriod: dcs:"P1D"
+value: C:EPA_CriteriaGases->Max_Value
+unit: C:EPA_CriteriaGases->Units
+
+Node: E:EPA_CriteriaGases->E3
+typeOf: dcs:StatVarObservation
+variableMeasured: C:EPA_CriteriaGases->AQI_Name
+observationDate: C:EPA_CriteriaGases->Date
+observationAbout: E:EPA_CriteriaGases->E0
+observationPeriod: dcs:"P1D"
+value: C:EPA_CriteriaGases->AQI_Value
+'''
+
+# Template MCF for Air Quality Site
+TEMPLATE_MCF_AIR_QUALITY_SITE = '''
+Node: E:EPA_CriteriaGases->E0
+typeOf: dcs:AirQualitySite
+dcid: C:EPA_CriteriaGases->Site_Number
+name: C:EPA_CriteriaGases->Site_Name
+location: C:EPA_CriteriaGases->Site_Location
+containedInPlace: C:EPA_CriteriaGases->County
+'''
+
+# Convert to CamelCase (splitting on spaces)
+def get_camel_case(camel_case, s):
+    if s in camel_case:
+        return camel_case[s]
+    parts = s.lower().split()
+    result = ''
+    for i in range(len(parts)):
+        result += parts[i][0].upper()
+        if len(parts[i]) > 0: 
+            result += parts[i][1:]
+    camel_case[s] = result
+    return result
+
+
+# Hardcoded for consistency in StatisticalVariable names
+def get_suffix(observation):
+    parameter = observation['Parameter Name']
+    standard = observation['Pollutant Standard']
+    if parameter == 'Ozone':
+        if standard == 'Ozone 8-hour 2015':
+            return 'Ozone_8hour_2015'
+        print(standard)
+        return 'Ozone'
+    elif parameter == 'Sulfur dioxide':
+        if standard == 'SO2 1-hour 2010':
+            return 'SO2_1hour_2010'
+        elif standard == 'SO2 3-hour 1971':
+            return 'SO2_3hour_1971'
+        print(standard)
+        return 'SO2'
+    elif parameter == 'Carbon monoxide':
+        if standard == 'CO 1-hour 1971':
+            return 'CO_1hour_1971'
+        elif standard == 'CO 8-hour 1971':
+            return 'CO_8hour_1971'
+        print(standard)
+        return 'CO'
+    elif parameter == 'Nitrogen dioxide (NO2)':
+        if standard == 'NO2 1-hour':
+            return 'NO2_1hour'
+        print(standard)
+        return 'NO2'
+    print(standard)
+    return ''
+
+
+def create_csv(csv_file_path):
+    with open(csv_file_path, 'w', newline='') as f_out:
+        writer = csv.DictWriter(f_out,
+                                fieldnames=CSV_COLUMNS,
+                                lineterminator='\n')
+        writer.writeheader()
+
+
+def write_csv(csv_file_path, reader, camel_case):
+    with open(csv_file_path, 'a', newline='') as f_out:
+        writer = csv.DictWriter(f_out,
+                                fieldnames=CSV_COLUMNS,
+                                lineterminator='\n')
+        for observation in reader: 
+            suffix = get_suffix(observation)
+            new_row = {
+                'Date': observation['Date Local'],
+                'Site_Number': 'epa/{state}{county}{site}'.format(
+                    state=observation['State Code'], county=observation['County Code'], site=observation['Site Num']),
+                'Site_Name': observation['Local Site Name'],
+                'Site_Location': '[latLong {lat} {long}]'.format(
+                    lat=observation['Latitude'], long=observation['Longitude']),
+                'County': 'dcid:geoId/' + observation['State Code'] + observation['County Code'],  # geoID for county
+                'Mean_Name': f'dcs:Mean_Concentration_AirPollutant_{suffix}',
+                'Mean_Value': observation['Arithmetic Mean'],
+                'Max_Name': f'dcs:Max_Concentration_AirPollutant_{suffix}',
+                'Max_Value': observation['1st Max Value'],
+                'AQI_Name': f'dcs:AirQualityIndex_AirPollutant_{suffix}',
+                'AQI_Value': observation['AQI'],
+                'Units': get_camel_case(camel_case, observation['Units of Measure']),
+                'Method': get_camel_case(camel_case, observation['Method Name'])
+            }
+            writer.writerow(new_row)
+
+
+def write_tmcf(tmcf_file_path):
+    with open(tmcf_file_path, 'w') as f_out:   
+        f_out.write(TEMPLATE_MCF_AIR_QUALITY_SITE)
+        f_out.write(TEMPLATE_MCF)
+
+
+if __name__ == '__main__':
+    camel_case = {
+        '': '',
+        ' - ': '',
+    }
+    create_csv('EPA_CriteriaGases.csv')
+    for (dirpath, dirnames, filenames) in os.walk(SOURCE_DATA):
+        for filename in filenames:
+            if filename.endswith('.zip'):
+                print(filename)
+                with ZipFile(dirpath + os.sep + filename) as zf:
+                    with zf.open(filename[:-4] + '.csv', 'r') as infile:
+                        reader = csv.DictReader(TextIOWrapper(infile, 'utf-8'))
+                        write_csv('EPA_CriteriaGases.csv', reader, camel_case)  
+    write_tmcf('EPA_CriteriaGases.tmcf')
+    
--- scripts/epa/airdata/criteria_gases/criteria_gases_test.py
+++ scripts/epa/airdata/criteria_gases/criteria_gases_test.py
@@ -12,29 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 '''
-Unit tests for generate_tmcf.py
+Unit tests for criteria_gases.py
 
-Usage: python3 generate_tmcf_test.py
+Usage: python3 criteria_gases_test.py
 '''
 import unittest, csv, os, tempfile
-from generate_tmcf import join, write_csv, write_tmcf
+from criteria_gases import create_csv, write_csv, write_tmcf
 
 module_dir_ = os.path.dirname(__file__)
 
-class TestGenerateTmcfTest(unittest.TestCase):
+class TestCriteriaGasesTest(unittest.TestCase):
 
     def test_write_csv(self):
         with tempfile.TemporaryDirectory() as tmp_dir: 
-            d = {}
-            with open(os.path.join(module_dir_, 'test_data/test_data.csv'), 'r') as f:
+            camel_case = {
+                '': '',
+                ' - ': '',
+            }
+            test_csv = os.path.join(tmp_dir, 'test_csv.csv')
+            create_csv(test_csv)
+            with open(os.path.join(module_dir_, 'test_data/test_import_data.csv'), 'r') as f:
                 reader = csv.DictReader(f)
-                for row in reader:
-                    join(d, row)
+                write_csv(test_csv, reader, camel_case)             
 
-                test_csv = os.path.join(tmp_dir, 'test_csv.csv')
-                write_csv(test_csv, d)             
-
-                expected_csv = os.path.join(module_dir_, 'test_data/expected_csv.csv')
+                expected_csv = os.path.join(module_dir_, 'test_data/test_import.csv')
                 with open(test_csv, 'r') as test: 
                     test_str: str = test.read()
                     with open(expected_csv, 'r') as expected:
@@ -48,7 +49,7 @@ class TestGenerateTmcfTest(unittest.TestCase):
             test_tmcf = os.path.join(tmp_dir, 'test_tmcf.tmcf')
             write_tmcf(test_tmcf)
             
-            expected_tmcf = os.path.join(module_dir_, 'EPA_AirQuality.tmcf')
+            expected_tmcf = os.path.join(module_dir_, 'EPA_CriteriaGases.tmcf')
             with open(test_tmcf, 'r') as test: 
                     test_str: str = test.read()
                     with open(expected_tmcf, 'r') as expected:
