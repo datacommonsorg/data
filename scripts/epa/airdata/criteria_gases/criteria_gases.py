8d06e6f0753d8381c78b7dbec89cad7613c0255d
Natalie Diaz <nhdiaz@google.com>
2021-06-25 11:28:43 -0700

    lint

--- scripts/epa/airdata/criteria_gases/criteria_gases.py
+++ scripts/epa/airdata/criteria_gases/criteria_gases.py
@@ -20,10 +20,10 @@ Usage: python3 criteria_gases.py <end_year>
 import csv, os, sys, requests, io, zipfile
 
 POLLUTANTS = [
-  '44201',
-  '42401',
-  '42101',
-  '42602',
+    '44201',
+    '42401',
+    '42101',
+    '42602',
 ]
 
 START_YEAR = 1980
@@ -187,9 +187,10 @@ if __name__ == '__main__':
     for pollutant in POLLUTANTS:
         for year in range(START_YEAR, int(end_year) + 1):
             filename = f'daily_{pollutant}_{year}'
-            response = requests.get(f'https://aqs.epa.gov/aqsweb/airdata/{filename}.zip')
+            response = requests.get(
+                f'https://aqs.epa.gov/aqsweb/airdata/{filename}.zip')
             with zipfile.ZipFile(io.BytesIO(response.content)) as zf:
-                  with zf.open(f'{filename}.csv' ,'r') as infile:
-                      reader = csv.DictReader(io.TextIOWrapper(infile, 'utf-8'))
-                      write_csv('EPA_CriteriaGases.csv', reader)
+                with zf.open(f'{filename}.csv', 'r') as infile:
+                    reader = csv.DictReader(io.TextIOWrapper(infile, 'utf-8'))
+                    write_csv('EPA_CriteriaGases.csv', reader)
     write_tmcf('EPA_CriteriaGases.tmcf')
