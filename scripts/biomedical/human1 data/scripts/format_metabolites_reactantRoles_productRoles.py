a60efc5c99dde424fcd8c5d6cc5500ee59c90e73
khoahoang1891999 <67357807+khoahoang1891999@users.noreply.github.com>
2021-06-28 15:38:44 -0400

    Modify scripts
    Modify format_metabolites_reactantRoles_productRoles.py
    4 output csv, modify dcid columns

--- scripts/biomedical/human1 data/scripts/format_metabolites_reactantRoles_productRoles.py
+++ scripts/biomedical/human1 data/scripts/format_metabolites_reactantRoles_productRoles.py
@@ -1,8 +1,8 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
 def main():
-    import sys
-    import os
-    import numpy as np
-    import pandas as pd
     
     metabolite_tsv, reactant_tsv, product_tsv = sys.argv[1],sys.argv[2],sys.argv[3]
 
@@ -23,14 +23,18 @@ def main():
     df_metabolites = df_metabolites[~pd.isna(df_metabolites["chemicalFormula"])]
     # modify id to humanGEMID format
     df_metabolites["id"] = df_metabolites["id"].str[2:]
+
     # Create new column for dcid
-    df_metabolites["dcid"] = df_metabolites["chembl"]
-    # use chemcialFormula as dcid if chemblID is not available
-    df_metabolites["dcid"] = df_metabolites["dcid"].fillna(df_metabolites["chemicalFormula"])
-    # use chemcialFormula as dcid if chemblID is not available
-    df_metabolites["dcid"] = df_metabolites["dcid"].fillna(df_metabolites["id"])
+    df_metabolites["metabolite_dcid"] = df_metabolites["chembl"]
+    # use metaNetX as dcid if chemblID is not available
+    df_metabolites["metabolite_dcid"] = df_metabolites["metabolite_dcid"].fillna(df_metabolites["metanetx.chemical"])
+    # use chemical name as dcid if metaNetX is not available
+    df_metabolites["metabolite_dcid"] = df_metabolites["metabolite_dcid"].fillna(df_metabolites["name"])
     # format dcid -> bio/
-    df_metabolites["dcid"] = "bio" + "/" + df_metabolites["dcid"].str.replace(":","")
+    df_metabolites["metabolite_dcid"] = "bio" + "/" + df_metabolites["metabolite_dcid"].str.replace(":","")
+    df_metabolites["metabolite_dcid"] =  df_metabolites["metabolite_dcid"].str.replace(",","_")
+
+
     # Format chemblID, remove ":"
     df_metabolites["chembl"] = df_metabolites["chembl"].str.replace(":","")
     #convert chebiID and chemicalName to string with quotation
@@ -44,14 +48,19 @@ def main():
     # modify compartment to Enum type
     df_metabolites["compartment"] = df_metabolites["compartment"].map(compartment_dict)
 
+    df_metabolicCellularCompartment = df_metabolites[["id", "compartment","metabolite_dcid"]].copy()
+    df_metabolicCellularCompartment["metabolic_compartment_dcid"] = "bio/" + df_metabolicCellularCompartment["id"]
+
     output_path = os.path.join(os.getcwd(), "metabolites.csv")
     df_metabolites.to_csv(output_path, index = None)
+    output_path = os.path.join(os.getcwd(), "metabolicCellularCompartment.csv")
+    df_metabolicCellularCompartment.to_csv(output_path, index = None)
 
     ### generate productRoles.csv
     df_productRoles = pd.read_csv(product_tsv, sep='\t') # add to Metabolite property
     df_productRoles["speciesID"] = df_productRoles["speciesID"].str[2:]
     df_productRoles["reactionID"] = df_productRoles["reactionID"].str[2:]
-    df_productRoles = df_productRoles.merge(df_metabolites, left_on = "speciesID", right_on="id")[["reactionID", "dcid"]]
+    df_productRoles = df_productRoles.merge(df_metabolicCellularCompartment, left_on = "speciesID", right_on="id")[["reactionID", "metabolic_compartment_dcid"]]
     df_productRoles["reactionID"] = "bio/" +  df_productRoles["reactionID"].astype("str")
 
     output_path = os.path.join(os.getcwd(), "productRoles.csv")
@@ -61,7 +70,7 @@ def main():
     df_reactantRoles = pd.read_csv(reactant_tsv, sep='\t')
     df_reactantRoles["speciesID"] = df_reactantRoles["speciesID"].str[2:]
     df_reactantRoles["reactionID"] = df_reactantRoles["reactionID"].str[2:]
-    df_reactantRoles = df_reactantRoles.merge(df_metabolites, left_on = "speciesID", right_on="id")[["reactionID", "dcid"]]
+    df_reactantRoles = df_reactantRoles.merge(df_metabolicCellularCompartment, left_on = "speciesID", right_on="id")[["reactionID", "metabolic_compartment_dcid"]]
     df_reactantRoles["reactionID"] = "bio/" +  df_reactantRoles["reactionID"].astype(str)
 
     output_path = os.path.join(os.getcwd(), "reactantRoles.csv")
@@ -69,5 +78,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
