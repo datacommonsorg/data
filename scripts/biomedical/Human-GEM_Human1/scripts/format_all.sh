ac6dd536b7af5de9efca6bed558c505eb85156db
khoahoang1891999 <khoang2@u.rochester.edu>
2021-07-12 23:47:27 -0400

    Update format_all.sh

--- scripts/biomedical/Human-GEM_Human1/scripts/format_all.sh
+++ scripts/biomedical/Human-GEM_Human1/scripts/format_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python format_metabolites.py metabolites.tsv reactantRoles.tsv productRoles.tsv
+python map_human1_hmdb.py metabolites.tsv virtual.tsv hmdb.csv
+python format_metabolites.py metabolites.tsv reactantRoles.tsv productRoles.tsv humanGEM_HMDB_map.csv
 python format_genes.py genes.tsv geneRoles.tsv
 python format_reactions.py reactions.tsv
 python format_groups.py groups.tsv groupMemberships.tsv
