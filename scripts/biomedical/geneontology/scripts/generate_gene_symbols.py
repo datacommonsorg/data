27d167a32cb91b6fdad56d6bf81b58c25fb4b525
Padma Gundapaneni <padmag@uw.edu>
2021-07-28 22:47:58 -0700

    lint

--- scripts/biomedical/geneontology/scripts/generate_gene_symbols.py
+++ scripts/biomedical/geneontology/scripts/generate_gene_symbols.py
@@ -8,11 +8,11 @@ import pandas as pd
 
 def main():
     """Main function to generate the gene symbols."""
-	file_path = sys.argv[1]
-	species = sys.argv[2]
-	output_file = sys.argv[3]
-	data = add_headers_remove_rows(file_path, species)
-	return_gene_symbols(data, output_file)
+    file_path = sys.argv[1]
+    species = sys.argv[2]
+    output_file = sys.argv[3]
+    data = add_headers_remove_rows(file_path, species)
+    return_gene_symbols(data, output_file)
 
 
 def add_headers_remove_rows(file_path, species):
@@ -23,23 +23,28 @@ def add_headers_remove_rows(file_path, species):
     Returns:
         a file with a correctly-formatted header
     """
-    data = pd.read_csv(file_path, sep="\t",
-                       names=["DB", "DBObjectID", "DBObjectSymbol", "Qualifier", "GOID", "DBReference", "EvidenceCode",
-                              "With/From", "Aspect", "DBObjectName", "DBObjectSynonym", "DBObjectType", "Taxon", "Date",
-                              "AssignedBy"])
+    data = pd.read_csv(file_path,
+                       sep="\t",
+                       names=[
+                           "DB", "DBObjectID", "DBObjectSymbol", "Qualifier",
+                           "GOID", "DBReference", "EvidenceCode", "With/From",
+                           "Aspect", "DBObjectName", "DBObjectSynonym",
+                           "DBObjectType", "Taxon", "Date", "AssignedBy"
+                       ])
     if species == "mouse":
         data = data[31:]
-    elif species == "chicken" or species == "human":
+    elif species in ("chicken", "human"):
         data = data[41:]
-    elif species == "chicken_isoform" or species == "human_isoform":
+    elif species in ("chicken_isoform", "human_isoform"):
         data = data[27:]
-    elif species == "fly" or species == "zebrafish":
+    elif species in ("fly", "zebrafish"):
         data = data[33:]
     elif species == "yeast":
         data = data[35:]
     elif species == "worm":
         data = data[127909:]
     return data
+
 
 def return_gene_symbols(data, output_file):
     """
@@ -49,11 +54,12 @@ def return_gene_symbols(data, output_file):
     Returns:
         a csv file with gene symbols
     """
-	print("Returning gene symbols...")
-	data = pd.DataFrame(data.DBObjectSymbol.unique().tolist())
-	data.columns = ['GeneSymbol']
-	data.to_csv(output_file)
-	print("Returned gene symbols!")
+    print("Returning gene symbols...")
+    data = pd.DataFrame(data.DBObjectSymbol.unique().tolist())
+    data.columns = ['GeneSymbol']
+    data.to_csv(output_file)
+    print("Returned gene symbols!")
+
 
 if __name__ == "__main__":
     main()
--- scripts/biomedical/geneontology/scripts/parse_goa_file.py
+++ scripts/biomedical/geneontology/scripts/parse_goa_file.py
@@ -4,75 +4,121 @@ with optionally-mapped UniProt items from a .gaf file.
 Run "python3 parse_goa_file.py.
 '''
 
+from collections import defaultdict
 import warnings
 import sys
 import pandas as pd
 import numpy as np
-from collections import defaultdict
 
 warnings.filterwarnings("ignore")
 
-"""Mapping of aspect values in file to aspect enum."""
+# Mapping of aspect values in file to aspect enum.
 ASPECT_MAP = {
     "F": "dcs:GeneOntologyTypeMolecularFunction",
     "C": "dcs:GeneOntologyTypeCellularComponent",
     "P": "dcs:GeneOntologyTypeBiologicalProcess"
 }
-
 """Mapping of evidence codes in file to evidence code enum."""
 EVIDENCE_MAP = {
-    "IEA": "dcs:GeneOntologyEvidenceInferredFromElectronicAnnotation",
-    "IDA": "dcs:GeneOntologyEvidenceInferredFromDirectAssay",
-    "TAS": "dcs:GeneOntologyEvidenceTraceableAuthorStatement",
-    "IPI": "dcs:GeneOntologyEvidenceInferredFromPhysicalInteraction",
-    "IEP": "dcs:GeneOntologyEvidenceInferredFromExpressionPattern",
-    "ISS": "dcs:GeneOntologyEvidenceInferredFromSequenceOrStructureSimilarity",
-    "NAS": "dcs:GeneOntologyEvidenceNontraceableAuthorStatement",
-    "IMP": "dcs:GeneOntologyEvidenceInferredFromMutantPhenotype",
-    "ISA": "dcs:GeneOntologyEvidenceInferredFromSequenceAlignment",
-    "HDA": "dcs:GeneOntologyEvidenceInferredFromHighThroughputDirectAssay",
-    "EXP": "dcs:GeneOntologyEvidenceInferredFromExperiment",
-    "ND": "dcs:GeneOntologyEvidenceNoBiologicalDataAvailable",
-    "HEP": "dcs:GeneOntologyEvidenceInferredFromHighThroughputExpressionPattern",
-    "IC": "dcs:GeneOntologyEvidenceInferredByCurator",
-    "RCA": "dcs:GeneOntologyEvidenceInferredByReviewedComputationalAnalysis",
-    "HMP": "dcs:GeneOntologyEvidenceInferredFromHighThroughputMutantPhenotype",
-    "IGI": "dcs:GeneOntologyEvidenceInferredFromGeneticInteraction",
-    "IKR": "dcs:GeneOntologyEvidenceInferredFromKeyResidues",
-    "IGC": "dcs:GeneOntologyEvidenceInferredFromGenomicContext",
-    "ISO": "dcs:GeneOntologyEvidenceInferredFromSequenceOrthology",
-    "ISM": "dcs:GeneOntologyEvidenceInferredFromSequenceModel",
-    "IBA": "dcs:GeneOntologyEvidenceInferredFromBiologicalAspectOfAncestor"
+    "IEA":
+        "dcs:GeneOntologyEvidenceInferredFromElectronicAnnotation",
+    "IDA":
+        "dcs:GeneOntologyEvidenceInferredFromDirectAssay",
+    "TAS":
+        "dcs:GeneOntologyEvidenceTraceableAuthorStatement",
+    "IPI":
+        "dcs:GeneOntologyEvidenceInferredFromPhysicalInteraction",
+    "IEP":
+        "dcs:GeneOntologyEvidenceInferredFromExpressionPattern",
+    "ISS":
+        "dcs:GeneOntologyEvidenceInferredFromSequenceOrStructureSimilarity",
+    "NAS":
+        "dcs:GeneOntologyEvidenceNontraceableAuthorStatement",
+    "IMP":
+        "dcs:GeneOntologyEvidenceInferredFromMutantPhenotype",
+    "ISA":
+        "dcs:GeneOntologyEvidenceInferredFromSequenceAlignment",
+    "HDA":
+        "dcs:GeneOntologyEvidenceInferredFromHighThroughputDirectAssay",
+    "EXP":
+        "dcs:GeneOntologyEvidenceInferredFromExperiment",
+    "ND":
+        "dcs:GeneOntologyEvidenceNoBiologicalDataAvailable",
+    "HEP":
+        "dcs:GeneOntologyEvidenceInferredFromHighThroughputExpressionPattern",
+    "IC":
+        "dcs:GeneOntologyEvidenceInferredByCurator",
+    "RCA":
+        "dcs:GeneOntologyEvidenceInferredByReviewedComputationalAnalysis",
+    "HMP":
+        "dcs:GeneOntologyEvidenceInferredFromHighThroughputMutantPhenotype",
+    "IGI":
+        "dcs:GeneOntologyEvidenceInferredFromGeneticInteraction",
+    "IKR":
+        "dcs:GeneOntologyEvidenceInferredFromKeyResidues",
+    "IGC":
+        "dcs:GeneOntologyEvidenceInferredFromGenomicContext",
+    "ISO":
+        "dcs:GeneOntologyEvidenceInferredFromSequenceOrthology",
+    "ISM":
+        "dcs:GeneOntologyEvidenceInferredFromSequenceModel",
+    "IBA":
+        "dcs:GeneOntologyEvidenceInferredFromBiologicalAspectOfAncestor"
 }
-
 """Mapping of qualifier values in file to qualifier enum."""
 QUALIFIER_MAP = {
-    "enables": "dcs:GeneOntologyQualifierEnables",
-    "NOT|enables": "dcs:GeneOntologyQualifierNotEnables",
-    "involved_in": "dcs:GeneOntologyQualifierInvolvedIn",
-    "NOT|involved_in": "dcs:GeneOntologyQualifierNotInvolvedIn",
-    "located_in": "dcs:GeneOntologyQualifierLocatedIn",
-    "NOT|located_in": "dcs:GeneOntologyQualifierNotLocatedIn",
-    "part_of": "dcs:GeneOntologyQualifierPartOf",
-    "NOT|part_of": "dcs:GeneOntologyQualifierNotPartOf",
-    "is_active_in": "dcs:GeneOntologyQualifierIsActiveIn",
-    "NOT|is_active_in": "dcs:GeneOntologyQualifierNotIsActiveIn",
-    "contributes_to": "dcs:GeneOntologyQualifierContributesTo",
-    "NOT|contributes_to": "dcs:GeneOntologyQualifierNotContributesTo",
-    "colocalizes_with": "dcs:GeneOntologyQualifierColocalizesWith",
-    "NOT|colocalizes_with": "dcs:GeneOntologyQualifierNotColocalizesWith",
-    "acts_upstream_of": "dcs:GeneOntologyQualifierActsUpstreamOf",
-    "NOT|acts_upstream_of": "dcs:GeneOntologyQualifierNotActsUpstreamOf",
-    "acts_upstream_of_or_within": "dcs:GeneOntologyQualifierActsUpstreamOfOrWithin",
-    "NOT|acts_upstream_of_or_within": "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithin",
-    "acts_upstream_of_positive_effect": "dcs:GeneOntologyQualifierActsUpstreamOfPositiveEffect",
-    "NOT|acts_upstream_of_positive_effect": "dcs:GeneOntologyQualifierNotActsUpstreamOfPositiveEffect",
-    "acts_upstream_of_or_within_positive_effect": "dcs:GeneOntologyQualifierActsUpstreamOfOrWithinPositiveEffect",
-    "NOT|acts_upstream_of_or_within_positive_effect": "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithinPositiveEffect",
-    "acts_upstream_of_negative_effect": "dcs:GeneOntologyQualifierActsUpstreamOfNegativeEffect",
-    "NOT|acts_upstream_of_negative_effect": "dcs:GeneOntologyQualifierNotActsUpstreamOfNegativeEffect",
-    "acts_upstream_of_or_within_negative_effect": "dcs:GeneOntologyQualifierActsUpstreamOfOrWithinNegativeEffect",
-    "NOT|acts_upstream_of_or_within_negative_effect": "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithinNegativeEffect"
+    "enables":
+        "dcs:GeneOntologyQualifierEnables",
+    "NOT|enables":
+        "dcs:GeneOntologyQualifierNotEnables",
+    "involved_in":
+        "dcs:GeneOntologyQualifierInvolvedIn",
+    "NOT|involved_in":
+        "dcs:GeneOntologyQualifierNotInvolvedIn",
+    "located_in":
+        "dcs:GeneOntologyQualifierLocatedIn",
+    "NOT|located_in":
+        "dcs:GeneOntologyQualifierNotLocatedIn",
+    "part_of":
+        "dcs:GeneOntologyQualifierPartOf",
+    "NOT|part_of":
+        "dcs:GeneOntologyQualifierNotPartOf",
+    "is_active_in":
+        "dcs:GeneOntologyQualifierIsActiveIn",
+    "NOT|is_active_in":
+        "dcs:GeneOntologyQualifierNotIsActiveIn",
+    "contributes_to":
+        "dcs:GeneOntologyQualifierContributesTo",
+    "NOT|contributes_to":
+        "dcs:GeneOntologyQualifierNotContributesTo",
+    "colocalizes_with":
+        "dcs:GeneOntologyQualifierColocalizesWith",
+    "NOT|colocalizes_with":
+        "dcs:GeneOntologyQualifierNotColocalizesWith",
+    "acts_upstream_of":
+        "dcs:GeneOntologyQualifierActsUpstreamOf",
+    "NOT|acts_upstream_of":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOf",
+    "acts_upstream_of_or_within":
+        "dcs:GeneOntologyQualifierActsUpstreamOfOrWithin",
+    "NOT|acts_upstream_of_or_within":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithin",
+    "acts_upstream_of_positive_effect":
+        "dcs:GeneOntologyQualifierActsUpstreamOfPositiveEffect",
+    "NOT|acts_upstream_of_positive_effect":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOfPositiveEffect",
+    "acts_upstream_of_or_within_positive_effect":
+        "dcs:GeneOntologyQualifierActsUpstreamOfOrWithinPositiveEffect",
+    "NOT|acts_upstream_of_or_within_positive_effect":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithinPositiveEffect",
+    "acts_upstream_of_negative_effect":
+        "dcs:GeneOntologyQualifierActsUpstreamOfNegativeEffect",
+    "NOT|acts_upstream_of_negative_effect":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOfNegativeEffect",
+    "acts_upstream_of_or_within_negative_effect":
+        "dcs:GeneOntologyQualifierActsUpstreamOfOrWithinNegativeEffect",
+    "NOT|acts_upstream_of_or_within_negative_effect":
+        "dcs:GeneOntologyQualifierNotActsUpstreamOfOrWithinNegativeEffect"
 }
 
 
@@ -87,12 +133,12 @@ def main():
         output_file = sys.argv[3]
         try:
             proteins = sys.argv[4]
-        except:
+        except IndexError:
             proteins = None
     cleaned_data = clean_data(file_path, species)
     db_ref = get_db_references(cleaned_data)
     with_from = get_with_from(cleaned_data)
-    combine_files(with_from, db_ref, cleaned_data, species, proteins, output_file)
+    combine_files(with_from, db_ref, cleaned_data, proteins, output_file)
 
 
 def clean_data(file, species):
@@ -108,15 +154,18 @@ def clean_data(file, species):
     file["Aspect"] = file["Aspect"].map(ASPECT_MAP)
     file["EvidenceCode"] = file["EvidenceCode"].map(EVIDENCE_MAP)
     file["Qualifier"] = file["Qualifier"].map(QUALIFIER_MAP)
-    file["DBObjectSymbolAssembly"] = file["DBObjectSymbol"].apply(map_gene_symbol, species = species)
+    file["DBObjectSymbolAssembly"] = file["DBObjectSymbol"].apply(
+        map_gene_symbol, species=species)
 
     if species != "sgd":
-        file["GeneAssembly1"] = file["DBObjectSymbolAssembly"].str.extract("(.*),")
+        file["GeneAssembly1"] = file["DBObjectSymbolAssembly"].str.extract(
+            "(.*),")
     else:
         file["GeneAssembly1"] = file["DBObjectSymbolAssembly"]
 
-    if (species != "sgd"):
-        file["GeneAssembly2"] = file["DBObjectSymbolAssembly"].str.extract(",(.*)")
+    if species != "sgd":
+        file["GeneAssembly2"] = file["DBObjectSymbolAssembly"].str.extract(
+            ",(.*)")
 
     file["Date"] = pd.to_datetime(file["Date"], format="%Y%m%d")
     file["Taxon"] = file["Taxon"].str.extract(":(.*)")
@@ -136,17 +185,21 @@ def add_headers_remove_rows(file, species):
     Returns:
         a file with a correctly-formatted header
     """
-    data = pd.read_csv(file, sep="\t",
-                       names=["DB", "DBObjectID", "DBObjectSymbol", "Qualifier", "GOID", "DBReference", "EvidenceCode",
-                              "With/From", "Aspect", "DBObjectName", "DBObjectSynonym", "DBObjectType", "Taxon", "Date",
-                              "AssignedBy"])
+    data = pd.read_csv(file,
+                       sep="\t",
+                       names=[
+                           "DB", "DBObjectID", "DBObjectSymbol", "Qualifier",
+                           "GOID", "DBReference", "EvidenceCode", "With/From",
+                           "Aspect", "DBObjectName", "DBObjectSynonym",
+                           "DBObjectType", "Taxon", "Date", "AssignedBy"
+                       ])
     if species == "mouse":
         data = data[31:]
-    elif species == "chicken" or species == "human":
+    elif species in ("chicken", "human"):
         data = data[41:]
-    elif species == "chicken_isoform" or species == "human_isoform":
+    elif species in ("chicken_isoform", "human_isoform"):
         data = data[27:]
-    elif species == "fly" or species == "zebrafish":
+    elif species in ("fly", "zebrafish"):
         data = data[33:]
     elif species == "yeast":
         data = data[35:]
@@ -164,7 +217,7 @@ def map_gene_symbol(symbol, species):
         the gene assemblies for the gene
     """
     symbol = str(symbol)
-    if species == "human" or species == "human_isoform":
+    if species in ("human", "human_isoform"):
         str1 = "bio/hg19_" + symbol
         str2 = "bio/hg38_" + symbol
     elif species == "mouse":
@@ -182,13 +235,14 @@ def map_gene_symbol(symbol, species):
     elif species == "zebrafish":
         str1 = "bio/danRer10_" + symbol
         str2 = "bio/danRer11_" + symbol
-    elif species == "chicken" or species == "chicken_isoform":
+    elif species in ("chicken", "chicken_isoform"):
         str1 = "bio/galGal5_" + symbol
         str2 = "bio/galGal6_" + symbol
     if str2 == "":
-        return str1
+        assembly = str1
     else:
-        return str1 + "," + str2
+        assembly = str1 + "," + str2
+    return assembly
 
 
 def get_db_references(file):
@@ -221,7 +275,13 @@ def get_db_references(file):
         split_db_ref[name] = split_db_ref[name].apply(convert_to_strings)
     split_db_ref = split_db_ref.replace(np.nan, "", regex=True)
     split_db_ref["index"] = (split_db_ref.reset_index()).index
-    split_db_ref.rename(columns = {"MGI":"MGI_DB", "PMID":"PMID_DB", "FB":"FB_DB", "ZFIN": "ZFIN_DB"}, inplace = True)
+    split_db_ref.rename(columns={
+        "MGI": "MGI_DB",
+        "PMID": "PMID_DB",
+        "FB": "FB_DB",
+        "ZFIN": "ZFIN_DB"
+    },
+                        inplace=True)
     return split_db_ref
 
 
@@ -281,7 +341,7 @@ def map_proteins(proteins):
     """
     file_path = proteins
     print("Mapping proteins...")
-    proteins = pd.read_csv(file_path, sep = "\t")
+    proteins = pd.read_csv(file_path, sep="\t")
     proteins = proteins[["GeneSymbol", "Entry name"]]
     proteins['Entry name'] = "bio/" + proteins["Entry name"]
     proteins = proteins.groupby(['GeneSymbol'])['Entry name'].apply(list)
@@ -291,11 +351,11 @@ def map_proteins(proteins):
     return proteins
 
 
-def combine_files(with_from, db, file, species, proteins, output_file):
+def combine_files(with_from, database, file, proteins, output_file):
     """
     Args:
         with_from: a dataframe with a column for each referenced gene/protein database with the
-        gene/protein reference number in the respective database's column 
+        gene/protein reference number in the respective database's column
         db: a dataframe with a column for each referenced database with the database ID in
         the respective database's column
         file: a tab-separated gene ontology annotation file
@@ -306,14 +366,20 @@ def combine_files(with_from, db, file, species, proteins, output_file):
         the final cleaned and combined gene ontology annotation file
     """
     file["index"] = (file.reset_index()).index
-    merged = pd.merge(with_from, db, on = "index")
-    merged = pd.merge(merged, file, on = "index")
-    merged = merged.drop(['index', 'DBObjectSynonym', 'DBObjectSymbolAssembly', 'GOIDNumber'], axis = 1)
+    merged = pd.merge(with_from, database, on="index")
+    merged = pd.merge(merged, file, on="index")
+    merged = merged.drop(
+        ['index', 'DBObjectSynonym', 'DBObjectSymbolAssembly', 'GOIDNumber'],
+        axis=1)
     if len(sys.argv) > 4:
         proteins = map_proteins(proteins)
-        merged = pd.merge(merged, proteins, left_on = "DBObjectSymbol", right_on = "GeneSymbol", how = "left")
-        merged.rename(columns = {'Entry name':'protein_dcid'}, inplace = True)
-    merged = merged.replace(np.nan, '', regex = True)
+        merged = pd.merge(merged,
+                          proteins,
+                          left_on="DBObjectSymbol",
+                          right_on="GeneSymbol",
+                          how="left")
+        merged.rename(columns={'Entry name': 'protein_dcid'}, inplace=True)
+    merged = merged.replace(np.nan, '', regex=True)
     merged.to_csv(output_file)
     print("Finished cleaning file!")
     print("First 10 lines of output file...")
