461361de0892452e536e2653bb928149110ddbf3
Samantha Piekos <spiekos@google.com>
2021-08-03 15:57:32 -0700

    Update parse_air_quality.py

--- scripts/us_cdc/environmental_health_toxicology/parse_air_quality.py
+++ scripts/us_cdc/environmental_health_toxicology/parse_air_quality.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 '''
-Author: Padma Gundapaneni @padma-g
-Date: 7/28/21
+Author: Samantha Piekos @spiekos and Padma Gundapaneni @padma-g
+Date: 8/3/21
 Description: This script cleans up a csv file on census tract or county level
 air quality (ozone or 2.5 PM) data downloaded from the CDC.
 URL: https://data.cdc.gov/browse?category=Environmental+Health+%26+Toxicology
@@ -25,85 +25,102 @@ python3 parse_air_quality.py input_file output_file
 import sys
 import pandas as pd
 
-# Mapping of column names in file to StatVar names.
-STATVARS = {
-    "DS_PM_pred": "Mean_Concentration_AirPollutant_PM2.5",
-    "DS_O3_pred": "Mean_Concentration_AirPollutant_Ozone",
-    "PM25_max_pred": "Max_Concentration_AirPollutant_PM2.5",
-    "PM25_med_pred": "Median_Concentration_AirPollutant_PM2.5",
-    "PM25_mean_pred": "Mean_Concentration_AirPollutant_PM2.5",
-    "PM25_pop_pred": "PopulationWeighted_Concentration_AirPollutant_PM2.5",
-    "O3_max_pred": "Max_Concentration_AirPollutant_Ozone",
-    "O3_med_pred": "Median_Concentration_AirPollutant_Ozone",
-    "O3_mean_pred": "Mean_Concentration_AirPollutant_Ozone",
-    "O3_pop_pred": "PopulationWeighted_Concentration_AirPollutant_Ozone"
-}
-
 # Mapping of month abbreviations to month numbers.
 MONTH_MAP = {
-    "JAN": 1,
-    "FEB": 2,
-    "MAR": 3,
-    "APR": 4,
-    "MAY": 5,
-    "JUN": 6,
-    "JUL": 7,
-    "AUG": 8,
-    "SEP": 9,
-    "OCT": 10,
-    "NOV": 11,
-    "DEC": 12
+    "JAN": "01",
+    "FEB": "02",
+    "MAR": "03",
+    "APR": "04",
+    "MAY": "05",
+    "JUN": "06",
+    "JUL": "07",
+    "AUG": "08",
+    "SEP": "09",
+    "OCT": "10",
+    "NOV": "11",
+    "DEC": "12"
 }
 
+def generate_census_tract_dcid(ctfips):
+    list_dcids = []
+    for index, ctfip in ctfips.items():
+        dcid = "geoId/" + str(ctfip).zfill(11)
+        list_dcids.append(dcid)
+    return(list_dcids) 
 
-def main():
-    """Main function to generate the cleaned csv file."""
-    file_path = sys.argv[1]
-    output_file = sys.argv[2]
-    clean_air_quality_data(file_path, output_file)
+def generate_county_dcid(data):
+    list_dcids = []
+    for index, row in data.iterrows():
+        list_dcids.append("geoId/" + str(row["statefips"]).zfill(2)\
+         + str(row["countyfips"]).zfill(3))
+    return(list_dcids)
 
+def generate_date(data):
+    list_dates = []
+    for index, date in data.items():
+        day = date[:2]
+        month = MONTH_MAP[date[2:5]]
+        year = date[-4:]
+        list_dates.append(year + "-" + month + "-" + day)
+    return(list_dates)
 
-def clean_air_quality_data(file_path, output_file):
+def generate_date_county_ozone(data):
+    list_dates = []
+    for index, row in data.iterrows():
+        date = str(row["Year"]) + "-" + MONTH_MAP[row["Month"]] \
+         + "-" + str(row["Day"]).zfill(2)
+        list_dates.append(date)
+    return(list_dates)
+
+def clean_census_tract_data(input_file):
+    data = pd.read_csv(input_file)
+    data["dcid"] = generate_census_tract_dcid(data["ctfips"])
+    print("dcids generated...")
+    data["date"] = generate_date(data["date"])
+    print("Dates formatted...")
+    data = data.drop(labels=["year", "ctfips", "latitude", "longitude"], axis=1)
+    return(data)
+
+def clean_county_data(input_file):
+    if "Ozone" in input_file:
+        data = pd.read_csv(input_file, sep=";")
+        data["dcid"] = generate_county_dcid(data)
+        print("dcids generated...")
+        data["date"] = generate_date_county_ozone(data)
+        print("Dates formatted...")
+        data = data.drop(labels=["Year", "Month", "Day"], axis=1)
+    elif "PM2.5" in input_file:
+        data = pd.read_csv(input_file)
+        print("dcids generated...")
+        data["dcid"] = generate_county_dcid(data)
+        print("Dates formatted...")
+        data["date"] = generate_date(data["date"])
+        data = data.drop(labels=["year"], axis=1)
+    return(data)
+
+def clean_air_quality_data(input_file, output_file):
     """
     Args:
-        file_path: path to a comma-separated CDC air quality data file
+        input_file: path to a comma-separated CDC air quality data file
         output_file: path for the cleaned csv to be stored
     Returns:
         a cleaned csv file
     """
     print("Cleaning file...")
-    data = pd.read_csv(file_path)
-    if "Ozone" in file_path and "County" in file_path:
-        data["Month"] = data["Month"].map(MONTH_MAP)
-        data["date"] = pd.to_datetime(data[["Year", "Month", "Day"]],
-                                      yearfirst=True)
-    else:
-        data["date"] = pd.to_datetime(data["date"], yearfirst=True)
-    if "PM2.5" in file_path:
-        census_tract = "DS_PM"
-    elif "Ozone" in file_path:
-        census_tract = "DS_O3"
-    if "Census" in file_path:
-        data = pd.melt(data,
-                       id_vars=[
-                           'year', 'date', 'statefips', 'countyfips', 'ctfips',
-                           'latitude', 'longitude', census_tract + '_stdd'
-                       ],
-                       value_vars=[str(census_tract + '_pred')],
-                       var_name='StatisticalVariable',
-                       value_name='Value')
-        data.rename(columns={census_tract + '_stdd': 'Error'}, inplace=True)
-        data["dcid"] = "geoId/" + data["ctfips"].astype(str)
-        data['StatisticalVariable'] = data['StatisticalVariable'].map(STATVARS)
-    elif "County" in file_path and "PM" in file_path:
-        data["countyfips"] = "1200" + data["countyfips"].astype(str)
-        data["dcid"] = "geoId/" + data["countyfips"].astype(str)
-    elif "County" in file_path and "Ozone" in file_path:
-        data["countyfips"] = "1200" + data["countyfips"].astype(str)
-        data["dcid"] = "geoId/" + data["countyfips"].astype(str)
+    if "Census" in input_file:
+        data = clean_census_tract_data(input_file)
+    elif "County" in input_file:
+        data = clean_county_data(input_file)
+    data = data.drop(labels=["statefips", "countyfips"], axis=1)
+    print("Writing to output file...")
     data.to_csv(output_file, index=False)
     print("Finished cleaning file!")
 
+def main():
+    """Main function to generate the cleaned csv file."""
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    clean_air_quality_data(input_file, output_file)
 
 if __name__ == "__main__":
     main()
