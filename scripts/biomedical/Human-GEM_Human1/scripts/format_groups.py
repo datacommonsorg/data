28293717e572c9843d7f6576a2607ad6fe5c147f
khoahoang1891999 <khoang2@u.rochester.edu>
2021-08-11 10:29:42 -0400

    Update format_groups.py
    Breaks down to smaller chunk of code

--- scripts/biomedical/Human-GEM_Human1/scripts/format_groups.py
+++ scripts/biomedical/Human-GEM_Human1/scripts/format_groups.py
@@ -6,18 +6,20 @@ import sys
 import os
 import pandas as pd
 
+# Define dictionary to map values in "kind" column, value is mapped to
+# a category of HierachyTypeEnum class
+HIERACHY_DICT = {"partonomy": "dcs:HierachyTypePartonomy"}
 
-def main():
-    # Define dictionary to map values in "kind" column, value is mapped to
-    # a category of HierachyTypeEnum class
-    hierachy_dict = {"partonomy": "dcs:HierachyTypePartonomy"}
-    # read in 2 data files as the second and third arguments
-    groups_tsv, group_memberships_tsv = sys.argv[1], sys.argv[2]
-
-    ### generate groups.csv
+def format_group_data(groups_tsv):
+    """Format groups.tsv file
+    Args:
+        groups.tsv: groups.tsv file path
+    Returns:
+        Write formatted csv file: groups.csv
+    """
     df_groups = pd.read_csv(groups_tsv, sep='\t')
     # map values in "kind" columns to hierachy_dict
-    df_groups["kind"] = df_groups["kind"].map(hierachy_dict)
+    df_groups["kind"] = df_groups["kind"].map(HIERACHY_DICT)
     # use "name" as dcid, format name:
     # empty space to underscore, lower string, remove comma
     df_groups["dcid"] = "bio/" + df_groups["name"].str.replace(" ", "_")
@@ -30,6 +32,17 @@ def main():
     # create formatted csv
     df_groups.to_csv(output_path, index=None)
 
+    return df_groups
+
+def format_group_membership_data(group_memberships_tsv, df_groups):
+    """Format groupMemberships.tsv file
+    Args:
+        group_memberships_tsv: groupMemberships.tsv file path
+        df_groups: formatted groups.csv dataframe
+                result of format_group_data()
+    Returns:
+        Write formatted csv file: groupMemberships.csv
+    """
     ### generate groupMemberships.csv
     df_group_memberships = pd.read_csv(group_memberships_tsv, sep='\t')
     # merge groupMemberships datafram with groups dataframe
@@ -43,6 +56,25 @@ def main():
     output_path = os.path.join(os.getcwd(), "groupMemberships.csv")
     # create formatted csv
     df_group_memberships.to_csv(output_path, index=None)
+
+def format_group_and_group_membership(groups_tsv, group_memberships_tsv):
+    """Format groups.tsv and groupMemberships.tsv file
+    Args:
+        groups_tsv: groups.tsv file path
+        group_memberships_tsv: groupMembersgips.tsv file path
+    Returns:
+        Write formatted csv file: groups.csv and groupMemberships.csv
+    """
+    # format groups.tsv
+    df_groups = format_group_data(groups_tsv)
+    # format groupMemberships.tsv
+    format_group_membership_data(group_memberships_tsv, df_groups)
+
+def main():
+
+    # read in 2 data files as the second and third arguments
+    groups_tsv, group_memberships_tsv = sys.argv[1], sys.argv[2]
+    format_group_and_group_membership(groups_tsv, group_memberships_tsv)
 
 
 if __name__ == '__main__':
