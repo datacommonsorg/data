596a275da331f7aab9ee30757f12a7131a951a7f
Thejesh GN <i@thejeshgn.com>
2021-08-25 14:36:53 +0530

    Intial pincode

--- scripts/india_post/pincodes/preprocess.py
+++ scripts/india_post/pincodes/preprocess.py
@@ -22,6 +22,7 @@ import pandas as pd
 import numpy as np
 import urllib.request
 
+from india.formatters import CodeFormatter
 
 class IndiaPostPincodesDataLoader:
 
@@ -31,31 +32,25 @@ class IndiaPostPincodesDataLoader:
         self.clean_df = None
         self.clean_csv = clean_csv
 
-    @staticmethod
-    def format_title(s):
-        # Converts to title case, except for the words like `of`, `and` etc
-        name_list = s.split(' ')
-        first_list = [name_list[0].capitalize()]
-        for name in name_list[1:]:
-            first_list.append(name if name in
-                              ["of", "and"] else name.capitalize())
-        return " ".join(first_list)
-
-    @staticmethod
-    def format_code(s):
-        # Converts into two character length code
-        # If the value is `0` then it makes it empty
-        # If the length is single character then it prepends it
-        # with `0` to make it two character length
-        s = s.zfill(2)
-        return "" if s == "00" else s
-
 
     def process(self):
         self.raw_df = pd.read_csv(self.pincode_csv, dtype=str)
-        self.raw_df.fillna('', inplace=True)
-        print(self.raw_df)
+        self.raw_df.fillna('', inplace=True)        
+        self.raw_df.drop_duplicates(subset=["Pincode","StateName","District"], inplace=True)
+        self.raw_df.drop(["CircleName","RegionName","DivisionName","OfficeName","OfficeType","Delivery","Latitude","Longitude"],axis=1,inplace=True,errors="ignore")
+        self.raw_df.sort_values(by=["Pincode","StateName","District"], inplace=True)
+        self.clean_df = self.raw_df
+        df = self.raw_df
+        print(df)
+        new_df = df.groupby("Pincode","StateName").cumcount()
+           # .set_index(["Pincode","StateName","col"])
+           # .unstack("col")
+           # .sort_index(level=(1,0), axis=1))
 
+
+
+
+        print(new_df)
 
     def save(self):
         self.clean_df.to_csv(self.clean_csv, index=False, header=True)
@@ -63,14 +58,12 @@ class IndiaPostPincodesDataLoader:
 
 def main():
     """Runs the program."""
-    lgd_csv = os.path.join(os.path.dirname(__file__),
-                           "./data/lgd_allStateofIndia_export.csv")
     wikidata_csv = os.path.join(
         os.path.dirname(__file__),
-        "./data/wikidata_india_states_and_ut_export.csv")
+        "./data/pincode.csv")
     clean_csv = os.path.join(os.path.dirname(__file__),
-                             "LocalGovernmentDirectory_States.csv")
-    loader = IndiaPostPincodesDataLoader(lgd_csv, wikidata_csv,
+                             "IndiaPost_Pincodes.csv")
+    loader = IndiaPostPincodesDataLoader(wikidata_csv,
                                                      clean_csv)
     loader.process()
     loader.save()
