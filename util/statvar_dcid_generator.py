c3cd4f25dfd483db3b4ccfd857b6e802cc018759
Anush Kini <anushk@google.com>
2021-09-02 15:34:13 +0530

    Changes according to review comments
    1. Used pylint and yapf to clean code
    2. Removed the class based methods
    3. #TODO -> # TODO
    4. Added comments to global constants and variables.
    5. Added checks to ensure keys are in the dict in get_stat_var_dcid
    6. Improved docstring

--- util/statvar_dcid_generator.py
+++ util/statvar_dcid_generator.py
@@ -11,157 +11,180 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""A general class to generate dcid for statistical variables."""
+"""A utility to generate dcid for statistical variables."""
 
 import copy
 import re
 
+#Constants
+_QUANTITY_REGEX = r"\[(?P<value>-|-?\d+(\.\d+)?) (?P<quantity>[A-Za-z]+)\]"
+_QUANTITY_RANGE_REGEX = (r"\[(?P<lower_limit>-|-?\d+(\.\d+)?) "
+                         r"(?P<upper_limit>-|-?\d+(\.\d+)?) "
+                         r"(?P<quantity>[A-Za-z]+)\]")
 
-class StatVarDcidGenerator:
-    """Helper class to generate dcid for a statistical variable."""
-    def __init__(self, stat_type_lookup=None, ignore_props=None):
-        if stat_type_lookup is None:
-            self.STAT_TYPE_LOOKUP = {
-                'meanValue': 'Mean',
-                'medianValue': 'Median'
-            }
-        else:
-            self.STAT_TYPE_LOOKUP = stat_type_lookup
+# This is a lookup to ensure "Mean" and "Median" are used in the dcid when
+# statType is meanValue or medianValue"
+_stat_type_lookup = {"meanValue": "Mean", "medianValue": "Median"}
 
-        if ignore_props is None:
-            self.IGNORE_PROPS = ['unit', 'Node', 'memberOf', 'typeOf']
-        else:
-            self.IGNORE_PROPS = ignore_props
+# These properties are ignored during dcid generation
+_ignore_props = {"unit", "Node", "memberOf", "typeOf"}
 
-        self.quantity_regex = '\[(-|\d+(\.\d+)?) [a-zA-Z]+\]'
-        self.quantity_range_regex = '\[(-|\d+(\.\d+)?) (-|\d+(\.\d+)?) [a-zA-Z]+\]'
 
-    def _capitalize_word(self, word):
-        """Changes the first letter of a word to upper case."""
-        if (len(word) != 0):
-            return (word[0].upper() + word[1:])
-        else:
-            return None
+def capitalize_word(word):
+    """Capitalizes the first character of a string.
 
-    def _generate_quantity_range_name(self, s):
-        """Generate a name for a quantity range.
+    This function differs from the builtin function str.capitalize() in that it
+    changes only the case of the first character and ignores the case of other
+    characters.
+    """
+    if word:
+        return word[0].upper() + word[1:]
+    return None
 
-        Expected syntax of s - [<lower_limit> <upper_limit> <quantity>]
-        """
-        str_list = s.split()
 
-        lower_limit = str_list[0][1:]
-        upper_limit = str_list[1]
-        quantity = str_list[2][:-1]
+def generate_quantity_range_name(match_dict):
+    """Generate a name for a quantity range.
 
-        quantity = self._capitalize_word(quantity)
-        if (upper_limit == '-'):
-            return f"{lower_limit}OrMore{quantity}"
-        elif (lower_limit == '-'):
-            return f"{upper_limit}OrLess{quantity}"
-        else:
-            return f"{lower_limit}To{upper_limit}{quantity}"
+    Args:
+        match_dict: A dictionary containing quantity range regex groups.
+          Expected syntax of match_dict is
+          {
+            "lower_limit": <value>,
+            "upper_limit": <value>,
+            "quantity": <value>
+          }
 
-    def _generate_quantity_name(self, s):
-        """Generate a name for a quantity.
+    Returns:
+        A string representing the quantity range name to be used in the dcid.
+    """
+    lower_limit = match_dict["lower_limit"]
+    upper_limit = match_dict["upper_limit"]
+    quantity = match_dict["quantity"]
 
-        Expected syntax of s- [<value> <quantity>]
-        """
-        str_list = s.split()
+    quantity = capitalize_word(quantity)
+    if upper_limit == "-":
+        return f"{lower_limit}OrMore{quantity}"
 
-        value = str_list[0][1:]
-        quantity = str_list[1][:-1]
+    if lower_limit == "-":
+        return f"{upper_limit}OrLess{quantity}"
 
-        quantity = self._capitalize_word(quantity)
-        return f"{value}{quantity}"
+    return f"{lower_limit}To{upper_limit}{quantity}"
 
-    def get_stat_var_dcid(self, stat_var_dict, ignore_props=None):
-        """Generates the dcid given a statistical variable.
-        
-        Arguments:
-        stat_var_dict -- A dictionary with property:values of the statistical variable as key-value pairs
-        ignore_props -- A list of properties to ignore from stat_var_dict when generating the dcid
 
-        The ignore_props can be used to account for dependent PVs which we want 
-        to ignore when generating the dcid.
-        For example in the following statVar,
-        {
+def generate_quantity_name(match_dict):
+    """Generate a name for a quantity.
+
+    Args:
+        match_dict: A dictionary containing quantity regex groups.
+          Expected syntax of match_dict
+          {
+            "value": <value>,
+            "quantity": <value>
+          }
+
+    Returns:
+        A string representing the quantity name to be used in the dcid.
+    """
+    value = match_dict["value"]
+    quantity = match_dict["quantity"]
+
+    quantity = capitalize_word(quantity)
+    return f"{value}{quantity}"
+
+
+def get_stat_var_dcid(stat_var_dict, ignore_props=None):
+    """Generates the dcid given a statistical variable.
+
+    Args:
+        stat_var_dict: A dictionary with property: values of the statistical
+          variable as key-value pairs.
+        ignore_props: A list of properties to ignore from stat_var_dict when
+          generating the dcid. The ignore_props can be used to account for
+          dependent PVs to ignore when generating the dcid. For example in the
+          following statVar,
+          {
             populationType: Person
             measuredProperty: count
             statType: measuredValue
             healthInsurance: NoHealthInsurance
             armedForceStatus: Civilian
             institutionalization: USC_NonInstitutionalized
-        }
-        Since the healthInsurance property indicates they are Civilian and
-        USC_NonInstitutionalized, ignore_props can be the list
-        ['armedForceStatus', 'institutionalization']. During the dcid 
-        generation process, these PVs will not be considered.
-        """
-        #TODO: Account for dependent PVs automatically
-        #TODO: Add support for naming boolean constraints
-        #TODO: Stripping measurement properties (USC_) from constraints
-        #TODO: Replacing NAICS industry codes with the NAICS names
-        #TODO: Renaming cause of death properties
-        #TODO: Renaming DEA drug names
-        #TODO: Prepend or append text to some constraints to improve readability
-        #TODO: InsuredUmemploymentRate should become Rate_Insured_Unemployment
+          }
+          since the healthInsurance property indicates they are Civilian and
+          USC_NonInstitutionalized, ignore_props can be the list
+          ["armedForceStatus", "institutionalization"]. During the dcid
+          generation process, these PVs will not be considered.
 
-        dcid_list = list()
-        has_denominator = False
-        svd = copy.deepcopy(stat_var_dict)
+    Returns:
+        A string represting the dcid of the statistical variable.
+    """
+    # TODO: Allow support for values with namespaces (dcs: , schema:, ...)
+    # TODO: Add support for naming boolean constraints
+    # TODO: Stripping measurement properties (USC_) from constraints
+    # TODO: Replacing NAICS industry codes with the NAICS names
+    # TODO: Renaming cause of death properties
+    # TODO: Renaming DEA drug names
+    # TODO: Prepend or append text to some constraints to improve readability
+    # TODO: InsuredUmemploymentRate should become Rate_Insured_Unemployment
 
-        if ignore_props is None:
-            ignore_props = self.IGNORE_PROPS
+    dcid_list = list()
+    denominator_suffix = ''
+    svd = copy.deepcopy(stat_var_dict)
 
-        for prop in ignore_props:
-            if prop in svd:
-                del svd[prop]
+    if ignore_props is None:
+        ignore_props = _ignore_props
 
-        if 'measurementDenominator' in svd:
-            has_denominator = True
-            denominator_suffix = 'AsAFractionOf_' + svd[
-                'measurementDenominator']
-            del svd['measurementDenominator']
+    for prop in ignore_props:
+        if prop in svd:
+            del svd[prop]
 
-        if 'measurementQualifier' in svd:
-            token = self._capitalize_word(svd['measurementQualifier'])
+    if "measurementDenominator" in svd:
+        denominator_suffix = "AsAFractionOf_" + svd["measurementDenominator"]
+        del svd["measurementDenominator"]
+
+    if "measurementQualifier" in svd:
+        token = capitalize_word(svd["measurementQualifier"])
+        dcid_list.append(token)
+        del svd["measurementQualifier"]
+
+    if ("statType" in svd) and (svd["statType"]
+                                not in ["measuredValue", "Unknown"]):
+        if svd["statType"] in _stat_type_lookup:
+            dcid_list.append(_stat_type_lookup[svd["statType"]])
+        else:
+            token = capitalize_word(svd["statType"])
             dcid_list.append(token)
-            del svd['measurementQualifier']
+    del svd["statType"]
 
-        if (svd['statType'] not in ['measuredValue', 'Unknown']):
-            if svd['statType'] in self.STAT_TYPE_LOOKUP:
-                dcid_list.append(self.STAT_TYPE_LOOKUP[svd['statType']])
-            else:
-                token = self._capitalize_word(svd['statType'])
-                dcid_list.append(token)
-
-        del svd['statType']
-
-        token = self._capitalize_word(svd['measuredProperty'])
+    if "measuredProperty" in svd:
+        token = capitalize_word(svd["measuredProperty"])
         dcid_list.append(token)
-        del svd['measuredProperty']
+        del svd["measuredProperty"]
 
-        token = self._capitalize_word(svd['populationType'])
+    if "populationType" in svd:
+        token = capitalize_word(svd["populationType"])
         dcid_list.append(token)
-        del svd['populationType']
+        del svd["populationType"]
 
-        constraint_props = sorted(svd.keys(), key=str.casefold)
-        for prop in constraint_props:
-            if re.match(self.quantity_range_regex, svd[prop]):
-                q_name = self._generate_quantity_range_name(svd[prop])
-                dcid_list.append(q_name)
+    constraint_props = sorted(svd.keys(), key=str.casefold)
+    for prop in constraint_props:
+        match = re.match(_QUANTITY_RANGE_REGEX, svd[prop])
+        if match:
+            q_name = generate_quantity_range_name(match.groupdict())
+            dcid_list.append(q_name)
 
-            elif re.match(self.quantity_regex, svd[prop]):
-                q_name = self._generate_quantity_name(svd[prop])
+        else:
+            match = re.match(_QUANTITY_REGEX, svd[prop])
+            if match:
+                q_name = generate_quantity_name(match.groupdict())
                 dcid_list.append(q_name)
 
             else:
-                token = self._capitalize_word(svd[prop])
+                token = capitalize_word(svd[prop])
                 dcid_list.append(token)
 
-        if (has_denominator):
-            dcid_list.append(denominator_suffix)
+    if denominator_suffix:
+        dcid_list.append(denominator_suffix)
 
-        dcid = '_'.join(dcid_list)
-        return dcid
+    dcid = "_".join(dcid_list)
+    return dcid
