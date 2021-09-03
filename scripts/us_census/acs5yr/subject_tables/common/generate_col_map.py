c4d7993f14174ab98962cdddaf7f46c82e5d2116
Sharad Shriram <sharadshriram@google.com>
2021-09-03 15:58:09 +0000

    revises spec.json, add more functions to generate_col_map.py based on reviews

--- scripts/us_census/acs5yr/subject_tables/common/generate_col_map.py
+++ scripts/us_census/acs5yr/subject_tables/common/generate_col_map.py
@@ -1,7 +1,9 @@
 """Module to generate a column map to their corresponding statistical variable defintions."""
-
 import os
+import sys
+import logging
 import json
+import re
 
 import pandas as pd
 
@@ -10,6 +12,36 @@ os.system(
     'curl https://raw.githubusercontent.com/Abilityguy/data/dcid_gen/util/statvar_dcid_generator.py -LO statvar_dcid_generator.py'
 )
 from statvar_dcid_generator import get_stat_var_dcid
+
+# intitalize the logger
+logging.basicConfig(
+    filename='column_map_generatetion_task.log',
+    format="{asctime} {processName:<12} {message} ({filename}:{lineno})",
+    style="{",
+    filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# regex for the definition of QuantityRanges
+# From: utils/statvar_dcid_generator.py
+_QUANTITY_RANGE_REGEX = (r"\[(?P<lower_limit>-|-?\d+(\.\d+)?) "
+                         r"(?P<upper_limit>-|-?\d+(\.\d+)?) "
+                         r"(?P<quantity>[A-Za-z]+)\]")
+
+# list of properties that every statistical variable should contain
+_MANDATORY_PROPS = ['measuredProperty', 'populationType', 'statType']
+
+# common properties and their associated populationTypes
+# TODO: Establish if this map generalizes across the different subject tables
+_DISAMBIGUATING_POP_TYPE = {'income': 'Household', 'earnings': 'Person'}
+
+# list of keys expected in the JSON spec, if they are not defined, we set as an
+# empty dictionary
+_JSON_KEYS = [
+    'populationType', 'measurement', 'pvs', 'inferredSpec',
+    'enumSpecializations', 'pvs', 'universePVs'
+]
 
 
 def generate_stat_var_map(spec_dict: dict, column_list: list) -> dict:
@@ -54,12 +86,12 @@ class GenerateColMapBase:
         self.column_map = {}
         self.delimiter = delimiter
 
-    def _update_stat_var_dict(self, stat_var, part_list):
-        """changes certain properties of the stat_var dict based on column name"""
-        # if Margin of Error columns are present change the statType
-        if 'Margin of Error' in part_list:
-            stat_var['statType'] = 'marginOfError'
-        return stat_var
+        # fill missing keys in JSON spec with empty values
+        for key in self.features:
+            try:
+                self.features[key]
+            except KeyError:
+                self.features[key] = {}
 
     def _generate_stat_vars_from_spec(self):
         """generates stat_var nodes for each column in column list and returns a dictionary of columns with their respective stat_var nodes
@@ -76,8 +108,16 @@ class GenerateColMapBase:
         for col in self.column_list:
             if 'ignoreColumns' in self.features and col not in self.features[
                     'ignoreColumns']:
-                self.column_map[col] = self._column_to_statvar(col)
-        #TODO: Enable validating the generate columnMap: _valid_col_map():
+                try:
+                    self.column_map[col] = self._column_to_statvar(col)
+                except Exception as e:
+                    exec_type, exec_obj, exec_tb = sys.exc_info()
+                    logger.error(
+                        f"Exeception: {exec_type} occured in generate_col_map.py at line {exec_tb.tb_lineno}"
+                    )
+                    continue
+
+        #TODO: Enable validating the generate columnMap. Dependent implementation _isvalid_column_map
         return self.column_map
 
     def _column_to_statvar(self, column):
@@ -101,13 +141,12 @@ class GenerateColMapBase:
 
         for part in part_list:
             # set the base for special cases like median, etc.
-            if not measurement_assigned and 'measurement' in self.features:
-                if part in self.features['measurement']:
-                    stat_var.update(self.features['measurement'][part])
-                    measurement_assigned = True
+            if not measurement_assigned and part in self.features['measurement']:
+                stat_var.update(self.features['measurement'][part])
+                measurement_assigned = True
 
         # set the default statVar definition
-        if not measurement_assigned and 'measurement' in self.features and '_DEFAULT' in self.features[
+        if not measurement_assigned and '_DEFAULT' in self.features[
                 'measurement']:
             stat_var.update(self.features['measurement']['_DEFAULT'])
 
@@ -116,44 +155,116 @@ class GenerateColMapBase:
             stat_var['populationType'] = self._get_population_type(part_list)
 
         # associate pvs to stat_var
-        if 'pvs' in self.features:
-            #p = property, k = dictionary of substrings in column that points to a propertyValue
-            for p, k in self.features['pvs'].items():
-                for c, v in k.items():
-                    ## check if the current key of dict matches with any substring from the tokens of a column name
-                    for part in part_list:
-                        # check if column substring matches with a column token
-                        # the check is done by ignoring the case
-                        if c.lower() in part.lower():
-                            stat_var[p] = v
-                            # add inferred properties from property 'p' to the stat_var
-                            if 'inferredSpec' in self.features and p in self.features[
-                                    'inferredSpec']:
-                                stat_var.update(
-                                    self.features['inferredSpec'][p])
+        #p = property, k = dictionary of substrings in column that points to a propertyValue
+        for p, k in self.features['pvs'].items():
+            for c, v in k.items():
+                ## check if the current key of dict matches with any substring from the tokens of a column name
+                for part in part_list:
+                    # check if column substring matches with a column token
+                    # the check is done by ignoring the case
+                    if c.lower() in part.lower():
+                        if p in stat_var:
+                            logger.warning(
+                                f"For column: {column} | Property {p} has an existing value {stat_var[p]} which is modified to value {p}"
+                            )
+                        stat_var[p] = v
 
-        #TODO: If quantityRanges occurs as values in multiple properties, find the property that relates to the column
-        #Suggestion: using _DISAMBIGUATING_POP_TYPE for pvs (income and earnings -- in the test)
+        # If quantityRanges occurs as values in multiple properties, we pick the related property based on
+        # populationType using _DISAMBIGUATING_POP_TYPE for pvs. Example: In
+        # subject table S2702,  income (for Households) and earnings(for Person)
+        # appears based on the values and needs to be disambiguated.
+        for k, v in _DISAMBIGUATING_POP_TYPE.items():
+            try:
+                if not _DISAMBIGUATING_POP_TYPE[k] in stat_var['populationType']:
+                    del stat_var[
+                        k]  #remove property that does not match with the mapping
+            except:  #found no pvs that need to be disambiguated
+                continue
 
-        #TODO: Add units and scalingFactor
-
-        # if the column is Margin of Error, update statType key
-        stat_var = self._update_stat_var_dict(stat_var, part_list)
         ## add Universe PVs based on the populationType of StatVar
         dependent_properties = None
-        if 'universePVs' in self.features:
-            for elem in self.features['universePVs']:
-                if stat_var['populationType'] == elem['populationType']:
-                    # check if all constraints of this populationType is in stat_var
-                    if (set(elem['constraintProperties']).issubset(
-                            set(list(stat_var.keys())))):
-                        #TODO: add dependent PVs to the stat_var dict if it is not present
-                        dependent_properties = list(elem['dependentPVs'].keys())
+        for elem in self.features['universePVs']:
+            if stat_var['populationType'] == elem['populationType']:
+                # check if all constraints of this populationType is in stat_var
+                if (set(elem['constraintProperties']).issubset(
+                        set(list(stat_var.keys())))):
+                    dependent_properties = list(elem['dependentPVs'].keys())
+                # if dependent PVs are not present, add them to stat_var
+                else:
+                    #TODO: Can be improved?
+                    stat_var.update(elem['dependentPVs'])
+
+        # add inferred propoerties if applicable
+        for p in stat_var:
+            if stat_var[p] in self.features['inferredSpec']:
+                stat_var.update(self.features['inferredSpec'][p])
 
         # generating dcid using the utils/statvar_dcid_generator.py
-        stat_var['dcid'] = get_stat_var_dcid(stat_var,
-                                             ignore_props=dependent_properties)
+        stat_var['Node'] = 'dcid:' + get_stat_var_dcid(
+            stat_var, ignore_props=dependent_properties)
+        #prefix the values if they are not QuantityRanges with dcs:
+        stat_var = self._format_stat_var_node(stat_var)
+
+        #check if the stat_var dict has the mandatory properties
+        if self._isvalid_stat_var(stat_var):
+            return stat_var
+        else:
+            try:
+                raise ValueError
+            except ValueError:
+                logger.error(
+                    f'One or more mandatory properties of the stat_var is missing. \n Dump of the stat_var:: {stat_var}'
+                )
+
+    def _keep_only_enum_specializations(self):
+        """removes the stat var nodes for columns that are defined as generalizations of a group.
+      Example: if the enumSpecicalization is defined as
+      {
+        "Under 6 years": "Under 19 years",
+        "6 to 18 years": "Under 19 years",
+      }
+      It means that "Under 6 years" and "6 to 18 years" are specializations of Under 19 years.
+      And hence we do not add a stat var for "Under 19 Years". 
+      If the enumSpecialisation field is empty this will not be done.
+      """
+        # iterate over the column_names, since the column_names are descriptive in
+        # the census tables
+        for column_name in self.column_map:
+            for k, v in self.features['enumSpecializations']:
+                if k in column_name and v in column_name:
+                    del self.column_map[v]
+
+    def _isvalid_stat_var(self, stat_var):
+        """method validates if stat_var has mandatory properties, specified in _MANDATORY_PROPS"""
+        if set(_MANDATORY_PROPS).issubset(set(list(stat_var.keys()))):
+            return True
+        else:
+            return False
+
+    def _format_stat_var_node(self, stat_var):
+        """utility to format the stat_var dict values to ensure they conform to the specifications of StatVar"""
+        # add typeOf property to the node if undefined
+        if 'typeOf' not in stat_var:
+            stat_var['typeOf'] = 'dcs:StatisticalVariable'
+
+        # check if all values that are not quantity ranges are prefixed
+        for k, v in stat_var.items():
+            # if value is not a bracketed quantity range
+            if re.match(_QUANTITY_RANGE_REGEX, v):
+                continue
+            # if value is not prefixed apriori with dcs: or schema:
+            elif 'schema:' in v or 'dcs:' in v or 'dcid:' in v:
+                continue
+            # add dcid: prefix to the value
+            else:
+                stat_var[k] = 'dcid:' + v
         return stat_var
+
+    def _isvalid_column_map(self):
+        """check the generated column_map against a set of validation rules"""
+        ##TODO[sharadshriram]: Needs to be implemented
+        ## same stat_var dcid occurs for multiple columns -- #TODO
+        pass
 
     def _get_population_type(self, part_list):
         """From tokenized column name, find the most relevant populationType from the JSON Spec """
--- scripts/us_census/acs5yr/subject_tables/common/generate_col_map_test.py
+++ scripts/us_census/acs5yr/subject_tables/common/generate_col_map_test.py
@@ -9,12 +9,8 @@ from .generate_col_map import generate_stat_var_map
 class GenerateColMapTest(unittest.TestCase):
     """ Test Cases for checking the generation of column map from JSON Spec"""
 
-    def test_generating_column_map(self):
-        """tests the column map on a subset of columns from S2702 """
-        spec_path = './testdata/spec.json'
-        column_file = './testdata/columns.txt'
-        expected_out = './testdata/expected.json'
-
+    def _get_file_inputs(self, spec_path, column_file, expected_out):
+        """utility function to get all inputs from files"""
         # base path from where the script runs
         base_path = os.path.dirname(__file__)
 
@@ -31,8 +27,19 @@ class GenerateColMapTest(unittest.TestCase):
         expected_map = json.load(f)
         f.close()
 
+        return spec_dict, column_list, expected_map
+
+    def test_generating_column_map(self):
+        """tests the column map on a subset of columns from S2702 """
+        spec_path = './testdata/spec.json'
+        column_file = './testdata/columns.txt'
+        expected_out = './testdata/expected.json'
+
+        spec_dict, column_list, expected_map = self._get_file_inputs(
+            spec_path, column_file, expected_out)
         # create the column map for the inputs
         generated_map = generate_stat_var_map(spec_dict, column_list)
+
         # validate
         self.assertEqual(expected_map, generated_map)
 
@@ -42,23 +49,11 @@ class GenerateColMapTest(unittest.TestCase):
         column_file = './testdata/s2702_columns.txt'
         expected_out = './testdata/s2702_expected.json'
 
-        # base path from where the script is run
-        base_path = os.path.dirname(__file__)
-
-        f = open(os.path.join(base_path, spec_path), 'r')
-        spec_dict = json.load(f)
-        f.close()
-
-        f = open(os.path.join(base_path, column_file), 'r')
-        column_list = f.read().splitlines()
-        f.close()
-
-        f = open(os.path.join(base_path, expected_out), 'r')
-        expected_map = json.load(f)
-        f.close()
-
+        spec_dict, column_list, expected_map = self._get_file_inputs(
+            spec_path, column_file, expected_out)
         #generate column map for s2702 table
         generated_map = generate_stat_var_map(spec_dict, column_list)
+
         #validate
         self.assertEqual(expected_map, generated_map)
 
