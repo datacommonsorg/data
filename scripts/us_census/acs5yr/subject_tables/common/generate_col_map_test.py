998a4c5177c9c7a01e607ab1bdbd05128c78ab99
Sharad Shriram <sharadshriram@google.com>
2021-09-03 05:11:26 +0000

    making generate_col_map_test run along with other unittests on discover

--- scripts/us_census/acs5yr/subject_tables/common/generate_col_map_test.py
+++ scripts/us_census/acs5yr/subject_tables/common/generate_col_map_test.py
@@ -1,7 +1,9 @@
 """Tests for generateColMap."""
-import unittest
+import os
 import json
-from generate_col_map import generate_stat_var_map
+import unittest
+
+from .generate_col_map import generate_stat_var_map
 
 
 class GenerateColMapTest(unittest.TestCase):
@@ -9,20 +11,23 @@ class GenerateColMapTest(unittest.TestCase):
 
     def test_generating_column_map(self):
         """tests the column map on a subset of columns from S2702 """
-        spec_path = 'testdata/spec.json'
-        column_file = 'testdata/columns.txt'
-        expected_out = 'testdata/expected.json'
+        spec_path = './testdata/spec.json'
+        column_file = './testdata/columns.txt'
+        expected_out = './testdata/expected.json'
+
+        # base path from where the script runs
+        base_path = os.path.dirname(__file__)
 
         # load the files
-        f = open(spec_path, 'r')
+        f = open(os.path.join(base_path, spec_path), 'r')
         spec_dict = json.load(f)
         f.close()
 
-        f = open(column_file, 'r')
+        f = open(os.path.join(base_path, column_file), 'r')
         column_list = f.read().splitlines()
         f.close()
 
-        f = open(expected_out, 'r')
+        f = open(os.path.join(base_path, expected_out), 'r')
         expected_map = json.load(f)
         f.close()
 
@@ -33,26 +38,28 @@ class GenerateColMapTest(unittest.TestCase):
 
     def test_column_map_for_s2702(self):
         """tests the column map generation for all columns of s2702 table"""
-        spec_path = 'testdata/spec.json'
-        column_file = 'testdata/s2702_columns.txt'
-        expected_out = 'testdata/s2702_expected.json'
+        spec_path = './testdata/spec.json'
+        column_file = './testdata/s2702_columns.txt'
+        expected_out = './testdata/s2702_expected.json'
 
-        f = open(spec_path, 'r')
+        # base path from where the script is run
+        base_path = os.path.dirname(__file__)
+
+        f = open(os.path.join(base_path, spec_path), 'r')
         spec_dict = json.load(f)
         f.close()
 
-        f = open(column_file, 'r')
+        f = open(os.path.join(base_path, column_file), 'r')
         column_list = f.read().splitlines()
         f.close()
 
-        f = open(expected_out, 'r')
+        f = open(os.path.join(base_path, expected_out), 'r')
         expected_map = json.load(f)
         f.close()
 
         #generate column map for s2702 table
         generated_map = generate_stat_var_map(spec_dict, column_list)
         #validate
-        #TODO: This assertion needs to be reviewed.
         self.assertEqual(expected_map, generated_map)
 
 
