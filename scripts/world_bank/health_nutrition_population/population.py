49729543d608e90465dd510311b603dfa8134dae
Aarushi1104 <aarushitiwari@google.com>
2021-10-04 13:02:41 +0530

    Update population.py

--- scripts/world_bank/health_nutrition_population/population.py
+++ scripts/world_bank/health_nutrition_population/population.py
@@ -62,37 +62,6 @@ def get_df(serieses):
             df2.loc[len(df2)] = row
     return df2
 
-def get_mcf(series_lst):
-    '''
-    gets mcf by splitting description and using fstrings
-    '''
-    nodes = []
-    with open("World_bank_hnp_population.mcf", 'w', encoding = 'utf-8') as file:
-        mcf = ''
-        for current_series in series_lst:
-            print('\n'+current_series)
-            node, age, gender = '', '', ''
-            age = current_series[9:11]
-            gender = ''
-            if 'MA' in current_series:
-                gender = 'Male'
-            else:
-                gender = 'Female'
-            property_dict = {'typeOf': 'dcs:StatisticalVariable',
-                     'description': f'Age population, {gender}, Age {age}, interpolated',
-                     'populationType': 'dcs:Person', 'measuredProperty':
-                     'dcs:count','gender': 'dcs:{gender}',
-                     'statType': 'dcs:measuredValue', 'age': '[Years {age}]'}
-            node = get_statvars(gender, age)
-            desc = f'Age population, age {age}, {gender.lower()}, interpolated'
-            nodes.append(node)
-            property_dict['node'] = node
-            
-            for i in range(len(poperty_dict.keys())):
-                mcf = mcf + f'{property_dict.keys[i]}:'
-                + ' {property_dict[property_dict.keys()[i]}\n'
-        file.write(mcf)
-
 def get_statvars(gender, age):
     property_dict = {'typeOf': 'dcs:StatisticalVariable',
                      'description': f'Age population, {gender}, Age {age}, interpolated',
