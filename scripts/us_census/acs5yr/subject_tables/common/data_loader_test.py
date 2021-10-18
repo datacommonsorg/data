d02ce557d20e265da1f7e5165d1b22cb0f8b3750
Sharad Shriram <sharadshriram@google.com>
2021-10-18 13:11:19 +0000

    updating the tests after merging PR#513

--- scripts/us_census/acs5yr/subject_tables/common/data_loader_test.py
+++ scripts/us_census/acs5yr/subject_tables/common/data_loader_test.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 """ Test for the data loader for the subject tables"""
 import unittest
-
+import os
 from .data_loader import process_subject_tables
+
+base_path = os.path.dirname(__file__)
 
 
 # TODO: Use a smaller spec for tests
@@ -23,10 +25,11 @@ class DataLoaderBaseTest(unittest.TestCase):
     def test_zip_file_input(self):
         process_subject_tables(
             table_prefix='test_zip_alabama',
-            input_path='testdata/s2702_alabama.zip',
-            output_dir='./',
-            column_map_path='testdata/column_map_from_zip_expected.json',
-            spec_path='testdata/spec_s2702.json',
+            input_path=os.path.join(base_path, "./testdata/s2702_alabama.zip"),
+            output_dir=os.path.join(base_path, "./testdata/"),
+            column_map_path=os.path.join(
+                base_path, "./testdata/column_map_from_zip_expected.json"),
+            spec_path=os.path.join(base_path, "./testdata/spec_s2702.json"),
             debug=False,
             delimiter='!!',
             has_percent=True)
@@ -34,10 +37,12 @@ class DataLoaderBaseTest(unittest.TestCase):
     def test_csv_file_input(self):
         process_subject_tables(
             table_prefix='test_csv_2013',
-            input_path='testdata/ACSST5Y2013_S2702.csv',
-            output_dir='./',
-            column_map_path='testdata/column_map_from_zip_expected.json',
-            spec_path='testdata/spec_s2702.json',
+            input_path=os.path.join(base_path,
+                                    "./testdata/ACSST5Y2013_S2702.csv"),
+            output_dir=os.path.join(base_path, "./testdata/"),
+            column_map_path=os.path.join(
+                base_path, "./testdata/column_map_from_zip_expected.json"),
+            spec_path=os.path.join(base_path, "./testdata/spec_s2702.json"),
             debug=False,
             delimiter='!!',
             has_percent=True)
