04310962e96e405d858645eb23e143aade9a516a
Aarushi1104 <aarushitiwari@google.com>
2021-10-20 19:51:11 +0530

    Update population.py

--- scripts/world_bank/health_nutrition_population/population/population.py
+++ scripts/world_bank/health_nutrition_population/population/population.py
@@ -12,7 +12,7 @@ flags.DEFINE_string("gender", "FE MA", "Gender: FE for female, MA for male")
 flags.DEFINE_string("country", "USA", "Country codes")
 #maximum the API Can display is 32767
 flags.DEFINE_integer("per_page", 32, "rows per page")
-flags.DEFINE_string("path", "WorldBankHNP_Population_Tests.csv", "Path of final csv")
+flags.DEFINE_string("output_csv", "WorldBankHNP_Population_Tests.csv", "Path of final csv")
 
 
 
@@ -25,9 +25,6 @@ def get_df(serieses, per_page, country):
     df2 = pd.DataFrame(columns = ['Series Name', 'Series Code', 'Country Code', 'Country',
                                   'Year', 'Value'])
     for current_series in serieses:
-        # maximum that can be displayed per page according to API rules
-        #will not need to change this in the forseeable future
-        MAX_PER_PAGE = 32767
         url = f"https://api.worldbank.org/v2/country/{country}/indicator/{current_series}?"
         url = url + f"format=JSON&per_page={per_page}"
         response = requests.get(url)
@@ -59,21 +56,53 @@ def get_statvars(gender, age):
 
 def get_csv(df_in, df_out_path):
     """Creation of csv according to tmcf:"""
-    df2 = pd.DataFrame(columns = ['Country', 'Year', "StatVar", 'Population'])
+    df2 = pd.DataFrame(columns = ['observationAbout', 'Year', "StatVar", 'Population'])
     for line in range(len(df_in)):
         gender, statvar = '', ''
         gender = df_in['Series Name'][line].split(',')[-2].strip()
         gender = gender[0].upper() + gender[1:]
         age = df_in['Series Name'][line].split(',')[-3].strip().split()[-1]
         statvar = get_statvars(gender, age)
-        addto_df2 = [df_in['Country'][line], df_in['Year'][line], statvar,
+        addto_df2 = ["country/" + df_in['Country Code'][line], df_in['Year'][line], statvar,
                                    df_in['Value'][line]]
         df2.loc[len(df2.index)] = addto_df2
     df2.to_csv(df_out_path)
     
+def get_mcf(series_lst):
+    '''
+    gets mcf by splitting description and using fstrings
+    '''
+    nodes = []
+    with open("World_bank_hnp_population.mcf", 'w', encoding = 'utf-8') as file:
+        mcf = ''
+        for current_series in series_lst:
+            print('\n'+current_series)
+            node, age, gender = '', '', ''
+            age = current_series[9:11]
+            gender = ''
+            if 'MA' in current_series:
+                gender = 'Male'
+            else:
+                gender = 'Female'
+            property_dict = {'typeOf': 'dcs:StatisticalVariable',
+                     'description': f'Age population, {gender}, Age {age}, interpolated',
+                     'populationType': 'dcs:Person', 'measuredProperty':
+                     'dcs:count','gender': 'dcs:{gender}',
+                     'statType': 'dcs:measuredValue', 'age': '[Years {age}]'}
+            node = get_statvars(gender, age)
+            desc = f'Age population, age {age}, {gender.lower()}, interpolated'
+            nodes.append(node)
+            property_dict['node'] = node
+            
+            for i in range(len(poperty_dict.keys())):
+                mcf = mcf + f'{property_dict.keys[i]}:'
+                + ' {property_dict[property_dict.keys()[i]}\n'
+        file.write(mcf)
+    
 def main(argv):
     series  = [f"SP.POP.AG{age:02d}.{gender}.IN" for age in range(FLAGS.age)
                for gender in (FLAGS.gender.split())]
+    get_mcf(series)
     df = get_df(series, FLAGS.per_page, FLAGS.country)
     get_csv(df, FLAGS.path)
    
