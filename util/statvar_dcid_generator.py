28cb6eece09bd5d05732c74e7291fed7d9127d29
Anush Kini <33577829+Abilityguy@users.noreply.github.com>
2022-01-04 16:30:27 +0530

    Artifacts for S2418(Median Earnings By Class Of Worker) Table (#584)
    * Artifacts for S2418 Table
    
    * Fix for failing decimal point precision
    
    * Minor fix to _EXCLUDE_DIRS
    
    * CSV comparision done using assert_frame_equal

--- util/statvar_dcid_generator.py
+++ util/statvar_dcid_generator.py
@@ -50,7 +50,8 @@ _QUANTITY_RANGE_REGEX_2 = re.compile(r'\[(?P<quantity>[A-Za-z_/\d]+) '
 # These are the default properties ignored during dcid generation
 _DEFAULT_IGNORE_PROPS = ('unit', 'Node', 'memberOf', 'typeOf',
                          'constraintProperties', 'name', 'description',
-                         'descriptionUrl', 'label', 'url', 'alternateName')
+                         'descriptionUrl', 'label', 'url', 'alternateName',
+                         'scalingFactor')
 
 # Regex to match prefixes to be removed from constraints. The regex checks for
 # specific prefixes followed by an upper case letter or underscore. This helps
