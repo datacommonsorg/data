8fd4d21ddb2bce0be7d0848cc50825ebe4d3efdd
Aarushi1104 <aarushitiwari@google.com>
2022-01-06 11:58:53 +0530

    Update population.py

--- scripts/world_bank/health_nutrition_population/population/population.py
+++ scripts/world_bank/health_nutrition_population/population/population.py
@@ -1,3 +1,20 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''
+gets pupolation (age 0 to 25) data for all world bank countries, and makes a csv acording to TMCF
+'''
+
 import requests
 import pandas as pd
 from absl import app
