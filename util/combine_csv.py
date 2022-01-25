94fde1c428b8234312c7074462eb40327fd951f5
Ravi Bhoot <bhoot@google.com>
2022-01-25 19:14:22 +0530

    lint fix

--- util/combine_csv.py
+++ util/combine_csv.py
@@ -12,6 +12,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('config_path', None,
                     'Path to json file containing config for combining files')
 
+
 # get updated df from file path
 def get_df_from_file(file_path: str, col_map: dict, output_columns: dict):
     df = pd.read_csv(file_path)
@@ -19,7 +20,7 @@ def get_df_from_file(file_path: str, col_map: dict, output_columns: dict):
     for cur_col in output_columns:
         if cur_col not in df.columns:
             if 'required' not in output_columns[cur_col]:
-               output_columns[cur_col]['required'] = False
+                output_columns[cur_col]['required'] = False
             if output_columns[cur_col]['required']:
                 raise ValueError(f'{cur_col} required but not found')
             else:
@@ -28,11 +29,12 @@ def get_df_from_file(file_path: str, col_map: dict, output_columns: dict):
                 df[cur_col] = output_columns[cur_col]['default_value']
     return df[list(output_columns)]
 
+
 def write_combined_df(config_path: str):
     config_path = os.path.expanduser(config_path)
     with open(config_path) as fp:
         conf = json.load(fp)
-    
+
     conf['input_files_expanded'] = {}
     df = pd.DataFrame()
 
@@ -44,22 +46,27 @@ def write_combined_df(config_path: str):
         for cur_file in matching_files:
             # set of files
             if cur_file not in conf['input_files_expanded']:
-                conf['input_files_expanded'][cur_file] = conf['input_files'][cur_exp]
-    
+                conf['input_files_expanded'][cur_file] = conf['input_files'][
+                    cur_exp]
+
     for cur_file in conf['input_files_expanded']:
-        cur_df = get_df_from_file(cur_file, conf['input_files_expanded'][cur_file], conf['output_columns'])
+        cur_df = get_df_from_file(cur_file,
+                                  conf['input_files_expanded'][cur_file],
+                                  conf['output_columns'])
         # concat df
         df = pd.concat([df, cur_df])
-    
+
     # write to output
     output_path = conf['output_file']
     output_path = os.path.expanduser(output_path)
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     df.to_csv(conf['output_file'], index=False)
 
+
 def main(argv):
     write_combined_df(FLAGS.config_path)
 
+
 if __name__ == '__main__':
-  flags.mark_flags_as_required(['config_path'])
-  app.run(main)
+    flags.mark_flags_as_required(['config_path'])
+    app.run(main)
