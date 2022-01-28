5b78901f0a146be549dee0358f28e5b057848106
SenthamizhanV <sendhucool@gmail.com>
2022-01-28 19:22:47 +0530

    format check

--- scripts/india/geo/districts.py
+++ scripts/india/geo/districts.py
@@ -59,7 +59,7 @@ class IndiaDistrictsMapper:
             df_districts_by_state['districtLabel'] == district_name]
         if len(df_districts.index) == 1:
             return list(df_districts["LGDDistrictCode"])[0]
-        
+
         # Try match using closestDistrictLabel (Close Name Alternatives)
         df_districts = df_districts_by_state.loc[
             df_districts_by_state['closestDistrictLabel'] == district_name]
