b840806afaefdcf2677b67aa34af88fcc38e268c
Prashanth Radhakrishnan <shanth@google.com>
2022-02-03 18:50:43 +0000

    address PR comments

--- tools/split_csv.sh
+++ tools/split_csv.sh
@@ -13,23 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Utility to split a single CSV file into chunks with header replicated in each
-# of the outputs. Produces shards in the same directory as input CSV.  For
-# example, /path/to/dir/input.csv produces /path/to/dir/input_shard_*.csv files.
-
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 <csv_to_split> [row_size_per_chunk]"
+  echo "Usage: $0 <csv_to_split> [num_lines_per_shard]"
   exit 1
 fi
 
 HEADER=$(head -1 $1)
 if [ -n "$2" ]; then
-  CHUNK=$2
+  LINES_PER_SHARD=$2
 else
-  CHUNK=1000
+  LINES_PER_SHARD=10000
 fi
 FNAME=$(echo "$1" | sed 's/\.csv$//')
-tail -n +2 $1 | split -l $CHUNK - ${FNAME}_shard_ --additional-suffix=.csv
+tail -n +2 $1 | \
+  split -l $LINES_PER_SHARD - ${FNAME}_shard_ --additional-suffix=.csv
 for i in ${FNAME}_shard_*; do
   sed -i -e "1i$HEADER" "$i"
 done
