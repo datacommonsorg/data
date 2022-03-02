ff736cdbf6709ffdb6dfc7e65148bc8e1ff1f2e3
Ravi Bhoot <bhoot@google.com>
2022-03-02 15:25:10 +0530

    add requests worappers and test

---
+++ tools/download_utils/requests_wrappers.py
@@ -0,0 +1,72 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Wrapper functions for easy use of requests library.
+"""
+
+import requests
+import json
+import time
+
+def request_url_json(url: str) -> dict:
+  """Get JSON object version of reponse to GET request to given URL.
+
+  Args:
+    url: URL to make the GET request.
+
+  Returns:
+    JSON decoded response from the GET call.
+      Empty dict is returned in case the call fails.
+  """
+  print(url)
+  try:
+    req = requests.get(url)
+    # print(req.url)
+  except requests.exceptions.ReadTimeout:
+    print('Timeout occoured, retrying after 10s.')
+    time.sleep(10)
+    try:
+      req = requests.get(url)
+    except requests.exceptions.ReadTimeout:
+      print('Timeout occoured, request failed.')
+      return {}
+
+  if req.status_code == requests.codes.ok:
+    response_data = req.json()
+  else:
+    response_data = {'http_err_code': req.status_code}
+    print('HTTP status code: ' + str(req.status_code))
+  return response_data
+
+def request_post_json(url: str, data_: dict) -> dict:
+  """Get JSON object version of reponse to POST request to given URL.
+
+  Args:
+    url: URL to make the POST request.
+    data_: payload for the POST request
+
+  Returns:
+    JSON decoded response from the POST call.
+      Empty dict is returned in case the call fails.
+  """
+  headers = {'Content-Type': 'application/json'}
+  req = requests.post(url, data=json.dumps(data_), headers=headers)
+  print(req.request.url)
+  
+  if req.status_code == requests.codes.ok:
+    response_data = req.json()
+  else:
+    response_data = {'http_err_code': req.status_code}
+    print('HTTP status code: ' + str(req.status_code))
+  return response_data
