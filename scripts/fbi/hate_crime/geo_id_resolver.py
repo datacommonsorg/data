c22ab55e9acef0f147cb74604e721da9a13bb68d
Anush Vishwanath Kini <anushk@google.com>
2022-03-06 07:30:39 +0000

    Review and lint fixes

--- scripts/fbi/hate_crime/geo_id_resolver.py
+++ scripts/fbi/hate_crime/geo_id_resolver.py
@@ -1,10 +1,10 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,8 +27,8 @@ from county_to_dcid import COUNTY_MAP
 
 _CITY_GEOCODES_PATH = os.path.join(_SCRIPT_PATH, '../crime/city_geocodes.csv')
 
-_US_GEO_CODE_UPDATE_MAP = {
-    # Mapping geos in data to geos in csv / map
+_US_ALPHA_CODE_UPDATE_MAP = {
+    # Mapping state alpha codes in data to geos in csv / map
     'NB': 'NE',
     'GM': 'GU'
 }
@@ -131,6 +131,8 @@ _CITY = {
 with open(os.path.join(_CITY_GEOCODES_PATH), encoding='utf8') as csvfile:
     csv_reader = csv.reader(csvfile)
     for a in csv_reader:
+        if _CITY.get(a[0], ''):  # if key already exists in _CITY
+            assert False, f'Duplicate city found {a}'
         _CITY[a[0]] = a[1]
 
 
@@ -148,13 +150,10 @@ def _state_to_dcid(state_code: str) -> str:
     if state_code in _IGNORE_STATE_ABBR:
         return ''
 
-    if state_code in _US_GEO_CODE_UPDATE_MAP:
-        state_code = _US_GEO_CODE_UPDATE_MAP[state_code]
+    # if state_code is in the map, return it's value else retain the original
+    state_code = _US_ALPHA_CODE_UPDATE_MAP.get(state_code, state_code)
 
-    if state_code in USSTATE_MAP:
-        return USSTATE_MAP[state_code]
-    else:
-        return ''
+    return USSTATE_MAP.get(state_code, '')
 
 
 def _get_county_variants(county: str) -> list:
@@ -167,7 +166,7 @@ def _get_county_variants(county: str) -> list:
     Returns:
         A list of the possible variants of the county name.
     """
-    county = county.replace(' County Police Department', ' County')
+    county = county.replace(' County Police Department', '')
     county_variants = [
         county, county + ' County', county + ' Parish', county + ' Borough'
     ]
@@ -179,7 +178,7 @@ def _county_to_dcid(state_abbr: str, county: str) -> str:
     Takes the county name and the state code it's contained in, the dcid of the
     county is returned.
 
-    Args: 
+    Args:
         state_abbr: The alpha2 code of the state.
         county: The county name.
 
@@ -190,18 +189,17 @@ def _county_to_dcid(state_abbr: str, county: str) -> str:
     if state_abbr in _IGNORE_STATE_ABBR:
         return ''
 
-    if state_abbr in _US_GEO_CODE_UPDATE_MAP:
-        state_abbr = _US_GEO_CODE_UPDATE_MAP[state_abbr]
+    # if state_abbr is in the map, return it's value else retain the original
+    state_abbr = _US_ALPHA_CODE_UPDATE_MAP.get(state_abbr, state_abbr)
 
     county_valid_names = _get_county_variants(county)
 
     if state_abbr in COUNTY_MAP:
-        for county in county_valid_names:
-            if county in COUNTY_MAP[state_abbr]:
-                return COUNTY_MAP[state_abbr][county]
-        return ''
-    else:
-        return ''
+        for c in county_valid_names:
+            if c in COUNTY_MAP[state_abbr]:
+                return COUNTY_MAP[state_abbr][c]
+
+    return ''
 
 
 def _city_to_dcid(state_abbr: str, city_name: str) -> str:
@@ -219,28 +217,26 @@ def _city_to_dcid(state_abbr: str, city_name: str) -> str:
     """
     if state_abbr in _IGNORE_STATE_ABBR:
         return ''
-    elif state_abbr in _US_GEO_CODE_UPDATE_MAP:
-        state_abbr = _US_GEO_CODE_UPDATE_MAP[state_abbr]
+
+    # if state_abbr is in the map, return it's value else retain the original
+    state_abbr = _US_ALPHA_CODE_UPDATE_MAP.get(state_abbr, state_abbr)
 
     city_state = city_name.lower().strip() + ' ' + state_abbr.lower().strip()
 
     if city_state in _IGNORE_CITIES:
         return ''
-    elif city_state in _US_GEO_CODE_UPDATE_MAP:
-        city_state = _US_GEO_CODE_UPDATE_MAP[city_state]
 
     if city_state in _CITY:
         return 'geoId/' + _CITY[city_state]
-    else:
-        return ''
+    return ''
 
 
 def convert_to_place_dcid(state_abbr: str,
                           geo: str = '',
                           geo_type: str = 'State') -> str:
     """
-    Resolves the geoId based on the FBI Hate crime dataset Agency Type and Name. 
-    If a geoId could not be resolved, the function returns an empty string ('').
+    Resolves the geoId based on the FBI Hate crime dataset Agency Type and Name.
+    If a geoId could not be resolved, the function returns an empty string.
 
     Args:
         state_abbr: The alpha2 code of the state.
@@ -250,21 +246,20 @@ def convert_to_place_dcid(state_abbr: str,
     Returns:
         The dcid of the geo. Returns an empty string if the geo is not found.
     """
+    geo_dcid = ''
     if state_abbr in _IGNORE_STATE_ABBR:
-        return ''
+        return geo_dcid
 
-    if state_abbr in _US_GEO_CODE_UPDATE_MAP:
-        state_abbr = _US_GEO_CODE_UPDATE_MAP[state_abbr]
+    # if state_abbr is in the map, return it's value else retain the original
+    state_abbr = _US_ALPHA_CODE_UPDATE_MAP.get(state_abbr, state_abbr)
 
     if geo_type == 'State':
-        return _state_to_dcid(state_abbr)
+        geo_dcid = _state_to_dcid(state_abbr)
 
     elif geo_type == 'County':
-        return _county_to_dcid(state_abbr, geo)
+        geo_dcid = _county_to_dcid(state_abbr, geo)
 
     elif geo_type == 'City':
-        return _city_to_dcid(state_abbr, geo)
+        geo_dcid = _city_to_dcid(state_abbr, geo)
 
-    else:
-        # geo type currently not handled
-        return ''
+    return geo_dcid
--- scripts/fbi/hate_crime/geo_id_resolver_test.py
+++ scripts/fbi/hate_crime/geo_id_resolver_test.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,33 +11,90 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for geo_id_resolver.py"""
 
 import unittest
-from .geo_id_resolver import *
+from . import geo_id_resolver
 
 
 class GeoIdResolverTest(unittest.TestCase):
 
     def test_georesolver_state(self):
-        state = 'CA'
-        geoId = convert_to_place_dcid(state, geo_type='State')
-        self.assertEqual(geoId, 'geoId/06')
+        state = 'CA'  # California geoId/06
+        geo_id = geo_id_resolver.convert_to_place_dcid(state, geo_type='State')
+        self.assertEqual(geo_id, 'geoId/06')
 
         state = 'GM'  # Testing _US_GEO_CODE_UPDATE_MAP
-        geoId = convert_to_place_dcid(state, geo_type='State')
-        self.assertEqual(geoId, 'geoId/66')
+        geo_id = geo_id_resolver.convert_to_place_dcid(state, geo_type='State')
+        self.assertEqual(geo_id, 'geoId/66')
+
+        state = 'ZZ'  # ZZ is not resolved
+        geo_id = geo_id_resolver.convert_to_place_dcid(state, geo_type='State')
+        self.assertEqual(geo_id, '')
 
     def test_georesolver_city(self):
         # zwolle la,2283685	scripts/fbi/crime/city_geocodes.csv.
-        geoId = convert_to_place_dcid('LA', geo='Zwolle', geo_type='City')
-        self.assertEqual(geoId, 'geoId/2283685')
+        geo_id = geo_id_resolver.convert_to_place_dcid('LA',
+                                                       geo='Zwolle',
+                                                       geo_type='City')
+        self.assertEqual(geo_id, 'geoId/2283685')
+
+        # big bear ca: 0606406 from the initialised _CITY map
+        geo_id = geo_id_resolver.convert_to_place_dcid('CA',
+                                                       geo='Big Bear',
+                                                       geo_type='City')
+        self.assertEqual(geo_id, 'geoId/0606406')
+
+        # fox valley wi is not resolved
+        geo_id = geo_id_resolver.convert_to_place_dcid('WI',
+                                                       geo='Fox Valley',
+                                                       geo_type='City')
+        self.assertEqual(geo_id, '')
 
     def test_georesolver_county(self):
         # AL (Alabama), Autauga County has dcid geoId/01001
-        geoId = convert_to_place_dcid('AL',
-                                      geo='Autauga County',
-                                      geo_type='County')
-        self.assertEqual(geoId, 'geoId/01001')
+        geo_id = geo_id_resolver.convert_to_place_dcid('AL',
+                                                       geo='Autauga County',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, 'geoId/01001')
+
+        # AL (Alabama), Winston County has geoId/01133
+        # Suffix 'County' will be added by _get_county_variants function
+        geo_id = geo_id_resolver.convert_to_place_dcid('AL',
+                                                       geo='Winston',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, 'geoId/01133')
+
+        # AK (Alaska), Anchorage Borough has dcid geoId/02020
+        geo_id = geo_id_resolver.convert_to_place_dcid('AK',
+                                                       geo='Anchorage Borough',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, 'geoId/02020')
+
+        # LA (Louisiana), Allen Parish has dcid geoId/22003
+        geo_id = geo_id_resolver.convert_to_place_dcid('LA',
+                                                       geo='Allen Parish',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, 'geoId/22003')
+
+        # GM (Guam), Guam County has dcid geoId/66010
+        # The alpha code GM is changed to GU from _US_GEO_CODE_UPDATE_MAP
+        geo_id = geo_id_resolver.convert_to_place_dcid('GM',
+                                                       geo='Guam County',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, 'geoId/66010')
+
+        # AL (Alabama), Z County is not resolved
+        geo_id = geo_id_resolver.convert_to_place_dcid('AL',
+                                                       geo='Z',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, '')
+
+        # ZZ, Z County is not resolved
+        geo_id = geo_id_resolver.convert_to_place_dcid('ZZ',
+                                                       geo='Z',
+                                                       geo_type='County')
+        self.assertEqual(geo_id, '')
 
 
 if __name__ == '__main__':
