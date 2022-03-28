7e3893f537bda53a9d3346c26241164c6cd4d3db
Anush Vishwanath Kini <anushk@google.com>
2022-03-28 09:00:03 +0000

    Lint fixes to scripts and added config.json

--- scripts/us_cdc/heat_related_illness/clean_data.py
+++ scripts/us_cdc/heat_related_illness/clean_data.py
@@ -14,8 +14,8 @@
 """A script to download EPH Tracking data."""
 
 import os
-from bs4 import BeautifulSoup
 import csv
+from bs4 import BeautifulSoup
 
 from absl import app
 from absl import flags
@@ -26,12 +26,6 @@ flags.DEFINE_string('output_path', None,
                     'Path to directory where data will be written')
 _FLAGS = flags.FLAGS
 
-# Maps data to it's url on cdc
-_URL_MAP = {
-    'hospitalization_heat_stress': 'https://ephtracking.cdc.gov/qr/431/37/ALL/ALL/1/2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020/0?AgeBandId=1,2,3,4,5&GenderId=1,2',
-    'emergency_visits_heat_stress': 'https://ephtracking.cdc.gov/qr/438/37/ALL/ALL/1/2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020/0?AgeBandId=1,2,3,4,5&GenderId=1,2',
-    'summertime_heat_related_deaths': 'https://ephtracking.cdc.gov/qr/370/1/ALL/ALL/1/2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020/0'
-}
 
 def table_to_csv(html, csv_path: str):
     """Downloads a HTML table as a CSV file."""
@@ -39,21 +33,26 @@ def table_to_csv(html, csv_path: str):
     table = soup.select_one("table")
     headers = [th.text for th in table.select("tr th")]
 
-    with open(csv_path, "w") as csv_f:
-        wr = csv.writer(csv_f)
-        wr.writerow(headers)
-        wr.writerows([[td.text for td in row.find_all("td")] for row in table.select("tr + tr")])
+    with open(csv_path, "w", encoding='utf-8') as csv_f:
+        writer = csv.writer(csv_f)
+        writer.writerow(headers)
+        writer.writerows([[td.text
+                           for td in row.find_all("td")]
+                          for row in table.select("tr + tr")])
+
 
 def main(argv):
     if _FLAGS.output_path is None:
         _FLAGS.output_path = os.path.dirname(os.path.abspath(__file__))
 
     for file_name in os.listdir(_FLAGS.input_path):
-        if file_name.endswith('.html'): # If file is a html file
+        if file_name.endswith('.html'):  # If file is a html file
             file_path = os.path.join(_FLAGS.input_path, file_name)
             with open(file_path, 'r', encoding='utf-8') as f:
-                cleaned_csv_path = os.path.join(_FLAGS.output_path, file_name[:-5]+'.csv')
+                cleaned_csv_path = os.path.join(_FLAGS.output_path,
+                                                file_name[:-5] + '.csv')
                 table_to_csv(f.read(), cleaned_csv_path)
+
 
 if __name__ == "__main__":
     flags.mark_flags_as_required(['input_path'])
