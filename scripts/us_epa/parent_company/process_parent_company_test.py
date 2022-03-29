3060907e53e6b2b466438d04c54c774a762eddaa
Jehangir Amjad <jehangiramjad@gmail.com>
2022-03-28 21:44:00 -0700

    Formatting edits

--- scripts/us_epa/parent_company/process_parent_company_test.py
+++ scripts/us_epa/parent_company/process_parent_company_test.py
@@ -34,23 +34,23 @@ _EXPECTED_COUNTERS = {
     # Company Name: 'US Government' in YEAR 2012 is the first entry for the
     # company and does not have a zip code which will be flagged.
     'missing_zip':
-    set([('EpaParentCompany/BrownAndProvidencePlantations', '2020'),
-         ('EpaParentCompany/UsGovernment', '2012')]),
+        set([('EpaParentCompany/BrownAndProvidencePlantations', '2020'),
+            ('EpaParentCompany/UsGovernment', '2012')]),
     # There are two cases of ownership not set.
     'percent_ownership_not_found':
-    set([('EpaParentCompany/UsGovernment', '2012'),
-         ('EpaParentCompany/UsGovernment', '2015')]),
+        set([('EpaParentCompany/UsGovernment', '2012'),
+            ('EpaParentCompany/UsGovernment', '2015')]),
     # epaGhgrpFacilityId/9999 does not exist in the existing_facilities.csv.
     'facility_does_not_exist':
-    set(['epaGhgrpFacilityId/9999']),
+        set(['epaGhgrpFacilityId/9999']),
     # Facility Id: 1001799 in Year 2000 has no company name.
     'company_name_not_found':
-    set(['epaGhgrpFacilityId/1001799']),
+        set(['epaGhgrpFacilityId/1001799']),
     # Company name: 'National Fuel Gas Company' and Facility Id: 1001829
     # do not have a Year.
     'year_does_not_exist':
-    set([('EpaParentCompany/NationalFuelGasCompany',
-          'epaGhgrpFacilityId/1001829')])
+        set([('EpaParentCompany/NationalFuelGasCompany',
+            'epaGhgrpFacilityId/1001829')])
 }
 
 
