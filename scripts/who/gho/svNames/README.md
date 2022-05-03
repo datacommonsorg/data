2b5d96ffc0b0b70de386244d1d5a691c83c7250e
Jennifer Chen <chejennifer@google.com>
2022-05-03 11:43:08 -0700

    address pr comments

--- scripts/who/gho/svNames/README.md
+++ scripts/who/gho/svNames/README.md
@@ -1,13 +1,17 @@
 ## To generate names for WHO stat vars
 1. This script reads the who_properties.mcf and the who_gho_stat_vars.mcf files that are checked into the schema (and google3) repo. To copy these files over:
-  ```
-  WHO_PROPS_DIR=<directory-of-g4-client>/third_party/datacommons/schema/core
-  WHO_SV_DIR=<directory-of-g4-client>/third_party/datacommons/schema/stat_vars
 
-  cd scripts/who/gho/svNames
-  cp $WHO_PROPS_DIR/who_properties.mcf .
-  cp $WHO_SV_DIR/who_gho_stat_vars.mcf .
-  ```
+    a. fork and clone [datacommons schema github repository](https://github.com/datacommonsorg/schema)
+
+    b. run:
+      ```
+      WHO_PROPS_DIR=<directory-of-forked-and-cloned-datacommons-schema-repository>/core
+      WHO_SV_DIR=<directory-of-g4-client>/datacommons/import/mcf/resolved_mcf/raw_stat_vars/who_gho
+
+      cd scripts/who/gho/svNames
+      cp $WHO_PROPS_DIR/who_properties.mcf .
+      cp $WHO_SV_DIR/who_gho_stat_vars.mcf .
+      ```
 2. run the script: 
 ```
 python3 who_gho_sv_name.py
--- scripts/who/gho/svNames/who_gho_sv_name.py
+++ scripts/who/gho/svNames/who_gho_sv_name.py
@@ -12,18 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import json
-
+import os
 from absl import app
+from absl import flags
 
 INDICATOR_MAP_FILE = "indicator_map.json"
 DUPLICATE_INDICATOR_NAMES_FILE = "duplicate_indicator_names.json"
 
+FLAGS = flags.FLAGS
+flags.DEFINE_string('who_properties_file', 'who_properties.mcf',
+                    'path to mcf file with WHO property nodes.')
+flags.DEFINE_string('who_sv_file', 'who_gho_stat_vars.mcf',
+                    'path to mcf file with WHO stat var nodes.')
+flags.DEFINE_string('output_dir', '',
+                    'dir to output generated MCF file and artifacts')
 
-def generate_indicator_map_using_properties_mcf():
+
+def generate_indicator_map_using_properties_mcf(properties_file, output_dir):
+    """ 
+    Generate a map of WHO indicator code to indicator name using an MCF file of
+    WHO property nodes.
+    Args:
+        properties_file: path to the mcf file with WHO property nodes
+        output_dir: directory to output the map
+    """
     indicator_map = {}
     # who_properties.mcf is the schema file for all the properties in WHO stat vars
-    mcf_file = open("who_properties.mcf")
-    mcf_file_lines = mcf_file.readlines()
+    mcf_file_lines = []
+    with open(properties_file) as mcf_file:
+        mcf_file_lines = mcf_file.readlines()
     i_code = ""
     i_name = ""
     for line in mcf_file_lines:
@@ -41,12 +58,21 @@ def generate_indicator_map_using_properties_mcf():
         if line.startswith("description"):
             i_name = line[14:]
             i_name = i_name.strip().replace("\"", "")
-    f = open(INDICATOR_MAP_FILE, "w+")
-    f.write(json.dumps(indicator_map))
+    if i_code and i_name:
+        indicator_map[i_code] = i_name
+    with open(os.path.join(output_dir, INDICATOR_MAP_FILE), "w+") as f:
+        f.write(json.dumps(indicator_map))
 
 
-def generate_duplicate_indicator_names():
-    with open(INDICATOR_MAP_FILE, "r+") as indicator_map:
+def generate_duplicate_indicator_names(output_dir):
+    """ 
+    Generate a map of WHO indicator names to list of indicator codes that match
+    that name (only for indicator names that have multiple matching codes).
+    Args:
+        output_dir: directory to output the map
+    """
+    with open(os.path.join(output_dir, INDICATOR_MAP_FILE),
+              "r+") as indicator_map:
         indicator_map = json.load(indicator_map)
     name_to_code = {}
     for code, name in indicator_map.items():
@@ -57,11 +83,24 @@ def generate_duplicate_indicator_names():
     for name in name_to_code:
         if len(name_to_code[name]) > 1:
             duplicates[name] = name_to_code[name]
-    f = open(DUPLICATE_INDICATOR_NAMES_FILE, "w+")
-    f.write(json.dumps(duplicates))
+    with open(os.path.join(output_dir, DUPLICATE_INDICATOR_NAMES_FILE),
+              "w+") as f:
+        f.write(json.dumps(duplicates))
 
 
 def get_name(i_name, dcid, i_code, name_replacements, dup_i_names):
+    """
+    Generate a stat var name using the indicator name, indicator code, and dcid
+    of the stat var.
+    Args:
+        i_name: indicator name
+        dcid: dcid of the stat var
+        i_code: indicator code
+        name_replacements: map of string to replacement string to use in the
+            name.
+        dup_i_names: map of indicator name to list of indicator codes that use
+            that name for indicator names used by more than one indicator code.
+    """
     # dcids should either be in the form WHO/<i_code>_<additional property values> OR WHO/<i_code>
     dcid_split = dcid.split(i_code + "_")
     additional_values = ""
@@ -81,50 +120,68 @@ def get_name(i_name, dcid, i_code, name_replacements, dup_i_names):
     return name
 
 
-def generate_mcf_with_processed_names():
-    with open(INDICATOR_MAP_FILE, "r+") as indicator_map:
+def generate_mcf_with_processed_names(properties_file, sv_file,
+                                      name_replacements_file, output_dir):
+    """
+    Generate a new mcf file with generated stat var names.
+    Args:
+        properties_file: path to the mcf file with WHO property nodes
+        sv_file: path to the mcf file with WHO stat var nodes that need names
+            generated.
+        output_dir: directory to output the map
+    """
+    generate_indicator_map_using_properties_mcf(properties_file, output_dir)
+    generate_duplicate_indicator_names(output_dir)
+    with open(os.path.join(output_dir, INDICATOR_MAP_FILE),
+              "r+") as indicator_map:
         indicator_map = json.load(indicator_map)
-    with open(DUPLICATE_INDICATOR_NAMES_FILE, "r+") as dup_i_names:
+    with open(os.path.join(output_dir, DUPLICATE_INDICATOR_NAMES_FILE),
+              "r+") as dup_i_names:
         dup_i_names = json.load(dup_i_names)
-    with open("name_replacements.json", "r+") as name_replacements:
+    with open(name_replacements_file, "r+") as name_replacements:
         name_replacements = json.load(name_replacements)
     # original who stat vars mcf
-    mcf_file = open("who_gho_stat_vars.mcf")
-    mcf_file_lines = mcf_file.readlines()
+    mcf_file_lines = []
+    with open(sv_file) as mcf_file:
+        mcf_file_lines = mcf_file.readlines()
     # the new who stat vars with names updated
-    processed_file = open("updated_who_gho_stat_vars.mcf", "w")
-    missing_i_code_names = set()
-    curr_node_lines = []
-    dcid = ""
-    i_code = ""
-    for line in mcf_file_lines:
-        curr_node_lines.append(line)
-        if line == '\n':
-            i_name = indicator_map.get(i_code, "")
-            if not i_name:
-                missing_i_code_names.add(i_code)
+    with open(os.path.join(output_dir, "updated_who_gho_stat_vars.mcf"),
+              "w") as processed_file:
+        missing_i_code_names = set()
+        curr_node_lines = []
+        dcid = ""
+        i_code = ""
+        for idx, line in enumerate(mcf_file_lines):
+            if line.startswith("name"):
                 continue
-            name = get_name(i_name, dcid, i_code, name_replacements,
-                            dup_i_names)
-            curr_node_lines.insert(1, "name: \"" + name + "\"\n")
-            processed_file.writelines(curr_node_lines)
-            curr_node_lines = []
-            dcid = ""
-            i_code = ""
-            continue
-        if line.startswith("Node"):
-            dcid = line[11:].strip()
-        if line.startswith("measuredProperty"):
-            # measuredProperty will be who/<i-code>
-            i_code = line.split("/")[1].strip()
-    f = open("missing_i_code_names.json", "w+")
-    f.write(json.dumps(list(missing_i_code_names)))
+            curr_node_lines.append(line)
+            if line == '\n' or idx == len(mcf_file_lines) - 1:
+                i_name = indicator_map.get(i_code, "")
+                if not i_name:
+                    missing_i_code_names.add(i_code)
+                else:
+                    name = get_name(i_name, dcid, i_code, name_replacements,
+                                    dup_i_names)
+                    curr_node_lines.insert(1, "name: \"" + name + "\"\n")
+                    processed_file.writelines(curr_node_lines)
+                curr_node_lines = []
+                dcid = ""
+                i_code = ""
+                continue
+            if line.startswith("Node"):
+                dcid = line[11:].strip()
+            if line.startswith("measuredProperty"):
+                # measuredProperty will be who/<i-code>
+                i_code = line.split("/")[1].strip()
+    with open(os.path.join(output_dir, "missing_i_code_names.json"), "w+") as f:
+        f.write(json.dumps(list(missing_i_code_names)))
 
 
 def main(_):
-    generate_indicator_map_using_properties_mcf()
-    generate_duplicate_indicator_names()
-    generate_mcf_with_processed_names()
+    generate_mcf_with_processed_names(FLAGS.who_properties_file,
+                                      FLAGS.who_sv_file,
+                                      "name_replacements.json",
+                                      FLAGS.output_dir)
 
 
 if __name__ == '__main__':
