b5953795e302ddb5a5b62078df9c5d5364d87e40
Sinan Yumurtacı <snny@google.com>
2022-05-23 15:35:46 -0700

    first rough implementation of generating TMCF for StatVarObservations

--- scripts/fema/nri/generate_statvars.py
+++ scripts/fema/nri/generate_statvars.py
@@ -39,7 +39,7 @@ IGNORED_FIELDS = [
 COMPOSITE_ROW_LAYERS = ["National Risk Index", "Expected Annual Loss", "Social Vulnerability", "Community Resilience"]
 
 COMPOSITE_MCF_FORMAT = """
-Node: dcid:{mProp}_{mQual}_NaturalHazardImpact
+Node: dcid:{nodeDCID}
 typeOf: dcs:StatisticalVariable
 populationType: dcs:NaturalHazardImpact
 measuredProperty: dcs:{mProp}
@@ -59,6 +59,27 @@ def get_nth_dash_from_field(row, i):
 def drop_spaces(string):
 	return string.replace(" ", "")
 
+def tmcf_from_row(row, statVarDCID):
+	"""
+	Given a row of NRIDataDictionary describing a measure, generates the TMCF for that StatVar.
+	Returns the TMCF as a string.
+	"""
+
+	# the "Sort" field is an unique, monotonically increasing ID.
+	# 	there will be gaps in this ID, which should be OK? 
+	# as of 2022-05-23, the "Version" field for all data is "November 2021"
+	# "Field Name" in the data dictionary holds the name of the column in the data CSV
+	TMCF = f"""
+Node: E:FEMA_NRI->E{row["Sort"]}
+typeOf: dcs:StatVarObservation
+variableMeasured: dcs:{statVarDCID}
+observationAbout: C:FEMA_NRI_Counties->DCID_GeoID
+observationDate: "{row["Version"]}"
+value: C:FEMA_NRI_Counties->{row["Field Name"]} 
+"""
+
+	return TMCF
+
 def statvar_from_row(row):
 	if is_composite_row(row):
 		return statvar_from_composite_row(row)
@@ -72,9 +93,11 @@ def statvar_from_composite_row(row):
 	measuredProperty = drop_spaces(get_nth_dash_from_field(row, 0))
 	measurementQualifier = drop_spaces(get_nth_dash_from_field(row, 1))
 
-	formatted = COMPOSITE_MCF_FORMAT.format(mProp=measuredProperty, mQual = measurementQualifier)
+	dcid = f"{measuredProperty}_{measurementQualifier}_NaturalHazardImpact"
 
-	return formatted
+	formatted = COMPOSITE_MCF_FORMAT.format(nodeDCID = dcid, mProp=measuredProperty, mQual = measurementQualifier)
+
+	return formatted, dcid
 
 def statvar_from_individual_hazard_row(row):
 	"""
@@ -131,7 +154,7 @@ def statvar_from_individual_hazard_row(row):
 	if "Expected Annual Loss" in measuredProperty:
 		formatted += f"unit: USDollar\n"
 
-	return formatted
+	return formatted, dcid
 
 
 dd = pd.read_csv("source_data/NRIDataDictionary.csv")
@@ -140,12 +163,22 @@ logging.info(f"[info] ignoring {len(IGNORED_FIELDS)} fields in NRIDataDictionary
 
 dd = dd[~dd["Field Name"].isin(IGNORED_FIELDS)]
 
-out = ""
+schema_out = ""
+tmcf_out = ""
 for _, row in dd.iterrows():
-	statvar_mcf = statvar_from_row(row)
-	out += statvar_mcf
+	statvar_mcf, statvar_dcid = statvar_from_row(row)
+	statobs_tmcf = tmcf_from_row(row, statvar_dcid)
+	
+	schema_out += statvar_mcf
+	tmcf_out += statobs_tmcf
 
-outfile_filename = "fema_nri_schema.mcf"
-with open(outfile_filename, "w") as outfile:
-	logging.info(f"Writing StatVar MCF to {outfile_filename}")
-	outfile.write(out)
+schema_outfile_filename = "fema_nri_schema.mcf"
+tmcf_outfile_filename = "fema_nri_counties.tmcf"
+
+with open(schema_outfile_filename, "w") as outfile:
+	logging.info(f"Writing StatVar MCF to {schema_outfile_filename}")
+	outfile.write(schema_out)
+
+with open(tmcf_outfile_filename, "w") as outfile:
+	logging.info(f"Writing County TMCF to {tmcf_outfile_filename}")
+	outfile.write(tmcf_out)
