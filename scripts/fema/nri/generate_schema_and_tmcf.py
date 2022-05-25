fce38df6f53131b2a94340b2142bc66e25977f44
Sinan Yumurtacı <snny@google.com>
2022-05-25 15:56:27 -0700

    refactor the schema generation code to be cleaner

--- scripts/fema/nri/generate_schema_and_tmcf.py
+++ scripts/fema/nri/generate_schema_and_tmcf.py
@@ -74,212 +74,259 @@ DATACOMMONS_ALIASES = {
 
 
 def apply_datacommon_alias(string):
-  """
+    """
 	Given a string, returns the defined alias for the Data Commons import.
 	If no alias is found, returns the string as is.
 	"""
-  string = string.strip()
-  if string in DATACOMMONS_ALIASES:
-    return DATACOMMONS_ALIASES[string]
-  else:
-    print(f"could not find alias for {string.replace(' ', '!')}")
-    return string
+    string = string.strip()
+    if string in DATACOMMONS_ALIASES:
+        return DATACOMMONS_ALIASES[string]
+    else:
+        logging.info(f"could not find alias for {string.replace(' ', '!')}")
+        return string
 
 
 def get_nth_dash_from_field_alias(row, i):
-  """
+    """
 	Given a row containing a list of words separated with dashes surrounded by
         spaces  in the "Field Alias" column and an index i, finds the i-th
         string between those dashes.
 	Returns the string without the spaces associated with the neighboring dashes.
 	"""
-  return row["Field Alias"].split(" - ")[i]
+    return row["Field Alias"].split(" - ")[i]
 
 
 def drop_spaces(string):
-  """
+    """
 	Given a string, removes the space characters contained in that string.
 	Returns a new string without the spaces.
 	"""
-  return string.replace(" ", "")
+    return string.replace(" ", "")
 
 
 def tmcf_from_row(row, index, statvar_dcid):
-  """
+    """
 	Given a row of NRIDataDictionary describing a measure, generates the TMCF
     for that StatVar.
 	Returns the TMCF as a string.
 	"""
 
-  # as of 2022-05-23, the "Version" field for all data is "November 2021"
-  # "Field Name" in the data dictionary holds the name of the column in the data CSV
-  return TMCF_FORMAT.format(
-      index=index,
-      statvar_dcid=statvar_dcid,
-      obs_date=row["Version"],
-      field_name=row["Field Name"])
+    # as of 2022-05-23, the "Version" field for all data is "November 2021"
+    # "Field Name" in the data dictionary holds the name of the column in the data CSV
+    return TMCF_FORMAT.format(
+        index=index,
+        statvar_dcid=statvar_dcid,
+        obs_date=row["Version"],
+        field_name=row["Field Name"])
 
 
-def statvar_from_row(row):
-  """
+def schema_and_dcid_from_row(row):
+    """
 	Given a row of NRIDataDictionary, computes the corresponding StatVar Schema.
 	Returns the StatVar MCF node as a string.
 	"""
 
-  if is_composite_row(row):
-    return statvar_from_composite_row(row)
-  else:
-    return statvar_from_individual_hazard_row(row)
+    if is_composite_row(row):
+        properties = extract_properties_from_composite_row(row)
+        schema, statvar_dcid = format_composite_field_properties_to_schema(
+            properties)
+
+    else:
+        properties = extract_properties_from_ind_hazard_row(row)
+        schema, statvar_dcid = format_ind_hazard_field_properties_to_schema(
+            properties)
+
+    return schema, statvar_dcid
 
 
 def is_composite_row(row):
-  """
+    """
 	Given a row of NRIDataDictionary, computes whether that row is a measure of 
         all hazards in aggregate (composite).
 	Returns boolean True if so, False otherwise.
 	"""
-  return row["Relevant Layer"] in COMPOSITE_ROW_LAYERS
+    return row["Relevant Layer"] in COMPOSITE_ROW_LAYERS
 
 
-def statvar_from_composite_row(row):
-  """
-	Helper function for statvar_from_row().
-	Processes rows that represent data at the composite level (all hazards
-    combined)
-	"""
-  measuredProperty = apply_datacommon_alias(
-      drop_spaces(get_nth_dash_from_field_alias(row, 0)))
-  unit = apply_datacommon_alias(
-      drop_spaces(get_nth_dash_from_field_alias(row, 1)))
+def extract_properties_from_composite_row(row):
+    """
+    Given a row of the data dictionary representing a composite field,
+    extracts properties relevant to the schema and TMCF.
 
-  if measuredProperty == "ExpectedLoss":
-    dcid = f"Annual_{measuredProperty}_NaturalHazardImpact"
-  else:
-    dcid = f"{measuredProperty}_NaturalHazardImpact"
+    Returns a dictionary with the following keys:
+      - measured_property
+      - unit
+    """
+    measured_property = apply_datacommon_alias(
+        drop_spaces(get_nth_dash_from_field_alias(row, 0)))
+    unit = apply_datacommon_alias(
+        drop_spaces(get_nth_dash_from_field_alias(row, 1)))
 
-  formatted = COMPOSITE_MCF_FORMAT.format(
-      node_dcid=dcid, m_prop=measuredProperty)
-
-  if measuredProperty == "ExpectedLoss":
-    formatted += "measurementQualifier: Annual\n"
-
-  return formatted, dcid
+    return {"measured_property": measured_property, "unit": unit}
 
 
-def statvar_from_individual_hazard_row(row):
-  """
-	Helper function for statvar_from_row().
-	Processes rows that represent data about individual hazard types.
-	"""
+def format_composite_field_properties_to_schema(properties):
+    """
+    Given properties of a composite field, formats a valid Schema for the
+    StatisticalVariable representing that field.
+    
+    Returns the tuple:
+        (Schema MCF in string format, the dcid of the StatVar)
+    """
+    measured_property = properties["measured_property"]
 
-  hazardType = drop_spaces(get_nth_dash_from_field_alias(row, 0)) + "Event"
-  measuredProperty = apply_datacommon_alias(
-      get_nth_dash_from_field_alias(row, 1))
-
-  unit = ""
-  # we want to cut off score/rating from measuredProperty and make it a unit
-  if "Score" in measuredProperty or "Rating" in measuredProperty:
-    unit = measuredProperty.split(' ')[-1]
-    measuredProperty = apply_datacommon_alias(measuredProperty[:-len(unit)])
-    unit = apply_datacommon_alias(unit)
-
-  measurementQualifier = ""
-  if measuredProperty == "ExpectedLoss":
-    measurementQualifier = "Annual"
-
-  measuredProperty = drop_spaces(measuredProperty)
-
-  impactedThing = ""
-  if row["Field Alias"].count("-") > 1:
-    impactedThing = drop_spaces(get_nth_dash_from_field_alias(row, 2))
-    if impactedThing == "Total":
-      impactedThing = ""
-
-  statType = ""
-  if "Population" in impactedThing:
-    if impactedThing == "PopulationEquivalence":
-      statType = "ValueOfStatisticalLifeEquivalent"
+    if measured_property == "ExpectedLoss":
+        dcid = f"Annual_{measured_property}_NaturalHazardImpact"
     else:
-      statType = "Count"
+        dcid = f"{measured_property}_NaturalHazardImpact"
 
-    impactedThing = "People"
+    formatted = COMPOSITE_MCF_FORMAT.format(
+        node_dcid=dcid, m_prop=measured_property)
 
-  # create a list of names that might go on the dcid
-  dcid_list = [
-      measurementQualifier, statType, measuredProperty, hazardType,
-      impactedThing
-  ]
+    if measured_property == "ExpectedLoss":
+        formatted += "measurementQualifier: Annual\n"
 
-  # drop empty dcid_list elements
-  dcid_list = [element for element in dcid_list if element]
+    return formatted, dcid
 
-  # join the rest with underscores to obtain the final dcid
-  dcid = "_".join(dcid_list)
 
-  formatted = HAZARD_MCF_FORMAT_BASE.format(
-      node_dcid=dcid, haz_type=hazardType, m_prop=drop_spaces(measuredProperty))
+def extract_properties_from_ind_hazard_row(row):
+    """
+    Given a row of the data dictionary representing a individual hazard field,
+    extracts properties relevant to the schema and TMCF.
 
-  if statType:
-    formatted += f"statType: {statType}\n"
+    Returns a dictionary with the following keys:
+      - hazard_type
+      - measured_property
+      - measurement_qualifier
+      - impacted_thing
+      - unit
+    """
+    hazard_type = drop_spaces(get_nth_dash_from_field_alias(row, 0)) + "Event"
+    measured_property = apply_datacommon_alias(
+        get_nth_dash_from_field_alias(row, 1))
 
-  if impactedThing:
-    formatted += f"impactedThing: {impactedThing}\n"
+    unit = ""
+    # we want to cut off score/rating from measured_property and make it a unit
+    if "Score" in measured_property or "Rating" in measured_property:
+        unit = measured_property.split(' ')[-1]
+        measured_property = apply_datacommon_alias(
+            measured_property[:-len(unit)])
+        unit = apply_datacommon_alias(unit)
 
-  if measurementQualifier:
-    formatted += f"measurementQualifier: {measurementQualifier}\n"
+    measurement_qualifier = ""
+    if measured_property == "ExpectedLoss":
+        measurement_qualifier = "Annual"
 
-  return formatted, dcid
+    measured_property = drop_spaces(measured_property)
+
+    impacted_thing = ""
+    if row["Field Alias"].count("-") > 1:
+        impacted_thing = drop_spaces(get_nth_dash_from_field_alias(row, 2))
+        if impacted_thing == "Total":
+            impacted_thing = ""
+
+    # impacted_thing might be "population" or "population equivalence"
+    if "Population" in impacted_thing:
+        impacted_thing = "People"
+
+    return {
+        "hazard_type": hazard_type,
+        "measured_property": measured_property,
+        "measurement_qualifier": measurement_qualifier,
+        "impacted_thing": impacted_thing,
+        "unit": unit
+    }
+
+
+def format_ind_hazard_field_properties_to_schema(properties):
+    """
+    Given properties of a individual hazard field, formats a valid Schema for
+    the StatisticalVariable representing that field.
+
+    Returns the tuple:
+        (Schema MCF in string format, the dcid of the StatVar)
+    """
+    hazard_type = properties["hazard_type"]
+    measured_property = properties["measured_property"]
+    measurement_qualifier = properties["measurement_qualifier"]
+    impacted_thing = properties["impacted_thing"]
+    unit = properties["unit"]
+
+    # create a list of names that might go on the dcid
+    dcid_list = [
+        measurement_qualifier, measured_property, "NaturalHazardImpact",
+        hazard_type, impacted_thing
+    ]
+
+    # drop empty dcid_list elements
+    dcid_list = [element for element in dcid_list if element]
+
+    # join the rest with underscores to obtain the final dcid
+    dcid = "_".join(dcid_list)
+
+    formatted = HAZARD_MCF_FORMAT_BASE.format(
+        node_dcid=dcid,
+        haz_type=hazard_type,
+        m_prop=drop_spaces(measured_property))
+
+    if impacted_thing:
+        formatted += f"impactedThing: {impacted_thing}\n"
+
+    if measurement_qualifier:
+        formatted += f"measurementQualifier: {measurement_qualifier}\n"
+
+    return formatted, dcid
 
 
 if __name__ == "__main__":
-  # if field alias includes any of these strings, we skip that row from the
-  # schema and TMCF generation
-  field_alias_strings_to_skip = []
+    # if field alias includes any of these strings, we skip that row from the
+    # schema and TMCF generation
+    field_alias_strings_to_skip = []
 
-  if FLAG_SKIP_EAL_COMPONENTS:
-    field_alias_strings_to_skip.extend(EAL_COMPONENTS)
-  if FLAG_SKIP_IMPACTED_THING_COMPONENTS:
-    field_alias_strings_to_skip.extend(IMPACTED_THING_COMPONENTS)
-  if FLAG_SKIP_NON_SCORE_RELATIVE_MEASURES:
-    field_alias_strings_to_skip.extend(NON_SCORE_RELATIVE_MEASURES)
+    if FLAG_SKIP_EAL_COMPONENTS:
+        field_alias_strings_to_skip.extend(EAL_COMPONENTS)
+    if FLAG_SKIP_IMPACTED_THING_COMPONENTS:
+        field_alias_strings_to_skip.extend(IMPACTED_THING_COMPONENTS)
+    if FLAG_SKIP_NON_SCORE_RELATIVE_MEASURES:
+        field_alias_strings_to_skip.extend(NON_SCORE_RELATIVE_MEASURES)
 
-  # load the dataset and drop the ignored fields
-  dd = pd.read_csv(NRI_DATADICTIONARY_INFILE_FILENAME)
+    # load the dataset and drop the ignored fields
+    dd = pd.read_csv(NRI_DATADICTIONARY_INFILE_FILENAME)
 
-  logging.info(
-      f"[info] ignoring {len(IGNORED_FIELDS)} fields in NRIDataDictionary")
-  dd = dd[~dd["Field Name"].isin(IGNORED_FIELDS)]
-  dd = dd.reset_index()
+    logging.info(
+        f"[info] ignoring {len(IGNORED_FIELDS)} fields in NRIDataDictionary")
+    dd = dd[~dd["Field Name"].isin(IGNORED_FIELDS)]
+    dd = dd.reset_index()
 
-  schema_out = ""
-  tmcf_out = ""
+    schema_out = ""
+    tmcf_out = ""
 
-  for index, row in dd.iterrows():
-    skipped = False
+    for index, row in dd.iterrows():
+        skipped = False
 
-    should_skip_component = any([
-        eal_comp in row["Field Alias"]
-        for eal_comp in field_alias_strings_to_skip
-    ])
-    if should_skip_component:
-      logging.info(
-          (f"Skipping individual hazard row {row['Field Alias']}"
-           " because it is an EAL component and FLAG_SKIP_EAL_COMPONENTS is "
-           f"{FLAG_SKIP_EAL_COMPONENTS}"))
-      skipped = True
+        should_skip_component = any([
+            eal_comp in row["Field Alias"]
+            for eal_comp in field_alias_strings_to_skip
+        ])
+        if should_skip_component:
+            logging.info((
+                f"Skipping individual hazard row {row['Field Alias']}"
+                " because it is an EAL component and FLAG_SKIP_EAL_COMPONENTS is "
+                f"{FLAG_SKIP_EAL_COMPONENTS}"))
+            skipped = True
 
-    if not skipped:
-      statvar_mcf, statvar_dcid = statvar_from_row(row)
-      statobs_tmcf = tmcf_from_row(row, index, statvar_dcid)
+        if not skipped:
+            statvar_mcf, statvar_dcid = schema_and_dcid_from_row(row)
+            statobs_tmcf = tmcf_from_row(row, index, statvar_dcid)
 
-      schema_out += statvar_mcf + "\n"
-      tmcf_out += statobs_tmcf
+            schema_out += statvar_mcf + "\n"
+            tmcf_out += statobs_tmcf
 
-  # write out the results
-  with open(SCHEMA_OUTFILE_FILENAME, "w") as outfile:
-    logging.info(f"Writing StatVar MCF to {SCHEMA_OUTFILE_FILENAME}")
-    outfile.write(schema_out)
+    # write out the results
+    with open(SCHEMA_OUTFILE_FILENAME, "w") as outfile:
+        logging.info(f"Writing StatVar MCF to {SCHEMA_OUTFILE_FILENAME}")
+        outfile.write(schema_out)
 
-  with open(TMCF_OUTFILE_FILENAME, "w") as outfile:
-    logging.info(f"Writing County TMCF to {TMCF_OUTFILE_FILENAME}")
-    outfile.write(tmcf_out)
+    with open(TMCF_OUTFILE_FILENAME, "w") as outfile:
+        logging.info(f"Writing County TMCF to {TMCF_OUTFILE_FILENAME}")
+        outfile.write(tmcf_out)
