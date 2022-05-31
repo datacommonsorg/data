f5896a7829e51644ba869943f7cb87679232e503
Sinan Yumurtacı <snny@google.com>
2022-05-31 18:51:20 +0000

    add tract level data (along with the existing county data)

--- scripts/fema/nri/process_data.py
+++ scripts/fema/nri/process_data.py
@@ -1,27 +1,50 @@
 import pandas as pd
 import numpy as np
 
-INPUT_DATA_PATH = "source_data/NRI_Table_Counties.csv"
-OUTPUT_DATA_PATH = "output/nri_counties_table.csv"
+INPUT_TO_OUTPUT_PATHS = {
+    "source_data/NRI_Table_Counties.csv":"output/nri_counties_table.csv",
+    "source_data/NRI_Table_CensusTracts.csv":"output/nri_tracts_table.csv",
+}
+def fips_to_geoid(row):
+    """
+    Given a row of CSV data from the FEMA NRI source table, calculated the GeoID of the place
+    in the DC format, which includes trailing and leading zeros.
 
-data_table = pd.read_csv(INPUT_DATA_PATH)
+    Handles the distinction between rows for counties and tracts.
 
-# we zfill to 5, because in a State-County FIPS, state takes up to
-# 2 digis and the county 3 (for a total of 5), however, in the FEMA
-# dataset, the trailing zero for States with State FIPS < 10 is ommitted
-# for example;
-# California State FIPS is 6
-# Alameda County, CA FIPS is 1.
-# The correct GeoId would be 06001
-# However, the FEMA study puts this down at 6001 in the STCOFIPS field
-fips_to_geoid = lambda row: "geoId/" + str(row["STCOFIPS"]).zfill(5) 
+    Returns the geoID as a string, with the "geoId/" prefix.
+    """
+    # (for counties)we zfill to 5, because in a State-County FIPS, state takes
+    # up to 2 digis and the county 3 (for a total of 5), however, in the FEMA
+    # dataset, the trailing zero for States with State FIPS < 10 is ommitted
+    # for example:
 
-# the TMCF generated in generate_schema_and_tmcf.py expect to find the 
-# geoID in the field "DCID_GeoID"
-data_table["DCID_GeoID"] = data_table.apply(fips_to_geoid, axis = 1)
+    # California State FIPS is 6
+    # Alameda County, CA FIPS is 1.
+    # The correct GeoId would be 06001
+    # However, the FEMA study puts this down at 6001 in the STCOFIPS field
 
-# we want to replace empty cells with 0s so that the import tool does not
-# have to assume what this is about [citation needed (snny)]
-data_table = data_table.fillna(0)
+    # tract FIPS are of length 11 
 
-data_table.to_csv(OUTPUT_DATA_PATH)
+    if "TRACTFIPS" in row:
+        field = "TRACTFIPS"
+        length = 11
+    else: # this is a county row
+        field = "STCOFIPS"
+        length = 5
+
+    return "geoId/" + str(row[field]).zfill(11)
+
+for input_path in INPUT_TO_OUTPUT_PATHS:
+    data_table = pd.read_csv(input_path)
+    output_path = INPUT_TO_OUTPUT_PATHS[input_path]
+
+    # the TMCF generated in generate_schema_and_tmcf.py expect to find the 
+    # geoID in the field "DCID_GeoID"
+    data_table["DCID_GeoID"] = data_table.apply(fips_to_geoid, axis = 1)
+
+    # we want to replace empty cells with 0s so that the import tool does not
+    # have to assume what this is about [citation needed (snny)]
+    data_table = data_table.fillna(0)
+
+    data_table.to_csv(output_path)
