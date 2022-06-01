2de5155f6b58575b2307d2e5b24b7c17389f18c2
Sinan Yumurtacı <snny@google.com>
2022-06-01 20:12:02 +0000

    complete process_data_test.py
    - moved the testing of a function with input file through the testing
    of its output to a golden file to a function
    - added tests for processing the tract data using that

--- scripts/fema/nri/process_data_test.py
+++ scripts/fema/nri/process_data_test.py
@@ -28,95 +28,44 @@ class FormatGeoIDTest(unittest.TestCase):
             process_data.fips_to_geoid({"TRACTFIPS": "11001001002"}),
             "geoId/11001001002")
 
+def check_function_on_file_gives_golden(fn, inp_f, golden_f):
+    """
+    Given a function fn that takes an input file location inp_f and an output
+    location, calls the function with the input file path string and a temporary
+    output location.
+    
+    Checks if the output file is equivalent to the golden_f passed in
+
+    Returns True if the check passes, False otherwise.
+    """
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        test_csv_file = os.path.join(module_dir_, inp_f)
+        result_csv_file = os.path.join(tmp_dir, 'temp_test_output.tmp')
+        expected_csv_file = os.path.join(module_dir_,golden_f)
+        process_data.process_csv(test_csv_file, result_csv_file)
+
+        with open(result_csv_file, "r") as result_f:
+            result_str: str = result_f.read()
+            with open(expected_csv_file, "r") as expect_f:
+                expect_str: str = expect_f.read()
+                return result_str == expect_str
+
 class ProcessFemaNriFileTest(unittest.TestCase):
     def test_process_county_file(self):
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            test_csv_file = os.path.join(module_dir_,
-                                         'test_data/test_data_county.csv')
-            result_csv_file = os.path.join(tmp_dir, 'output/nri_counties_table.csv')
-            expected_csv_file = os.path.join(
-                module_dir_, 'test_data/expected_data_county.csv')
-            process_data.process_csv(test_csv_file, result_csv_file)
+        assertion = check_function_on_file_gives_golden(
+                fn = process_data.process_csv,
+                inp_f = 'test_data/test_data_county.csv',
+                golden_f = 'test_data/expected_data_county.csv' 
+                )
+        self.assertTrue(assertion)
 
-            with open(result_csv_file, "r") as result_f:
-                result_str: str = result_f.read()
-                with open(expected_csv_file, "r") as expect_f:
-                    expect_str: str = expect_f.read()
-                    self.assertEqual(result_str, expect_str)
-
-#     def test_clean_crime_file(self):
-#         with tempfile.TemporaryDirectory() as tmp_dir:
-#             test_csv_file = os.path.join(module_dir_,
-#                                          'test_data/test_data_basic.csv')
-#             result_csv_file = os.path.join(tmp_dir, 'city_crime.csv')
-#             expected_csv_file = os.path.join(
-#                 module_dir_, 'test_data/expected_city_crime.csv')
-#             create_formatted_csv_file([test_csv_file], result_csv_file)
-#
-#             with open(result_csv_file, "r") as result_f:
-#                 result_str: str = result_f.read()
-#                 with open(expected_csv_file, "r") as expect_f:
-#                     expect_str: str = expect_f.read()
-#                     self.assertEqual(result_str, expect_str)
-#
-#             # os.remove(result_csv_file)
-#
-#     def test_calculate_crimes(self):
-#         crime = {
-#             'Year': 2019,
-#             'State': 'WISCONSIN',
-#             'City': 'New Berlin',
-#             'Population': '39752.0',
-#             'Violent': '20.0',
-#             'ViolentMurderAndNonNegligentManslaughter': '1.0',
-#             'ViolentRape': '6.0',
-#             'Rape2': '0',
-#             'ViolentRobbery': '3.0',
-#             'ViolentAggravatedAssault': '10.0',
-#             'Property': '452.0',
-#             'PropertyBurglary': '32.0',
-#             'PropertyLarcenyTheft': '407.0',
-#             'PropertyMotorVehicleTheft': '13.0',
-#             'PropertyArson': '0.0',
-#             'Geocode': '5556375'
-#         }
-#         calculate_crimes(crime)
-#         self.assertEqual(
-#             crime, {
-#                 'Year': 2019,
-#                 'State': 'WISCONSIN',
-#                 'City': 'New Berlin',
-#                 'Population': '39752.0',
-#                 'Violent': 20,
-#                 'ViolentMurderAndNonNegligentManslaughter': 1,
-#                 'ViolentRape': 6,
-#                 'Rape2': 0,
-#                 'ViolentRobbery': 3,
-#                 'ViolentAggravatedAssault': 10,
-#                 'Property': 452,
-#                 'PropertyBurglary': 32,
-#                 'PropertyLarcenyTheft': 407,
-#                 'PropertyMotorVehicleTheft': 13,
-#                 'PropertyArson': 0,
-#                 'Total': 472,
-#                 'Geocode': '5556375'
-#             })
-#
-#     def test_create_tmcf(self):
-#         with tempfile.TemporaryDirectory() as tmp_dir:
-#             expected_tmcf_file = os.path.join(
-#                 module_dir_, 'test_data/expected_fbi_crime.tmcf')
-#             result_tmcf_file = os.path.join(tmp_dir, 'FBI_crime.tmcf')
-#
-#             create_tmcf_file(result_tmcf_file)
-#
-#             with open(result_tmcf_file, "r") as result_f:
-#                 result_str: str = result_f.read()
-#                 with open(expected_tmcf_file, "r") as expect_f:
-#                     expect_str: str = expect_f.read()
-#                     self.assertEqual(result_str, expect_str)
-#
-#             os.remove(result_tmcf_file)
+    def test_process_tract_file(self):
+        assertion = check_function_on_file_gives_golden(
+                fn = process_data.process_csv,
+                inp_f = 'test_data/test_data_tract.csv',
+                golden_f = 'test_data/expected_data_tract.csv' 
+                )
+        self.assertTrue(assertion)
 
 if __name__ == '__main__':
     unittest.main()
