5a9ded1e233fc395fefbfa300142feb7b3dbac5a
Billy Babis <billybabis@gmail.com>
2022-05-31 20:38:25 -0400

    add tested report.json, other minor edits. Committing before trying git lfs migrate per documentation

--- scripts/rff/preprocess_csv_test.py
+++ scripts/rff/preprocess_csv_test.py
@@ -1,20 +1,27 @@
 """Tests for preprocess_csv.py"""
 
-import os, sys, unittest
+import os
+import sys
+import tempfile
+import unittest
 
 # Allows the following module imports to work when running as a script
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from rff import preprocess_csv
 
+
 class ProcessTest(unittest.TestCase):
+
     def test_preprocess(self):
-        src_fldr = f"test_data/input/prism/daily/county"
-        output_csv = f"test_data/output/WeatherVariability_Counties.csv"
-        preprocess_csv.main(src_fldr, output_csv)
-        expected_csv = output_csv.replace("output", "expected")
-        with open(expected_csv) as wantf:
-            with open(output_csv) as gotf:
-                self.assertEqual(gotf.read(), wantf.read())
+        src_fldr = "test_data/input/prism/daily/county"
+        expected_csv = "test_data/expected/WeatherVariability_Counties.csv"
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            output_csv = f"{tmp_dir}/WeatherVariability_Counties.csv"
+            preprocess_csv.main(src_fldr, output_csv)
+            with open(expected_csv, 'rb') as wantf:
+                with open(output_csv, 'rb') as gotf:
+                    self.assertEqual(gotf.read(), wantf.read())
+
 
 if __name__ == '__main__':
     unittest.main()
