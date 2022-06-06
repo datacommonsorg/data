2a988e09a8fcf112ccf604d514991383ff9de7bd
Samantha Piekos <spiekos@google.com>
2022-06-06 16:11:57 -0700

    Create download.sh

---
+++ scripts/biomedical/mesh/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p scratch; cd scratch
+curl -o mesh.xml https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/?_gl=1*1d16qfo*_ga*Nzc3NTczNDAwLjE2NTAzMjE2MzA.*_ga_7147EPK006*MTY1NDU1NjkzMS4yLjAuMTY1NDU1NjkzMS4w*_ga_P1FPTH9PL4*MTY1NDU1NjkzMS4yLjAuMTY1NDU1NjkzMS4w
