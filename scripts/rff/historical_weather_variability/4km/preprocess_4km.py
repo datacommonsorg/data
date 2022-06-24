3d21a134e5d218e80fd144bad8fdbd34642ebb05
Prashanth R <shanth@google.com>
2022-06-24 08:12:20 -0700

    Fix lint error

--- scripts/rff/historical_weather_variability/4km/preprocess_4km.py
+++ scripts/rff/historical_weather_variability/4km/preprocess_4km.py
@@ -83,7 +83,7 @@ def main(src_fldr, output_csv):
                 ##  are stored in individual geotiff files (i.e. 2021.tif)
                 for interval_gtif in glob.glob(f"{path}/*.tif"):
                     gtiff = gdal.Open(interval_gtif)
-                    ## Each geotiff consists of 3-5 bands corresponding with the 
+                    ## Each geotiff consists of 3-5 bands corresponding with the
                     ## statistics computed for the given climate variable
                     for bandnum in range(1, gtiff.RasterCount + 1):
                         band = gtiff.GetRasterBand(bandnum)
