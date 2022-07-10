8d915d2818a4fbe2c60d805631f906bc93b19356
Billy Babis <billybabis@gmail.com>
2022-07-10 10:40:07 -0400

    run yapf to format

--- scripts/rff/projected_weather_variability/preprocess_forecast.py
+++ scripts/rff/projected_weather_variability/preprocess_forecast.py
@@ -21,9 +21,7 @@ if __name__ == '__main__':
             'rff/raw_data/gcm/NorESM1-M/daily/025deg/agg_year/pr/stats/2021.tif'
         )
         preprocess_raster.create_grids("0.25deg", grids_csv, sample_gtif)
-    src_folder = os.path.join(
-        SCRIPTS_DIR,
-        "rff/raw_data/gcm/NorESM1-M/daily/025deg"
-    )
+    src_folder = os.path.join(SCRIPTS_DIR,
+                              "rff/raw_data/gcm/NorESM1-M/daily/025deg")
     output_csv_fname = f"{CURR_DIR}/WeatherVariability_Forecast.csv"
     main(src_folder, output_csv_fname)
