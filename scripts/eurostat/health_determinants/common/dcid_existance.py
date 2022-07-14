913a52f79d46bd1ef0c75fbc9ec6fb5caf644933
Chandrashekar Harish <chharish@google.com>
2022-07-14 11:24:19 +0530

    new file:   scripts/eurostat/health_determinants/common/dcid_existance.py

---
+++ scripts/eurostat/health_determinants/common/dcid_existance.py
@@ -0,0 +1,30 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datacommons
+
+
+def check_dcid_existance(nodes):
+    nodes_response = datacommons.get_property_values(
+        nodes,
+        "typeOf",
+        out=True,
+        value_type=None,
+        limit=datacommons.utils._MAX_LIMIT)
+    node_status = {}
+    for node, value in nodes_response.items():
+        if value == []:
+            node_status[node] = False
+        else:
+            node_status[node] = True
+    return node_status
