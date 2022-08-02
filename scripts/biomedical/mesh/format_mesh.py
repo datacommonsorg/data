94be1f0cff023af2dd435da35a046d50750e9ee1
Suhana Bedi <suhanabedi@google.com>
2022-08-01 22:43:09 -0500

    style: add comments and text-value quotes

--- scripts/biomedical/mesh/format_mesh.py
+++ scripts/biomedical/mesh/format_mesh.py
@@ -36,6 +36,7 @@ def format_mesh_xml(mesh_xml):
         df1 = pandas df after parsing
     """
     document = parse(mesh_xml)
+    ## column names for parsed xml tags 
     dfcols = [
         'DescriptorID', 'DescriptorName', 'DateCreated-Year',
         'DateCreated-Month', 'DateCreated-Day', 'DateRevised-Year',
@@ -46,9 +47,12 @@ def format_mesh_xml(mesh_xml):
     ]
     df = pd.DataFrame(columns=dfcols)
     for item in document.iterfind('DescriptorRecord'):
+        ## parses the Descriptor ID
         d1 = item.findtext('DescriptorUI')
+        ## parses the Descriptor Name
         elem = item.find(".//DescriptorName")
         d1_name = elem.findtext("String")
+        ## parses the Date of Creation 
         date_created = item.find(".//DateCreated")
         if not date_created:
             d1_created_year = np.nan
@@ -58,6 +62,7 @@ def format_mesh_xml(mesh_xml):
             d1_created_year = date_created.findtext("Year")
             d1_created_month = date_created.findtext("Month")
             d1_created_day = date_created.findtext("Day")
+        ## parses the Date of Revision
         date_revised = item.find(".//DateRevised")
         if not date_revised:
             d1_revised_year = np.nan
@@ -67,6 +72,7 @@ def format_mesh_xml(mesh_xml):
             d1_revised_year = date_revised.findtext("Year")
             d1_revised_month = date_revised.findtext("Month")
             d1_revised_day = date_revised.findtext("Day")
+        ## parses the Date of Establishment
         date_established = item.find(".//DateEstablished")
         if not date_established:
             d1_established_year = np.nan
@@ -82,7 +88,9 @@ def format_mesh_xml(mesh_xml):
         else:
             tree_num = []
             for i in range(len(tree_list)):
+                ## parses the Tree Number
                 tree_num.append(tree_list.findtext("TreeNumber"))
+        ## parses the NLM Classification Number
         nlm_num = item.findtext("NLMClassificationNumber")
         if not nlm_num:
             nlm_num = np.nan
@@ -98,9 +106,12 @@ def format_mesh_xml(mesh_xml):
             l1 = quantifier_list.findall(".//AllowableQualifier")
             for i in range(len(l1)):
                 l2 = l1[i].find(".//QualifierReferredTo")
+                ## parses the Qualifier ID
                 qualID.append(l2.findtext("QualifierUI"))
+                ## parses the Qualifier Name
                 l3 = l2.find(".//QualifierName")
                 qual_name.append(l3.findtext("String"))
+                ## parses the Qualifier Abbreviation 
                 qual_abbr.append(l1[i].findtext("Abbreviation"))
 
         concept_list = item.find(".//ConceptList")
@@ -118,8 +129,11 @@ def format_mesh_xml(mesh_xml):
             termUI = []
             termName = []
             for i in range(len(c1)):
+                ## parses the Concept ID 
                 conceptID.append(c1[i].findtext("ConceptUI"))
+                ## parses the Scope Note
                 scopeNote.append(c1[i].findtext("ScopeNote"))
+                ## parses the Concept Name
                 c2 = c1[i].find(".//ConceptName")
                 conceptName.append(c2.findtext("String"))
                 c3 = c1[i].find(".//TermList")
@@ -127,6 +141,7 @@ def format_mesh_xml(mesh_xml):
                 subtermUI = []
                 subtermName = []
                 for j in range(len(c4)):
+                    ## parses the Term ID 
                     subtermUI.append(c4[j].findtext("TermUI"))
                     subtermName.append(c4[j].findtext("String"))
                 termUI.append(subtermUI)
@@ -160,6 +175,7 @@ def date_modify(df1):
     df1['DateEstablished'] = df1['DateEstablished-Year'].astype(
         str) + "-" + df1['DateEstablished-Month'].astype(
             str) + "-" + df1['DateEstablished-Day'].astype(str)
+    ## drop repetitive column values
     df1 = df1.drop(columns=[
         'DateCreated-Year', 'DateCreated-Month', 'DateCreated-Day',
         'DateRevised-Year', 'DateRevised-Month', 'DateRevised-Day',
@@ -178,18 +194,24 @@ def format_descriptor_df(df):
         df_1 = df with descriptor properties only
     """
     df_1 = df
+    ## drops columns not required for the descriptor file
     df_1 = df_1.drop(columns=[
         'QualifierID', 'QualifierName', 'QualifierAbbreviation', 'ConceptID',
         'ConceptName', 'ScopeNote', 'TermID', 'TermName'
     ])
+    ## splits the descriptors with multiple tree numbers, into multiple rows
     df_1 = df_1.apply(lambda x: x.explode() if x.name in
                       ['TreeNumber'] else x)
+    ## creates descriptor dcid
     df_1['Descriptor_dcid'] = 'bio/' + df_1['DescriptorID'].astype(str)
+    ## adds quotes for Descriptor Name text type column
     df_1['DescriptorName'] = '"' + df_1.DescriptorName + '"'
+    ## retrieves the descriptor parent ID using tree number 
     df_1['DescriptorParentID'] = df_1['TreeNumber'].str[:-4]
     map_dict = dict(zip(df_1['TreeNumber'], df_1['Descriptor_dcid']))
     df_1 = df_1.replace({"DescriptorParentID": map_dict})
     df_1["DescriptorParentID"] = np.where(df_1['DescriptorParentID'].str[0] == "b", df_1["DescriptorParentID"], np.nan)
+    ## drops the duplicate rows
     df_1 = df_1.drop_duplicates()
     return df_1
 
@@ -204,6 +226,7 @@ def format_qualifier_df(df):
         df_2 = df with qualifier properties only
     """
     df_2 = df
+    ## drops columns not required for the qualifier file
     df_2 = df_2.drop(columns=[
         'DescriptorName', 'DateCreated-Year', 'DateCreated-Month',
         'DateCreated-Day', 'DateRevised-Year', 'DateRevised-Month',
@@ -211,10 +234,19 @@ def format_qualifier_df(df):
         'DateEstablished-Day', 'ConceptID', 'ConceptName', 'ScopeNote', 
         'TermID', 'TermName', 'TreeNumber', 'NLMClassificationNumber'
     ])
+    ## unzips the qualifer ID and qualifier name lists
     df_2 = (df_2.set_index('DescriptorID').apply(
         lambda x: x.apply(pd.Series).stack()).reset_index().drop('level_1', 1))
+    col_names_quote = ['QualifierName', 'QualifierAbbreviation']
+    ## adds quotes from text type columns and replaces "nan" with np.nan
+    for col in col_names_quote:
+        df_2.update('"' + df_2[[col]].astype(str) + '"')
+        df_2[col] = df_2[col].replace(["\"nan\""],np.nan)
+    ## creates qualifier and descriptor dcids
     df_2['Qualifier_dcid'] = 'bio/' + df_2['QualifierID'].astype(str)
     df_2['Descriptor_dcid'] = 'bio/' + df_2['DescriptorID'].astype(str)
+    ## drops the duplicate rows
+    df_2 = df_2.drop_duplicates()
     return df_2
 
 
@@ -228,6 +260,7 @@ def format_concept_df(df):
         df_3 = df with concept properties only
     """
     df_3 = df
+    ## drops columns not required for the concept file
     df_3 = df_3.drop(columns=[
         'DescriptorName', 'DateCreated-Year', 'DateCreated-Month',
         'DateCreated-Day', 'DateRevised-Year', 'DateRevised-Month',
@@ -235,15 +268,22 @@ def format_concept_df(df):
         'DateEstablished-Day', 'QualifierID', 'QualifierName',
         'QualifierAbbreviation', 'TermID', 'TermName', 'TreeNumber', 'NLMClassificationNumber'
     ])
+    ## strips the empty string from the Scope Note
     df_3['ScopeNote'] = df_3['ScopeNote'].str[0]
     df_3['ScopeNote'] = df_3['ScopeNote'].str.strip()
+    ## unzips the concept name and concept IDs list
     df_3 = df_3 = (df_3.set_index('DescriptorID').apply(
         lambda x: x.apply(pd.Series).stack()).reset_index().drop('level_1', 1))
+    ## adds quotes from text type columns and replaces "nan" with np.nan
     col_names_quote = ['ScopeNote', 'ConceptName']
     for col in col_names_quote:
         df_3.update('"' + df_3[[col]].astype(str) + '"')
+        df_3[col] = df_3[col].replace(["\"nan\""],np.nan)
+    ## generates concept and descriptor dcids
     df_3['Concept_dcid'] = 'bio/' + df_3['ConceptID'].astype(str)
     df_3['Descriptor_dcid'] = 'bio/' + df_3['DescriptorID'].astype(str)
+    ## drops the duplicate rows
+    df_3 = df_3.drop_duplicates()
     return df_3
 
 
@@ -257,6 +297,7 @@ def format_term_df(df):
         df_4 = df with term properties only
     """
     df_4 = df
+    ## drops columns not required for the term file
     df_4 = df_4.drop(columns=[
         'DateCreated-Year', 'DateCreated-Month', 'DateCreated-Day',
         'DateRevised-Year', 'DateRevised-Month', 'DateRevised-Day',
@@ -264,13 +305,21 @@ def format_term_df(df):
         'QualifierID', 'QualifierName', 'QualifierAbbreviation', 'ScopeNote', 
         'DescriptorName', 'DescriptorID', 'TreeNumber', 'NLMClassificationNumber'
     ])
+    ## unzips the list of columns, namely concept and term ID and name
     df_4 = df_4.apply(lambda x: x.explode() if x.name in
                       ['ConceptID', 'ConceptName', 'TermID', 'TermName'] else x)
     df_4 = df_4.reset_index(drop=True)
     df_4 = df_4.apply(lambda x: x.explode()
                       if x.name in ['TermID', 'TermName'] else x)
+    col_names_quote = ['TermName', 'ConceptName']
+    ## adds quotes from text type columns and replaces "nan" with np.nan
+    for col in col_names_quote:
+        df_4.update('"' + df_4[[col]].astype(str) + '"')
+        df_4[col] = df_4[col].replace(["\"nan\""],np.nan)
+    ## generates term and concept dcids
     df_4['Term_dcid'] = 'bio/' + df_4['TermID'].astype(str)
     df_4['Concept_dcid'] = 'bio/' + df_4['ConceptID'].astype(str)
+    df_4 = df_4.drop_duplicates()
     return df_4
 
 
