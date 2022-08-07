cc7ee7206fabb9bc2f37f84efa276cf676068e00
Pulkit Sachdeva <sachdevapulkit@google.com>
2022-08-07 01:37:18 -0700

    Fix tests for preprocess_data_test.py

--- scripts/climate_trace/preprocess_data_test.py
+++ scripts/climate_trace/preprocess_data_test.py
@@ -4,19 +4,25 @@ It imports two test datasets and checks if the processed output matches the
 expected output.
 """
 import unittest
-
+import os
 import pandas as pd
-from preprocess_data import MergeAndProcessData
+import preprocess_data
+
+module_dir_ = os.path.dirname(__file__)
+
+
+def _GetTestPath(relative_path):
+    return os.path.join(module_dir_, relative_path)
 
 
 class PreprocessDataTest(unittest.TestCase):
     """This class has the method required to test preprocess_data script."""
 
     def test_MergeAndProcessData(self):
-        expected_df = pd.read_csv("test_data/expected_output.csv")
-        forest_df = pd.read_csv("test_data/forest.csv")
-        df = pd.read_csv("test_data/other_sectors.csv")
-        processed = MergeAndProcessData(forest_df, df)
+        expected_df = pd.read_csv(_GetTestPath("test_data/expected_output.csv"))
+        forest_df = pd.read_csv(_GetTestPath("test_data/forest.csv"))
+        df = pd.read_csv(_GetTestPath("test_data/other_sectors.csv"))
+        processed = preprocess_data.MergeAndProcessData(forest_df, df)
         sort_column_list = ["country", "year", "statvar"]
         self.assertTrue(
             processed.sort_values(
