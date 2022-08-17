1461179ddf1e39e8cb2e1eb446b6abd1f6428416
Sinan Yumurtacı <snny@google.com>
2022-08-17 22:23:05 +0000

    simplify bash script

--- scripts/us_fema/national_risk_index/sh/e2e.sh
+++ scripts/us_fema/national_risk_index/sh/e2e.sh
@@ -1,3 +1,8 @@
-sh/download_data.sh
-sh/process_data.sh
+#!/bin/bash
 
+sh/download_data.sh
+
+# Process the data
+python3 generate_schema_and_tmcf.py
+# process_data.py depends on artifacts produced by generate_schema_and_tmcf.py
+python3 process_data.py
--- scripts/us_fema/national_risk_index/sh/process_data.sh
+++
@@ -1,3 +0,0 @@
-python3 generate_schema_and_tmcf.py
-# process_data.py depends on artifacts produced by generate_schema_and_tmcf.py
-python3 process_data.py
