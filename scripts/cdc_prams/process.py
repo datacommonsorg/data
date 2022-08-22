2a4f7605663008ab4ee8fd61ec6901e86fa44a01
Chandrashekar Harish <chharish@google.com>
2022-08-22 15:46:46 +0530

    deleted:    dc_generated/report.json 	deleted:    dc_generated/summary_report.html 	new file:   scripts/cdc_prams/README.md 	modified:   scripts/cdc_prams/constants.py 	modified:   scripts/cdc_prams/process.py 	deleted:    scripts/cdc_prams/process_national.py 	deleted:    scripts/cdc_prams/test_data/expected_files/test_census.mcf

--- scripts/cdc_prams/process.py
+++ scripts/cdc_prams/process.py
@@ -49,9 +49,12 @@ flags.DEFINE_string("input_path", default_input_path, "Import Data File's List")
 
 def prams(input_url: list, statvar_col:dict) -> pd.DataFrame:
     final_df = pd.DataFrame()
-    df_all = pd.DataFrame()
+    df_all = pd.DataFrame([])
     sv_names = []
     for file in input_url:
+        flag = "States"
+        if "All-Sites" in file:
+            flag = "National"
         data = tb.read_pdf(file, pages = 'all')
         df = pd.concat(data)
         file_name = os.path.basename(file)
@@ -60,10 +63,18 @@ def prams(input_url: list, statvar_col:dict) -> pd.DataFrame:
         .replace('New York City','geoId/3651000').replace('All Sites','country/USA')
         print(file_name.replace('-PRAMS-MCH-Indicators-508.pdf',''))
         df.reset_index(drop=True, inplace=True)
-        df = df.drop(['Unnamed: 0','Unnamed: 1','Unnamed: 2','Unnamed: 3','Unnamed: 4'], 1)
-        df.columns = ['statVar', '2016_CI', '2017_sampleSize', '2017_CI', 
-        '2018_sampleSize', '2018_CI', '2019_sampleSize', '2019_CI', 
-        '2020_sampleSize', '2020_CI', 'Overall_2020_CI','Geo']
+        if flag == "States":
+            df = df.drop(['Unnamed: 0','Unnamed: 1','Unnamed: 2','Unnamed: 3','Unnamed: 4'], 1)
+            df.columns = ['statVar', '2016_CI', '2017_sampleSize', '2017_CI', 
+            '2018_sampleSize', '2018_CI', '2019_sampleSize', '2019_CI', 
+            '2020_sampleSize', '2020_CI', 'Overall_2020_CI','Geo']
+        elif flag=="National":
+            # print(df.columns)
+            df.columns = ['statVar', '2016_CI', '2017_sampleSize','2017_Nan', 
+            '2017_CI', '2018_sampleSize','2018_Nan', '2018_CI',
+            '2019_sampleSize','2019_Nan', '2019_CI', '2020_sampleSize',
+            '2020_Nan', '2020_CI','Geo']
+            df.drop(['2017_Nan','2018_Nan','2019_Nan','2020_Nan'], 1, inplace=True)
         df.insert(1,'2016_sampleSize', np.NaN)   
         final_df = df.copy()
         final_df['statVar'] = final_df['statVar'].str.replace('• ', '')
@@ -87,14 +98,18 @@ def prams(input_url: list, statvar_col:dict) -> pd.DataFrame:
                     df1.loc[i, '2019_CI'] = df1.loc[i+1, '2019_CI']
                     df1.loc[i, '2020_sampleSize'] = df1.loc[i+1, '2020_sampleSize']
                     df1.loc[i, '2020_CI'] = df1.loc[i+1, '2020_CI']
-                    df1.loc[i, 'Overall_2020_CI'] = df1.loc[i+1, 'Overall_2020_CI']
+                    if flag=="States":
+                        df1.loc[i, 'Overall_2020_CI'] = df1.loc[i+1, 'Overall_2020_CI']
                     df1.drop([i+1, i+2], inplace=True)
                     df1.reset_index(drop=True, inplace=True)
                     break
 
         for i in range(len(df1)):
             if df1.loc[i, 'statVar'][-4:].strip().strip('§').isnumeric():
-                df1.loc[i, '2016_sampleSize'] = df1.loc[i, 'statVar'][-4:].strip().strip('§')
+                if flag=="States":
+                    df1.loc[i, '2016_sampleSize'] = df1.loc[i, 'statVar'][-4:].strip().strip('§')
+                elif flag=="National":
+                    df1.loc[i, '2016_sampleSize'] = df1.loc[i, 'statVar'][-5:].strip().strip('§')
                 l1 = len(df1.loc[i, 'statVar'])
                 l2 = len(df1.loc[i, '2016_sampleSize'])
                 df1.loc[i, 'statVar'] = df1.loc[i, 'statVar'][:l1-l2].strip()
@@ -104,10 +119,23 @@ def prams(input_url: list, statvar_col:dict) -> pd.DataFrame:
 
         for i in range(len(df1)):
             if df1.loc[i, 'statVar'] == 'Experienced IPV during the 12 months before pregnancy by a':
+                if flag=="National":
+                    df1.loc[i, 'statVar'] = df1.loc[i, 'statVar'].replace('a2.5 ','a')
+                    df1.loc[i+1, '2020_CI'] = 2.5
                 df1.loc[i+1, 'statVar'] = df1.loc[i, 'statVar'] + df1.loc[i+1, 'statVar']
                 df1.drop([i], inplace=True)
                 df1.reset_index(drop=True, inplace=True)
                 break
+
+        if flag=="National":
+            for i in range(len(df1)):
+                if df1.loc[i, 'statVar'] == 'Teeth cleaned during pregnancy by a dentist or dental':
+                    df1.loc[i+1, 'statVar'] = df1.loc[i+1, 'statVar'].replace('40.0 ','')
+                    df1.loc[i+1, '2020_CI'] = 40.0
+                    df1.loc[i+1, 'statVar'] = df1.loc[i, 'statVar'] + df1.loc[i+1, 'statVar']
+                    df1.drop([i], inplace=True)
+                    df1.reset_index(drop=True, inplace=True)
+                    break
         df2 = df1.copy()
         main_header = ['Nutrition',
         'Pre-pregnancy Weight',
@@ -153,77 +181,100 @@ def prams(input_url: list, statvar_col:dict) -> pd.DataFrame:
         ,'main_header','sub_header_delete_flag'])
         df2['SV'] = df2['newStatVar']
         df2 = df2.replace({'SV': statvar_col})
+        df2.to_csv("SV1.csv")
         df2 = df2.replace({'Geo': _PLACE_MAP})
         df2 = df2.reset_index(drop=True)
-        split_col = ['2016_CI','2017_CI','2018_CI','2019_CI','2020_CI']
-        for i in split_col:
-            df2[i] = df2[i].fillna(pd.NA)
-            temp_df = df2[i].str.split(r"\s+|-", expand=True)
-            siz = temp_df.shape[1]
-            if siz==1:
-                temp_df = temp_df.rename(columns={temp_df.columns[0]: i+'_PERCENT'})
-                temp_df[i+'_LOWER'] = ""
-                temp_df[i+'_UPPER'] = ""
-            elif siz==3:
-                temp_df = temp_df.rename(columns={temp_df.columns[0]: i+'_PERCENT',temp_df.columns[1]: i+'_LOWER',temp_df.columns[2]: i+'_UPPER'})
-                temp_df[i+'_LOWER'] = temp_df[i+'_LOWER'].str.replace('(','')
-                temp_df[i+'_UPPER'] = temp_df[i+'_UPPER'].str.replace(')','')
-            df2 = pd.concat([df2,temp_df],axis=1)
-        df2 = df2.drop(columns=['newStatVar','2016_CI','2017_CI','2018_CI','2019_CI','2020_CI','Overall_2020_CI'])  
-        df2 = df2[['Geo', 'SV','2016_sampleSize', '2017_sampleSize', '2018_sampleSize',
+
+        if flag=="States":
+            split_col = ['2016_CI','2017_CI','2018_CI','2019_CI','2020_CI']
+            for i in split_col:
+                df2[i] = df2[i].fillna(pd.NA)
+                temp_df = df2[i].str.split(r"\s+|-", expand=True)
+                siz = temp_df.shape[1]
+                if siz==1:
+                    temp_df = temp_df.rename(columns={temp_df.columns[0]: i+'_PERCENT'})
+                    temp_df[i+'_LOWER'] = ""
+                    temp_df[i+'_UPPER'] = ""
+                elif siz==3:
+                    temp_df = temp_df.rename(columns={temp_df.columns[0]: i+'_PERCENT',temp_df.columns[1]: i+'_LOWER',temp_df.columns[2]: i+'_UPPER'})
+                    temp_df[i+'_LOWER'] = temp_df[i+'_LOWER'].str.replace('(','')
+                    temp_df[i+'_UPPER'] = temp_df[i+'_UPPER'].str.replace(')','')
+                df2 = pd.concat([df2,temp_df],axis=1)
+            df2 = df2.drop(columns=['newStatVar','2016_CI','2017_CI','2018_CI','2019_CI','2020_CI','Overall_2020_CI'])
+       
+        if flag=="National":
+            df2 = df2[['Geo','SV', '2016_sampleSize','2016_CI',
+            '2017_sampleSize','2017_CI','2018_sampleSize','2018_CI',
+            '2019_sampleSize','2019_CI','2020_sampleSize','2020_CI']]
+        elif flag=="States":   
+            df2 = df2[['Geo', 'SV','2016_sampleSize', '2017_sampleSize', '2018_sampleSize',
         '2019_sampleSize', '2020_sampleSize',  '2016_CI_PERCENT',
         '2016_CI_LOWER', '2016_CI_UPPER', '2017_CI_PERCENT', '2017_CI_LOWER',
         '2017_CI_UPPER', '2018_CI_PERCENT', '2018_CI_LOWER', '2018_CI_UPPER',
         '2019_CI_PERCENT', '2019_CI_LOWER', '2019_CI_UPPER', '2020_CI_PERCENT',
         '2020_CI_LOWER', '2020_CI_UPPER']]
-        df2['2018_CI_UPPER'] = df2['2018_CI_UPPER'].replace('.','0.0')
-        df2['2018_CI_LOWER'] = df2['2018_CI_LOWER'].replace('.','0.0')
+            df2['2018_CI_UPPER'] = df2['2018_CI_UPPER'].replace('.','0.0')
+            df2['2018_CI_LOWER'] = df2['2018_CI_LOWER'].replace('.','0.0')
+        
         sv_melt = ['sample_size','percent_sv','lower_level','upper_level']
+
         for i1 in sv_melt:
-            temp_df1 = df2.copy()
+            temp_df = df2.copy()
             if i1=="sample_size":
-                temp_df1['SV'] = 'SampleSize_Count'+df2['SV']
-                temp_df1 = temp_df1.drop(columns=['2016_CI_PERCENT','2017_CI_PERCENT',
-                '2019_CI_PERCENT','2020_CI_PERCENT',
-                '2018_CI_PERCENT','2016_CI_LOWER','2016_CI_UPPER','2017_CI_LOWER',
-                '2017_CI_UPPER','2018_CI_LOWER','2018_CI_UPPER','2019_CI_LOWER',
-                '2019_CI_UPPER','2020_CI_LOWER','2020_CI_UPPER'])
-                temp_df1 = temp_df1.melt(id_vars=['Geo','SV'], var_name='Year'\
+                temp_df['SV'] = 'SampleSize_Count'+df2['SV']
+                if flag=="National":
+                    temp_df = temp_df.drop(columns=['2016_CI','2017_CI','2018_CI','2019_CI','2020_CI'])
+                elif flag=="States":
+                    temp_df = temp_df.drop(columns=['2016_CI_PERCENT','2017_CI_PERCENT',
+                    '2019_CI_PERCENT','2020_CI_PERCENT',
+                    '2018_CI_PERCENT','2016_CI_LOWER','2016_CI_UPPER','2017_CI_LOWER',
+                    '2017_CI_UPPER','2018_CI_LOWER','2018_CI_UPPER','2019_CI_LOWER',
+                    '2019_CI_UPPER','2020_CI_LOWER','2020_CI_UPPER'])
+                temp_df = temp_df.melt(id_vars=['Geo','SV'], var_name='Year'\
                         ,value_name='Observation')
             elif i1=="percent_sv":
-                temp_df1['SV'] = 'Percent'+df2['SV']
-                temp_df1 = temp_df1.drop(columns=['2016_sampleSize','2017_sampleSize',
-                '2018_sampleSize','2019_sampleSize','2020_sampleSize',
-                '2016_CI_LOWER','2016_CI_UPPER','2017_CI_LOWER','2017_CI_UPPER',
-                '2018_CI_LOWER','2018_CI_UPPER','2019_CI_LOWER','2019_CI_UPPER',
-                '2020_CI_LOWER','2020_CI_UPPER'])
-                temp_df1 = temp_df1.melt(id_vars=['Geo','SV'], var_name='Year'\
+                temp_df['SV'] = 'Percent'+df2['SV']
+                if flag=="National":
+                    temp_df = temp_df.drop(columns=['2016_sampleSize',
+                    '2017_sampleSize','2018_sampleSize','2019_sampleSize',
+                    '2020_sampleSize'])
+                elif flag=="States":
+                    temp_df = temp_df.drop(columns=['2016_sampleSize','2017_sampleSize',
+                    '2018_sampleSize','2019_sampleSize','2020_sampleSize',
+                    '2016_CI_LOWER','2016_CI_UPPER','2017_CI_LOWER','2017_CI_UPPER',
+                    '2018_CI_LOWER','2018_CI_UPPER','2019_CI_LOWER','2019_CI_UPPER',
+                    '2020_CI_LOWER','2020_CI_UPPER'])
+                temp_df = temp_df.melt(id_vars=['Geo','SV'], var_name='Year'\
                         ,value_name='Observation')
-            elif i1=="lower_level":
-                temp_df1['SV'] = 'ConfidenceIntervalLowerLimit_Count'+df2['SV']
-                temp_df1 = temp_df1.drop(columns=['2016_sampleSize','2017_sampleSize',
+            elif i1=="lower_level" and flag=="States":
+                temp_df['SV'] = 'ConfidenceIntervalLowerLimit_Count'+df2['SV']
+                temp_df = temp_df.drop(columns=['2016_sampleSize','2017_sampleSize',
                 '2018_sampleSize','2019_sampleSize','2020_sampleSize',
                 '2016_CI_UPPER','2017_CI_UPPER','2018_CI_UPPER','2019_CI_UPPER',
                 '2020_CI_UPPER','2016_CI_PERCENT',
                 '2017_CI_PERCENT','2019_CI_PERCENT','2020_CI_PERCENT',
                 '2018_CI_PERCENT'])
-                temp_df1 = temp_df1.melt(id_vars=['Geo','SV'], var_name='Year'\
+                temp_df = temp_df.melt(id_vars=['Geo','SV'], var_name='Year'\
                         ,value_name='Observation')
-            elif i1=="upper_level":
-                temp_df1['SV'] = 'ConfidenceIntervalUpperLimit_Count'+df2['SV']
-                temp_df1 = temp_df1.drop(columns=['2016_sampleSize','2017_sampleSize',
+            elif i1=="upper_level" and flag=="States":
+                temp_df['SV'] = 'ConfidenceIntervalUpperLimit_Count'+df2['SV']
+                temp_df = temp_df.drop(columns=['2016_sampleSize','2017_sampleSize',
                 '2018_sampleSize','2019_sampleSize','2020_sampleSize',
                 '2016_CI_LOWER','2017_CI_LOWER','2018_CI_LOWER','2019_CI_LOWER',
                 '2020_CI_LOWER','2016_CI_PERCENT',
                 '2017_CI_PERCENT','2019_CI_PERCENT','2020_CI_PERCENT',
                 '2018_CI_PERCENT'])
-                temp_df1 = temp_df1.melt(id_vars=['Geo','SV'], var_name='Year'\
+                temp_df = temp_df.melt(id_vars=['Geo','SV'], var_name='Year'\
                         ,value_name='Observation')
-            df_all = pd.concat([df_all,temp_df1],axis=0)
+            else:
+                continue
+            
+            df_all = pd.concat([df_all,temp_df],axis=0)
         # Replacing Year column with the correct Values.
         for old,new in _YEAR.items():
             df_all['Year'] = df_all['Year'].replace(old,new)
         df_all.reset_index(drop=True, inplace=True)
+        df_all = df_all.sort_values(by=['Geo'], kind="stable")
     return df_all    
 
 class US_Prams:
@@ -252,7 +303,7 @@ class US_Prams:
         with open(self.tmcf_file_path, 'w+', encoding="UTF-8") as f_out:
             f_out.write(_TMCF_TEMPLATE.rstrip('\n'))
 
-    def _generate_mcf(self, sv_names: list, mcf_file_path: str)-> None:#, statVar:dict) 
+    def _generate_mcf(self, sv_names: list, mcf_file_path: str)-> None:
         """
         This method generates MCF file w.r.t
         dataframe headers and defined MCF template
@@ -334,7 +385,8 @@ class US_Prams:
                     pvs.append(f"timePeriodRelativeToPregnancy: dcs:{prop3}")
                 
                 elif "CigaretteSmoking3MonthsBeforePregnancy" in prop or\
-                    "CigaretteSmokingLast3MonthsOfPregnancy"in prop or "CigaretteSmokingPostpartum" in prop or\
+                    "CigaretteSmokingLast3MonthsOfPregnancy"in prop or \
+                    "CigaretteSmokingPostpartum" in prop or\
                     "ECigaretteSmoking3MonthsBeforePregnancy" in prop or\
                     "ECigaretteSmokingLast3MonthsOfPregnancy" in prop:
                     pvs.append(f"tobaccoUsageType: dcs:{prop4}")
@@ -413,7 +465,7 @@ class US_Prams:
                 _MCF_TEMPLATE.format(dcid=resolved_dcid, xtra_pvs='\n'.join(pvs)))
         mcf = '\n'.join(mcf_nodes)
         # Writing Genereated MCF to local path.
-        with open(self.mcf_file_path, 'w+', encoding='utf-8') as f_out:
+        with open(mcf_file_path, 'w+', encoding='utf-8') as f_out:
             f_out.write(mcf.rstrip('\n'))
         return dcid_nodes
 
@@ -428,27 +480,21 @@ class US_Prams:
         df_all = prams(self.input_files,statvar_col)
         sv_names += df_all["SV"].to_list()
         sv_names = list(set(sv_names))
-        sv_names.sort()
-        updated_sv = self._generate_mcf(sv_names, self.mcf_file_path)
-        df_all["SV"] = df_all["SV"].map(updated_sv)
         # Creating Output Directory
         output_path = os.path.dirname(self.cleaned_csv_file_path)
         if not os.path.exists(output_path):
             os.mkdir(output_path)
+        sv_names.sort()
+        updated_sv = self._generate_mcf(sv_names, self.mcf_file_path)
+        df_all["SV"] = df_all["SV"].map(updated_sv)    
         self._generate_tmcf()
-        df_all = df_all.sort_values(by=["Geo"])
         df_all.to_csv(self.cleaned_csv_file_path, index=False)
 
 def main(_):
     input_path = _FLAGS.input_path
-    input_path = "/Users/chharish/us_prams/data/scripts/cdc_prams/input_files_copy"
     # input_path = "/Users/chharish/us_prams/data/scripts/cdc_prams/test_data/datasets"
-    if not os.path.exists(input_path):
-        os.mkdir(input_path)
-    ip_files_copy = os.listdir(input_path)
-    ip_files_copy = [os.path.join(input_path, file) for file in ip_files_copy]
-    # ip_files_copy = ["/Users/chharish/us_prams/data/scripts/cdc_prams/input_files/"+
-    # "Iowa-PRAMS-MCH-Indicators-508.pdf"]
+    ip_files = os.listdir(input_path)
+    ip_files = [os.path.join(input_path, file) for file in ip_files]
     # Defining Output Files
     data_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                   "output")
@@ -458,7 +504,7 @@ def main(_):
     tmcf_path = os.path.join(data_file_path, tmcf_name)
     mcf_path = os.path.join(data_file_path, mcf_name)
     cleaned_csv_path = os.path.join(data_file_path, csv_name)
-    loader = US_Prams(ip_files_copy, cleaned_csv_path,mcf_path,tmcf_path)
+    loader = US_Prams(ip_files, cleaned_csv_path,mcf_path,tmcf_path)
     loader.process()
 
 if __name__ == "__main__":
