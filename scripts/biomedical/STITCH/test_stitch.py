9ba0d5cfd6876bb74ca96632d5e6e9eee46d91a6
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2022-09-02 17:01:56 -0700

    Update test_stitch.py

--- scripts/biomedical/STITCH/test_stitch.py
+++ scripts/biomedical/STITCH/test_stitch.py
@@ -16,7 +16,7 @@ Author: Mariam Benazouz
 Date: 08/24/2022
 Name: test_stitch.py
 Description: runs unit tests for compile_stitch.py, drug_interactions.py, 
-    create_mapping_file.py, protein_mapping2.py, protein_actions.py
+    create_mapping_file.py, protein_drug_interactions.py, protein_actions.py
 Run: python3 stitch_test.py
 '''
 import unittest
@@ -33,37 +33,30 @@ class TestStitch(unittest.TestCase):
     
     def test_main(self):
         """Test in the main function"""
-        # Read in the expected output files into pandas dataframes
-        PATH_PROTEIN_INTERACTIONS = 'test-data/original_files/protein_drug_interactions_test.tsv'
-        PATH_ACTIONS = 'test-data/original_files/actions_test.tsv'
-        PATH_DRUG_INTERACTIONS = 'test-data/original_files/drug_interactions_test.tsv'
-#         PATH_ARCHIVED_ENSEMBLS = 'test-data/original_files/archived_emsembls.txt'
-#         PATH_ARCHIVED_ACCESSIONS = 'test-data/original_files/archived_accessions.txt'
-#         PATH_UNIPROT_MAPPING = 'test-data/original_files/uniprot_name_mapping.dat'
-        archived_accessions_df=pd.read_csv('archived_uniprot_test.csv')
-        uniprot_mapping_df=pd.read_csv('uniprot_id_mapping_test.csv')
-        archived_ensembls_df=pd.read_csv('archived_ensembls_test.csv')
+        PATH_PROTEIN_INTERACTIONS = 'test-data/protein_drug_interactions_test.tsv'
+        PATH_ACTIONS = 'test-data/actions_test.tsv'
+        PATH_DRUG_INTERACTIONS = 'test-data/drug_interactions_test.tsv'
+        archived_accessions_df=pd.read_csv('test-data/archived_uniprot_test.csv')
+        uniprot_mapping_df=pd.read_csv('test-data/uniprot_id_mapping_test.csv')
+        archived_ensembls_df=pd.read_csv('test-data/archived_ensembls_test.csv')
         
-        PATH_SOURCES= 'test-data/original_files/sources_test.tsv'
-        PATH_INCHIKEYS = 'test-data/original_files/inchikeys_test.tsv'
-        PATH_CHEMICALS = 'test-data/original_files/chemicals_test.tsv'     
+        PATH_SOURCES= 'test-data/sources_test.tsv'
+        PATH_INCHIKEYS = 'test-data/inchikeys_test.tsv'
+        PATH_CHEMICALS = 'test-data/chemicals_test.tsv'     
 
         drug_df_expected=pd.read_csv('test-data/drugs_expected.csv', 
             dtype={'source_cid': pd.Int64Dtype(), 'BindingDB_code':object, 
-            'molecular_weight':object})
-        drug_interactions_expected=pd.read_csv('test-data/drug_interactions_expected.csv')
-        mapping_df_expected=pd.read_csv('test-data/mapping_expected.csv')
-        mapping_df_expected1=pd.read_csv('test-data/mapping_expected.csv')
+            'molecular_weight':object}, keep_default_na=False)
+        drug_interactions_expected=pd.read_csv('test-data/drug_interactions_expected.csv', keep_default_na=False)
+        mapping_df_expected=pd.read_csv('test-data/mapping_expected.csv', keep_default_na=False)
+        mapping_df_expected1=pd.read_csv('test-data/mapping_expected.csv', keep_default_na=False)
         mapping_df_expected['sort'] = mapping_df_expected['Protein stable ID'].str.extract('(\d+)', expand=False).astype(int)
         mapping_df_expected.sort_values('sort',inplace=True, ascending=True)
         mapping_df_expected = mapping_df_expected.drop('sort', axis=1)
-
-        protein_drug_interactions_expected=pd.read_csv('test-data/protein_drug_interactions_expected.csv')        
-        actions_df_expected = pd.read_csv('test-data/actions_expected.csv')
+        protein_drug_interactions_expected=pd.read_csv('test-data/protein_drug_interactions_expected.csv', keep_default_na=False)         actions_df_expected = pd.read_csv('test-data/actions_expected.csv', keep_default_na=False)
     
         # Run all the functions in compile_drugs.py 
-        drug_df = compile_drug_info(PATH_SOURCES, PATH_INCHIKEYS, PATH_CHEMICALS)  
-        drug_df_actual=drug_df.replace("", np.nan, regex=True)
+        drug_df_actual = compile_drug_info(PATH_SOURCES, PATH_INCHIKEYS, PATH_CHEMICALS)  
    
         # Run all the functions in drug_interactions.py 
         drug_interactions_actual = pubchem_dcid(PATH_DRUG_INTERACTIONS)
@@ -73,19 +66,17 @@ class TestStitch(unittest.TestCase):
         uniprot_with_old_accessions = add_old_accessions(archived_accessions_df, df_uniprot)
         mapping_df_actual = combine_dfs(df_ensembls, uniprot_with_old_accessions)
         mapping_df_actual = mapping_df_actual.drop_duplicates(keep = 'first')
-        mapping_df_actual['dcid']='bio/' + mapping_df_actual['UniProtKB-ID']
+        mapping_df_actual['dcid'] = 'bio/' + mapping_df_actual['UniProtKB-ID']
         mapping_df_actual['sort'] = mapping_df_actual['Protein stable ID'].str.extract('(\d+)', expand=False).astype(int)
-        mapping_df_actual.sort_values('sort',inplace=True, ascending=True)
+        mapping_df_actual.sort_values('sort',inplace = True, ascending = True)
         mapping_df_actual = mapping_df_actual.drop('sort', axis=1)
         
         # Run all the functions in protein_drug_interactions.py
         protein_drug_interactions_actual = create_final_mapped_df(PATH_PROTEIN_INTERACTIONS, 
             mapping_df_expected1)
-        protein_drug_interactions_actual = protein_drug_interactions_actual.replace("", np.nan, regex = True)
         
         # Run all the functions in protein_actions.py
         actions_df_actual = create_final_mapped_actions_df(PATH_ACTIONS, mapping_df_expected1)
-        actions_df_actual = actions_df_actual.replace("", np.nan, regex = True)
         
         # Compare expected and actual output files
         assert_frame_equal(drug_df_expected.reset_index(drop = True), 
