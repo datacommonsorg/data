717877f771a32a75039874166c99dfd5dbe86eff
Sanika Prasad <sanikap@google.com>
2022-09-07 10:23:15 +0530

    Lint

--- scripts/eurostat/health_determinants/common/sv_config.py
+++ scripts/eurostat/health_determinants/common/sv_config.py
@@ -33,10 +33,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                             ,
         "hlth_ehis_al1u": "'Percent_' + df['frequenc_alcohol'] + \
             '_AlcoholConsumption' + '_In_Count_Person_' + df['deg_urb'] + \
             '_' + df['sex']"
+
+
 
 
 
@@ -47,10 +51,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                             ,
         "hlth_ehis_de10": "'Percent_' + df['frequenc_alcohol'] + \
             '_AlcoholConsumption' + '_In_Count_Person_' + df['isced11'] + \
             '_' + df['sex']"
+
+
 
 
 
@@ -61,10 +69,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                           ,
         "hlth_ehis_al1i": "'Percent_' + df['frequenc_alcohol'] + \
             '_AlcoholConsumption' + '_In_Count_Person_' + df['sex'] + '_' + \
             df['quant_inc']"
+
+
 
 
 
@@ -75,10 +87,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                       ,
         "hlth_ehis_al3e": "'Percent_' + df['frequenc_alcohol'] + \
             '_BingeDrinking' + '_In_Count_Person_' + df['isced11'] + '_' + \
             df['sex']"
+
+
 
 
 
@@ -89,6 +105,8 @@ file_to_sv_mapping = {
 
 
 
+
+
                       ,
         "hlth_ehis_al3i": "'Percent_' + df['frequenc_alcohol'] + \
             '_BingeDrinking' + '_In_Count_Person_' + df['sex'] + '_' + \
@@ -96,17 +114,22 @@ file_to_sv_mapping = {
 
 
 
+
+
                             ,
         "hlth_ehis_al2u": "'Percent_HazardousAlcoholConsumption'+\
             '_In_Count_Person_' + df['deg_urb'] + '_' + df['sex']"
+
 
                                                                   ,
         "hlth_ehis_al2e": "'Percent_HazardousAlcoholConsumption'+\
             '_In_Count_Person_' + df['isced11'] + '_' + df['sex']"
 
+
                                                                   ,
         "hlth_ehis_al2i": "'Percent_HazardousAlcoholConsumption'+\
             '_In_Count_Person_' + df['sex'] + '_' + df['quant_inc']"
+
 
                                                                     ,
     },
@@ -118,10 +141,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                             ,
         "hlth_ehis_pe9i": "'Percent_' + df['physact'] + '_' + \
             'HealthEnhancingPhysicalActivity' + '_In_Count_Person_' + \
             df['sex'] + '_' + df['quant_inc']"
+
+
 
 
 
@@ -132,10 +159,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                             ,
         "hlth_ehis_pe1e": "'Percent_' + 'WorkRelatedPhysicalActivity' + '_' + \
             df['levels'] + '_In_Count_Person_' + df['isced11'] + '_' + \
             df['sex']"
+
+
 
 
 
@@ -146,10 +177,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                             ,
         "hlth_ehis_pe1u": "'Percent_' + 'WorkRelatedPhysicalActivity_' \
             + df['levels'] + '_In_Count_Person_' + df['deg_urb'] + '_' + \
             df['sex']"
+
+
 
 
 
@@ -160,10 +195,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                             ,
         "hlth_ehis_pe3i": "'Percent_' + df['physact'] + \
             '_NonWorkRelatedPhysicalActivity'+ '_In_Count_Person_' + \
             df['sex'] + '_' + df['quant_inc']"
+
+
 
 
 
@@ -174,10 +213,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                           ,
         "hlth_ehis_pe2e": "'Percent_' + df['duration'] + '_' + \
             'HealthEnhancingPhysical'+ 'Activity_In_Count_Person_' + \
             df['isced11'] + '_' + df['sex']"
+
+
 
 
 
@@ -188,10 +231,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                               ,
         "hlth_ehis_pe2u": "'Percent_' + df['duration'] + \
             '_HealthEnhancingPhysicalActivity' + '_In_Count_Person_' + \
             df['deg_urb'] + '_' + df['sex']"
+
+
 
 
 
@@ -202,10 +249,14 @@ file_to_sv_mapping = {
 
 
 
+
+
                                             ,
         "hlth_ehis_pe9c": "'Percent_' + df['physact'] + \
             '_HealthEnhancingPhysicalActivity'+ '_In_Count_Person_' + \
             df['citizen'] + '_' + df['sex']"
+
+
 
 
 
@@ -216,6 +267,8 @@ file_to_sv_mapping = {
 
 
 
+
+
                                               ,
         "hlth_ehis_pe2m": "'Percent_' + df['duration'] + \
             '_NonWorkRelatedPhysicalActivity'+ '_In_Count_Person_' + \
@@ -223,14 +276,19 @@ file_to_sv_mapping = {
 
 
 
+
+
                                         ,
         "hlth_ehis_de9" : "'Percent_' + df['isced11'] + '_PhysicalActivity' + \
             '_In_Count_Person_' + df['sex']"
+
 
                                             ,
         "hlth_ehis_pe6e": "'Percent_' + 'AtLeast30MinutesPerDay' + \
             '_WalkingOrCycling_' + 'PhysicalActivity' + '_In_Count_Person_' + \
             df['isced11'] + '_' + df['sex']"
+
+
 
 
 
@@ -242,16 +300,20 @@ file_to_sv_mapping = {
         "'Percent'+'_'+df['smoking']+'_'+'TobaccoProducts'+\
         '_In_Count_Person_'+df['sex']+'_'+df['c_birth']"
 
+
                                                         ,
     "hlth_ehis_sk1c":
         "'Percent'+'_'+df['smoking']+'_'+'TobaccoProducts'+\
         '_In_Count_Person_'+df['citizen']+'_'+df['sex']"
+
 
                                                         ,
     "hlth_ehis_sk1e":
         "'Percent'+\
         '_'+df['smoking']+'_TobaccoProducts'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
+
 
 
 
@@ -263,6 +325,8 @@ file_to_sv_mapping = {
 
 
 
+
+
                                                           ,
     "hlth_ehis_sk1u":
         "'Percent'+\
@@ -271,26 +335,33 @@ file_to_sv_mapping = {
 
 
 
+
+
                                                         ,
     "hlth_ehis_sk2i":
         "'Percent_FormerSmoker_Daily_TobaccoProducts'+\
         '_In_Count_Person_'+df['sex']+'_'+df['quant_inc']"
+
 
                                                           ,
     "hlth_ehis_sk2e":
         "'Percent_FormerSmoker_Daily_TobaccoProducts'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
 
+
                                                         ,
     "hlth_ehis_sk3e":
         "'Percent_Daily_'+df['smoking']+'_TobaccoSmoking'+'_Cigarettes'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
 
                                                         ,
     "hlth_ehis_sk3i":
         "'Percent_Daily'+'_'+df['smoking']+'_TobaccoSmoking'+\
         '_Cigarettes'+\
         '_In_Count_Person_'+df['sex']+'_'+df['quant_inc']"
+
+
 
 
 
@@ -302,21 +373,27 @@ file_to_sv_mapping = {
 
 
 
+
+
                                                         ,
     "hlth_ehis_sk4e":
         "'Percent'+'_'+df['frequenc_tobacco']+'_ExposureToTobaccoSmoke'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
 
                                                         ,
     "hlth_ehis_sk4u":
         "'Percent'+'_'+df['frequenc_tobacco']+'_ExposureToTobaccoSmoke'+\
         '_In_Count_Person_'+df['deg_urb']+'_'+df['sex']"
 
+
                                                         ,
     "hlth_ehis_sk5e":
         "'Percent_Daily_TobaccoSmoking_'+\
         df['duration']+'_TobaccoProducts'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
+
 
 
 
@@ -328,11 +405,15 @@ file_to_sv_mapping = {
 
 
 
+
+
                                                         ,
     "hlth_ehis_de3":
         "'Percent_Daily_TobaccoSmoking'+\
         '_Cigarettes'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
+
 
 
 
@@ -344,11 +425,15 @@ file_to_sv_mapping = {
 
 
 
+
+
                                                           ,
     "hlth_ehis_de5":
         "'Percent_Daily'+'_'+df['smoking']+'_TobaccoSmoking'+\
         '_Cigarettes'+\
         '_In_Count_Person_'+df['isced11']+'_'+df['sex']"
+
+
 
 
 
