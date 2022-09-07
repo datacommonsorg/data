8671761db5a53b1fc72d4d5bb3bc44823598be80
Sanika Prasad <sanikap@google.com>
2022-09-07 16:55:30 +0530

    new file:   scripts/us_epa/air_emissions_inventory/README.md 	new file:   scripts/us_epa/air_emissions_inventory/download.py 	new file:   scripts/us_epa/air_emissions_inventory/download.sh 	deleted:    scripts/us_epa/air_emissions_inventory/input_files/2008.CSV 	deleted:    scripts/us_epa/air_emissions_inventory/input_files/2011.CSV 	deleted:    scripts/us_epa/air_emissions_inventory/input_files/2014.CSV 	deleted:    scripts/us_epa/air_emissions_inventory/input_files/2017.CSV 	deleted:    scripts/us_epa/air_emissions_inventory/output/airpollution_emission_trends_county_tier1.csv 	deleted:    scripts/us_epa/air_emissions_inventory/output/airpollution_emission_trends_county_tier1.tmcf 	modified:   scripts/us_epa/air_emissions_inventory/process.py 	new file:   scripts/us_epa/air_emissions_inventory/process_test.py 	new file:   scripts/us_epa/air_emissions_inventory/test_data/datasets/2008_allstates.csv 	new file:   scripts/us_epa/air_emissions_inventory/test_data/datasets/2011_allstates.csv 	new file:   scripts/us_epa/air_emissions_inventory/test_data/datasets/2014_allstates.csv 	new file:   scripts/us_epa/air_emissions_inventory/test_data/datasets/2017_allstates.csv 	new file:   scripts/us_epa/air_emissions_inventory/test_data/expected_files/expected_airpollution_emission_trends_county_tier1.csv 	renamed:    scripts/us_epa/air_emissions_inventory/output/airpollution_emission_trends_county_tier1.mcf -> scripts/us_epa/air_emissions_inventory/test_data/expected_files/expected_airpollution_emission_trends_county_tier1.mcf 	new file:   scripts/us_epa/air_emissions_inventory/test_data/expected_files/expected_airpollution_emission_trends_county_tier1.tmcf

--- scripts/us_epa/air_emissions_inventory/process.py
+++ scripts/us_epa/air_emissions_inventory/process.py
@@ -27,7 +27,7 @@ sys.path.insert(
     1, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../'))
 
 from util.statvar_dcid_generator import get_statvar_dcid
-from util.county_to_dcid import COUNTY_MAP
+from util.alpha2_to_dcid import USSTATE_MAP
 
 FLAGS = flags.FLAGS
 default_input_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -37,21 +37,20 @@ flags.DEFINE_string("input_path", default_input_path, "Import Data File's List")
 _MCF_TEMPLATE = ("Node: dcid:{pv1}\n"
                  "typeOf: dcs:StatisticalVariable\n"
                  "populationType: dcs:Emissions\n"
-                 "emissionSourceType: dcs:NonBiogenicEmissionSource\n"
-                 "measurementQualifier: dcs:Annual{pv2}{pv3}\n"
+                 "measurementQualifier: dcs:Annual{pv2}{pv3}{pv4}\n"
                  "statType: dcs:measuredValue\n"
                  "measuredProperty: dcs:amount\n")
 
 _TMCF_TEMPLATE = (
-    "Node: E:airpollution_emission_trends_tier1->E0\n"
+    "Node: E:airpollution_emission_trends_county_tier1->E0\n"
     "typeOf: dcs:StatVarObservation\n"
-    "variableMeasured: C:airpollution_emission_trends_tier1->SV\n"
-    "measurementMethod: C:airpollution_emission_trends_tier1->"
-    "Measurement_Method\n"
-    "observationAbout: C:airpollution_emission_trends_tier1->geo_Id\n"
-    "observationDate: C:airpollution_emission_trends_tier1->year\n"
+    "variableMeasured: C:airpollution_emission_trends_county_tier1->SV\n"
+    "measurementMethod: C:airpollution_emission_trends_county_tier1->"
+    "measurement_method\n"
+    "observationAbout: C:airpollution_emission_trends_county_tier1->geo_Id\n"
+    "observationDate: C:airpollution_emission_trends_county_tier1->year\n"
     "unit: Ton\n"
-    "value: C:airpollution_emission_trends_tier1->observation\n")
+    "value: C:airpollution_emission_trends_county_tier1->observation\n")
 
 
 def _add_missing_columns(df: pd.DataFrame) -> pd.DataFrame:
@@ -65,6 +64,7 @@ def _add_missing_columns(df: pd.DataFrame) -> pd.DataFrame:
         df (pd.DataFrame): modified df as output
     """
     # Dropping the rows which contain PrescribedFire, Wildfire or Miscellaneous.
+    # combining thats why extra added
     # As they are not a part of StationaryFuelCombustion,
     # IndustrialAndOtherProcesses or Transportation group.
     df = df.drop(df[(df['SV'].str.contains('NaturalResources')) |
@@ -98,7 +98,7 @@ def _add_missing_columns(df: pd.DataFrame) -> pd.DataFrame:
     }
     df.loc[:, ('SV')] = df['SV'].replace(sourcegroups, regex=True)
     df = df.groupby(['year', 'geo_Id', 'SV']).sum().reset_index()
-    df['Measurement_Method'] = 'dcAggregate/EPA_NationalEmissionInventory'
+    df['measurement_method'] = 'dcAggregate/EPA_NationalEmissionInventory_Tier1Summary'
     return df
 
 
@@ -125,6 +125,7 @@ def _replace_pollutant(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
         }
     })
     return df
+
 
 def _replace_source_category(df: pd.DataFrame, column_name: str) ->\
                             pd.DataFrame:
@@ -173,7 +174,7 @@ def _replace_source_category(df: pd.DataFrame, column_name: str) ->\
     return df
 
 
-def _county_emissions(file_path: str, year:str) -> pd.DataFrame:
+def _national_emissions(df_national) -> pd.DataFrame:
     """
     Reads the file for state emissions data and cleans it for concatenation
     in Final CSV.
@@ -184,23 +185,77 @@ def _county_emissions(file_path: str, year:str) -> pd.DataFrame:
     Returns:
         df (pd.DataFrame): provides the cleaned df as output
     """
-    df = pd.read_csv(file_path, header=0)
+    df_national['geo_Id'] = 'country/USA'
+    df_national = df_national.groupby(
+        ['geo_Id', 'SV', 'year', 'measurement_method']).sum().reset_index()
+    return df_national
+
+
+def _state_emissions(df_state) -> pd.DataFrame:
+    """
+    Reads the file for state emissions data and cleans it for concatenation
+    in Final CSV.
+
+    Args:
+        file_path (str): path to excel file as the input
+
+    Returns:
+        df (pd.DataFrame): provides the cleaned df as output
+    """
+    df_state['geo_Id'] = (df_state['geo_Id'].map(str)).str[:len('geoId/NN')]
+    df_state = df_state.groupby(['year', 'geo_Id', 'SV',
+                                 'measurement_method']).sum().reset_index()
+
+    return df_state
+
+
+def _county_emissions(file_path: str, year: str) -> pd.DataFrame:
+    """
+    Reads the file for state emissions data and cleans it for concatenation
+    in Final CSV.
+
+    Args:
+        file_path (str): path to excel file as the input
+
+    Returns:
+        df (pd.DataFrame): provides the cleaned df as output
+    """
+    df = pd.read_csv(file_path, header=0, encoding='utf-8')
     df['year'] = year
-    # df['County'] = df['County'].astype(str) + " "
+    df_final = pd.DataFrame()
     df['geo_Id'] = ''
-    for index, row in df.iterrows():
-        temp = [val for key, val in COUNTY_MAP[row["Address"]].items() if row["County"] == key.replace(' County', '').replace(' Parish', '')]
-        df['geo_Id'][index] = df['geo_Id'][index] + (str(temp[:1]))
     # Dropping Unwanted Columns
-    df = df.drop(columns=['Address','County',' Unit of Measure','Pollutant Type'])
-    df = _replace_pollutant(df, 'Pollutant')
-    df = _replace_source_category(df, 'Tier')
-    df['SV'] = 'Annual_Amount_Emissions_' + df['Tier'] +\
-            '_NonBiogenicEmissionSource_' + df['Pollutant']
+    df = _replace_pollutant(df, 'POLLUTANT')
+    df = _replace_source_category(df, 'TIER')
+    df = df.rename(columns={'EMISSIONS': 'observation'})
+    df['geo_Id'] = 'geoId/' + (df['STATE_FIPS'].map(str)).str.zfill(2)\
+                + (df['COUNTY_FIPS'].map(str)).str.zfill(3)
+    df['measurement_method'] = 'EPA_NationalEmissionInventory_Tier1Summary'
+    df = process_file(df)
+    df_state = df.copy()
+    df_state = _state_emissions(df_state)
+    df_final = pd.concat([df_state, df_final])
+    df_final = pd.concat([df, df_final])
+    df_national = df.copy()
+    df_national = _national_emissions(df_national)
+    df_final = pd.concat([df_national, df_final])
+    return df_final
+
+
+def process_file(df):
+
+    df['emissionsourcetype'] = np.where(df['TIER'] == 'NaturalResources',
+                                        'BiogenicEmissionSource',
+                                        'NonBiogenicEmissionSource')
+    df['SV'] = 'Annual_Amount_Emissions_' + df['TIER'] +'_'+\
+        df['emissionsourcetype'] +'_'+ df['POLLUTANT']
     # Making copy and using group by to get Aggregated Values.
     df_agg = _add_missing_columns(df)
-    df['Measurement_Method'] = 'EPA_NationalEmissionInventory'
     df = pd.concat([df, df_agg])
+    df = df.drop(columns=[
+        'STATE', 'STATE_FIPS', 'UNIT OF MEASURE', 'POLLUTANT TYPE',
+        'COUNTY_FIPS', 'POLLUTANT', 'TIER', 'emissionsourcetype', 'COUNTY'
+    ])
     return df
 
 
@@ -244,13 +299,12 @@ class USCountyAirPollutionEmissionTrends:
             sv_replacement (dict) : Dictionary to replace SV names 
                                     with SV generator names
         """
-
+        emissiontype = ""
         final_mcf_template = ""
         sv_replacement = {}
         sv_checker = {
             "typeOf": "dcs:StatisticalVariable",
             "populationType": "dcs:Emissions",
-            "emissionSourceType": "dcs:NonBiogenicEmissionSource",
             "measurementQualifier": "dcs:Annual",
             "statType": "dcs:measuredValue",
             "measuredProperty": "dcs:amount"
@@ -267,13 +321,17 @@ class USCountyAirPollutionEmissionTrends:
             else:
                 source = '\nemissionSource: dcs:' + sv_property[-3]
                 sv_checker['emissionSource'] = sv_property[-3]
+            if "BiogenicEmissionSource" in sv_property[-2]:
+                emissiontype = '\nemissionSourceType: dcs:' + sv_property[-2]
+                sv_checker['emissionSourceType'] = sv_property[-2]
             pollutant = '\nemittedThing: dcs:' + sv_property[-1]
             sv_checker['emittedThing'] = 'dcs:' + sv_property[-1]
             generated_sv = get_statvar_dcid(sv_checker)
             if (generated_sv != sv):
                 sv_replacement[sv] = generated_sv
             final_mcf_template += _MCF_TEMPLATE.format(
-                pv1=generated_sv, pv2=source, pv3=pollutant) + "\n"
+                pv1=generated_sv, pv2=source, pv3=pollutant,
+                pv4=emissiontype) + "\n"
 
         # Writing Genereated MCF to local path.
         with open(self._mcf_file_path, 'w+', encoding='utf-8') as f_out:
@@ -300,14 +358,13 @@ class USCountyAirPollutionEmissionTrends:
         sv_list = []
 
         for file_path in self._input_files:
-            # Taking the File name out of the complete file address
-            # Used -1 to pickup the last part which is file name
-            # Read till -4 inorder to remove the .csv extension
+            # Taking the year out of the complete file address
+            # Used -18 and -14 to pickup year which is file name
             # and get the year.
-            year = file_path.split("/")[-1][:-4]
-            df = _county_emissions(file_path,year)
+            year = file_path[-18:-14].strip()
+            df = _county_emissions(file_path, year)
             final_df = pd.concat([final_df, df])
-            sv_list += df["SV"].to_list()
+            sv_list += df['SV'].to_list()
 
         final_df = final_df.sort_values(
             by=['geo_Id', 'year', 'SV', 'observation'])
@@ -317,12 +374,14 @@ class USCountyAirPollutionEmissionTrends:
         sv_list.sort()
         sv_replacement = self._generate_mcf(sv_list)
         self._generate_tmcf()
+
         final_df.loc[:, ('SV')] = final_df['SV'].replace(sv_replacement,
                                                          regex=True)
         final_df.to_csv(self._cleaned_csv_file_path, index=False)
 
 
 def main(_):
+    ip_files = ""
     input_path = FLAGS.input_path
     try:
         ip_files = os.listdir(input_path)
@@ -330,6 +389,7 @@ def main(_):
         logger.info("Run the download.py script first.")
         sys.exit(1)
     ip_files = [input_path + os.sep + file for file in ip_files]
+
     output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                     "output")
     # Defining Output Files
