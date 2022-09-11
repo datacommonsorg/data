db21bd9cadd896fe0c6abc66e1caa28d6291f84a
Chelsi-create <chelsiworld@gmail.com>
2022-09-11 14:40:45 +0530

    tmcf and mcf files added

--- scripts/india_nfhs/tmcf_mcf.py
+++ scripts/india_nfhs/tmcf_mcf.py
@@ -1,5 +1,82 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pandas as pd
 import os
+import json
+import re
+
+dict_isoCode = {
+    "Andhra Pradesh": "IN-AP",
+    "Andhra Pradesh Old": "IN-AP",
+    "Arunachal Pradesh": "IN-AR",
+    "Assam": "IN-AS",
+    "Bihar": "IN-BR",
+    "Chattisgarh": "IN-CT",
+    "Chhattisgarh": "IN-CT",
+    "Goa": "IN-GA",
+    "Gujarat": "IN-GJ",
+    "Haryana": "IN-HR",
+    "Himachal Pradesh": "IN-HP",
+    "Jharkhand": "IN-JH",
+    "Jharkhand#": "IN-JH",
+    "Karnataka": "IN-KA",
+    "Kerala": "IN-KL",
+    "Madhya Pradesh": "IN-MP",
+    "Madhya Pradesh#": "IN-MP",
+    "Maharashtra": "IN-MH",
+    "Manipur": "IN-MN",
+    "Meghalaya": "IN-ML",
+    "Mizoram": "IN-MZ",
+    "Nagaland": "IN-NL",
+    "Nagaland#": "IN-NL",
+    "Odisha": "IN-OR",
+    "Punjab": "IN-PB",
+    "Rajasthan": "IN-RJ",
+    "Sikkim": "IN-SK",
+    "Tamil Nadu": "IN-TN",
+    "Tamilnadu": "IN-TN",
+    "Telengana": "IN-TG",
+    "Telangana": "IN-TG",
+    "Tripura": "IN-TR",
+    "Uttarakhand": "IN-UT",
+    "Uttar Pradesh": "IN-UP",
+    "West Bengal": "IN-WB",
+    "Andaman and Nicobar Islands": "IN-AN",
+    "Andaman & Nicobar Islands": "IN-AN",
+    "Andaman & N. Island": "IN-AN",
+    "A & N Islands": "IN-AN",
+    "Chandigarh": "IN-CH",
+    "Dadra and Nagar Haveli": "IN-DN",
+    "Dadra & Nagar Haveli": "IN-DN",
+    "Dadar Nagar Haveli": "IN-DN",
+    "Daman and Diu": "IN-DD",
+    "Daman & Diu": "IN-DD",
+    "Delhi": "IN-DL",
+    "Jammu and Kashmir": "IN-JK",
+    "Jammu & Kashmir": "IN-JK",
+    "Ladakh": "IN-LA",
+    "Lakshadweep": "IN-LD",
+    "Lakshwadeep": "IN-LD",
+    "Pondicherry": "IN-PY",
+    "Puducherry": "IN-PY",
+    "Puduchery": "IN-PY",
+    "Dadra and Nagar Haveli and Daman and Diu": "IN-DN_DD",
+    "all India": "IN",
+    "all-India": "IN",
+    "All India": "IN"
+}
 
 TMCF_ISOCODE = """Node: E:{dataset_name}->E0
 typeOf: schema:Place
@@ -17,7 +94,8 @@ observationPeriod: "P1Y"
 value: C:{dataset_name}->{statvar}
 """
 
-MCF_NODES = """Node: dcid:{statvar}
+MCF_NODES = """
+Node: dcid:{statvar}
 description: "{description}"
 typeOf: dcs:StatisticalVariable
 populationType: schema:Person
@@ -48,6 +126,9 @@ class NFHSDataLoaderBase(object):
         self.raw_df = None
         self.cols_to_extract = list(self.cols_dict.keys())[3:]
 
+    def generate_csv(self):
+        pass
+
     def create_mcf_tmcf(self):
         """
         Class method to generate MCF and TMCF files for the current dataset.
@@ -75,6 +156,22 @@ class NFHSDataLoaderBase(object):
                     mcf.write(
                         MCF_NODES.format(
                             statvar=self.cols_dict[variable],
-                            description=self.cols_dict[variable]))
+                            # descript=self.cols_dict[variable].replace('_', ' '),
+                            description = re.sub(r"(\w)([A-Z])", r"\1 \2", self.cols_dict[variable].replace('_', ' '))))
 
                     statvars_written.append(self.cols_dict[variable])
+
+with open('india_nfhs.json') as json_file:
+    cols_to_nodes = json.load(json_file)
+
+module_dir = os.path.dirname(__file__)
+
+if __name__ == '__main__':
+    dataset_name = "NFHS_Health"
+    data_folder = os.path.join(module_dir, '../data/')
+    loader = NFHSDataLoaderBase(data_folder=data_folder,
+                               dataset_name=dataset_name,
+                               cols_dict=cols_to_nodes,
+                               module_dir=module_dir)
+
+    loader.create_mcf_tmcf()
