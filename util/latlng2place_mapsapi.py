b1656bf09e8737fef4ea770d6ff229db5384322e
Prashanth Radhakrishnan <shanth@google.com>
2022-10-12 17:01:54 +0000

    Add README

--- util/latlng2place_mapsapi.py
+++ util/latlng2place_mapsapi.py
@@ -1,10 +1,26 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Library that converts a lat/lng into one or more place DCIDs using Maps API.
 
 Currently supports resolution from lat/lng to admin-area2 (county-equivalent)
 places, admin-area1 (state-equivalent) places and countries.
 
+IMPORTANT NOTE: Please do not hardcode the API key in the calling programs,
+instead pass it as arguments while running them.
+
 Example:
-  ll2p = latlng2place_mapsapi.Resolver(my_cache_file)
+  ll2p = latlng2place_mapsapi.Resolver(api_key='XYZ')
   try:
     dcids = ll2p.resolve(12.0, -34.0)
   except:
