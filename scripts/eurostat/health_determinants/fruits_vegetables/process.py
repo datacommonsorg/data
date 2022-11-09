93eb3faf2e3794fb16407b2ce8aca93fa371f2ae
Naveen N <naveenkn@google.com>
2022-11-09 16:31:29 +0530

    eurostat - fruits and vegetables changes

--- scripts/eurostat/health_determinants/fruits_vegetables/process.py
+++ scripts/eurostat/health_determinants/fruits_vegetables/process.py
@@ -195,7 +195,7 @@ class EuroStatFruitsVegetables(EuroStat):
 
 if __name__ == '__main__':
     input_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                              "input_files")                        
+                              "input_files")
     ip_files = os.listdir(input_path)
     ip_files = [input_path + os.sep + file for file in ip_files]
 
