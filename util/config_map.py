ed07a62a8915bd97017a66ede4edb5ea92b93326
Ajai Tirumali <ajaits@google.com>
2022-11-18 16:55:53 +0530

    Add utility files from PR 764

---
+++ util/config_map.py
@@ -0,0 +1,230 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''Class to store configuration parameters as a dictionary.
+
+Creates global flags:
+--config_file
+--config_params
+
+Usage:
+   # Load config map from a json file with comments and trailing ,:
+   #   {
+   #     'param1': <value>,
+   #
+   #     # Additional params
+   #     'param2': { 1: 'abc', 2: 'def', },
+   #   }
+   #
+   config_map = get_config_map_from_file('my_config.json')
+   config_map.get('param1', 123) # returns <value>
+   config_map.get('new_param', 'lmn') # returns lmn
+
+   # To load from command-line flags, set the following:
+   #  python3 ... \
+   #   --config_file=<my-config-file.json \
+   #   --config_params={'param1': <new-value> }'
+   #
+   config_map = ConfigMap()
+   config_map.get('param1', 123) # returns <new-value>
+'''
+
+import ast
+import collections.abc
+import pprint
+import sys
+
+from absl import app
+from absl import flags
+from absl import logging
+from collections import OrderedDict
+from typing import Union
+
+_FLAGS = flags.FLAGS
+
+flags.DEFINE_string('config_file', '', 'File with configuration parameters.')
+flags.DEFINE_string(
+    'config_params', '',
+    'Python dictionary as a string with config parameters. '
+    'Overrides settings loaded from --config_file.')
+flags.DEFINE_integer('log_level', logging.INFO,
+                     'Log level messages to be shown.')
+
+_FLAGS(sys.argv)  # Allow invocation without app.run()
+
+
+def _deep_update(src: dict, add_dict: dict) -> dict:
+    '''Deep update of parameters in add_dict into src.
+
+    Args:
+      src: source dictionary into which new parameters are added.
+      add_dict: dictionary with new parameters to be added.
+
+    Returns:
+      src dictionary with updated parameters.
+
+    Note:
+    Assumes the new dictionary has same type(dict/list) for updated parameters.
+    '''
+    for k, v in add_dict.items():
+        if isinstance(v, collections.abc.Mapping):
+            src[k] = _deep_update(src.get(k, {}), v)
+        elif isinstance(v, list):
+            # TODO: deep update of list
+            src[k].extend(v)
+        elif isinstance(v, set):
+            # TODO: deep update of set
+            src[k].update(v)
+        else:
+            src[k] = v
+    return src
+
+
+def get_py_dict_from_file(filename: str) -> dict:
+    '''Load a python dict from a file.
+
+    Args:
+      filename: JSON or a python file containing dict of parameter to value mappings.
+        The file can have comments and extra commas at the end.
+        Example: '{ 'abc': 123, 'def': 'lmn' }
+        Note: It assumes bools are in Python: True, False and None is used for 'null'.
+
+    Returns:
+      dictionary loaded from the file.
+
+    Raises:
+      exceptions on parsing errors string dict from literal_eval()
+    '''
+    logging.info(f'Loading python dict from {filename}...')
+    with open(filename) as file:
+        dict_str = file.read()
+
+    # Load the map assuming a python dictionary.
+    # Can also be used with JSON with trailing commas and comments.
+    param_dict = ast.literal_eval(dict_str)
+    logging.debug(f'Loaded {filename} into dict {param_dict}')
+    return param_dict
+
+
+class ConfigMap:
+    '''Class to store config mapping of named parameters to values as a dictionary.'''
+
+    def __init__(self, config_dict: dict = None,
+                 filename: str = _FLAGS.config_file,
+                 override_params: str = _FLAGS.config_params):
+        self._config_dict = dict()
+        # Add configs from input args.
+        if config_dict:
+            self.add_configs(config_dict)
+        # Add configs from file.
+        if filename:
+            self.load_config_file(filename)
+        # Add additional configs from dictionary string.
+        if override_params:
+            param_dict = ast.literal_eval(override_params)
+            self.add_configs(param_dict)
+        logging.set_verbosity(self.get('log_level', _FLAGS.log_level))
+        logging.debug(f'Using config: {self.get_configs()}')
+
+    def load_config_file(self, filename: str) -> dict:
+        '''Load configs from a file overwriting any existing parameter with a new value.
+
+        Args:
+            filename: a py or json file with a dictionary of parameter:value mappings.
+
+        Returns:
+          dictionary with all config parameters after updates from the file.
+          '''
+        self.add_configs(get_py_dict_from_file(filename))
+        return self._config_dict
+
+    def add_configs(self, configs: dict) -> dict:
+        '''Add new or replace existing config parameters
+
+        Nested parameters with dict, or list values are replaced.
+        Use update_config() for a deep-update of nested parameters.
+
+        Args:
+            configs: dictionary with new parameter:value mappings
+
+        Returns:
+            dictionary with all parameter:value mappings.
+        '''
+        if configs:
+            self._config_dict.update(configs)
+        return self._config_dict
+
+    def update_config(self, configs: dict) -> dict:
+        '''Does a deep update of the dict updating nested dicts as well.
+
+        Args:
+            configs: dictionary with additional parameter:value mappings.
+
+        Returns:
+            dictionary with all parameter:value mappings.
+        '''
+        return _deep_update(self._config_dict, configs)
+
+    def get(self,
+            parameter: str,
+            default_value=None) -> Union[str, int, float, list, dict]:
+        '''Return the value of a named config parameter.
+
+        Args:
+            parameter: name of the parameter to lookup
+            default_value: Default value to be returned if the parameter doesn't exist.
+
+        Returns:
+            value of the parameter in the config dict if it exists or the default_value.
+        '''
+        return self._config_dict.get(parameter, default_value)
+
+    def get_configs(self) -> dict:
+        '''Return a reference to the config dictionary.
+
+        Any modifications to the dict is reflected within this object as well.
+        '''
+        return self._config_dict
+
+    def set_config(self, parameter: str, value):
+        '''Set the value for a parameter overwriting one if it already exists
+        Args:
+          parameter: Name of the parameter
+          value: Value to be set.
+        '''
+        self._config_dict[parameter] = value
+
+    def get_config_str(self) -> str:
+        '''Returns the config dictionary as a pretty string.'''
+        return pprint.format(self._config_dict, indent=4)
+
+    def write_config(filename: str):
+        '''Write the config dictionary into a file.
+
+        Args:
+          filename: name of the file to write.
+        '''
+        with open(filename, 'w') as file:
+            file.write(self.get_config_str())
+
+
+def get_config_map_from_file(filename: str) -> ConfigMap:
+    '''Returns a ConfigMap object with parameters loaded from a file.
+
+    Args:
+      filename: name of the file to load.
+
+    Returns:
+      ConfigMap object with all the parameters loaded into the config_dict.
+    '''
+    return ConfigMap(filename=filename)
---
+++ util/counters.py
@@ -0,0 +1,210 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''Class for dictionary of named counters.'''
+
+import sys
+import time
+
+
+class Counters():
+    '''Dictionary of named counters.
+
+    Example usage:
+      counters = Counters(prefix='my_process')
+      ...
+      counters.add_counter('input_rows')
+         .add_counter('output_rows', 10)
+
+      # Print counters on STDERR
+      counters.print_counters()
+      #   my_process_input_rows = 1
+      #  my_process_output_rows = 10
+
+    Note: This object is not thread-safe.
+    '''
+
+    # Default settings.
+    _default_options = {
+        # Enable debug counters with additional suffixes.
+        'debug': False,
+
+        # emit counters once every 30 secs.
+        'show_every_n_sec': 30,
+
+        # counter for records processed, used for computing processing rate.
+        'processed_counter': 'inputs',
+        # counter with total inputs, used for computing remaining time.
+        'total_counter': 'total',
+    }
+
+    def __init__(self,
+                 counters_dict: dict = None,
+                 prefix: str = '',
+                 options: dict = None):
+        '''Initialize the counters.
+
+        Args:
+          counters_dict: dictionary of pre-existing counters to updated.
+            Note that it updates existing reference to counters.
+          debug: Enable or disable debug context counters.
+          options: Dictionary with parameter:values.
+        '''
+        self._counters = counters_dict
+        if counters_dict is None:
+            self._counters = {}
+        self._prefix = prefix
+        self._options = dict(self._default_options)
+        if options:
+            self._options.update(options)
+
+        # Internal state
+        self._start_time = time.perf_counter()
+
+    def _get_counter_name(self, name: str, debug_context: str = None):
+        '''Returns the name of the counter with debug context.'''
+        name = f'{self._prefix}{name}'
+        if debug_context:
+            name = name + f'_{debug_context}'
+        return name
+
+    def _add_counter(self,
+                     counter_name: str,
+                     value: int = 1,
+                     debug_context: str = ''):
+        '''Incrment counter with name by given value.'''
+        name = self._get_counter_name(counter_name)
+        self._counters[name] = self._counters.get(name, 0) + value
+        if debug_context and self._options.get('debug', False):
+            # Add debug counter with the context message.
+            ext_name = self._get_counter_name(counter_name, debug_context)
+            self._counters[ext_name] = self._counters.get(ext_name, 0) + value
+
+    def _update_processing_rate(self):
+        '''Update the processing rate and remaining time.
+        Uses the option: 'processed' to get the counter for processing rate
+        and option 'inputs' to estimate remaining time.
+        '''
+        time_taken = time.perf_counter() - self._start_time + 0.0001
+        self._add_counter('process_elapsed_time', time_taken)
+        num_processed = self.get_counter(self._options['processed_counter'])
+        rate = 0.0001
+        if num_processed:
+            rate = num_processed / time_taken
+            self._add_counter('processing_rate', rate)
+        totals = self.get_counter(self._options['total_counter'])
+        if totals:
+            self._add_counter('process_remaining_time',
+                              max(0, (totals - num_processed)) / rate)
+
+    def _print_counters_periodically(self):
+        '''Display the counters periodically by time or by number of calls
+        based on the option 'show_counters_every_sec' or 'show_counters_every_n'.
+        '''
+        interval = self._options.get('show_every_n_sec', 30)
+        if interval and isinstance(interval, int):
+            elapsed_time = int(time.perf_counter() - self._start_time)
+            if elapsed_time > 0 and elapsed_time % interval:
+                self.print_counters()
+
+    def add_counter(self,
+                    counter_name: str,
+                    value: int = 1,
+                    debug_context: str = None):
+        '''Increment a named counter and degun counter by the given value.
+        Also displays counters periodically based on option 'show_every_n_sec'.
+
+        Args:
+            name: Name of the counter to update
+            value: value to be added to the counter
+            debug_context: optional suffix for the debuf counter.
+
+        Returns:
+          this Counters object
+        '''
+        self._add_counter(counter_name, value, debug_context)
+        # Diplay all counters if required.
+        self._print_counters_periodically()
+        return self
+
+    def add_counters(self, counters_dict: dict):
+        '''Add all counters from the given dict.
+
+        Args:
+          counters_dict: dictionary of counter values.
+
+        Returns:
+          this Counters object
+        '''
+        if counters_dict:
+            for counter, value in counters_dict.items():
+                self.add_counter(counter, value)
+        return self
+
+    def set_counter(self,
+                    name: str,
+                    value: int,
+                    debug_context: str = None):
+        '''Set the value of a counter overwriting any previous value.
+
+        Args:
+          name: Name of the counter to set.
+          value: counter value to set it to.
+          debug_context: Debug context for the counter.
+
+        Returns:
+          this Counters object
+        '''
+        self._counters[self._get_counter_name(name)] = value
+        if debug_context:
+            self._counters[self._get_counter_name(name, debug_context)] = value
+        return self
+
+    def get_counters(self) -> dict:
+        '''Return the dictionary of all counter:values.'''
+        return self._counters
+
+    def get_counter(self, name: str) -> int:
+        '''Return the value of a named counter.
+
+        Args:
+          name: Name of the counter to lookup.
+
+        Returns:
+          value if the counter if it exists, 0 otherwise.
+        '''
+        return self._counters.get(self._get_counter_name(name), 0)
+
+    def get_counters_string(self) -> str:
+        '''Returns a formatted string of counter and values sorted by name.'''
+        lines = ['Counters:']
+        for c in sorted(self._counters.keys()):
+            v = self._counters[c]
+            if isinstance(v, int):
+                lines.append(f'{c:>50s} = {v:>10d}')
+            elif isinstance(v, float):
+                lines.append(f'{c:>50s} = {v:>10.2f}')
+            else:
+                lines.append(f'{c:>50s} = {v}')
+        return '\n'.join(lines)
+
+    def print_counters(self, file=sys.stderr):
+        '''Print the counter values.
+        If a file is specified, emits the counters to the file.
+        If no file is specified, displays on error console.
+
+        Args:
+            file: file handle to emit counters string.
+        '''
+        self._update_processing_rate()
+        print(self.get_counters_string(), file=file)
