d117f6ec5cd864f7867bd1227c1d8458eea72f1d
lucy-kind <kind@google.com>
2022-12-07 21:53:30 -0800

    Upload script and output .csv
    Output .csv and preprocess script (TODO: change country name to 'country/ABC')

---
+++ scripts/fao/test.py
@@ -0,0 +1,60 @@
+import csv
+import pycountry
+
+# Open the original .csv file for reading
+with open('ExchangeRate_Currency.csv', 'r', encoding='ISO-8859-1') as csv_in:
+    # Open the new .csv file for writing
+    with open('output.csv', 'w') as csv_out:
+        # Create a reader and writer object
+        reader = csv.reader(csv_in)
+        writer = csv.writer(csv_out)
+        headers = next(reader)
+
+        # Define columns for cleaned .csv file
+        headers[0] = 'Country'
+        headers[1] = 'ISOCurrencyCode'
+        headers[2] = 'Currency'
+        headers[3] = 'Year-Month'
+        headers[4] = 'ObservationPeriod'
+        headers[5] = 'ExchangeRatePerUSD'
+        headers[6] = 'ExchangeRatePerUSD, Standardized'
+        headers[7] = '# Comments'
+        headers[8:14] = ''
+
+        writer.writerow(headers)
+
+        # Clean rows in the original file
+        for row in reader:
+            new_row = []
+
+            new_row.append(row[2])
+            new_row.append(row[3])
+            new_row.append(row[5])
+
+            # Create Year-Month column
+            if row[9] == '7021':
+              new_row.append(row[8])
+            else:
+              new_row.append(row[8] + '-' + row[9][2:])
+
+            # Define Observation Period:
+            if row[9] == '7021':
+                new_row.append('P1Y')
+            else:
+                new_row.append('P1M')
+
+            # Define value as local or standardized exchange rate per USD
+            if row[3] == 'LCU':
+                new_row.append(row[12])
+            else:
+                new_row.append('')
+            if row[3] == 'SLC':
+                new_row.append(row[12])
+            else:
+                new_row.append('')
+
+            # Create a new column called "# Comments" with original row information
+            new_row.append("#" + str(reader.line_num) + ' row original')
+
+
+            writer.writerow(new_row)
