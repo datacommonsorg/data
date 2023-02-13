e056b0dfcb66083f369ade505340e9b631702bf0
Jehangir Amjad <jehangiramjad@gmail.com>
2023-02-13 13:25:00 -0800

    replacing // with ++

--- scripts/us_census/enhanced_tmcf/process_etmcf.py
+++ scripts/us_census/enhanced_tmcf/process_etmcf.py
@@ -128,7 +128,7 @@ def _process_var_measured_new_column(
 
     # TODO: remove these checks when a better format for opaque mapping is agreed.
     query_format = query_format.replace("?", "__")
-    query_format = query_format.replace("&", "//")
+    query_format = query_format.replace("&", "++")
     query_format = query_format.replace("=", "--")
 
     # Verify that such a new column does not already exist.
