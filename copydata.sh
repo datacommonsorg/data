e411a8fa961689650091716793d49375a18a8800
Bo Xu <shifucun@users.noreply.github.com>
2023-02-15 09:26:27 -0800

    resolve comments

--- copydata.sh
+++ copydata.sh
@@ -18,22 +18,35 @@
 # The layout under each folder should match the storage layout in
 # https://docs.datacommons.org/custom_dc/upload_data.html
 
+
+set -x
+
+# Define a map from folder path under this repo, to the GCS path to copy data to.
 declare -A foldermap
 foldermap["scripts/stanford"]="gs://datcom-stanford-resources/imports"
 
 
+# This function run "git diff" between latest and the previosu commit on the
+# specified folder (from function argument $1) and output a list of file names.
 changes() {
   git diff --name-only --diff-filter=AMDR --cached HEAD^ $1
 }
 
+# Iterate target folder
 for folder in ${!foldermap[@]}; do
+  # Get the GCS path to copy data to
   gcs_path=${foldermap[$folder]}
-  if [[ $(changes "$folder") == "" ]];  then
+  # If the current commit touches files in the target folder, copy data.
+  if [[ $(changes "$folder") != "" ]];  then
     echo "Copy data to $gcs_path"
     for dir in $(find $folder -type d -maxdepth 1 -mindepth 1)
     do
       import_group=$(basename $dir)
       echo $import_group
+      # Should only copy content under /data folder for each import group.
+      # In GCS, there are /internal folders that should not be overwrite.
+      # TODO(shifucun): Handle copy GCS reference. In this case, user would
+      # store a link but not actual data in this repo.
       gsutil cp -r "$folder/$import_group/data/*" "$gcs_path/$import_group/data/"
     done
   fi
