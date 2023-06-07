8061182dd1ad275a45dc45beb7d19abc2759a520
Samantha Piekos <Samantha.Piekos@isbscience.org>
2023-06-07 15:13:15 -0700

    Add header

--- scripts/biomedical/ICTV_Taxonomy/scripts/download.sh
+++ scripts/biomedical/ICTV_Taxonomy/scripts/download.sh
@@ -1,7 +1,23 @@
-'''
-This file downloads the most recent version of the ICTV Master Species List and
-Virus Metadata Resource and prepares it for processing
-'''
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Author: Samantha Piekos
+Date: 03/22/2023
+Name: create_virus_taxonomic_ranking_enums
+Description: This file downloads the most recent version of the ICTV Master 
+Species List and Virus Metadata Resource and prepares it for processing
+"""
 #!/bin/bash
 
 # make input directory
