8f0dfc470227044a54895e98d69abd00a6fc1092
Keyur Shah <keyurs@google.com>
2023-08-03 17:05:05 -0700

    Revert integration_test.py changes.

--- import-automation/executor/test/integration_test.py
+++ import-automation/executor/test/integration_test.py
@@ -24,12 +24,10 @@ from test import utils
 NUM_LINES_TO_CHECK = 50
 
 CONFIGS = {
-    # 'github_repo_owner_username': os.environ['GITHUB_AUTH_USERNAME'],
-    'github_repo_owner_username': 'datacommonsorg',
+    'github_repo_owner_username': os.environ['GITHUB_AUTH_USERNAME'],
     'github_repo_name': 'data-demo',
     'github_auth_username': 'intrepiditee',
-    # 'github_auth_access_token': os.environ['GITHUB_AUTH_ACCESS_TOKEN']
-    'github_auth_access_token': ''
+    'github_auth_access_token': os.environ['GITHUB_AUTH_ACCESS_TOKEN']
 }
 
 
