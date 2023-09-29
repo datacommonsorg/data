a69beeb4b0ac146da38e901bec335986f265acf8
Natalie Diaz <nhdiaz@google.com>
2023-09-29 17:25:45 +0000

    test test

--- scripts/un/sdg/geography.py
+++ scripts/un/sdg/geography.py
@@ -26,9 +26,9 @@ import json
 import os
 import sys
 
-sys.path.append(
-    os.path.dirname(os.path.dirname(os.path.dirname(
-        os.path.abspath(__file__)))))
+#sys.path.append(
+#    os.path.dirname(os.path.dirname(os.path.dirname(
+#        os.path.abspath(__file__)))))
 
 module_dir_ = os.path.dirname(__file__)
 
