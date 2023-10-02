c8d1479dbbd1d7e9e8fa8c7a805fa6cd5c17e164
Natalie Diaz <nhdiaz@google.com>
2023-10-02 17:34:06 +0000

    test

--- scripts/un/sdg/geography_test.py
+++ scripts/un/sdg/geography_test.py
@@ -29,8 +29,25 @@ module_dir_ = os.path.dirname(__file__)
 
 FOLDER = os.path.join(module_dir_, 'testdata/test_geography')
 
+# Mock input data.
+SDG2TYPE = {'4': 'Country'}
+UN2SDG = {
+    'undata-geo:G00000020': '4',
+}
+SDG2UN = {'4': 'undata-geo:G00000020'}
+UN2DC = {
+    'undata-geo:G00000020': ('country/AFG', 'Country', 'Afghanistan'),
+    'undata-geo:G00003250': ('country/ARE', 'Country', 'United Arab Emirates'),
+    'undata-geo:G00100000': ('Earth', 'Place', 'World'),
+    'undata-geo:G00114000': ('asia', 'Continent', 'Asia'),
+    'undata-geo:G00119000': ('SouthernAsia', 'UNGeoRegion', 'Southern Asia'),
+    'undata-geo:G00120000': ('WesternAsia', 'UNGeoRegion', 'Western Asia'),
+    'undata-geo:G00403000': ('undata-geo/G00403000', 'GeoRegion',
+                             'Landlocked developing countries (LLDCs)'),
+    'undata-geo:G00404000': ('undata-geo/G00404000', 'GeoRegion',
+                             'Least developed countries (LDCs)'),
+}
 UN2DC2 = {
-    'undata-geo:G00000010': ('undata-geo/G00000010', 'GeoRegion', 'Abu Dhabi'),
     'undata-geo:G00000030': ('undata-geo/G00000030', 'GeoRegion', 'Ajman')
 }
 
@@ -39,30 +56,18 @@ UN2DC2_FULL = {
     **UN2DC2,
     **{
         'undata-geo:G00403300': ('undata-geo/G00403300', 'GeoRegion', 'Landlocked developing countries (LLDCs): Asia'),
-        'undata-geo:G00404100': ('undata-geo/G00404100', 'GeoRegion', 'Least developed countries (LDCs): Africa'),
         'undata-geo:G00404300': ('undata-geo/G00404300', 'GeoRegion', 'Least developed countries (LDCs): Asia'),
-        'undata-geo:G00405200': ('undata-geo/G00405200', 'GeoRegion', 'Small Island Developing States (SIDS): Americas'),
-        'undata-geo:G00405400': ('undata-geo/G00405400', 'GeoRegion', 'Small Island Developing States (SIDS): Oceania')
     }
 }
 
-NEW_SUBJECTS = {('undata-geo/G00000030', 'GeoRegion'),
-                ('undata-geo/G00000010', 'GeoRegion')}
+# Test intermediate output data.
+NEW_SUBJECTS = {('undata-geo/G00000030', 'GeoRegion')}
 CONTAINMENT = {
-    ('undata-geo/G00000010', 'GeoRegion'): ['Earth', 'asia', 'WesternAsia'],
     ('country/AFG', 'Country'): [
         'SouthernAsia', 'undata-geo/G00403000', 'undata-geo/G00403300',
         'undata-geo/G00404000', 'undata-geo/G00404300'
     ],
     ('undata-geo/G00000030', 'GeoRegion'): ['Earth', 'asia', 'WesternAsia'],
-    ('country/ALA', 'Country'): ['NorthernEurope'],
-    ('country/ALB', 'Country'): ['SouthernEurope'],
-    ('country/DZA', 'Country'): ['NorthernAfrica'],
-    ('country/AND', 'Country'): ['SouthernEurope'],
-    ('country/AGO', 'Country'): [
-        'SubSaharanAfrica', 'MiddleAfrica', 'undata-geo/G00404000',
-        'undata-geo/G00404100'
-    ]
 }
 
 
@@ -78,19 +83,11 @@ class GeographyTest(unittest.TestCase):
                                                  'Country', 'country/USA'))
 
     def test_write_un_places(self):
-        sdg2type = geography.get_sdg2type(
-            os.path.join(module_dir_, 'sdg-dataset/output/SDG_geographies.csv'))
-        un2sdg, _ = geography.get_sdg_un_maps(
-            os.path.join(
-                module_dir_,
-                'sssom-mappings/output_mappings/undata-geo__sdg-geo.csv'))
-        un2dc = geography.get_un2dc(
-            os.path.join(module_dir_, 'geography/places.csv'))
         with tempfile.TemporaryDirectory() as tmp_dir:
             output = os.path.join(tmp_dir, 'un_places.mcf')
             un2dc2, new_subjects = geography.write_un_places(
-                os.path.join(FOLDER, 'test_geographies.csv'), output, sdg2type,
-                un2sdg, un2dc)
+                os.path.join(FOLDER, 'test_geographies.csv'), output, SDG2TYPE,
+                UN2SDG, UN2DC)
             with open(output) as result:
                 with open(os.path.join(FOLDER,
                                        'expected_un_places.mcf')) as expected:
@@ -99,10 +96,8 @@ class GeographyTest(unittest.TestCase):
             self.assertEqual(new_subjects, NEW_SUBJECTS)
 
     def test_process_containment(self):
-        un2dc = geography.get_un2dc(
-            os.path.join(module_dir_, 'geography/places.csv'))
         containment = geography.process_containment(
-            os.path.join(FOLDER, 'test_geography_hierarchy.csv'), un2dc,
+            os.path.join(FOLDER, 'test_geography_hierarchy.csv'), UN2DC,
             UN2DC2_FULL)
         self.assertEqual(containment, CONTAINMENT)
 
@@ -116,16 +111,10 @@ class GeographyTest(unittest.TestCase):
                     self.assertEqual(result.read(), expected.read())
 
     def test_write_place_mappings(self):
-        _, sdg2un = geography.get_sdg_un_maps(
-            os.path.join(
-                module_dir_,
-                'sssom-mappings/output_mappings/undata-geo__sdg-geo.csv'))
-        un2dc = geography.get_un2dc(
-            os.path.join(module_dir_, 'geography/places.csv'))
         with tempfile.TemporaryDirectory() as tmp_dir:
             output = os.path.join(tmp_dir, 'place_mappings.csv')
-            geography.write_place_mappings(os.path.join(FOLDER, output), sdg2un,
-                                           un2dc, UN2DC2_FULL)
+            geography.write_place_mappings(os.path.join(FOLDER, output), SDG2UN,
+                                           UN2DC, UN2DC2_FULL)
             with open(output) as result:
                 with open(os.path.join(
                         FOLDER, 'expected_place_mappings.csv')) as expected:
