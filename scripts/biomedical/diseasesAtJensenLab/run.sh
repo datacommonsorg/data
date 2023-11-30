bf3332f724c50d479c4689ec45cf9928442d1240
Samantha Piekos <spiekos@google.com>
2023-11-29 22:49:18 -0800

    Update run.sh
    Add commands to combine codingGenes-textMining csv files into a single csv

--- scripts/biomedical/diseasesAtJensenLab/run.sh
+++ scripts/biomedical/diseasesAtJensenLab/run.sh
@@ -10,4 +10,6 @@ curl https://download.jensenlab.org/human_disease_experiments_full.tsv --output 
 # runs the script 
 python3 ../format_disease_jensen_lab.py
 
-
+# combine coding genes from textmining into single csv file
+( cat codingGenes-textMining-1.csv; tail -n +2 codingGenes-textMining-2.csv ) > codingGenes-textMining.csv
+rm codingGenes-textMining-*.csv
