dc5dcf16ca9652f773bfc9f9ed519c7da90e16d2
Jehangir Amjad <1021616+jehangiramjad@users.noreply.github.com>
2024-02-05 22:52:06 +0500

    [Import Automation] Support overriding GCS configs locally (#978)
    * Script + README to update and import from the local machine
    
    * Update imports locally
    
    * responding to reviewer comments
    
    * more reviewer comments
    
    * Enable overriding configs locally
    
    * removing the option to provide user script args as a flag
    
    * parsing user script configs from local file
    
    * typo fix
    
    * lint fixes
    
    * lint fixes
    
    * updating

--- import-automation/executor/schedule_update_import.py
+++ import-automation/executor/schedule_update_import.py
@@ -29,6 +29,8 @@ from app.service import file_uploader
 from app.service import github_api
 from google.cloud import storage
 
+_CONFIG_OVERRIDE_FILE = 'config_override.json'
+
 _FLAGS = flags.FLAGS
 
 flags.DEFINE_string('mode', '', 'Options: update or schedule.')
@@ -47,6 +49,17 @@ flags.DEFINE_string(
 _FLAGS(sys.argv)
 
 logging.basicConfig(level=logging.INFO)
+
+
+def _override_configs(filename: str,
+                      config: configs.ExecutorConfig) -> configs.ExecutorConfig:
+    # Read configs from the local file.
+    d = json.load(open(filename))
+
+    # Update config with any fields and values provided in the local file.
+    # In case of any errors, the line below will raise an Exception which will
+    # report the problem which shoud be fixed in the local config json file.
+    return dataclasses.replace(config, **d["configs"])
 
 
 def _get_cloud_config() -> configs.ExecutorConfig:
@@ -155,7 +168,6 @@ def update(cfg: configs.ExecutorConfig,
 def main(_):
     mode = _FLAGS.mode
     absolute_import_path = _FLAGS.absolute_import_path
-    import_script_args = _FLAGS.import_script_args
 
     if not _FLAGS.config_project_id:
         raise Exception("Flag: config_project_if must be provided.")
@@ -177,8 +189,15 @@ def main(_):
     logging.info(f'Import: {absolute_import_path}')
     logging.info(f'Repo root directory: {repo_dir}')
 
-    # TODO: allow overriding/updating config params from a local config file as well.
+    # Loading configs from GCS and then using _CONFIG_OVERRIDE_FILE to
+    # override any fields provided in the file.
+    logging.info('Reading configs from GCS.')
     cfg = _get_cloud_config()
+
+    logging.info(
+        f'Updating any config fields from local file: {_CONFIG_OVERRIDE_FILE}.')
+    cfg = _override_configs(_CONFIG_OVERRIDE_FILE, cfg)
+
     if mode == 'update':
         logging.info("*************************************************")
         logging.info("***** Beginning Update. Can take a while. *******")
