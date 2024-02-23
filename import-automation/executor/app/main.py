0e91d7513e632c958ea68e8b7b71c7a344c2125c
Jehangir Amjad <jehangiramjad@gmail.com>
2024-02-23 14:15:20 -0800

    removing dashboard

--- import-automation/executor/app/main.py
+++ import-automation/executor/app/main.py
@@ -22,6 +22,7 @@ The endpoints are:
 """
 
 import dataclasses
+import logging
 
 import flask
 from google.cloud import scheduler
@@ -31,10 +32,10 @@ from app.executor import validation
 from app.executor.scheduler_job_manager import schedule_on_commit
 from app.executor import import_executor
 from app.service import file_uploader
-from app.service import dashboard_api
 from app.service import github_api
 from app.service import email_notifier
 from app.service import import_service
+from app.service import email_notifier
 
 
 def create_app():
@@ -69,7 +70,6 @@ def execute_imports():
             auth_username=config.github_auth_username,
             auth_access_token=config.github_auth_access_token),
         config=config,
-        dashboard=dashboard_api.DashboardAPI(config.dashboard_oauth_client_id),
         notifier=email_notifier.EmailNotifier(config.email_account,
                                               config.email_token),
         importer=import_service.ImportServiceClient(
@@ -79,10 +79,9 @@ def execute_imports():
             unresolved_mcf_bucket_name=config.storage_dev_bucket_name,
             resolved_mcf_bucket_name=config.storage_importer_bucket_name,
             client_id=config.importer_oauth_client_id))
-    result = executor.execute_imports_on_commit(commit_sha=commit_sha,
-                                                repo_name=repo_name,
-                                                branch_name=branch_name,
-                                                pr_number=pr_number)
+    result = executor.execute_imports_on_commit(commit_sha=commit_sha)
+    logging.info(result)
+    logging.info("END: Returning from REST endopoint.")
     return dataclasses.asdict(result)
 
 
@@ -103,9 +102,13 @@ def scheduled_updates():
             repo_name=config.github_repo_name,
             auth_username=config.github_auth_username,
             auth_access_token=config.github_auth_access_token),
-        config=config)
+        config=config,
+        notifier=email_notifier.EmailNotifier(config.email_account,
+                                              config.email_token))
     result = executor.execute_imports_on_update(
         task_info['absolute_import_name'])
+    logging.info(result)
+    logging.info("END: Returning from REST endopoint.")
     return dataclasses.asdict(result)
 
 
@@ -132,13 +135,11 @@ def schedule_data_refresh_cron_jobs():
         auth_username=config.github_auth_username,
         auth_access_token=config.github_auth_access_token)
 
-    res = import_executor.run_and_handle_exception(
-        None,  # run_id
-        None,  # dashboard
-        schedule_on_commit,
-        github,
-        config,
-        task_info['COMMIT_SHA'])
+    res = import_executor.run_and_handle_exception(schedule_on_commit, github,
+                                                   config,
+                                                   task_info['COMMIT_SHA'])
+    logging.info(res)
+    logging.info("END: Returning from REST endopoint.")
     return dataclasses.asdict(res)
 
 
