5941d2e0a4f9673ca2ce082f4a97f9644a836f56
Samantha Piekos <spiekos@google.com>
2024-03-05 16:17:11 -0800

    Update tests.sh
    add step that downloads a local copy of dta commons java test tool

--- scripts/biomedical/DISEASES_JensenLab/scripts/tests.sh
+++ scripts/biomedical/DISEASES_JensenLab/scripts/tests.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 """
 Author: Samantha Piekos
-Date: 02/26/2024
+Date: 03/05/2024
 Name: tests
 Description: This file runs the Data Commons Java tool to run standard
 tests on tmcf + CSV pairs for the DISEASES by JensenLab import.
@@ -21,17 +21,23 @@ tests on tmcf + CSV pairs for the DISEASES by JensenLab import.
 
 #!/bin/bash
 
-java -jar /Applications/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/codingGenes-knowledge.tmcf CSVs/codingGenes-knowledge.csv
+# download data commons java test tool version 0.1-alpha.1k
+mkdir -p tmp; cd tmp
+wget https://github.com/datacommonsorg/import/releases/download/0.1-alpha.1k/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar
+cd ..
+
+# run tests
+java -jar tmp/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/codingGenes-knowledge.tmcf CSVs/codingGenes-knowledge.csv
 mv dc_generated codingGenes-knowledge
 
-java -jar /Applications/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/codingGenes-textmining.tmcf CSVs/codingGenes-textmining.csv
+java -jar tmp/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/codingGenes-textmining.tmcf CSVs/codingGenes-textmining.csv
 mv dc_generated codingGenes-textmining
 
-java -jar /Applications/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/experiment.tmcf CSVs/codingGenes-experiments.csv
+java -jar tmp/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/experiment.tmcf CSVs/codingGenes-experiments.csv
 mv dc_generated experiment
 
-java -jar /Applications/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/nonCodingGenes-knowledge.tmcf CSVs/nonCodingGenes-knowledge.csv
+java -jar tmp/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/nonCodingGenes-knowledge.tmcf CSVs/nonCodingGenes-knowledge.csv
 mv dc_generated nonCodingGenes-knowledge
 
-java -jar /Applications/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/nonCodingGenes-textmining.tmcf CSVs/nonCodingGenes-textmining.csv
+java -jar tmp/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint tMCFs/nonCodingGenes-textmining.tmcf CSVs/nonCodingGenes-textmining.csv
 mv dc_generated nonCodingGenes-textmining
