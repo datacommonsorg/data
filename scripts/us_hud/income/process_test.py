0e2115e9ed844441ebc90d88ab67a7365a9feac7
Kuru S <kuru@google.com>
2024-12-13 09:26:57 +0000

    us_hud_income 20241213 changes 3

--- scripts/us_hud/income/process_test.py
+++ scripts/us_hud/income/process_test.py
@@ -19,16 +19,20 @@ import filecmp
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 TEST_DIR = os.path.join(script_dir, 'testdata')
-OUTPUT_DIR = os.path.join(TEST_DIR, 'output') 
+OUTPUT_DIR = os.path.join(TEST_DIR, 'output')
 
 # Ensure the module is loaded correctly
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from us_hud.income import process  
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)))))
+
+from us_hud.income import process
 
 
 class ProcessTest(unittest.TestCase):
+
     def test_get_url(self):
         """Test the get_url function and check if it returns the correct URL for the given year."""
         year = 2022
@@ -37,26 +41,22 @@ class ProcessTest(unittest.TestCase):
             'https://www.huduser.gov/portal/datasets/il/il22/Section8-FY22.xlsx'
         )
         year = 1997
-        self.assertEqual(
-            process.get_url(year),
-            ''
-        )
+        self.assertEqual(process.get_url(year), '')
 
     def test_process_with_dynamic_csv(self):
         matches = {'dcs:geoId/02110': 'dcs:geoId/0236400'}
         output_data = []
-        input_folder = TEST_DIR  
+        input_folder = TEST_DIR
 
         if not os.path.exists(OUTPUT_DIR):
             os.makedirs(OUTPUT_DIR)
-        df= process.process(2006, matches, input_folder)
-        df.to_csv(os.path.join(OUTPUT_DIR,"output_test.csv"),index=False)
-        same = filecmp.cmp(
-            os.path.join(OUTPUT_DIR,"output_test.csv"),
-            os.path.join(TEST_DIR,"expected_output.csv")
-            )
+        df = process.process(2006, matches, input_folder)
+        df.to_csv(os.path.join(OUTPUT_DIR, "output_test.csv"), index=False)
+        same = filecmp.cmp(os.path.join(OUTPUT_DIR, "output_test.csv"),
+                           os.path.join(TEST_DIR, "expected_output.csv"))
         # Assert that the files are identical
         self.assertTrue(same)
+
 
 if __name__ == '__main__':
     unittest.main()
