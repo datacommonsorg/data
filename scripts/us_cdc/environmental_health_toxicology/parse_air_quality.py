41b77e62f18c9b8fe247d171b8b7ec2059ada666
Sudhisha K <sudhisha@google.com>
2025-01-20 07:49:27 +0000

    CDC_imports_AutoRefresh

--- scripts/us_cdc/environmental_health_toxicology/parse_air_quality.py
+++ scripts/us_cdc/environmental_health_toxicology/parse_air_quality.py
@@ -76,93 +76,106 @@ def add_prefix_zero(value, length):
 def clean_air_quality_data(config, importname, inputpath, outputpath):
     """
     Args:
-        file_path: path to a comma-separated CDC air quality data file
-        output_file: path for the cleaned csv to be stored
+        config: dictionary of all info required for this import 
+                such as source urls, input and output file names
+                for the 4 cdc imports.
+        importname: name of the import
+        inputpath: path to a comma-separated CDC air quality data file
+        outputpath: path for the cleaned csv to be stored
     Returns:
         a cleaned csv file
     """
-    global output_file_name
-    logging.info(f"import name from command line {importname}")
-    for config1 in config:
-        if config1["import_name"] == importname:
-            files = config1["files"]
-            for file_info in files:
-                output_file_name = file_info["output_file_name"]
-                input_file_name = file_info["input_file_name"]
-                input_file_path = os.path.join(inputpath, input_file_name)
-                output_file_path = os.path.join(outputpath, output_file_name)
-                logging.info(f"input_file_name: {input_file_name}")
-                logging.info(f"output_file_name: {output_file_name}")
-                if str(input_file_name).endswith('.csv'):
-                    logging.info(f"Cleaning {input_file_name} ....")
-                    logging.info(f"Cleaning {input_file_path} ....")
-                    try:
-                        data = pd.read_csv(input_file_path)
-                        data["date"] = pd.to_datetime(data["date"],
-                                                      yearfirst=True)
-                        data["date"] = pd.to_datetime(data["date"],
-                                                      format="%Y-%m-%d")
+    try:
+        global output_file_name
+        logging.info(f"import name from command line {importname}")
+        for config1 in config:
+            if config1["import_name"] == importname:
+                files = config1["files"]
+                for file_info in files:
+                    output_file_name = file_info["output_file_name"]
+                    input_file_name = file_info["input_file_name"]
+                    input_file_path = os.path.join(inputpath, input_file_name)
+                    output_file_path = os.path.join(outputpath,
+                                                    output_file_name)
+                    logging.info(f"input_file_name: {input_file_name}")
+                    logging.info(f"output_file_name: {output_file_name}")
+                    if str(input_file_name).endswith('.csv'):
+                        logging.info(f"Cleaning {input_file_name} ....")
+                        logging.info(f"Cleaning {input_file_path} ....")
+                        try:
+                            data = pd.read_csv(input_file_path)
+                            data["date"] = pd.to_datetime(data["date"],
+                                                          yearfirst=True)
+                            data["date"] = pd.to_datetime(data["date"],
+                                                          format="%Y-%m-%d")
 
-                        if "PM2.5" in input_file_name:
-                            census_tract = "ds_pm"
-                        elif "Ozone" in input_file_name:
-                            census_tract = "ds_o3"
-                        if "Census" in input_file_name:
                             if "PM2.5" in input_file_name:
-                                data = pd.melt(data,
-                                               id_vars=[
-                                                   'year', 'date', 'statefips',
-                                                   'countyfips', 'ctfips',
-                                                   'latitude', 'longitude'
-                                               ],
-                                               value_vars=[
-                                                   str(census_tract + '_pred'),
-                                                   str(census_tract + '_stdd')
-                                               ],
-                                               var_name='StatisticalVariable',
-                                               value_name='Value')
+                                census_tract = "ds_pm"
                             elif "Ozone" in input_file_name:
-                                data = pd.melt(
-                                    data,
-                                    id_vars=[
-                                        'year', 'date', 'statefips',
-                                        'countyfips', 'ctfips', 'latitude',
-                                        'longitude', census_tract + '_stdd'
-                                    ],
-                                    value_vars=[str(census_tract + '_pred')],
-                                    var_name='StatisticalVariable',
-                                    value_name='Value')
-                            data.rename(
-                                columns={census_tract + '_stdd': 'Error'},
-                                inplace=True)
-                            max_length = data['ctfips'].astype(
-                                str).str.len().max()
-                            data['ctfips'] = data['ctfips'].astype(str).apply(
-                                lambda x: add_prefix_zero(x, max_length))
-                            data["dcid"] = "geoId/" + data["ctfips"].astype(str)
-                            data['StatisticalVariable'] = data[
-                                'StatisticalVariable'].map(STATVARS)
-                        elif "County" in input_file_name and "PM" in input_file_name:
-                            data["statefips"] = data["statefips"].astype(
-                                str).str.zfill(2)
-                            data["countyfips"] = data["countyfips"].astype(
-                                str).str.zfill(3)
-                            data["dcid"] = "geoId/" + data["statefips"] + data[
-                                "countyfips"]
-                        elif "County" in input_file_name and "Ozone" in input_file_name:
-                            data["statefips"] = data["statefips"].astype(
-                                str).str.zfill(2)
-                            data["countyfips"] = data["countyfips"].astype(
-                                str).str.zfill(3)
-                            data["dcid"] = "geoId/" + data["statefips"] + data[
-                                "countyfips"]
-                        data.to_csv(output_file_path,
-                                    float_format='%.6f',
-                                    index=False)
-                        logging.info(
-                            f"Finished cleaning file {output_file_name}!")
-                    except:
-                        logging.info("Not reading input file...!")
+                                census_tract = "ds_o3"
+                            if "Census" in input_file_name:
+                                if "PM2.5" in input_file_name:
+                                    data = pd.melt(
+                                        data,
+                                        id_vars=[
+                                            'year', 'date', 'statefips',
+                                            'countyfips', 'ctfips', 'latitude',
+                                            'longitude'
+                                        ],
+                                        value_vars=[
+                                            str(census_tract + '_pred'),
+                                            str(census_tract + '_stdd')
+                                        ],
+                                        var_name='StatisticalVariable',
+                                        value_name='Value')
+                                elif "Ozone" in input_file_name:
+                                    data = pd.melt(
+                                        data,
+                                        id_vars=[
+                                            'year', 'date', 'statefips',
+                                            'countyfips', 'ctfips', 'latitude',
+                                            'longitude', census_tract + '_stdd'
+                                        ],
+                                        value_vars=[
+                                            str(census_tract + '_pred')
+                                        ],
+                                        var_name='StatisticalVariable',
+                                        value_name='Value')
+                                data.rename(
+                                    columns={census_tract + '_stdd': 'Error'},
+                                    inplace=True)
+                                max_length = data['ctfips'].astype(
+                                    str).str.len().max()
+                                data['ctfips'] = data['ctfips'].astype(
+                                    str).apply(lambda x: add_prefix_zero(
+                                        x, max_length))
+                                data["dcid"] = "geoId/" + data["ctfips"].astype(
+                                    str)
+                                data['StatisticalVariable'] = data[
+                                    'StatisticalVariable'].map(STATVARS)
+                            elif "County" in input_file_name and "PM" in input_file_name:
+                                data["statefips"] = data["statefips"].astype(
+                                    str).str.zfill(2)
+                                data["countyfips"] = data["countyfips"].astype(
+                                    str).str.zfill(3)
+                                data["dcid"] = "geoId/" + data[
+                                    "statefips"] + data["countyfips"]
+                            elif "County" in input_file_name and "Ozone" in input_file_name:
+                                data["statefips"] = data["statefips"].astype(
+                                    str).str.zfill(2)
+                                data["countyfips"] = data["countyfips"].astype(
+                                    str).str.zfill(3)
+                                data["dcid"] = "geoId/" + data[
+                                    "statefips"] + data["countyfips"]
+                            data.to_csv(output_file_path,
+                                        float_format='%.6f',
+                                        index=False)
+                            logging.info(
+                                f"Finished cleaning file {output_file_name}!")
+                        except:
+                            logging.info("Not reading input file...!")
+    except Exception as e:
+        logging.fatal(f"Error while processing the data: {e}")
 
 
 def main(_):
