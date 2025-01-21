218133a8f652b8fd4f37b94a2b6d34a9d7205c8a
Rohit Bhande <rbhande@google.com>
2025-01-21 06:06:10 +0000

    Ejscreen semiautomatic2

--- scripts/us_epa/ejscreen/ejscreen.py
+++ scripts/us_epa/ejscreen/ejscreen.py
@@ -1,16 +1,16 @@
-# # Copyright 2023 Google LLC
-# #
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# # you may not use this file except in compliance with the License.
-# # You may obtain a copy of the License at
-# #
-# #     https://www.apache.org/licenses/LICENSE-2.0
-# #
-# # Unless required by applicable law or agreed to in writing, software
-# # distributed under the License is distributed on an "AS IS" BASIS,
-# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# # See the License for the specific language governing permissions and
-# # limitations under the License.
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import io
 import os
@@ -18,10 +18,20 @@ import zipfile
 import requests
 import pandas as pd
 import json
-from absl import logging
+from absl import logging, flags, app
+import sys
+
+_MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(_MODULE_DIR, '../../../util/'))
+print(_MODULE_DIR)
+import file_util
 
 logging.set_verbosity(logging.INFO)
 logger = logging
+_FLAGS = flags.FLAGS
+flags.DEFINE_string('config_path',
+                    'gs://unresolved_mcf/epa/ejscreen/config.json',
+                    'Path to config file')
 
 _MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 _CONFIG_PATH = os.path.join(_MODULE_DIR, 'config.json')
@@ -37,22 +47,32 @@ CSV_COLUMNS_BY_YEAR = config["CSV_COLUMNS_BY_YEAR"]
 ZIP_FILENAMES = config["ZIP_FILENAMES"]
 FILENAMES = config["FILENAMES"]
 TEMPLATE_MCF = config["TEMPLATE_MCF"]
-URL_TEMPLATE = config["URL_TEMPLATE"]
-URL_TEMPLATE_2023 = config.get("URL_TEMPLATE_2023", URL_TEMPLATE)
-URL_TEMPLATE_2024 = config.get("URL_TEMPLATE_2024", URL_TEMPLATE)
-
-# data: dictionary of dataframes in the format {year: dataframe}
-# outfilename: name of the csv that data will be written to
-# write_csv concatenates the dataframe from each year together
+BASE_URL = config["BASE_URL"]
+URL_SUFFIX = config["URL_SUFFIX"]
 
 
+# Function to build the correct URL for each year
+def build_url(year, zip_filename=None):
+    if zip_filename:
+        # Construct the URL for the zip file
+        if year in URL_SUFFIX:
+            url = f'{BASE_URL}/{year}/{URL_SUFFIX[year]}/{zip_filename}.zip'
+        else:
+            url = f'{BASE_URL}/{year}/{zip_filename}.zip'
+    else:
+        # Construct the URL for the CSV file
+        url = f'{BASE_URL}/{year}/{FILENAMES[year]}.csv'
+    return url
+
+
+# Data processing function
 def write_csv(data, outfilename):
     full_df = pd.DataFrame()
     for curr_year, one_year_df in data.items():
         one_year_df['year'] = curr_year
         full_df = pd.concat([full_df, one_year_df], ignore_index=True)
 
-    # sort by FIPS and make into dcid
+    # Sort by FIPS and make into dcid
     full_df = full_df.rename(columns={'ID': 'FIPS'})
     full_df = full_df.sort_values(by=['FIPS'], ignore_index=True)
     full_df['FIPS'] = 'dcid:geoId/' + (
@@ -63,65 +83,51 @@ def write_csv(data, outfilename):
 
 
 def write_tmcf(outfilename):
+    # Convert each item in TEMPLATE_MCF to a string, even if it's a dictionary
+    if isinstance(TEMPLATE_MCF, list):
+        # Convert each element to a string if it's not already
+        template_content = "\n".join(str(item) for item in TEMPLATE_MCF)
+    else:
+        template_content = str(
+            TEMPLATE_MCF
+        )  # In case it's not a list, just convert it to a string
+
     with open(outfilename, 'w') as f_out:
-        f_out.write(TEMPLATE_MCF)
+        f_out.write(template_content)
 
 
-if __name__ == '__main__':
+def main(_):
     dfs = {}
     for year in YEARS:
         logger.info(f"Processing year: {year}")
         columns = CSV_COLUMNS_BY_YEAR[year]
         zip_filename = ZIP_FILENAMES.get(year, None)
 
-        # Check if the year has a zip file or not
-        if zip_filename:
-            # Select the appropriate URL template based on the year
-            if year == '2023':
-                url = URL_TEMPLATE_2023.format(year=year,
-                                               zip_filename=zip_filename)
-            elif year == '2024':
-                url = URL_TEMPLATE_2024.format(year=year,
-                                               zip_filename=zip_filename)
-            else:
-                url = URL_TEMPLATE.format(year=year, zip_filename=zip_filename)
+        url = build_url(year, zip_filename)
 
-            logger.info(f"Requesting file: {url}")
-            response = requests.get(url, verify=False)
+        logger.info(f"Requesting file: {url}")
+        response = requests.get(url, verify=False)
 
-            if response.status_code == 200:
+        if response.status_code == 200:
+            if zip_filename:
                 with zipfile.ZipFile(io.BytesIO(response.content)) as zfile:
                     with zfile.open(f'{FILENAMES[year]}.csv', 'r') as newfile:
-                        # Specify encoding to handle special characters
-                        dfs[year] = pd.read_csv(
-                            newfile, usecols=columns,
-                            encoding='latin1')  # Added encoding='latin1'
-                logger.info(
-                    f"File downloaded and processed for {year} successfully")
+                        dfs[year] = pd.read_csv(newfile,
+                                                engine='python',
+                                                encoding='latin1',
+                                                usecols=columns)
             else:
-                logger.error(
-                    f"Failed to download file for {year}. HTTP Status Code: {response.status_code}"
-                )
+                dfs[year] = pd.read_csv(io.StringIO(response.text),
+                                        sep=',',
+                                        usecols=columns)
+            logger.info(
+                f"File downloaded and processed for {year} successfully")
         else:
-            url = URL_TEMPLATE.format(year=year, filename=FILENAMES[year])
-            logger.info(f"Requesting CSV file: {url}")
-            response = requests.get(url, verify=False)
+            logger.error(
+                f"Failed to download file for {year}. HTTP Status Code: {response.status_code}"
+            )
 
-            if response.status_code == 200:
-                # Specify encoding to handle special characters
-                dfs[year] = pd.read_csv(
-                    io.StringIO(response.text),
-                    sep=',',
-                    usecols=columns,
-                    encoding='latin1')  # Added encoding='latin1'
-                logger.info(
-                    f"CSV downloaded and processed for {year} successfully")
-            else:
-                logger.error(
-                    f"Failed to download CSV for {year}. HTTP Status Code: {response.status_code}"
-                )
-
-        # Rename weird column names to match other years
+        # Rename columns to match other years
         if year == '2024':
             cols_renamed = dict(zip(columns, NORM_CSV_COLUMNS1))
         else:
@@ -130,8 +136,12 @@ if __name__ == '__main__':
         dfs[year] = dfs[year].rename(columns=cols_renamed)
         logger.info(f"Columns renamed for {year} successfully")
 
-    logger.info("Writing data to csv")
-    write_csv(dfs, 'ejscreen_airpollutants.csv')
-    logger.info("Writing template to tmcf")
-    write_tmcf('ejscreen.tmcf')
-    logger.info("Process completed successfully")
+        logger.info("Writing data to CSV")
+        write_csv(dfs, 'ejscreen_airpollutants.csv')
+        logger.info("Writing template to TMCF")
+        write_tmcf('ejscreen.tmcf')
+        logger.info("Process completed successfully")
+
+
+if __name__ == '__main__':
+    app.run(main)
