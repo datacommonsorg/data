9f56d383971a585b7429480897b76406fe1b2843
Ajai <ajaits@google.com>
2025-01-24 15:36:27 +0530

    Add support for wildcard import files in manifest.json (#1198)
    * Add support for wildcard import files in manifest.json
    
    * copy source_files to GCS
    
    * fix unit tests
    
    * fix unit tests

--- import-automation/executor/app/executor/import_executor.py
+++ import-automation/executor/app/executor/import_executor.py
@@ -34,6 +34,9 @@ REPO_DIR = os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 sys.path.append(os.path.join(REPO_DIR, 'tools', 'import_differ'))
 sys.path.append(os.path.join(REPO_DIR, 'tools', 'import_validation'))
+sys.path.append(os.path.join(REPO_DIR, 'util'))
+
+import file_util
 
 from import_differ import ImportDiffer
 from import_validation import ImportValidation
@@ -536,13 +539,32 @@ class ImportExecutor:
         for import_input in import_inputs:
             for input_type in self.config.import_input_types:
                 path = import_input.get(input_type)
-                if path:
-                    dest = f'{output_dir}/{version}/{os.path.basename(path)}'
-                    self._upload_file_helper(
-                        src=os.path.join(import_dir, path),
-                        dest=dest,
-                    )
-                    setattr(uploaded, input_type, dest)
+                if not path:
+                    continue
+                for file in file_util.file_get_matching(
+                        os.path.join(import_dir, path)):
+                    if file:
+                        dest = f'{output_dir}/{version}/{os.path.basename(file)}'
+                        self._upload_file_helper(
+                            src=file,
+                            dest=dest,
+                        )
+                uploaded_dest = f'{output_dir}/{version}/{os.path.basename(path)}'
+                setattr(uploaded, input_type, uploaded_dest)
+
+        # Upload any files downloaded from source
+        source_files = [
+            os.path.join(import_dir, file)
+            for file in import_spec.get('source_files', [])
+        ]
+        source_files = file_util.file_get_matching(source_files)
+        for file in source_files:
+            dest = f'{output_dir}/{version}/source_files/{os.path.basename(file)}'
+            self._upload_file_helper(
+                src=file,
+                dest=dest,
+            )
+
         self.uploader.upload_string(
             version,
             os.path.join(output_dir, self.config.storage_version_filename))
@@ -565,7 +587,7 @@ class ImportExecutor:
 
         Args:
             import_spec: Specification of the import as a dict.
-        
+
         Returns:
             import_metadata_mcf node.
         """
