cc6b539a482c3a9a306c6cea74617e72d3b766f7
Bipin Abraham <bipina@google.com>
2025-01-27 11:16:42 +0000

    Modified folder structure and added retry module in download function

--- statvar_imports/usa_dol/minimum_wage/download_script/main.py
+++ statvar_imports/usa_dol/minimum_wage/download_script/main.py
@@ -2,8 +2,11 @@ import os
 import csv
 import requests
 import pandas as pd
+from absl import logging
 from bs4 import BeautifulSoup
+from retry import retry
 
+url = "https://www.dol.gov/agencies/whd/state/minimum-wage/history"
 source_folder = "source_files"
 input_folder = "input_files"
 os.makedirs(source_folder, exist_ok=True)
@@ -11,11 +14,14 @@ os.makedirs(input_folder, exist_ok=True)
 file_path = os.path.join(source_folder, "raw_data.html")
 file_path2 = os.path.join(source_folder, "raw_data.csv")
 
+@retry(tries=5,delay=3,backoff=5)
+def download_with_retry(url):
+    logging.info(f"Trying to access url : {url}")
+    return requests.get(url) 
 
 def extract_all_table_data(url):
-
     try:
-        response = requests.get(url)
+        response = download_with_retry(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         with open(file_path, 'w') as file:
@@ -39,28 +45,29 @@ def extract_all_table_data(url):
                 for index, table in enumerate(table_data):
                     writer.writerows(table)
                     writer.writerow([])
-
+            logging.info("Downloaded files")
             return table_data
         else:
             print(f"No tables found on the page {url}")
             return None
-
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching URL: {e}")
-        return None
+        logging.fatal(f"Error fetching URL: {e}")
 
+def main():
+    logging.info("Process starts")
+    all_tables_data = extract_all_table_data(url)
 
-url = "https://www.dol.gov/agencies/whd/state/minimum-wage/history"
-all_tables_data = extract_all_table_data(url)
+    if all_tables_data:
+        all_dfs = []
+        for index, table in enumerate(all_tables_data):
+            if index == 0:
+                df = pd.DataFrame(table[1:], columns=table[0])
+            else:
+                dfm = pd.DataFrame(table[1:], columns=table[0])
+                df = pd.merge(df, dfm, on=['State or otherjurisdiction'])
 
-if all_tables_data:
-    all_dfs = []
-    for index, table in enumerate(all_tables_data):
-        if index == 0:
-            df = pd.DataFrame(table[1:], columns=table[0])
-        else:
-            dfm = pd.DataFrame(table[1:], columns=table[0])
-            df = pd.merge(df, dfm, on=['State or otherjurisdiction'])
-
-    transposed_df = df.transpose()
-    transposed_df.to_csv('input_files/final_data.csv', header=False)
+        transposed_df = df.transpose()
+        transposed_df.to_csv('input_files/final_data.csv', header=False)
+if __name__ == "__main__" : 
+    logging.set_verbosity(1)
+    main()
