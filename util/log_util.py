6d4a1b18949a8543be9b5afc2dbfad1605466759
rohit kumar <rohitrkumar@google.com>
2025-01-30 08:09:35 +0000

    Addressed review comments

--- util/log_util.py
+++ util/log_util.py
@@ -31,14 +31,33 @@ def log_struct(level: str, message: str, labels: dict):
 
 
 def log_metric(log_type: str, level: str, message: str, metric_labels: dict):
-    """Logs a structured message which can be used to create a log-based metric.
+    """Logs a structured message suitable for creating log-based metrics.
         In GCP Log Explorer, the labels will appear under `jsonPayload`.
 
+        Guidelines for GCP log-based metrics:
+        - Keep loggin queries simple and focused.
+            - More query fields might increase the chances of false negatives.
+            - Filter logs by resource.type to focus on specific resources. This is auto populated by 
+                GCP and does not need to be set here.
+        - The same log can be used to create multiple log-based metrics, 
+            especially when it contains multiple metric values (e.g. count, latency)
+            - If so, metric_labels can be used as additional filtering criteria.       
     Args:
         log_type: Log type identifier; used for filtering log lines when creating log-based metrics.
-            Published as `log_type` label in structured log.
+            - Published as `log_type` label in structured log.
+            - Use a unique log_type to filter log entries when creating log-based metrics. 
+            - Reduces the number of fields required in the filtering query. This simplifies the query.
+            - This method is more robust and easier to maintain than relying on labels, 
+                which might be shared by other, unrelated log entries.
         level: Log level ("INFO", "WARNING", "ERROR").
-        message: Log message.
-        metric_labels: Labels for the metric.
+        message: Log message. Provides a summary for display in the log explorer
+            but should not be used for log queries or metric label derivation.  Use `metric_labels` instead.
+        metric_labels: Labels for the metric. Might not be required for counter metrics when log_type alone might be sufficient.
+            - Used to derive metric label values and metric value. Also used in logging queries,
+                 especially when creating multiple log-based metrics from the same logs.
+            - For metric labels, use scalar values (string, number, boolean) whenever possible.
+            - This allows direct assignment to metric labels, avoiding the need for regular expressions.  
+            - When using the same `log_type` in different parts of your code,
+              maintain consistency in the labels used to guarantee correct population of log labels.
     """
     log_struct(level, message, {"log_type": log_type, **metric_labels})
