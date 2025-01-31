3ba71c819fe46d437a335701f0c72361d0ab0835
Harsha Vardhan Chandaluri <chandaluri@google.com>
2025-01-31 10:47:19 +0000

    fixed test

--- scripts/us_eia/opendata/process/main.py
+++ scripts/us_eia/opendata/process/main.py
@@ -25,7 +25,7 @@ sys.path.append(
     os.path.dirname(
         os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
-from us_eia.opendata.process import coal, common, elec, intl, ng, nuclear, pet, seds, total
+from .us_eia.opendata.process import coal, common, elec, intl, ng, nuclear, pet, seds, total
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('data_dir', 'tmp_raw_data', 'Raw data dir')
