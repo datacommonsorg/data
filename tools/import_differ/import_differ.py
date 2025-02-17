f31f141ba127f13260a488077167fac7962c5429
Vishal Gupta <vishg@google.com>
2025-02-17 06:12:57 +0000

    Dataflow based import differ

--- tools/import_differ/import_differ.py
+++ tools/import_differ/import_differ.py
@@ -17,6 +17,8 @@ import os
 import pandas as pd
 import random
 import sys
+import time
+from enum import Enum
 
 from absl import app
 from absl import flags
@@ -27,26 +29,34 @@ sys.path.append(_SCRIPT_DIR)
 
 import differ_utils
 
-SAMPLE_COUNT = 3
-GROUPBY_COLUMNS = 'variableMeasured,observationAbout,observationDate,measurementMethod,unit,observationPeriod'
-VALUE_COLUMNS = 'value,scalingFactor'
+_SAMPLE_COUNT = 3
+_GROUPBY_COLUMNS = 'variableMeasured,observationAbout,observationDate,measurementMethod,unit,observationPeriod'
+_VALUE_COLUMNS = 'value,scalingFactor'
+_DATAFLOW_PROJECT = 'datcom-204919'
+_DIFFER_JAR_LOCATION = '<path to differ jar>/differ-bundled-0.1.jar'  # required for local runner mode
+_TMP_LOCATION = '/tmp'
 
-FLAGS = flags.FLAGS
+Diff = Enum('Diff', [
+    ('ADDED', 1),
+    ('DELETED', 2),
+    ('MODIFIED', 3),
+    ('SAME', 4),
+    ('TOTAL', 5),
+])
+
+_FLAGS = flags.FLAGS
 flags.DEFINE_string(
-    'current_data', '', 'Path to the current MCF data \
-  (single mcf file or wildcard on local/GCS supported).')
+    'current_data', '', 'Path to the current data \
+  (wildcard on local/GCS supported).')
 flags.DEFINE_string(
-    'previous_data', '', 'Path to the previous MCF data \
-  (single mcf file or wildcard on local/GCS supported).')
+    'previous_data', '', 'Path to the previous data \
+  (wildcard on local/GCS supported).')
 flags.DEFINE_string('output_location', 'results', \
-  'Path to the output data folder.')
-
-flags.DEFINE_string(
-    'groupby_columns', GROUPBY_COLUMNS,
-    'Columns to group data for diff analysis in the order (var,place,time etc.).'
-)
-flags.DEFINE_string('value_columns', VALUE_COLUMNS,
-                    'Columns with statvar value for diff analysis.')
+  'Path (local/GCS) to the output data folder.')
+flags.DEFINE_string('file_format', 'mcf',
+                    'Format of the input data (mcf,tfrecord)')
+flags.DEFINE_string('runner_mode', 'local', 'Dataflow runner mode(local/cloud)')
+flags.DEFINE_string('job_name', 'differ', 'Name of the differ dataflow job.')
 
 
 class ImportDiffer:
@@ -55,12 +65,13 @@ class ImportDiffer:
   of two versions of the same dataset for import analysis. 
 
   Usage:
-  $ python import_differ.py --current_data=<path> --previous_data=<path>
+  $ python import_differ.py --current_data=<path> --previous_data=<path> --output_location=<path> \
+    --file_format=<mcf/tfrecord> --runner_mode=<local/cloud> --job_name=<name>
 
   Summary output generated is of the form below showing 
   counts of differences for each variable.  
 
-  variableMeasured   added  deleted  modified  same  total
+  variableMeasured   ADDED  DELETED  MODIFIED  SAME  TOTAL
   0   dcid:var1       1      0       0          0     1
   1   dcid:var2       0      2       1          1     4
   2   dcid:var3       0      0       1          0     1
@@ -78,11 +89,18 @@ class ImportDiffer:
                  current_data,
                  previous_data,
                  output_location,
-                 groupby_columns=GROUPBY_COLUMNS,
-                 value_columns=VALUE_COLUMNS):
+                 job_name,
+                 file_format,
+                 runner_mode,
+                 groupby_columns=_GROUPBY_COLUMNS,
+                 value_columns=_VALUE_COLUMNS):
         self.current_data = current_data
         self.previous_data = previous_data
-        self.output_location = output_location
+        self.output_path = os.path.join(output_location, job_name)
+        self.tmp_path = os.path.join(_TMP_LOCATION, job_name)
+        self.job_name = job_name
+        self.file_format = file_format
+        self.runner_mode = runner_mode
         self.groupby_columns = groupby_columns.split(',')
         self.value_columns = value_columns.split(',')
         self.variable_column = self.groupby_columns[0]
@@ -91,72 +109,89 @@ class ImportDiffer:
         self.diff_column = 'diff_result'
 
     def _cleanup_data(self, df: pd.DataFrame):
-        for column in ['added', 'deleted', 'modified', 'same']:
-            df[column] = df[column] if column in df.columns else 0
-            df[column] = df[column].fillna(0).astype(int)
+        for column in Diff:
+            df[column.name] = df[
+                column.name] if column.name in df.columns else 0
+            df[column.name] = df[column.name].fillna(0).astype(int)
 
     def _get_samples(self, row):
         years = sorted(row[self.time_column])
-        if len(years) > SAMPLE_COUNT:
+        if len(years) > _SAMPLE_COUNT:
             return [years[0]] + random.sample(years[1:-1],
-                                              SAMPLE_COUNT - 2) + [years[-1]]
+                                              _SAMPLE_COUNT - 2) + [years[-1]]
         else:
             return years
 
-    # Processes two dataset files to identify changes.
-    def process_data(self, previous_df: pd.DataFrame,
-                     current_df: pd.DataFrame) -> pd.DataFrame:
+    def run_pipeline(self):
         """ 
-        Process previous and current datasets to generate 
-        the intermediate data for point and series analysis.
-        Args:
-          current_df: dataframe with current (new) data
-          previous_df: dataframe with previous (old) data
-        Returns:
-          intermediate merged data for analysis
+        Runs dataflow job to process two datasets to identify changes.
         """
-        cur_df_columns = current_df.columns.values.tolist()
-        self.groupby_columns = [
-            i for i in self.groupby_columns if i in cur_df_columns
-        ]
-        self.value_columns = [
-            i for i in self.value_columns if i in cur_df_columns
-        ]
-        df1 = previous_df.loc[:, self.groupby_columns + self.value_columns]
-        df2 = current_df.loc[:, self.groupby_columns + self.value_columns]
-        df1['_value_combined'] = df1[self.value_columns]\
-          .apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
-        df2['_value_combined'] = df2[self.value_columns]\
-          .apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
-        df1.drop(columns=self.value_columns, inplace=True)
-        df2.drop(columns=self.value_columns, inplace=True)
-        # Perform outer join operation to identify differences.
-        result = pd.merge(df1,
-                          df2,
-                          on=self.groupby_columns,
-                          how='outer',
-                          indicator=self.diff_column)
-        result[self.diff_column] = result.apply(
-          lambda row: 'added' if row[self.diff_column] == 'right_only' \
-          else 'deleted' if row[self.diff_column] == 'left_only' \
-          else 'modified' if row['_value_combined_x'] != row['_value_combined_y'] \
-          else 'same', axis=1)
-        return result
+        if self.runner_mode == 'local':
+            args = {
+                'currentData': self.current_data,
+                'previousData': self.previous_data,
+                'diffOutput': os.path.join(self.output_path, 'diff'),
+                'statsOutput': os.path.join(self.output_path, 'stats')
+            }
+            if self.file_format == 'mcf':
+                args['useMcfFormat'] = 'true'
+            else:  # tfrecord format
+                args['useTfrFormat'] = 'true'
+            logging.info("Running local dataflow job")
+            cmd = f'java -jar {_DIFFER_JAR_LOCATION}'
+            for k, v in args.items():
+                cmd += f' --{k}={v}'
+            logging.info(cmd)
+            return os.system(cmd)
+        else:  # cloud runner
+            logging.info('Launching dataflow job for processing data...')
+            differ_utils.launch_dataflow_job(_DATAFLOW_PROJECT, self.job_name,
+                                             self.current_data,
+                                             self.previous_data,
+                                             self.file_format, self.output_path)
+            status = 'JOB_STATE_UNKNONW'
+            while (status != 'JOB_STATE_DONE' and status != 'JOB_STATE_FAILED'):
+                status = differ_utils.get_job_status(_DATAFLOW_PROJECT,
+                                                     self.job_name)
+                logging.info(
+                    f'Waiting for job {self.job_name} to complete. Status:{status}'
+                )
+                time.sleep(60)
+            if status == 'JOB_STATE_FAILED':
+                logging.error('Dataflow job failed to process input data')
+                return 1
+            else:
+                return 0
 
-    def point_analysis(self,
-                       in_data: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+    def process_data(self, diff_path: str,
+                     stats_path: str) -> (pd.DataFrame, pd.DataFrame):
+        column_list = self.groupby_columns + [
+            '_value_combined_x', '_value_combined_y', 'diff_result'
+        ]
+        logging.info("Loading data from: %s", diff_path)
+        diff = differ_utils.load_csv_data(diff_path, column_list, self.tmp_path)
+
+        logging.info("Loading data from: %s", stats_path)
+        counts = differ_utils.load_csv_data(
+            stats_path, self.groupby_columns + [Diff.TOTAL.name], self.tmp_path)
+        return diff, counts
+
+    def point_analysis(self, diff: pd.DataFrame,
+                       stats: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         """ 
         Performs point diff analysis to identify data point changes.
-        Args:
-          in_data: intermediate data generated by processing previous/current data
+
         Returns:
           summary and results from the analysis
         """
+        if diff.empty:
+            return pd.DataFrame(), pd.DataFrame()
+
         column_list = [
             self.variable_column, self.place_column, self.time_column,
             self.diff_column
         ]
-        result = in_data.loc[:, column_list]
+        result = diff.loc[:, column_list]
         result = result.groupby(
             [self.variable_column, self.diff_column],
             observed=True,
@@ -166,41 +201,60 @@ class ImportDiffer:
                                       axis=1)
         result[self.place_column] = result.apply(lambda row: random.sample(
             row[self.place_column],
-            min(SAMPLE_COUNT, len(row[self.place_column]))),
+            min(_SAMPLE_COUNT, len(row[self.place_column]))),
                                                  axis=1)
         result[self.time_column] = result.apply(self._get_samples, axis=1)
         summary = result.pivot(
           index=self.variable_column, columns=self.diff_column, values='size')\
           .reset_index().rename_axis(None, axis=1)
-        self._cleanup_data(summary)
-        summary['total'] = summary.apply(lambda row: row['added'] + row[
-            'deleted'] + row['modified'] + row['same'],
-                                         axis=1)
-        return summary, result
+        counts = stats.loc[:, [self.variable_column, Diff.TOTAL.name]]
+        counts = counts.groupby([self.variable_column],
+                                as_index=False)['TOTAL'].sum()
+        out_data = summary.merge(counts, on=[self.variable_column], how='outer')
+        self._cleanup_data(out_data)
+        out_data[Diff.SAME.name] = out_data.apply(lambda row: row[
+            Diff.TOTAL.name] - row[Diff.ADDED.name] - row[Diff.MODIFIED.name],
+                                                  axis=1)
+        result.sort_values(by=[self.diff_column, self.variable_column],
+                           inplace=True)
+        return out_data, result
 
-    def series_analysis(self,
-                        in_data: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+    def series_analysis(self, diff: pd.DataFrame,
+                        stats: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         """ 
         Performs series diff analysis to identify time series changes.
-        Args:
-          in_data: intermediate data generated by processing previous/current data
+
         Returns:
           summary and results from the analysis
         """
+        if diff.empty:
+            return pd.DataFrame(), pd.DataFrame()
+
         column_list = [
             self.variable_column, self.place_column, self.diff_column
         ]
-        result = in_data.loc[:, column_list]
+        result = diff.loc[:, column_list]
         result = result.groupby(column_list, as_index=False).size()
         result = result.pivot(
           index=[self.variable_column, self.place_column], columns=self.diff_column, values='size')\
           .reset_index().rename_axis(None, axis=1)
+        counts = stats.loc[:, [
+            self.variable_column, self.place_column, Diff.TOTAL.name
+        ]]
+        counts = counts.groupby([self.variable_column, self.place_column],
+                                as_index=False)['TOTAL'].sum()
+        result = result.merge(counts,
+                              on=[self.variable_column, self.place_column],
+                              how='outer')
         self._cleanup_data(result)
-        result[self.diff_column] = result.apply(lambda row: 'added' if row['added'] > 0 \
-          and row['deleted'] + row['modified'] + row['same'] == 0 \
-          else 'deleted' if row['deleted'] > 0 and row['added'] + row['modified'] + row['same'] == 0 \
-          else 'modified' if row['deleted'] > 0 or row['added'] > 0 or row['modified'] > 0 \
-          else 'same', axis=1)
+        result[Diff.SAME.name] = result.apply(lambda row: row[
+            Diff.TOTAL.name] - row[Diff.ADDED.name] - row[Diff.MODIFIED.name],
+                                              axis=1)
+        result[self.diff_column] = result.apply(lambda row: Diff.ADDED.name if row[Diff.ADDED.name] > 0 \
+          and row[Diff.DELETED.name] + row[Diff.MODIFIED.name] + row[Diff.SAME.name] == 0 \
+          else Diff.DELETED.name if row[Diff.DELETED.name] > 0 and row[Diff.ADDED.name] + row[Diff.MODIFIED.name] + row[Diff.SAME.name] == 0 \
+          else Diff.MODIFIED.name if row[Diff.DELETED.name] > 0 or row[Diff.ADDED.name] > 0 or row[Diff.MODIFIED.name] > 0 \
+          else Diff.SAME.name, axis=1)
         result = result[column_list]
         result = result.groupby(
             [self.variable_column, self.diff_column],
@@ -210,62 +264,66 @@ class ImportDiffer:
                                       axis=1)
         result[self.place_column] = result.apply(lambda row: random.sample(
             row[self.place_column],
-            min(SAMPLE_COUNT, len(row[self.place_column]))),
+            min(_SAMPLE_COUNT, len(row[self.place_column]))),
                                                  axis=1)
         summary = result.pivot(
           index=self.variable_column, columns=self.diff_column, values='size')\
           .reset_index().rename_axis(None, axis=1)
         self._cleanup_data(summary)
-        summary['total'] = summary.apply(lambda row: row['added'] + row[
-            'deleted'] + row['modified'] + row['same'],
-                                         axis=1)
+        summary[Diff.TOTAL.name] = summary.apply(
+            lambda row: row[Diff.ADDED.name] + row[Diff.DELETED.name] + row[
+                Diff.MODIFIED.name] + row[Diff.SAME.name],
+            axis=1)
+        result.sort_values(by=[self.diff_column, self.variable_column],
+                           inplace=True)
         return summary, result
 
     def run_differ(self):
-        logging.info('Loading data...')
-        current_dir = os.path.join(self.output_location, 'current')
-        if not os.path.exists(current_dir):
-            os.makedirs(current_dir)
-        current_df = differ_utils.load_data(self.current_data, current_dir)
-        previous_dir = os.path.join(self.output_location, 'previous')
-        if not os.path.exists(previous_dir):
-            os.makedirs(previous_dir)
-        previous_df = differ_utils.load_data(self.previous_data, previous_dir)
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+        if not os.path.exists(self.tmp_path):
+            os.makedirs(self.tmp_path)
+
+        logging.info('Running dataflow pipeline...')
+        status = self.run_pipeline()
+        if status > 0:
+            logging.error('Dataflow pipeline run failed.')
+            return
 
         logging.info('Processing data...')
-        in_data = self.process_data(previous_df, current_df)
+        diff, stats = self.process_data(
+            os.path.join(self.output_path, 'diff*'),
+            os.path.join(self.output_path, 'stats*'),
+        )
 
         logging.info('Point analysis:')
-        summary, result = self.point_analysis(in_data)
-        result.sort_values(by=[self.diff_column, self.variable_column],
-                           inplace=True)
+        summary, result = self.point_analysis(diff, stats)
         print(summary.head(10))
         print(result.head(10))
-        differ_utils.write_data(summary, self.output_location,
-                                'point_analysis_summary.csv')
-        differ_utils.write_data(result, self.output_location,
-                                'point_analysis_results.csv')
+        differ_utils.write_csv_data(summary, self.output_path,
+                                    'point_analysis_summary.csv', self.tmp_path)
+        differ_utils.write_csv_data(result, self.output_path,
+                                    'point_analysis_results.csv', self.tmp_path)
 
         logging.info('Series analysis:')
-        summary, result = self.series_analysis(in_data)
-        result.sort_values(by=[self.diff_column, self.variable_column],
-                           inplace=True)
+        summary, result = self.series_analysis(diff, stats)
         print(summary.head(10))
         print(result.head(10))
-        differ_utils.write_data(summary, self.output_location,
-                                'series_analysis_summary.csv')
-        differ_utils.write_data(result, self.output_location,
-                                'series_analysis_results.csv')
+        differ_utils.write_csv_data(summary, self.output_path,
+                                    'series_analysis_summary.csv',
+                                    self.tmp_path)
+        differ_utils.write_csv_data(result, self.output_path,
+                                    'series_analysis_results.csv',
+                                    self.tmp_path)
 
-        logging.info('Differ output written to folder: %s',
-                     self.output_location)
+        logging.info('Differ output written to %s', self.output_path)
 
 
 def main(_):
     '''Runs the differ.'''
-    differ = ImportDiffer(FLAGS.current_data, FLAGS.previous_data,
-                          FLAGS.output_location, FLAGS.groupby_columns,
-                          FLAGS.value_columns)
+    differ = ImportDiffer(_FLAGS.current_data, _FLAGS.previous_data,
+                          _FLAGS.output_location, _FLAGS.job_name,
+                          _FLAGS.file_format, _FLAGS.runner_mode)
     differ.run_differ()
 
 
