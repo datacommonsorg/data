25e8737b8ba5b6d146051ebbec357be2aa9dd8f2
Harish <102517942+HarishC727@users.noreply.github.com>
2025-03-05 20:49:24 +0530

    OECD Refresh and Automation (#1125)
    * Created test and download scripts for autoscript
    
    * fixed lint
    
    * added files for autorefresh
    
    * added try catch blocks
    
    * fixed try catch
    
    * resolved internal comments
    
    * fixed process script for population
    
    * corrected places and fixed scripts
    
    * fixed lint and test
    
    * reverted to PR comments
    
    * reverted to PR comments
    
    * modified manifest.json
    
    * fixed place bugs
    
    * resolved comments
    
    * fixed dups

---
+++ scripts/oecd/regional_demography/mortality/columns.py
@@ -0,0 +1,28 @@
+VAR_to_statsvars = {
+    'DEATH_RAT':
+        'Count_Death_AsAFractionOf_Count_Person',
+    'DEATH_RAM':
+        'Count_Death_Male_AsAFractionOf_Count_Person_Male',
+    'DEATH_RAF':
+        'Count_Death_Female_AsAFractionOf_Count_Person_Female',
+    'STD_MORTT':
+        'Count_Death_AgeAdjusted_AsAFractionOf_Count_Person',
+    'STD_MORTM':
+        'Count_Death_Male_AgeAdjusted_AsAFractionOf_Count_Person_Male',
+    'STD_MORTF':
+        'Count_Death_Female_AgeAdjusted_AsAFractionOf_Count_Person_Female',
+    'YOU_DEATH_RAT':
+        'Count_Death_Upto14Years_AsAFractionOf_Count_Person_Upto14Years',
+    'YOU_DEATH_RAM':
+        'Count_Death_Upto14Years_Male_AsAFractionOf_Count_Person_Upto14Years_Male',
+    'YOU_DEATH_RAF':
+        'Count_Death_Upto14Years_Female_AsAFractionOf_Count_Person_Upto14Years_Female',
+    'INF_MORTT':
+        'Count_Death_LessThan1Year_AsAFractionOf_Count_BirthEvent',
+    'INF_MORTM':
+        'Count_Death_LessThan1Year_Male_AsAFractionOf_Count_BirthEvent_Male',
+    'INF_MORTF':
+        'Count_Death_LessThan1Year_Female_AsAFractionOf_Count_BirthEvent_Female'
+}
+
+reindex_column = []
