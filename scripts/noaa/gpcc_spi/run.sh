721ffc991dfbf6be28c4664d9cf19733ff234c69
Harish <102517942+HarishC727@users.noreply.github.com>
2025-04-01 12:30:58 +0000

    temporary fix .sh script (#1286)

--- scripts/noaa/gpcc_spi/run.sh
+++ scripts/noaa/gpcc_spi/run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-PATH=$PATH:$(dirname $0)
-# Sleep as long as N jobs of given name are running in background
-function num_jobs {
-  local name="$1"; shift;
-  if [[ "$name" == "" ]]; then
-    echo  $(jobs -r | wc -l)
-  else
-    echo $(ps -ef | egrep "$name" | wc -l)
-  fi
-}
-function sleep_while_active {
-  max_jobs=$1; shift
-  job_name="$1"; shift
-  j=$(num_jobs $job_name);
-  while (( $j > ${max_jobs:-0} )); do
-    sleep 10;
-    j=$(num_jobs $job_name);
-  done;
-}
+# PATH=$PATH:$(dirname $0)
+# # Sleep as long as N jobs of given name are running in background
+# function num_jobs {
+#   local name="$1"; shift;
+#   if [[ "$name" == "" ]]; then
+#     echo  $(jobs -r | wc -l)
+#   else
+#     echo $(ps -ef | egrep "$name" | wc -l)
+#   fi
+# }
+# function sleep_while_active {
+#   max_jobs=$1; shift
+#   job_name="$1"; shift
+#   j=$(num_jobs $job_name);
+#   while (( $j > ${max_jobs:-0} )); do
+#     sleep 10;
+#     j=$(num_jobs $job_name);
+#   done;
+# }
 
 date=2025-01-02
 config=spi_9m_polygon
@@ -47,14 +47,14 @@ sh ../../../tools/pd_csv.sh -i output_files/gpcc_spi_pearson_09.csv -o output_fi
 set -x
 years=$(ls output_files/shard/gpcc_spi_pearson_09*.csv | sed -e 's/.*-year-//;s/-.*//' | egrep -o "[0-9]{4}" | sort | uniq)
 for yr in $years; do
- time python3 ../../earthengine/process_events.py \
+  time python3 ../../earthengine/process_events.py \
   --config=events_drought_gpcc_${config}.py \
   --input_csv=output_files/shard/gpcc_spi_pearson_09-year-${yr}-*.csv \
   --output_path=output_files/events_${config}/drought_${config}_${yr}_\
   --pprof_port=$((8180 + $yr )) \
   --place_cache_file=grid_1_contains.csv \
   2>&1 | tee tmp/events-$config-$yr.log &
-  sleep_while_active 10
+  # sleep_while_active 10
 done
 wait
 
