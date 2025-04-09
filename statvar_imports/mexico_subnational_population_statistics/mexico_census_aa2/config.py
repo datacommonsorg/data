74aaf596a5d68c05f9835a64bbdfc6947e2560bc
Sudhisha K <sudhisha@google.com>
2025-04-09 15:12:11 +0000

    MexicoCensus StatVar configuration update

---
+++ statvar_imports/mexico_subnational_population_statistics/mexico_census_aa2/config.py
@@ -0,0 +1,4 @@
+Mexico_Census_URL = [
+"https://data.humdata.org/dataset/05d82fd1-c2a8-402a-86f5-745837553eed/resource/12898001-e1e3-4ae6-8bb8-946319d12b65/download/mex_admpop_2021_v2.xlsx",
+"https://data.humdata.org/dataset/05d82fd1-c2a8-402a-86f5-745837553eed/resource/b07fc6f2-5637-4cee-9a80-a38154f3403f/download/mex_admpop_2024.xlsx"
+]
