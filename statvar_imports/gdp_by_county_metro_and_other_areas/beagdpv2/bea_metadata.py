f53020b350141b1b2fbb7f34c1595bc3128fb501
Sudhisha K <sudhisha@google.com>
2025-04-22 06:31:42 +0000

    BEAGDPv2 Statvar Configuration Update

---
+++ statvar_imports/gdp_by_county_metro_and_other_areas/beagdpv2/bea_metadata.py
@@ -0,0 +1,9 @@
+{
+  'header_rows' :1,
+  '#input_encoding': 'latin-1',
+  '#input_data_dialect': 'unix',
+  'output_columns':'observationAbout,observationDate,value,variableMeasured',
+  'mapped_columns':8,
+  'mapped_rows':1,
+  '#input_rows': 100
+}
