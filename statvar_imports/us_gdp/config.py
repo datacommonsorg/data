7e5bdf8b724ae7c7ef4dcd291e72307abee1afd8
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2025-06-13 17:00:12 +0530

    feat: Add initial files for US GDP import
    This commit introduces the initial directory structure and configuration
    files for importing US GDP data. It includes the  and
     files, as well as a sample  file.

---
+++ statvar_imports/us_gdp/config.py
@@ -0,0 +1,7 @@
+{
+    'required_statvar_properties': [
+        'populationType',
+        'measuredProperty',
+    ],
+    'output_only_new_statvars': False,
+}
---
+++ statvar_imports/us_gdp/pv_map.py
@@ -0,0 +1,119 @@
+{
+    "GeoFIPS": {
+        "observationAbout": "dcid:geoId/{@Number}"
+    },
+    "LineCode": {
+        "1": {
+            "populationType": "dcs:Person",
+            "measuredProperty": "dcs:Amount",
+            "unit": "dcs:USDollar",
+            "statType": "dcs:measuredValue",
+            "name": "Real GDP"
+        },
+        "2": {
+            "populationType": "dcs:Person",
+            "measuredProperty": "dcs:Amount",
+            "statType": "dcs:quantityIndex",
+            "name": "Chain-type quantity indexes for real GDP"
+        },
+        "3": {
+            "populationType": "dcs:Person",
+            "measuredProperty": "dcs:Amount",
+            "unit": "dcs:USDollar",
+            "statType": "dcs:measuredValue",
+            "name": "Current-dollar GDP"
+        }
+    },
+    "2001": {
+        "observationDate": "2001",
+        "value": "@Number"
+    },
+    "2002": {
+        "observationDate": "2002",
+        "value": "@Number"
+    },
+    "2003": {
+        "observationDate": "2003",
+        "value": "@Number"
+    },
+    "2004": {
+        "observationDate": "2004",
+        "value": "@Number"
+    },
+    "2005": {
+        "observationDate": "2005",
+        "value": "@Number"
+    },
+    "2006": {
+        "observationDate": "2006",
+        "value": "@Number"
+    },
+    "2007": {
+        "observationDate": "2007",
+        "value": "@Number"
+    },
+    "2008": {
+        "observationDate": "2008",
+        "value": "@Number"
+    },
+    "2009": {
+        "observationDate": "2009",
+        "value": "@Number"
+    },
+    "2010": {
+        "observationDate": "2010",
+        "value": "@Number"
+    },
+    "2011": {
+        "observationDate": "2011",
+        "value": "@Number"
+    },
+    "2012": {
+        "observationDate": "2012",
+        "value": "@Number"
+    },
+    "2013": {
+        "observationDate": "2013",
+        "value": "@Number"
+    },
+    "2014": {
+        "observationDate": "2014",
+        "value": "@Number"
+    },
+    "2015": {
+        "observationDate": "2015",
+        "value": "@Number"
+    },
+    "2016": {
+        "observationDate": "2016",
+        "value": "@Number"
+    },
+    "2017": {
+        "observationDate": "2017",
+        "value": "@Number"
+    },
+    "2018": {
+        "observationDate": "2018",
+        "value": "@Number"
+    },
+    "2019": {
+        "observationDate": "2019",
+        "value": "@Number"
+    },
+    "2020": {
+        "observationDate": "2020",
+        "value": "@Number"
+    },
+    "2021": {
+        "observationDate": "2021",
+        "value": "@Number"
+    },
+    "2022": {
+        "observationDate": "2022",
+        "value": "@Number"
+    },
+    "2023": {
+        "observationDate": "2023",
+        "value": "@Number"
+    }
+}
