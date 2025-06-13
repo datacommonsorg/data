aa4c0df4f84bdb547f3514eee63ffe52aff28140
Bipin Abraham <bipina@google.com>
2025-06-13 04:05:14 +0000

    updated the statvar processor to not add mmethod dcAggregate for aggregation type last

--- tools/statvar_importer/stat_var_processor.py
+++ tools/statvar_importer/stat_var_processor.py
@@ -719,7 +719,7 @@ class StatVarsMap:
         if not mmethod:
             current_pvs['measurementMethod'] = 'dcs:DataCommonsAggregate'
         elif not mmethod.startswith(
-                'dcAggregate/') and mmethod != 'DataCommonsAggregate':
+                'dcAggregate/') and mmethod != 'DataCommonsAggregate' and aggregation_type != 'last':
             current_pvs['measurementMethod'] = f'dcs:dcAggregate/{mmethod}'
         dup_svobs_key = self._config.get('duplicate_svobs_key')
         if dup_svobs_key in current_pvs:
