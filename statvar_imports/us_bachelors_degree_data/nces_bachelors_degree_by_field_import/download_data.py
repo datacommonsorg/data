b38d57d0e71520123d2f784e5599f6618528d30b
Sudhisha K <sudhisha@google.com>
2025-06-17 11:37:23 +0000

    NCES_Bachelors_Degree_By_Field_Import added preprocess script.

--- statvar_imports/us_bachelors_degree_data/nces_bachelors_degree_by_field_import/download_data.py
+++ statvar_imports/us_bachelors_degree_data/nces_bachelors_degree_by_field_import/download_data.py
@@ -23,15 +23,16 @@ def retry_method(url, headers=None):
     return response
 
 def download(urls, inputdir):
-    if not os.path.exists(inputdir):
-        os.makedirs(inputdir)
+    if not os.path.exists(inputdir):        
+        os.makedirs(inputdir, exist_ok=True)
         logging.info(f"Created download directory: {inputdir}")
-    count = 0
 
+    count = 0
     for url in urls:
         for i in range(13,32):
             try:
                 response = retry_method(url.format(i))
+                gender_code = None
                 if "40" in url:
                     gender_code = 40
                 elif "50" in url:
@@ -40,6 +41,7 @@ def download(urls, inputdir):
                     with open(os.path.join(inputdir,f"table_{gender_code}_{count}.xlsx"),"wb") as f:
                         for chunk in response.iter_content(chunk_size=8192):
                             f.write(chunk)
+                    logging.info(f"Successfully downloaded the data for the url {url.format(i)}")
                 else:
                     logging.warning(f"Skipping download for {url}. Received non-200 status code: {response.status_code}")
                 count+=1
---
+++ statvar_imports/us_bachelors_degree_data/nces_bachelors_degree_by_field_import/preprocess.py
@@ -0,0 +1,46 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, shutil
+from absl import logging, app
+
+def move_latest_files(inputdir, inputdir_latest):
+    if not os.path.exists(inputdir_latest):        
+        os.makedirs(inputdir_latest, exist_ok=True)
+        logging.info(f"Created download directory: {inputdir_latest}")
+    max_digit_male,max_digit_female = -1,-1
+    for filename in os.listdir(inputdir):
+        filename_last_digit = int(filename.split(".")[0].split("_")[-1])
+        if "40" in filename and filename_last_digit > max_digit_male:
+            max_digit_male = filename_last_digit
+        elif "50" in filename and filename_last_digit > max_digit_female:
+            max_digit_female = filename_last_digit
+    for filename in os.listdir(inputdir):
+        if str(max_digit_male) in filename or str(max_digit_female) in filename:
+            src_filepath = os.path.join(inputdir, filename)
+            dest_filepath = os.path.join(inputdir_latest, filename)
+            shutil.move(src_filepath, dest_filepath)
+            logging.info(f"Successfully moved the latest file {filename} from {inputdir} to {inputdir_latest}.")
+
+def main(_):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    input_dir = os.path.join(script_dir, "input_files")
+    input_dir_latest = os.path.join(script_dir, "input_files_latest")
+    try:
+        move_latest_files(input_dir,input_dir_latest)
+    except Exception as e:
+        logging.fatal(f"A critical error prevented pre process the data from  completing: {e}")
+
+if __name__ == "__main__":
+   app.run(main)
