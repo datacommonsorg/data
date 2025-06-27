b8d0bd256477c6cfa08b5d2c9ccddf6cc20a2b6e
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2025-06-27 14:16:59 +0530

    Refactor: Refactored and added tests for counters.py

--- util/counters_test.py
+++ util/counters_test.py
@@ -25,51 +25,50 @@ from counters import Counters, CounterOptions
 
 class TestCounters(unittest.TestCase):
 
-    def test_add_counter(self):
-        '''Verify increment and decrement counters.'''
+    def test_add_counter_increment(self):
         counters = Counters(prefix='test_')
-        counters.add_counter('inputs', 10,
-                             'test-case-1').add_counter('outputs', 2)
+        counters.add_counter('inputs', 10)
         self.assertEqual(10, counters.get_counter('inputs'))
-        self.assertEqual(2, counters.get_counter('outputs'))
-        counters.add_counter('inputs', 1, 'test-case-1')
+        counters.add_counter('inputs', 1)
         self.assertEqual(11, counters.get_counter('inputs'))
-        counters.add_counter('inputs', -5, 'test-case-1')
-        self.assertEqual(6, counters.get_counter('inputs'))
+
+    def test_add_counter_decrement(self):
+        counters = Counters(prefix='test_')
+        counters.add_counter('inputs', 10)
+        self.assertEqual(10, counters.get_counter('inputs'))
+        counters.add_counter('inputs', -5)
+        self.assertEqual(5, counters.get_counter('inputs'))
+
+    def test_add_counter_default_increment(self):
+        counters = Counters(prefix='test_')
         counters.add_counter('inputs')
-        self.assertEqual(7, counters.get_counter('inputs'))
+        self.assertEqual(1, counters.get_counter('inputs'))
 
-    def test_set_counter(self):
+    def test_set_counter_overwrites_value(self):
         '''Verify set_counter overrides current value.'''
-        counters = Counters(prefix='test2_', options=CounterOptions(debug=True))
-        counters.add_counter('lines', 1, 'file1')
-        counters.add_counter('lines', 10, 'file1')
-        counters.print_counters()
-        self.assertEqual(11, counters.get_counter('lines'))
-        self.assertEqual(11, counters.get_counter('lines_file1'))
-        counters.set_counter('lines', 100, 'file1')
+        counters = Counters(prefix='test2_')
+        counters.add_counter('lines', 1)
+        counters.set_counter('lines', 100)
         self.assertEqual(100, counters.get_counter('lines'))
-        self.assertEqual(100, counters.get_counter('lines_file1'))
 
-    def test_debug_counters(self):
+    def test_debug_counters_are_correctly_updated(self):
         '''Verify counters with debug string suffixes.'''
         counters = Counters(prefix='test3_', options=CounterOptions(debug=True))
         counters.add_counter('inputs', 10, 'test-case-2')
         counters.add_counter('inputs', 11, 'file2')
-        counters.add_counter('outputs', 2)
         self.assertEqual(21, counters.get_counter('inputs'))
         self.assertEqual(10, counters.get_counter('inputs_test-case-2'))
         self.assertEqual(11, counters.get_counter('inputs_file2'))
-        self.assertEqual(2, counters.get_counter('outputs'))
 
-    def test_counter_dict(self):
+    def test_counter_dict_is_shared(self):
         '''Verify counter dict is shared across counters.'''
         common_dict = {}
         counters1 = Counters(counters_dict=common_dict)
         counters2 = Counters(counters_dict=common_dict)
         counters1.add_counter('test_ctr', 1)
+        self.assertEqual(1, counters2.get_counter('test_ctr'))
 
-    def test_show_counters(self):
+    def test_show_counters_produces_correct_output(self):
         counters = Counters(prefix='test-',
                             options=CounterOptions(
                                 total_counter='file-rows',
@@ -78,8 +77,27 @@ class TestCounters(unittest.TestCase):
                             ))
         counters.add_counter('file-rows', 100)
         counters.add_counter('read-rows', 10)
-        counters.print_counters()
         c_str = counters.get_counters_string()
-        self.assertTrue(c_str.find('test-process_elapsed_time') > 0)
-        self.assertTrue(c_str.find('test-processing_rate') > 0)
-        self.assertTrue(counters.get_counter('process_remaining_time') > 0)
+        self.assertTrue(c_str.find('test-file-rows') > 0)
+        self.assertTrue(c_str.find('test-read-rows') > 0)
+
+    def test_min_counter(self):
+        counters = Counters()
+        counters.min_counter('min_val', 10)
+        self.assertEqual(10, counters.get_counter('min_val'))
+        counters.min_counter('min_val', 5)
+        self.assertEqual(5, counters.get_counter('min_val'))
+        counters.min_counter('min_val', 15)
+        self.assertEqual(5, counters.get_counter('min_val'))
+
+    def test_max_counter(self):
+        counters = Counters()
+        counters.max_counter('max_val', 10)
+        self.assertEqual(10, counters.get_counter('max_val'))
+        counters.max_counter('max_val', 5)
+        self.assertEqual(10, counters.get_counter('max_val'))
+        counters.max_counter('max_val', 15)
+        self.assertEqual(15, counters.get_counter('max_val'))
+
+if __name__ == '__main__':
+    unittest.main()
