0d66b93dff5363fb4cc1931f951c5d82e914aaa9
Megha18jain <jainme@google.com>
2025-07-01 19:00:46 +0530

    Update pep_asrh_download_script.py

--- statvar_imports/us_census/pep/us_census_pep_asrh/pep_asrh_download_script.py
+++ statvar_imports/us_census/pep/us_census_pep_asrh/pep_asrh_download_script.py
@@ -16,14 +16,23 @@ import os
 import shutil
 from absl import app
 from absl import logging
+from absl import flags
 import requests
 from retry import retry
 import ssl
 
 
-input_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "input_files"
+	
+# Define command-line flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    "input_path",
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "input_files"),
+    "Directory to store downloaded files.",
 )
+flags.DEFINE_integer("start_year", 2030, "Starting year for data search (inclusive).")
+flags.DEFINE_integer("end_year", 2022, "Ending year for data search (exclusive). The search will go up to `end_year + 1`.")
+
 _URLS_TO_SCAN = {
     "national": (
         "https://www2.census.gov/programs-surveys/popest/datasets/2020-{YEAR}/national/asrh/nc-est{YEAR}-alldata-r-file{i}.csv"
@@ -36,68 +45,62 @@ _URLS_TO_SCAN = {
     ),
 }
 
-
+def _check_and_add_url(url_to_check: str, key: str, files_to_download: dict):
+    """
+    Helper function to check if a URL is accessible and add it to the download list.
+    """
+    logging.info(f"checking url: {url_to_check}")
+    try:
+        # TODO: Provide a custom certificate bundle instead of disabling verification.
+        check_url = requests.head(
+            url_to_check, allow_redirects=True, verify=False
+        )
+        if check_url.status_code == 200:
+            files_to_download[key].append(url_to_check)
+            logging.info(f"Adding {url_to_check}")
+        else:
+            logging.warning(
+                f"Url not found: {url_to_check} with status code: {check_url.status_code}"
+            )
+    except requests.exceptions.RequestException as e:
+        logging.fatal(f"URL is not accessible {url_to_check} due to {e}")
+        
 @retry(
     tries=3,
     delay=2,
     backoff=2,
     exceptions=(requests.RequestException, Exception),
 )
-def add_future_urls():
+def add_future_urls(start_year: int, end_year: int):
   # Initialize the list to store files to download
-  files_to_download = {} 
-  for key, value in _URLS_TO_SCAN.items():
-    files_to_download[key] = []
-    for future_year in range(2030, 2022, -1):  # From 2030 to 2023
-      YEAR = future_year
-      if (
-          "{i}" in value
-      ):  # This URL contains the {i} variable, so we loop through i from 01 to 10
-
-        for i in range(1, 11):
-          formatted_i = (  # Ensure i is always 2 digits (01, 02, ..., 10)
-              f"{i:02}"
-          )
-          url_to_check = value.format(YEAR=YEAR, i=formatted_i)
-          logging.info(f"checking url: {url_to_check}    {i}")
-          try:
-            # TODO: Provide a custom certificate bundle instead of disabling verification.
-            check_url = requests.head(
-                url_to_check, allow_redirects=True, verify=False
-            )
-            if check_url.status_code == 200:
-              files_to_download[key].append(url_to_check)
-              logging.info(f"Adding {url_to_check}")
+    files_to_download = {}
+    for key, value in _URLS_TO_SCAN.items():
+        files_to_download[key] = []
+        # Loop from start_year down to end_year + 1
+        for current_year in range(start_year, end_year, -1):
+            YEAR = current_year
+            if (
+                "{i}" in value
+            ):  # This URL contains the {i} variable, so we loop through i from 01 to 10
+                for i in range(1, 11):
+                    # Ensure i is always 2 digits (01, 02, ..., 10)
+                    formatted_i = f"{i:02}"
+                    url_to_check = value.format(YEAR=YEAR, i=formatted_i)
+                    _check_and_add_url(url_to_check, key, files_to_download)
             else:
-              logging.warning(f"Url not found: {url_to_check} with status code: {check_url.status_code}")
-          except requests.exceptions.RequestException as e:
-            logging.fatal(f"URL is not accessible {url_to_check} due to {e}")
+                url_to_check = value.format(YEAR=YEAR)
+                _check_and_add_url(url_to_check, key, files_to_download)
+    return files_to_download  # Return the populated dictionary
 
-      else:
-        url_to_check = value.format(YEAR=YEAR)
-        logging.info(f"checking url: {url_to_check}")
-        try:
-          url_to_check = value.format(YEAR=YEAR)
-          # TODO: Provide a custom certificate bundle instead of disabling verification.
-          check_url = requests.head(
-              url_to_check, allow_redirects=True, verify=False
-          )
-          if check_url.status_code == 200:
-            files_to_download[key].append(url_to_check)
-            logging.info(f"Adding {url_to_check}")
-          else:
-            logging.warning(f"Url not found: {url_to_check} with status code: {check_url.status_code}")
-        except requests.exceptions.RequestException as e:
-          logging.fatal(f"URL is not accessible {value} due to {e}")
-  return files_to_download # Return the populated dictionary
-
-def download_files(files_to_download_dict):
+def download_files(files_to_download_dict:dict, download_base_path: str):
   for key, value in files_to_download_dict.items():
-    # Local path to save the downloaded file
+    download_folder = os.path.join(download_base_path, key)
+    if not (os.path.exists(download_folder)):
+            os.makedirs(download_folder)
+            logging.info(f"Created download directory: {download_folder}")
     for url in value:
       output_file_name = url.split("/")[-1]
-      download_folder = os.path.join(input_path, key)
-      output_file_path = f"{download_folder}/" + output_file_name
+      output_file_path = os.path.join(download_folder, output_file_name)
 
       # Send GET request
       # TODO: Provide a custom certificate bundle instead of disabling verification.
@@ -106,9 +109,7 @@ def download_files(files_to_download_dict):
       except requests.exceptions.RequestException as e:
         logging.fatal(f"Error downloading {url}: {e}")
         continue
-      if not (os.path.exists(download_folder)):
-        os.makedirs(download_folder)
-
+      
       # Save the file content
       with open(output_file_path, "wb") as f:
         f.write(response.content)
@@ -128,25 +129,26 @@ def main(_):
 
   # Clearing the download folder is it already exists
   try:
-    if os.path.exists(input_path):
-      if os.listdir(input_path):
-        for filename in os.listdir(input_path):
-          file_path = os.path.join(input_path, filename)
-          try:
-            if os.path.isdir(file_path):
-              shutil.rmtree(file_path)
-            else:
-              os.remove(file_path)
-          except Exception as e:
-            logging.error(f"Error while clearing {file_path}: {e}")
+        if os.path.exists(FLAGS.input_path):
+            logging.info(f"Clearing existing contents in: {FLAGS.input_path}")
+            # shutil.rmtree followed by os.makedirs is a cleaner way to clear and recreate
+            # an entire directory tree.
+            shutil.rmtree(FLAGS.input_path)
   except Exception as e:
-    logging.error(f"Error in clear_folder: {e}")
+        logging.error(f"Error in clearing folder {FLAGS.input_path}: {e}")
 
-  # add_future_urls
-  logging.info("Generating future URLs")
-  download_urls = add_future_urls()
-  logging.info("Starting download")
-  download_files(download_urls) # Pass the dictionary as an argument
+  # Ensure the base input_path exists after clearing
+  os.makedirs(FLAGS.input_path, exist_ok=True)
+  logging.info(f"Ensured fresh base download directory: {FLAGS.input_path}")
+
+
+  # Add future URLs 
+  logging.info(f"Generating URLs from {FLAGS.start_year} down to {FLAGS.end_year + 1}")
+  download_urls = add_future_urls(FLAGS.start_year, FLAGS.end_year)
+    
+  # Start download
+  logging.info("Starting download of identified files")
+  download_files(download_urls, FLAGS.input_path) # Pass the dictionary as an argument
 
 
 if __name__ == "__main__":
