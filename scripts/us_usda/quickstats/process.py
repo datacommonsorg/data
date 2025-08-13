97f89ad2b58126b3b9788d46fa724bb23df098c6
shapateriya <spateriya@google.com>
2025-08-13 06:05:16 +0000

    pr for usda_agriculture import

--- scripts/us_usda/quickstats/process.py
+++ scripts/us_usda/quickstats/process.py
@@ -21,25 +21,27 @@ You can request an API key here: https://quickstats.nass.usda.gov/api/
 
 If the key is not specified as above, it falls back to using a key specified
 in a GCS config file. However, that file is available to DC team members only.
+
 """
 
-import csv
-from datetime import datetime
-from itertools import repeat
 import json
-import multiprocessing
-from multiprocessing import get_context
-import os
-import sys
 
+import requests
+import sys
+import csv
+import multiprocessing
+from itertools import repeat
+import os
+import datetime
+from google.cloud import storage
+from retry import retry
 from absl import app
 from absl import flags
-from google.cloud import storage
-from ratelimit import limits, sleep_and_retry
-import requests
+from absl import logging
+from collections import deque
+import time
 
 API_BASE = 'https://quickstats.nass.usda.gov/api'
-
 CSV_COLUMNS = [
     'variableMeasured',
     'observationDate',
@@ -54,181 +56,291 @@ SKIPPED_COUNTY_CODES = set([
     '998',  # "OTHER" county code
 ])
 
-_GCS_PROJECT_ID = 'datcom-204919'
-_GCS_BUCKET = 'datcom-csv'
-_GCS_FILE_PATH = 'usda/agriculture_survey/config.json'
+_GCS_PROJECT_ID = "datcom-204919"
+_GCS_BUCKET = "datcom-csv"
+_GCS_FILE_PATH = "usda/agriculture_survey/config.json"
 
 _USDA_API_KEY = 'usda_api_key'
 
 _FLAGS = flags.FLAGS
-
+flags.DEFINE_string('mode', '', 'Options: download or process')
+flags.DEFINE_string('start_year', '2024', 'start_year')
 flags.DEFINE_string(_USDA_API_KEY, None, 'USDA quickstats API key.')
-flags.DEFINE_integer(
-    'start_year',
-    os.getenv('start_year',
-              datetime.now().year),
-    'Year from which data is processed.',
-)
-flags.DEFINE_integer(
-    'num_counties',
-    os.getenv('num_counties', 100),
-    'number of counties for which data is processed.',
-)
-flags.DEFINE_string('output_dir', 'output',
-                    'Output firectory for generated files.')
 
 
-def process_survey_data(year, svs, out_dir, usda_api_key):
-    start = datetime.now()
-    print('Start', year, '=', start)
+def process_survey_data(year, svs, input_dir, out_dir):
+    """
+    Processes survey data for the given year and saves the results.
 
-    os.makedirs(get_parts_dir(out_dir, year), exist_ok=True)
-    os.makedirs(get_response_dir(out_dir, year), exist_ok=True)
+    Args:
+        year: The year of the survey data.
+        svs: A list of survey variables to process.
+        input_dir: The output directory to save the processed data.
 
-    print('Processing survey data for year', year)
+    Returns:
+        None
+    """
+    start = datetime.datetime.now()
+    logging.info(f'Start, {year}, =, {start}')
+    try:
+        logging.info(f"start processing data for the year : {year}")
+        if _FLAGS.mode == "" or _FLAGS.mode == "download":
+            os.makedirs(get_parts_dir(input_dir, year), exist_ok=True)
+            os.makedirs(get_response_dir(input_dir, year), exist_ok=True)
+            os.makedirs(out_dir, exist_ok=True)
 
-    print('Getting county names')
-    county_names = get_param_values('county_name', usda_api_key)
-    county_names = county_names[:_FLAGS.num_counties]
-    print('# counties =', len(county_names))
+            logging.info('Getting county names')
+            county_names = get_param_values('county_name')
+            logging.info(f'# counties =, {len(county_names)}')
 
-    pool_size = max(2, multiprocessing.cpu_count() - 1)
+            pool_size = 2  #max(2, multiprocessing.cpu_count() - 1)
 
-    with get_context("spawn").Pool(pool_size) as pool:
-        pool.starmap(
-            fetch_and_write,
-            zip(county_names, repeat(year), repeat(svs), repeat(out_dir),
-                repeat(usda_api_key)),
+            with multiprocessing.Pool(pool_size) as pool:
+                pool.starmap(
+                    fetch_and_write,
+                    zip(county_names, repeat(year), repeat(svs),
+                        repeat(input_dir)))
+
+        if _FLAGS.mode == "" or _FLAGS.mode == "process":
+            write_aggregate_csv(year, input_dir, out_dir)
+
+        end = datetime.datetime.now()
+        logging.info(f'End, {year}, =, {end}')
+        logging.info(f'Duration, {year}, =, {str(end - start)}')
+    except Exception as e:
+        logging.fatal(f"Error while processing the data for year :{e}")
+
+
+def get_parts_dir(input_dir, year):
+    return f'{input_dir}/parts/{year}'
+
+
+def get_response_dir(input_dir, year):
+    return f"{input_dir}/response/{year}"
+
+
+def get_response_file_path(input_dir, year, county):
+    return f"{get_response_dir(input_dir, year)}/{county}.json"
+
+
+def write_aggregate_csv(year, input_dir, out_dir):
+    """
+    Writes aggregated data to a CSV file.
+
+    Args:
+        year: The year of the data to aggregate.
+        input_dir: The directory where the CSV file should be saved.
+
+    This function likely performs the following steps:
+
+    1. **Load data:** Reads the necessary data files for the given year. 
+    2. **Aggregate data:** Performs the aggregation logic (e.g., summing, averaging, grouping) on the loaded data.
+    3. **Write to CSV:** Writes the aggregated data to a CSV file within the specified output directory. 
+
+    The specific implementation will depend on the nature of the data and the desired aggregation.
+    """
+    logging.info(f"Aggregation starts for the year: {year}")
+    try:
+        parts_dir = get_parts_dir(input_dir, year)
+        part_files = os.listdir(parts_dir)
+        out_file = f"{out_dir}/ag-{year}.csv"
+
+        logging.info(f'Writing aggregate CSV, {out_file}')
+
+        with open(out_file, 'w', newline='') as out:
+            csv_writer = csv.DictWriter(out,
+                                        fieldnames=CSV_COLUMNS,
+                                        lineterminator='\n')
+            csv_writer.writeheader()
+            for part_file in part_files:
+                if part_file.endswith(".csv"):
+                    with open(f"{parts_dir}/{part_file}", 'r') as part:
+                        csv_writer.writerows(csv.DictReader(part))
+    except Exception as e:
+        logging.fatal(f"Error in write_aggregate_csv for year {year}: {e}")
+
+
+def fetch_and_write(county_name, year, svs, input_dir):
+    """
+    Fetches survey data for a given county and writes it to a CSV file.
+    Args:
+        county_name: The name of the county.
+        year: The year of the survey data.
+        svs: A list of survey variables to include in the output.
+        input_dir: The output directory for the CSV file.
+
+    Raises:
+        Exception: If any error occurs during the data fetching or writing process.
+    """
+    logging.info(f"Fetching data for county: {county_name}")
+    try:
+        out_file = f"{get_parts_dir(input_dir, year)}/{county_name.replace('[^a-zA-Z0-9]', '')}.csv"
+        api_data = get_survey_county_data(year, county_name, input_dir)
+        county_csv_rows = to_csv_rows(api_data, svs)
+        if county_csv_rows:
+            logging.info(
+                f"Writing {len(county_csv_rows)} rows for county {county_name} to file {out_file}"
+            )
+            with open(out_file, 'w', newline='') as out:
+                write_csv(out, county_csv_rows)
+        else:
+            logging.info(
+                f"No data to write for county {county_name}. Skipping file creation."
+            )
+        logging.info(
+            f"Writing {len(county_csv_rows)} rows for county {county_name} to file {out_file}"
         )
-
-    write_aggregate_csv(year, out_dir)
-
-    end = datetime.now()
-    print('End', year, '=', end)
-    print('Duration', year, '=', str(end - start))
+    except Exception as e:
+        logging.fatal(
+            f"Error processing data for county: {county_name}. Error: {e}")
 
 
-def get_parts_dir(out_dir, year):
-    return f'{out_dir}/parts/{year}'
+def get_survey_county_data(year, county, input_dir):
+    """
+    Fetches survey data for a given county and year.
+    Args:
+        year: The year of the survey data.
+        county: The name of the county.
+        input_dir: The output directory to save the response file.
+    Returns:
+        The downloaded survey data.
+    Raises:
+        Exception: If an error occurs during data fetching or writing.
+    """
 
+    logging.info(f"Fetching survey data for county: {county} and year: {year}")
 
-def get_response_dir(out_dir, year):
-    return f'{out_dir}/response/{year}'
+    response_file = get_response_file_path(input_dir, year, county)
+    if _FLAGS.mode == "process":
+        try:
+            if os.path.exists(response_file):
+                logging.info(f"Reading response from file: {response_file}")
+                with open(response_file, 'r') as f:
+                    response = json.load(f)
+            else:
+                logging.error(f"Response file not found: {response_file}")
+        except Exception as e:
+            logging.error(
+                f"Error while reading response file: {response_file}. Error: {e}"
+            )
 
+    if _FLAGS.mode == "" or _FLAGS.mode == "download":
 
-def get_response_file_path(out_dir, year, county):
-    return f'{get_response_dir(out_dir, year)}/{county}.json'
-
-
-def get_year_csv_file_path(out_dir, year):
-    return f'{out_dir}/ag-{year}.csv'
-
-
-def write_aggregate_csv(year, out_dir):
-    parts_dir = get_parts_dir(out_dir, year)
-    part_files = os.listdir(parts_dir)
-    out_file = get_year_csv_file_path(out_dir, year)
-
-    print('Writing aggregate CSV', out_file)
-
-    with open(out_file, 'w', newline='') as out:
-        csv_writer = csv.DictWriter(out,
-                                    fieldnames=CSV_COLUMNS,
-                                    lineterminator='\n')
-        csv_writer.writeheader()
-        for part_file in part_files:
-            if part_file.endswith('.csv'):
-                with open(f'{parts_dir}/{part_file}', 'r') as part:
-                    csv_writer.writerows(csv.DictReader(part))
-
-
-def write_consolidated_csv(years, out_dir):
-    out_file = f'{out_dir}/consolidated.csv'
-
-    print('Writing consolidated CSV', out_file)
-
-    with open(out_file, 'w', newline='') as out:
-        csv_writer = csv.DictWriter(out,
-                                    fieldnames=CSV_COLUMNS,
-                                    lineterminator='\n')
-        csv_writer.writeheader()
-        for year in years:
-            with open(get_year_csv_file_path(out_dir, year), 'r') as part:
-                csv_writer.writerows(csv.DictReader(part))
-
-
-def fetch_and_write(county_name, year, svs, out_dir, usda_api_key):
-    out_file = (
-        f"{get_parts_dir(out_dir, year)}/{county_name.replace('[^a-zA-Z0-9]', '')}.csv"
-    )
-    api_data = get_survey_county_data(year, county_name, out_dir, usda_api_key)
-    county_csv_rows = to_csv_rows(api_data, svs)
-    print(
-        'Writing',
-        len(county_csv_rows),
-        'rows for county',
-        county_name,
-        'to file',
-        out_file,
-    )
-    with open(out_file, 'w', newline='') as out:
-        write_csv(out, county_csv_rows)
-
-
-def get_survey_county_data(year, county, out_dir, usda_api_key):
-    print('Getting', year, 'survey data for county', county)
-
-    response_file = get_response_file_path(out_dir, year, county)
-    if os.path.exists(response_file):
-        print('Reading response from file', response_file)
-        with open(response_file, 'r') as f:
-            response = json.load(f)
-    else:
         params = {
-            'key': usda_api_key,
-            'source_desc': 'SURVEY',
+            'key': get_usda_api_key(),
+            'source_desc': "SURVEY",
             'year': year,
-            'county_name': county,
+            'county_name': county
         }
-        response = get_data(params)
-        with open(response_file, 'w') as f:
-            print('Writing response to file', response_file)
-            json.dump(response, f, indent=2)
+        try:
+            response = get_data(params)
+            if response is None:
+                logging.error(
+                    f"get_data() returned None. Check logs for details.")
+                print(f"No data found for: {county}")
 
-    if 'data' not in response:
-        eprint('No api records found for county', county)
-        return {'data': []}
+                return {'data': []}
+            with open(response_file, 'w') as f:
+                logging.info(f"Writing response to file: {response_file}")
+                json.dump(response, f, indent=2)
 
-    print('# api records for', county, '=', len(response['data']))
+            if 'data' not in response:
+                logging.info(f"No api records found for county: {county}")
+                return {'data': []}
+        except Exception as e:
+            logging.fatal(f"Error while fetching data for county: {county}")
     return response
 
 
-@sleep_and_retry
-@limits(calls=10, period=60)
+MAX_REQUESTS_PER_WINDOW = 2  # Maximum requests per window (1 request)
+RATE_LIMIT_WINDOW = 60  # Time window in seconds (1 minute)
+request_times = deque()
+FIVE_MINUTES_IN_SECONDS = 300  # 5 minutes in seconds
+
+
+def is_rate_limited():
+    current_time = time.time()
+    while request_times and request_times[0] <= current_time - RATE_LIMIT_WINDOW:
+        request_times.popleft()
+    return len(request_times) >= MAX_REQUESTS_PER_WINDOW
+
+
+@retry(tries=3,
+       delay=2,
+       backoff=2,
+       exceptions=requests.exceptions.RequestException
+      )  # Retry only for request exceptions
 def get_data(params):
-    return requests.get(f'{API_BASE}/api_GET', params=params).json()
+    """
+    Download data from the API with retry, rate limiting, and status code handling.
+
+    Args:
+        params (dict): Parameters for the API request.
+    """
+    while is_rate_limited():
+        time_until_reset = RATE_LIMIT_WINDOW - (time.time() - request_times[0])
+        logging.info(f"Rate limit hit. Waiting {time_until_reset:.1f} seconds.")
+        # time.sleep(time_until_reset)
+        time.sleep(60)
+
+    logging.info(f"Fetching data from API: {params}")
+
+    try:
+        response = requests.get(f'{API_BASE}/api_GET', params=params)
+        response.raise_for_status()
+
+        if response.status_code != 200:
+            logging.warning(
+                f"API returned non-200 status code: {response.status_code}. Retrying..."
+            )
+            raise requests.exceptions.RequestException(
+                f"Non-200 status code: {response.status_code}")
+
+        request_times.append(time.time())
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching data: {e}")
 
 
-def get_param_values(param, usda_api_key):
-    params = {'key': usda_api_key, 'param': param}
-    response = requests.get(f'{API_BASE}/get_param_values',
-                            params=params).json()
-    return [] if param not in response else response[param]
+def get_param_values(param):
+    """
+    Retrieves possible values for a given parameter from the API.
+
+    Args:
+        param: The name of the parameter to retrieve values for.
+
+    Returns:
+        A list of possible values for the given parameter, or an empty list if 
+        no values are found or an error occurs. 
+
+    Raises:
+        Exception: If an error occurs during the API request.
+    """
+    params = {'key': get_usda_api_key(), 'param': param}
+    try:
+        response = requests.get(f'{API_BASE}/get_param_values',
+                                params=params).json()
+        return [] if param not in response else response[param]
+
+    except Exception as e:
+        logging.error(f"Error while fetching parameter values: {e}")
+    return []
 
 
-"""Converts a quickstats data row to a DC CSV row.
+'''Converts a quickstats data row to a DC CSV row.
 
 data = quickstats data row
 svs = {name: {name: ..., sv: ..., unit: ...}}
 
 returns = {variableMeasured: ..., observationAbout: ..., value: ..., unit: ...}
-"""
+'''
 
 
 def to_csv_row(data_row, svs):
     name = data_row['short_desc']
-    if (data_row['domaincat_desc'] and
-            data_row['domaincat_desc'] != 'NOT SPECIFIED'):
+    if data_row['domaincat_desc'] and data_row[
+            'domaincat_desc'] != 'NOT SPECIFIED':
         name = f"{name}%%{data_row['domaincat_desc']}"
 
     if name not in svs:
@@ -240,17 +352,16 @@ def to_csv_row(data_row, svs):
         eprint('SKIPPED, Unsupported county code', county_code)
         return None
 
-    value = ((data_row['value'] if 'value' in data_row else
-              data_row['Value']).strip().replace(',', ''))
+    value = (data_row['value'] if 'value' in data_row else
+             data_row['Value']).strip().replace(',', '')
     if value in SKIPPED_VALUES:
         eprint('SKIPPED, Invalid value', f"'{value}'", 'for', name)
         return None
     value = int(value)
 
-    observation_about = (
-        f"dcid:geoId/{data_row['state_fips_code']}{county_code}"
-        if data_row['state_fips_code'] else 'dcid:country/USA')
-
+    observation_about = f"dcid:geoId/{data_row['state_fips_code']}{county_code}" if \
+      data_row[
+        'state_fips_code'] else 'dcid:country/USA'
     sv = svs[name]
 
     return {
@@ -265,17 +376,19 @@ def to_csv_row(data_row, svs):
 def to_csv_rows(api_data, svs):
     csv_rows = []
 
-    for data_row in api_data['data']:
-        csv_row = to_csv_row(data_row, svs)
-        if csv_row:
-            csv_rows.append(csv_row)
+    if api_data and 'data' in api_data:
+
+        for data_row in api_data['data']:
+            csv_row = to_csv_row(data_row, svs)
+            if csv_row:
+                csv_rows.append(csv_row)
 
     return csv_rows
 
 
 def load_svs():
     svs = {}
-    with open('sv.csv', newline='') as csvfile:
+    with open("sv.csv", newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             svs[row['name']] = row
@@ -292,30 +405,23 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def get_all_counties(usda_api_key):
+def get_multiple_years():
+
+    start = datetime.datetime.now()
+    logging.info(f'Start, {start}')
+
     svs = load_svs()
-    process_survey_data(2023, svs, 'output', usda_api_key)
+    start_year = _FLAGS.start_year
+    for year in range(int(start_year), datetime.datetime.now().year + 1):
+        process_survey_data(year, svs, "input", "output")
 
-
-def get_multiple_years(usda_api_key):
-    start = datetime.now()
-    print('Start', start)
-
-    out_dir = _FLAGS.output_dir
-    svs = load_svs()
-    years = range(_FLAGS.start_year, datetime.now().year + 1)
-    for year in years:
-        process_survey_data(year, svs, out_dir, usda_api_key)
-
-    write_consolidated_csv(years, out_dir)
-
-    end = datetime.now()
-    print('End', end)
-    print('Duration', str(end - start))
+    end = datetime.datetime.now()
+    logging.info(f'End, {end}')
+    logging.info(f'Duration, {str(end - start)}')
 
 
 def get_cloud_config():
-    print('Getting cloud config.')
+    logging.info(f'Getting cloud config.')
     storage_client = storage.Client(_GCS_PROJECT_ID)
     bucket = storage_client.bucket(_GCS_BUCKET)
     blob = bucket.blob(_GCS_FILE_PATH)
@@ -328,15 +434,14 @@ def load_usda_api_key():
 
 
 def get_usda_api_key():
-    _FLAGS(sys.argv)
     return _FLAGS.usda_api_key
 
 
 def main(_):
+
     load_usda_api_key()
-    usda_api_key = get_usda_api_key()
-    print('USDA API key', usda_api_key)
-    get_multiple_years(usda_api_key)
+    logging.info(f'USDA API key, {get_usda_api_key()}')
+    get_multiple_years()
 
 
 if __name__ == '__main__':
