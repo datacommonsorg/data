7bb5d6b5279fd1d63d964700d851cda858dba97e
Vishal Gupta <vishg@google.com>
2025-09-08 13:38:54 +0000

    Update import status in spanner

--- import-automation/executor/app/configs.py
+++ import-automation/executor/app/configs.py
@@ -42,6 +42,10 @@ class ExecutorConfig:
     # Name of the Cloud Storage bucket to store the generated data files
     # for importing to prod.
     storage_prod_bucket_name: str = 'datcom-prod-imports'
+    # Spanner instance details for import status.
+    spanner_project_id: str = 'datcom-store'
+    spanner_instance_id: str = 'dc-kg-test'
+    spanner_database_id: str = 'dc_graph_import'
     # Name of the Cloud Storage bucket that the Data Commons importer
     # outputs to.
     storage_importer_bucket_name: str = 'resolved_mcf'
