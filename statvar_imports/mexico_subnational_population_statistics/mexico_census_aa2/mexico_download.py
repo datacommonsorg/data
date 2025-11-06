208d3545f555c1d71ba11d9a965d6809b73b9416
Avantika Singh <avantikasi@google.com>
2025-11-06 09:34:53 +0000

    auto_refresh_fixes

--- statvar_imports/mexico_subnational_population_statistics/mexico_census_aa2/mexico_download.py
+++ statvar_imports/mexico_subnational_population_statistics/mexico_census_aa2/mexico_download.py
@@ -21,8 +21,10 @@ import pandas as pd
 
 Mexico_Census_URL = config.Mexico_Census_URL
 
-OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "input_files")
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                          "input_files")
 Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+
 
 @retry(tries=3, delay=5, backoff=2)
 def retry_method(url, headers=None):
@@ -30,32 +32,40 @@ def retry_method(url, headers=None):
     response.raise_for_status()
     return response
 
+
 def download_and_convert_excel_to_csv():
     logging.info("Starting download and conversion of Excel files...")
     KEYWORDS = ["adm0", "adm1", "adm2"]
     try:
         for url in Mexico_Census_URL:
             response = retry_method(url)
-            excel_file = pd.ExcelFile(io.BytesIO(response.content))
+            excel_file = pd.ExcelFile(io.BytesIO(response.content),
+                                      engine='openpyxl')
             for sheet_name in excel_file.sheet_names:
                 try:
-                    if any(keyword in sheet_name.lower() for keyword in KEYWORDS):
+                    if any(keyword in sheet_name.lower()
+                           for keyword in KEYWORDS):
                         df = excel_file.parse(sheet_name)
                         if "ISO3" in df.columns:
                             df = df.drop(columns=["ISO3"])
-                        csv_filename = os.path.join(OUTPUT_DIR, f"{sheet_name}.csv")
+                        csv_filename = os.path.join(OUTPUT_DIR,
+                                                    f"{sheet_name}.csv")
                         df.to_csv(csv_filename, index=False, encoding='utf-8')
-                        logging.info(f"Sheet '{sheet_name}' converted to: {csv_filename}")
+                        logging.info(
+                            f"Sheet '{sheet_name}' converted to: {csv_filename}"
+                        )
                 except Exception as e:
-                    logging.fatal(f"Failed to process the sheet '{sheet_name}' : {e}")
+                    logging.fatal(
+                        f"Failed to process the sheet '{sheet_name}' : {e}")
 
     except requests.exceptions.RequestException as e:
         logging.fatal(f"Failed to download Mexico Census data file: {e}")
         return None
 
-    
+
 def main(argv):
     download_and_convert_excel_to_csv()
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     app.run(main)
