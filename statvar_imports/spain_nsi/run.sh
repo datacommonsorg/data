08bf076ca10560445ae295880b3c537fd15a57d7
Harsha Vardhan Chandaluri <chandaluri@google.com>
2025-11-07 11:14:05 +0000

    updated config files and testdata

--- statvar_imports/spain_nsi/run.sh
+++ statvar_imports/spain_nsi/run.sh
@@ -14,6 +14,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/employment_unemployment_pvmap.csv \
     --config_file=$SCRIPT_PATH/employment_unemployment_metadata.csv \
     --output_path=$SCRIPT_PATH/output/employment_unemployment_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing activity_employment_unemployment_by_sex.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -22,6 +24,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/education_field_of_study_pvmap.csv \
     --config_file=$SCRIPT_PATH/education_field_of_study_metadata.csv \
     --output_path=$SCRIPT_PATH/output/education_field_of_study_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Education_Field_of_study.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -31,6 +35,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --places_resolved_csv=$SCRIPT_PATH/places_resolved.csv \
     --config_file=$SCRIPT_PATH/gross_birth_rate_by_province_metadata.csv \
     --output_path=$SCRIPT_PATH/output/gross_birth_rate_by_province_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Gross_Birth_Rate_by_province.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -39,6 +45,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/gross_birth_rate_national_pvmap.csv \
     --config_file=$SCRIPT_PATH/gross_birth_rate_national_metadata.csv \
     --output_path=$SCRIPT_PATH/output/gross_birth_rate_national_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Gross_Birth_Rate_national.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -48,6 +56,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --places_resolved_csv=$SCRIPT_PATH/places_resolved.csv \
     --config_file=$SCRIPT_PATH/gross_birth_rate_per_municipality_metadata.csv \
     --output_path=$SCRIPT_PATH/output/gross_birth_rate_per_municipality_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Gross_Birth_Rate_per_municipality.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -57,6 +67,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --places_resolved_csv=$SCRIPT_PATH/places_resolved.csv \
     --config_file=$SCRIPT_PATH/male_rate_at_birth_by_province_metadata.csv \
     --output_path=$SCRIPT_PATH/output/male_rate_at_birth_by_province_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Male_rate_at_birth_by_province.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -65,6 +77,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/education_sex_levels_of_education_pvmap.csv \
     --config_file=$SCRIPT_PATH/education_sex_levels_of_education_metadata.csv \
     --output_path=$SCRIPT_PATH/output/levels_of_education_gender_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Levels_of_education_gender.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -74,6 +88,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --places_resolved_csv=$SCRIPT_PATH/places_resolved.csv \
     --config_file=$SCRIPT_PATH/population_by_municipality_and_gender_metadata.csv \
     --output_path=$SCRIPT_PATH/output/population_by_municipality_and_gender_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Population_by_municipality_and_gender.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -82,7 +98,9 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/population_by_province_capitals_and_gender_pvmap.csv \
     --places_resolved_csv=$SCRIPT_PATH/places_resolved.csv \
     --config_file=$SCRIPT_PATH/population_by_province_capitals_and_gender_metadata.csv \
-    --output_path=$SCRIPT_PATH/output/Population_by_province_capitals_and_gender_output \
+    --output_path=$SCRIPT_PATH/output/population_by_province_capitals_and_gender_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Population_by_province_capitals_and_gender.csv failed!"; exit 1; }
 
 python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
@@ -91,6 +109,8 @@ python3 $SCRIPT_PATH/../../tools/statvar_importer/stat_var_processor.py \
     --pv_map=$SCRIPT_PATH/population_by_sex_and_age_group_pvmap.csv \
     --config_file=$SCRIPT_PATH/population_by_sex_and_age_group_metadata.csv \
     --output_path=$SCRIPT_PATH/output/population_by_sex_and_age_group_output \
+    --log_level=-2 \
+    --log_every_n=1000 \
     || { echo "Error: Processing Population_by_sex_and_age_group.csv failed!"; exit 1; }
 
 # --- Completion ---
