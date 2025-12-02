5480758739ef1192972eed920d43d8ecf4aa3a79
rohit kumar <rohitrkumar@google.com>
2025-12-02 16:02:58 +0000

    refactor: simplify dc props processing

--- scripts/us_census/acs5yr/subject_tables/common/datacommons_api_wrappers/datacommons_wrappers.py
+++ scripts/us_census/acs5yr/subject_tables/common/datacommons_api_wrappers/datacommons_wrappers.py
@@ -132,17 +132,14 @@ def fetch_dcid_properties_enums(dcid: str,
         data_["property"] = "<-domainIncludes"
         population_props = request_post_json(
             f'https://{api_prefix}api.datacommons.org/v2/node', data_)
-        dc_population_pvs = {}
         node_data = population_props.get('data', {}).get(dcid, {})
         arcs = node_data.get('arcs', {})
         domain_includes = arcs.get('domainIncludes', {})
         nodes_list = domain_includes.get('nodes', [])
-        dc_population_pvs[dcid] = {'in': nodes_list}
 
-        if dc_population_pvs[dcid]:
-            dc_props = {}
-            for prop_dict in dc_population_pvs[dcid]['in']:
-                dc_props[prop_dict['dcid']] = []
+        dc_props = {}
+        for prop_dict in nodes_list:
+            dc_props[prop_dict['dcid']] = []
 
         with open(os.path.join(cache_path, f'{dcid}_dc_props.json'), 'w') as fp:
             json.dump(dc_props, fp, indent=2)
@@ -159,19 +156,14 @@ def fetch_dcid_properties_enums(dcid: str,
         if data_['nodes']:
             population_props_types_resp = request_post_json(
                 f'https://{api_prefix}api.datacommons.org/v2/node', data_)
-            population_props_types = {}
             for property_name in data_['nodes']:
                 node_data = population_props_types_resp.get('data', {}).get(
                     property_name, {})
                 arcs = node_data.get('arcs', {})
                 range_includes = arcs.get('rangeIncludes', {})
                 nodes_list = range_includes.get('nodes', [])
-                population_props_types[property_name] = {'out': nodes_list}
-            for property_name in population_props_types:
-                if population_props_types[property_name]:
-                    for temp_dict in population_props_types[property_name][
-                            'out']:
-                        dc_props[property_name].append(temp_dict['dcid'])
+                for temp_dict in nodes_list:
+                    dc_props[property_name].append(temp_dict['dcid'])
             with open(os.path.join(cache_path, f'{dcid}_dc_props_types.json'),
                       'w') as fp:
                 json.dump(dc_props, fp, indent=2)
@@ -193,16 +185,12 @@ def fetch_dcid_properties_enums(dcid: str,
                     enum_values = request_post_json(
                         f'https://{api_prefix}api.datacommons.org/v2/node',
                         data_)
-                    enum_values_processed = {}
                     node_data = enum_values.get('data', {}).get(type_name, {})
                     arcs = node_data.get('arcs', {})
                     type_of = arcs.get('typeOf', {})
                     nodes_list = type_of.get('nodes', [])
-                    enum_values_processed[type_name] = {'in': nodes_list}
-                    enum_values = enum_values_processed
-                    if enum_values[type_name]:
-                        for temp_dict in enum_values[type_name]['in']:
-                            dc_props[property_name].append(temp_dict['dcid'])
+                    for temp_dict in nodes_list:
+                        dc_props[property_name].append(temp_dict['dcid'])
 
         with open(os.path.join(cache_path, f'{dcid}_dc_props_enum_values.json'),
                   'w') as fp:
