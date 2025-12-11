180127f00036716f7f3a5a0f746137ff23046c41
Vishal Gupta <vishg@google.com>
2025-12-11 21:50:30 +0530

    Read import config at runtime  (#1755)

--- import-automation/executor/main.py
+++ import-automation/executor/main.py
@@ -35,27 +35,66 @@ from app.service import github_api
 from app.service import email_notifier
 import dataclasses
 from app.executor.import_executor import ImportStatus, ImportStage
+from google.cloud import secretmanager
 
 import log_util
 
 _FLAGS = flags.FLAGS
 flags.DEFINE_string('import_name', '', 'Absoluate import name.')
-flags.DEFINE_string('import_config', '', 'Import executor configuration.')
+flags.DEFINE_string('import_config', '{}', 'Import executor configuration.')
 flags.DEFINE_boolean(
     'enable_cloud_logging', True,
     'Enable Google Cloud Logging for proper severity levels in GCP.')
 
 
 def _override_configs(import_name: str, import_dir: str,
-                      config: configs.ExecutorConfig) -> configs.ExecutorConfig:
+                      config: configs.ExecutorConfig,
+                      import_config: str) -> configs.ExecutorConfig:
+    """
+    Overrides import configs in the following order:
+    Values from latter configs will overwrite the values from the former configs.
+      - Base config (configs.py)
+      - Local config (config_override.json)
+      - Cloud config (secret manager)
+      - Manifest config (manifest.json)
+      - User config (command line)
+    """
+    user_config = json.loads(import_config)
+    # Use user provided config for further config processing.
+    config = dataclasses.replace(config, **user_config)
+
+    local_config = {}
+    if config.config_override_file:
+        override_file = os.path.join(REPO_DIR, config.config_override_file)
+        logging.info(
+            f'Reading import config from override file {override_file}')
+        with open(override_file, "r") as f:
+            local_config = json.load(f)
+            config = dataclasses.replace(config,
+                                         **local_config.get("configs", {}))
+
+    cloud_config = {}
+    if config.import_config_secret:
+        version_id = 'latest'
+        secret_id = f"projects/{config.gcp_project_id}/secrets/{config.import_config_secret}/versions/{version_id}"
+        logging.info(f'Reading import config from secret {secret_id}')
+        secret_value = secretmanager.SecretManagerServiceClient(
+        ).access_secret_version(name=secret_id).payload.data.decode("UTF-8")
+        cloud_config = json.loads(secret_value)
+        config = dataclasses.replace(config, **cloud_config.get("configs", {}))
+
     manifest_path = os.path.join(config.local_repo_dir, import_dir,
                                  config.manifest_filename)
-    logging.info('%s: Overriding config from manifest %s', import_name,
-                 manifest_path)
-    d = json.load(open(manifest_path))
-    logging.info('Import manifest:')
-    logging.info(json.dumps(d))
-    return dataclasses.replace(config, **d.get("config_override", {}))
+    manifest_config = {}
+    with open(manifest_path, "r") as f:
+        logging.info('Overriding config from manifest %s', manifest_path)
+        manifest_config = json.load(f)
+        logging.info(f'Import manifest: {json.dumps(manifest_config)}')
+        config = dataclasses.replace(
+            config, **manifest_config.get("config_override", {}))
+
+    config = dataclasses.replace(config, **user_config)
+    return config
 
 
 def run_import_job(absolute_import_name: str, import_config: str):
@@ -66,9 +105,12 @@ def run_import_job(absolute_import_name: str, import_config: str):
         f"Running import {absolute_import_name} with config:{import_config}")
     start_time = time.time()
     import_dir, import_name = absolute_import_name.split(':', 1)
-    cfg = json.loads(import_config)
-    config = configs.ExecutorConfig(**cfg)
-    config = _override_configs(import_name, import_dir, config)
+    base_config = configs.ExecutorConfig()
+    config = _override_configs(import_name, import_dir, base_config,
+                               import_config)
+    logging.info(f'Import config: {config}')
+    if config.dc_api_key:
+        os.environ['DC_API_KEY'] = config.dc_api_key
     executor = import_executor.ImportExecutor(
         uploader=file_uploader.GCSFileUploader(
             project_id=config.gcs_project_id,
@@ -82,14 +124,12 @@ def run_import_job(absolute_import_name: str, import_config: str):
         notifier=email_notifier.EmailNotifier(config.email_account,
                                               config.email_token),
         local_repo_dir=config.local_repo_dir)
-    logging.info('Import config:')
-    logging.info(config)
     import_executor.log_import_status(import_name, ImportStage.INIT,
                                       ImportStatus.SUCCESS)
     result = executor.execute_imports_on_update(absolute_import_name)
-    logging.info(f'Import result: {result}')
     import_executor.log_import_status(import_name, ImportStage.FINISH,
-                                      result.status, elapsed_time_secs),
+                                      result.status)
+    logging.info(f'Import result: {result}')
     elapsed_time_secs = int(time.time() - start_time)
     message = (f"Import Job [{absolute_import_name}] completed with status= "
                f"[{result.status}] in [{elapsed_time_secs}] seconds.)")
