1e0e15f85ef804cdcee07c8011244529f7eef8a7
Pulkeet Gupta <pulkeet@google.com>
2025-12-26 11:23:11 +0000

    Fixed lint issues for worldbank.py file

--- scripts/world_bank/wdi/worldbank.py
+++ scripts/world_bank/wdi/worldbank.py
@@ -562,8 +562,9 @@ def process(indicator_codes, worldbank_dataframe, saveOutput=True):
         worldbank_dataframe['ISO3166Alpha3'] = (
             worldbank_dataframe['ISO3166Alpha3'].replace({
                 'XKX': 'XKS'
-            }).apply(lambda code: "dcid:Earth"
-                     if code == "WLD" else ("dcid:ChannelIslands" if code == "CHI" else "dcid:country/" + code)))
+            }).apply(lambda code: "dcid:Earth" if code == "WLD" else
+                     ("dcid:ChannelIslands"
+                      if code == "CHI" else "dcid:country/" + code)))
         worldbank_dataframe['StatisticalVariable'] = \
             worldbank_dataframe['StatisticalVariable'].apply(
                 lambda code: "dcs:" + code)
