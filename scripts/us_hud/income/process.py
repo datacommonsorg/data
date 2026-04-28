29a5a708d0c748181309dfd71422c2d237de84dc
Nivedita Singh <niveditasing@google.com>
2026-04-28 17:57:01 +0000

    added logic to pass the firewall

--- scripts/us_hud/income/process.py
+++ scripts/us_hud/income/process.py
@@ -73,7 +73,7 @@ def download_with_retry(url):
         'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
-    response = requests.get(url, headers=headers, verify=False)
+    response = requests.get(url, headers=headers, timeout=60)
     response.raise_for_status()
     return response
 
