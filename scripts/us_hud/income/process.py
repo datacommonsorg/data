a1d88f27d90d92570d88ce2be69939f197caaf0e
Nivedita Singh <niveditasing@google.com>
2026-05-05 13:43:01 +0000

    fixed tests

--- scripts/us_hud/income/process.py
+++ scripts/us_hud/income/process.py
@@ -31,6 +31,7 @@ import python_calamine
 import requests
 from retry import retry
 
+
 FLAGS = flags.FLAGS
 flags.DEFINE_string('income_output_dir', 'output',
                     'Path to write cleaned CSVs.')
