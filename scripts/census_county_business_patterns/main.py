d20a64a54c27261fd491971f0f9cefe79f508bb3
Tarun Bali <balit@google.com>
2026-06-04 08:43:08 +0000

    CensusCountyBusinessPatterns_fix_timeout

--- scripts/census_county_business_patterns/main.py
+++ scripts/census_county_business_patterns/main.py
@@ -88,9 +88,9 @@ FILE_TYPES_CONFIG = [
 @retry(tries=3,
        delay=5,
        backoff=2,
-       exceptions=(requests.exceptions.ConnectionError, requests.exceptions.Timeout))
+       exceptions=requests.exceptions.ConnectionError)
 def retry_method(url, headers=None):
-    response = requests.get(url, headers=headers, timeout=(10, 300))
+    response = requests.get(url, stream=True, headers=headers, timeout=120)
     response.raise_for_status()
     return response
 
