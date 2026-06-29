5dd9fe42e097a8c3293ed47ea7fcd8cfa4f1817c
Nivedita Singh <niveditasing@google.com>
2026-06-29 09:11:17 +0000

    Revert changes to simplify.py as numpy dependency is pinned

--- scripts/us_census/geojsons_low_res/simplify.py
+++ scripts/us_census/geojsons_low_res/simplify.py
@@ -22,27 +22,11 @@
                         --out_path simplified-data/geoId-01-simple.geojson
 """
 
-import numpy as np
 import rdp
 import geojson
 from absl import app
 from absl import flags
 from absl import logging
-
-# Monkey-patch np.cross to support 2D vectors under NumPy 2.x which removed it.
-_orig_cross = np.cross
-
-
-def _robust_cross(a, b, *args, **kwargs):
-    a_arr = np.asarray(a)
-    b_arr = np.asarray(b)
-    if (not args and not kwargs and a_arr.ndim > 0 and b_arr.ndim > 0 and
-            a_arr.shape[-1] == 2 and b_arr.shape[-1] == 2):
-        return a_arr[..., 0] * b_arr[..., 1] - a_arr[..., 1] * b_arr[..., 0]
-    return _orig_cross(a, b, *args, **kwargs)
-
-
-np.cross = _robust_cross
 
 
 class GeojsonSimplifier:
