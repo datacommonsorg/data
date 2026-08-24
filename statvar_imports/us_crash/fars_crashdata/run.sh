67553733688980a569db99f9db253996eda40257
Krishnam Maheshwari <krishnamm@google.com>
2026-08-24 16:56:28 +0000

    Changes as per CRA analysis

--- statvar_imports/us_crash/fars_crashdata/run.sh
+++ statvar_imports/us_crash/fars_crashdata/run.sh
@@ -6,5 +6,7 @@ for FILE in gcs_output/input_files/ACCIDENT_*.csv; do
         --input_data="$FILE" \
         --pv_map=fars_crash_pvmap.csv \
         --config_file=fars_crash_metadata.csv \
-        --output_path="gcs_output/output/fars_crash_${YEAR}"
+        --output_path="gcs_output/output/fars_crash_${YEAR}" \
+        --output_counters="counters/fars_crash_${YEAR}_counters.csv" \
+        --existing_statvar_mcf=gs://unresolved_mcf/scripts/statvar/stat_vars.mcf
 done
