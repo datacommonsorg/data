54814b3e332ed35d378e8904e17aa53c9be3adbc
shouryasingh1616 <shouryasingh@google.com>
2026-09-10 06:21:05 +0000

    Fix EPACriteriaAirPollutants: remove station MCF instances, restore source_files, add date validation rules, and add unit tests

--- scripts/us_epa/airdata/air_quality_test.py
+++ scripts/us_epa/airdata/air_quality_test.py
@@ -30,8 +30,8 @@ class TestCriteriaGasesTest(unittest.TestCase):
     def test_write_csv(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             with open(
-                    os.path.join(module_dir_, 'test_data/test_import_data.csv'),
-                    'r') as f:
+                    os.path.join(module_dir_,
+                                 'test_data/test_import_data.csv'), 'r') as f:
                 test_csv = os.path.join(tmp_dir, 'test_csv.csv')
                 create_csv(test_csv)
 
@@ -102,6 +102,53 @@ class TestCriteriaGasesTest(unittest.TestCase):
             with open(test_csv, 'r') as f:
                 reader = list(csv.DictReader(f))
                 self.assertEqual(len(reader), 0)
+
+    def test_unit_mapping(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            test_csv = os.path.join(tmp_dir, 'test_csv.csv')
+            create_csv(test_csv)
+            observations = [
+                {
+                    'State Code': '01',
+                    'County Code': '073',
+                    'Site Num': '0023',
+                    'Parameter Code': '88101',
+                    'POC': '1',
+                    'Latitude': '33.55',
+                    'Longitude': '-86.81',
+                    'Pollutant Standard': 'PM25 24-hour 2012',
+                    'Date Local': '2021-01-01',
+                    'Units of Measure': 'Micrograms/cubic meter (LC)',
+                    'Arithmetic Mean': '3.8',
+                    '1st Max Value': '3.8',
+                    'AQI': '16',
+                    'Local Site Name': 'North Birmingham',
+                },
+                {
+                    'State Code': '01',
+                    'County Code': '073',
+                    'Site Num': '0023',
+                    'Parameter Code': '81102',
+                    'POC': '4',
+                    'Latitude': '33.55',
+                    'Longitude': '-86.81',
+                    'Pollutant Standard': 'PM10 24-hour 2006',
+                    'Date Local': '2021-01-01',
+                    'Units of Measure': 'Micrograms/cubic meter (25 C)',
+                    'Arithmetic Mean': '11',
+                    '1st Max Value': '11',
+                    'AQI': '10',
+                    'Local Site Name': 'North Birmingham',
+                },
+            ]
+            write_csv(test_csv, iter(observations))
+            with open(test_csv, 'r') as f:
+                rows = list(csv.DictReader(f))
+                self.assertEqual(len(rows), 2)
+                self.assertEqual(rows[0]['Units'],
+                                 'MicrogramsPerCubicMeter_lc')
+                self.assertEqual(rows[1]['Units'],
+                                 'MicrogramsPerCubicMeter_25C')
 
 
 if __name__ == '__main__':
