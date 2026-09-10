688ad4ec9eaa36cbcd34e6c6ce1aa19f8208ea67
Ashwani Srivastav <ashwanisr@google.com>
2026-09-10 09:25:32 +0000

    cra suggestion

--- tools/statvar_importer/place/place_resolver.py
+++ tools/statvar_importer/place/place_resolver.py
@@ -243,8 +243,6 @@ class PlaceResolver:
                 )
             if maps_result:
                 results[key] = maps_result
-            else:
-                self._set_failed_cache_value(key, {})
 
         # Collect all placeIds to be resolved that are not in cache.
         places_ids = {}
@@ -619,7 +617,6 @@ class PlaceResolver:
 
         # Lookup Google Maps API.
         if not self._maps_api_key:
-            self._set_failed_cache_value(place_key, {})
             logging.log_every_n(
                 logging.ERROR,
                 f'No maps key. Please set --maps_api_key for place lookup.',
@@ -684,7 +681,6 @@ class PlaceResolver:
 
         # Lookup Google Maps API.
         if not self._maps_api_key:
-            self._set_failed_cache_value(place_key, {})
             logging.log_every_n(
                 logging.ERROR,
                 f'No maps key. Please set --maps_api_key for place lookup.',
